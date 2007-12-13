e4d6ab04fa4ef54553324744c86df58bf4a3d058
Volker Grabsch <vog@notjusthosting.com>
2007-12-13 02:38:38 +0100

    Release 1.2

--- build_mingw_cross_env.sh
+++ build_mingw_cross_env.sh
@@ -3,7 +3,7 @@ set -ex
 
 
 #---
-#   MinGW cross compiling environment  (pre-2.0)
+#   MinGW cross compiling environment  (1.2)
 #   =================================
 #
 #   http://www.profv.de/mingw_cross_env/
