707caa4e1d2054d424111acb904b99936c5ef31a
Volker Grabsch <vog@notjusthosting.com>
2008-11-08 21:06:56 +0100

    translated package: tiff

---
+++ src/tiff.mk
@@ -0,0 +1,26 @@
+# LibTIFF
+# http://www.remotesensing.org/libtiff/
+
+PKG            := tiff
+$(PKG)_VERSION := 3.8.2
+$(PKG)_SUBDIR  := tiff-$($(PKG)_VERSION)
+$(PKG)_FILE    := tiff-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL     := ftp://ftp.remotesensing.org/pub/libtiff/$($(PKG)_FILE)
+$(PKG)_URL_2   := http://dl.maptools.org/dl/libtiff/$($(PKG)_FILE)
+$(PKG)_DEPS    := gcc pthreads zlib jpeg
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://www.remotesensing.org/libtiff/' | \
+    $(SED) -n 's,.*>v\([0-9][^<]*\)<.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        PTHREAD_LIBS='-lpthread -lws2_32' \
+        --without-x
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+endef
