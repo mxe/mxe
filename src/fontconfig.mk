363f6d5ac43e5c677096495dd384bfe6a42e8e82
Volker Grabsch <vog@notjusthosting.com>
2008-11-08 21:25:30 +0100

    translated package: fontconfig

---
+++ src/fontconfig.mk
@@ -0,0 +1,33 @@
+# fontconfig
+# http://fontconfig.org/
+
+PKG            := fontconfig
+$(PKG)_VERSION := 2.5.91
+$(PKG)_SUBDIR  := fontconfig-$($(PKG)_VERSION)
+$(PKG)_FILE    := fontconfig-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL     := http://fontconfig.org/release/$($(PKG)_FILE)
+$(PKG)_DEPS    := gcc freetype libxml2
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://fontconfig.org/release/' | \
+    $(SED) -n 's,.*fontconfig-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    # ensure there is no (buggy) attempt to install the *.dll.a file
+    # (remove this line of you want to link dynamically)
+    $(SED) 's,^install-data-local:.*,install-data-local:,' -i '$(1)/src/Makefile.in'
+    # wine confuses the cross-compiling detection, so set it explicitly
+    $(SED) 's,cross_compiling=no,cross_compiling=yes,' -i '$(1)/configure'
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --with-arch='$(TARGET)' \
+        --with-freetype-config='$(PREFIX)/$(TARGET)/bin/freetype-config' \
+        --enable-libxml2 \
+        LIBXML2_CFLAGS="`$(PREFIX)/$(TARGET)/bin/xml2-config --cflags`" \
+        LIBXML2_LIBS="`$(PREFIX)/$(TARGET)/bin/xml2-config --libs`"
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+endef
