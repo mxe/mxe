1c22349f2dde7b279bde4cd80915d4c3ea3eb920
Volker Grabsch <vog@notjusthosting.com>
2008-11-08 18:46:08 +0100

    translated package: gettext

---
+++ src/gettext.mk
@@ -0,0 +1,25 @@
+# gettext
+# http://www.gnu.org/software/gettext/
+
+PKG            := gettext
+$(PKG)_VERSION := 0.17
+$(PKG)_SUBDIR  := gettext-$($(PKG)_VERSION)/gettext-runtime
+$(PKG)_FILE    := gettext-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL     := ftp://ftp.gnu.org/pub/gnu/gettext/$($(PKG)_FILE)
+$(PKG)_DEPS    := gcc
+
+define $(PKG)_UPDATE
+    wget -q -O- 'ftp://ftp.gnu.org/pub/gnu/gettext/' | \
+    $(SED) -n 's,.*gettext-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    sort | \
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(2)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --enable-threads=win32
+    $(MAKE) -C '$(2)/intl' -j '$(JOBS)' install
+endef
