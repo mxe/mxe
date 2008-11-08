141fc3ea548cd3d1055665b7671f4ea7940c6272
Volker Grabsch <vog@notjusthosting.com>
2008-11-08 20:19:22 +0100

    translated package: libxslt

---
+++ src/libxslt.mk
@@ -0,0 +1,28 @@
+# libxslt
+# http://xmlsoft.org/XSLT/
+
+PKG            := libxslt
+$(PKG)_VERSION := 1.1.22
+$(PKG)_SUBDIR  := libxslt-$($(PKG)_VERSION)
+$(PKG)_FILE    := libxslt-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL     := ftp://xmlsoft.org/libxslt/$($(PKG)_FILE)
+$(PKG)_DEPS    := gcc libxml2 libgcrypt
+
+define $(PKG)_UPDATE
+    wget -q -O- 'ftp://xmlsoft.org/libxslt/' | \
+    $(SED) -n 's,.*LATEST_LIBXSLT_IS_\([0-9][^>]*\)</a>.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --without-debug \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --with-libxml-prefix='$(PREFIX)/$(TARGET)' \
+        LIBGCRYPT_CONFIG='$(PREFIX)/$(TARGET)/bin/libgcrypt-config' \
+        --without-python \
+        --without-plugins
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+endef
