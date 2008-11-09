06ae4538034838a92b8f0f7752f88c9290fcffc8
Volker Grabsch <vog@notjusthosting.com>
2008-11-09 03:00:32 +0100

    translated package: libgeotiff

---
+++ src/libgeotiff.mk
@@ -0,0 +1,27 @@
+# GeoTiff
+# http://www.remotesensing.org/geotiff/
+
+PKG            := libgeotiff
+$(PKG)_VERSION := 1.2.4
+$(PKG)_SUBDIR  := libgeotiff-$($(PKG)_VERSION)
+$(PKG)_FILE    := libgeotiff-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL     := ftp://ftp.remotesensing.org/pub/geotiff/libgeotiff/$($(PKG)_FILE)
+$(PKG)_URL_2   := http://dl.maptools.org/dl/geotiff/libgeotiff/$($(PKG)_FILE)
+$(PKG)_DEPS    := gcc zlib jpeg tiff proj
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://www.remotesensing.org/geotiff/geotiff.html' | \
+    $(SED) -n 's,.*libgeotiff-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    $(SED) 's,/usr/local,@prefix@,' -i '$(1)/bin/Makefile.in'
+    touch configure
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)'
+    $(MAKE) -C '$(1)' -j 1 all install EXEEXT=.remove-me
+    rm -fv '$(PREFIX)/$(TARGET)'/bin/*.remove-me
+endef
