801070b4a4dd27089803bdce04e1c1e5f99ef54e
Volker Grabsch <vog@notjusthosting.com>
2008-11-09 02:43:34 +0100

    translated package: proj

---
+++ src/proj.mk
@@ -0,0 +1,25 @@
+# proj
+# http://www.remotesensing.org/proj/
+
+PKG            := proj
+$(PKG)_VERSION := 4.6.0
+$(PKG)_SUBDIR  := proj-$($(PKG)_VERSION)
+$(PKG)_FILE    := proj-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL     := ftp://ftp.remotesensing.org/proj/$($(PKG)_FILE)
+$(PKG)_URL_2   := http://dl.maptools.org/dl/proj/$($(PKG)_FILE)
+$(PKG)_DEPS    := gcc
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://www.remotesensing.org/proj/' | \
+    $(SED) -n 's,.*proj-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    $(SED) 's,install-exec-local[^:],,' -i '$(1)/src/Makefile.in'
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)'
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+endef
