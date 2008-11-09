5561a06358dd3b757033f16c5c7ca20016764699
Volker Grabsch <vog@notjusthosting.com>
2008-11-09 03:37:06 +0100

    translated package: gdal

---
+++ src/gdal.mk
@@ -0,0 +1,41 @@
+# GDAL
+# http://www.gdal.org/
+
+PKG            := gdal
+$(PKG)_VERSION := 1.5.0
+$(PKG)_SUBDIR  := gdal-$($(PKG)_VERSION)
+$(PKG)_FILE    := gdal-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL     := http://www.gdal.org/dl/$($(PKG)_FILE)
+$(PKG)_DEPS    := gcc libpng tiff libgeotiff jpeg giflib curl geos
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://trac.osgeo.org/gdal/wiki/DownloadSource' | \
+    $(SED) -n 's,.*gdal-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        LIBS='-ljpeg' \
+        --with-png='$(PREFIX)/$(TARGET)' \
+        --with-libtiff='$(PREFIX)/$(TARGET)' \
+        --with-geotiff='$(PREFIX)/$(TARGET)' \
+        --with-jpeg='$(PREFIX)/$(TARGET)' \
+        --with-gif='$(PREFIX)/$(TARGET)' \
+        --with-curl='$(PREFIX)/$(TARGET)/bin/curl-config' \
+        --with-geos='$(PREFIX)/$(TARGET)/bin/geos-config' \
+        --with-expat=no \
+        --without-python \
+        --without-ngpython
+    $(MAKE) -C '$(1)'       -j '$(JOBS)' lib-target
+    $(MAKE) -C '$(1)'       -j '$(JOBS)' install-lib
+    $(MAKE) -C '$(1)/port'  -j '$(JOBS)' install
+    $(MAKE) -C '$(1)/gcore' -j '$(JOBS)' install
+    $(MAKE) -C '$(1)/frmts' -j '$(JOBS)' install
+    $(MAKE) -C '$(1)/alg'   -j '$(JOBS)' install
+    $(MAKE) -C '$(1)/ogr'   -j '$(JOBS)' install OGR_ENABLED=
+    $(MAKE) -C '$(1)/apps'  -j '$(JOBS)' install BIN_LIST=
+endef
