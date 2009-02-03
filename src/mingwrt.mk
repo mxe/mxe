50634bae5520284ccfd232bf824488e06367428b
Volker Grabsch <vog@notjusthosting.com>
2009-02-03 01:59:29 +0100

    don't install the DLL file of mingwrt, because there is no need for it

--- src/mingwrt.mk
+++ src/mingwrt.mk
@@ -4,18 +4,19 @@
 PKG            := mingwrt
 $(PKG)_VERSION := 3.15.1-mingw32
 $(PKG)_SUBDIR  := .
-$(PKG)_FILE    := mingwrt-$($(PKG)_VERSION).tar.gz
+$(PKG)_FILE    := mingwrt-$($(PKG)_VERSION)-dev.tar.gz
 $(PKG)_URL     := http://$(SOURCEFORGE_MIRROR)/mingw/$($(PKG)_FILE)
 $(PKG)_DEPS    :=
 
 define $(PKG)_UPDATE
     wget -q -O- 'http://sourceforge.net/project/showfiles.php?group_id=2435&package_id=11598' | \
-    $(SED) -n 's,.*mingwrt-\([0-9][^>]*\)-src\.tar.*,\1,p' | \
+    grep 'mingwrt-' | \
+    $(SED) -n 's,.*mingwrt-\([0-9][^>]*\)-dev\.tar.*,\1,p' | \
     head -1
 endef
 
 define $(PKG)_BUILD
     install -d '$(PREFIX)/$(TARGET)'
     cd '$(1)' && \
-        cp -rpv bin include lib '$(PREFIX)/$(TARGET)'
+        cp -rpv include lib '$(PREFIX)/$(TARGET)'
 endef
