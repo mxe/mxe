c5320b99d0ab91866aa3383c1a76035d264613c6
Volker Grabsch <vog@notjusthosting.com>
2009-02-03 01:42:58 +0100

    update version of package xmlwrapp

--- src/xmlwrapp.mk
+++ src/xmlwrapp.mk
@@ -2,16 +2,16 @@
 # http://sourceforge.net/projects/xmlwrapp/
 
 PKG            := xmlwrapp
-$(PKG)_VERSION := 0.5.0
+$(PKG)_VERSION := 0.5.1
 $(PKG)_SUBDIR  := xmlwrapp-$($(PKG)_VERSION)
-$(PKG)_FILE    := xmlwrapp-$($(PKG)_VERSION).tgz
+$(PKG)_FILE    := xmlwrapp-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL     := http://$(SOURCEFORGE_MIRROR)/xmlwrapp/$($(PKG)_FILE)
 $(PKG)_DEPS    := gcc libxml2 libxslt
 
 define $(PKG)_UPDATE
     wget -q -O- 'http://sourceforge.net/project/showfiles.php?group_id=142403&package_id=156331' | \
     grep 'xmlwrapp-' | \
-    $(SED) -n 's,.*xmlwrapp-\([0-9][^>]*\)\.tgz.*,\1,p' | \
+    $(SED) -n 's,.*xmlwrapp-\([0-9][^>]*\)\.tar.*,\1,p' | \
     head -1
 endef
 
