47c756ba46d971093b661def22643215d8b5ad06
Volker Grabsch <vog@notjusthosting.com>
2009-02-14 22:00:21 +0100

    new package: libgsasl  (by Martin Lambers)

---
+++ src/libgsasl.mk
@@ -0,0 +1,29 @@
+# Libgsasl
+# http://www.gnu.org/software/gsasl/
+
+PKG            := libgsasl
+$(PKG)_VERSION := 0.2.9
+$(PKG)_SUBDIR  := libgsasl-$($(PKG)_VERSION)
+$(PKG)_FILE    := libgsasl-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL     := ftp://alpha.gnu.org/pub/gnu/gsasl/$($(PKG)_FILE)
+$(PKG)_DEPS    := gcc libiconv libidn libntlm
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://git.savannah.gnu.org/gitweb/?p=gsasl.git;a=tags' | \
+    grep '<a class="list subject"' | \
+    $(SED) -n 's,.*<a[^>]*>\([0-9][^>]*\)<.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    # wine confuses the cross-compiling detection, so set it explicitly
+    $(SED) 's,cross_compiling=no,cross_compiling=yes,' -i '$(1)/configure'
+    cd '$(1)' && touch src/libgsasl-7.def && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --with-libiconv-prefix='$(PREFIX)/$(TARGET)' \
+        --with-libidn-prefix='$(PREFIX)/$(TARGET)' \
+        --with-libntlm-prefix='$(PREFIX)/$(TARGET)'
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+endef
