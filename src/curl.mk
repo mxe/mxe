62b02d542a4f3579f0601509cb34d6d580a2aa5a
Volker Grabsch <vog@notjusthosting.com>
2009-02-14 20:56:40 +0100

    new package: libidn  (by Martin Lambers)

--- src/curl.mk
+++ src/curl.mk
@@ -6,7 +6,7 @@ $(PKG)_VERSION := 7.19.3
 $(PKG)_SUBDIR  := curl-$($(PKG)_VERSION)
 $(PKG)_FILE    := curl-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL     := http://curl.haxx.se/download/$($(PKG)_FILE)
-$(PKG)_DEPS    := gcc gnutls
+$(PKG)_DEPS    := gcc gnutls libidn
 
 define $(PKG)_UPDATE
     wget -q -O- 'http://curl.haxx.se/changes.html' | \
@@ -24,6 +24,7 @@ define $(PKG)_BUILD
         --disable-shared \
         --prefix='$(PREFIX)/$(TARGET)' \
         --with-gnutls='$(PREFIX)/$(TARGET)' \
-        LIBS="-lgcrypt `$(PREFIX)/$(TARGET)/bin/gpg-error-config --libs`"
+        --with-libidn='$(PREFIX)/$(TARGET)' \
+        LIBS="-lgcrypt -liconv `$(PREFIX)/$(TARGET)/bin/gpg-error-config --libs`"
     $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 endef
---
+++ src/libidn.mk
@@ -0,0 +1,27 @@
+# libidn
+# ftp://ftp.gnu.org/gnu/libidn/
+
+PKG            := libidn
+$(PKG)_VERSION := 1.12
+$(PKG)_SUBDIR  := libidn-$($(PKG)_VERSION)
+$(PKG)_FILE    := libidn-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL     := ftp://ftp.gnu.org/gnu/libidn/$($(PKG)_FILE)
+$(PKG)_DEPS    := gcc libiconv
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://git.savannah.gnu.org/gitweb/?p=libidn.git;a=tags' | \
+    grep '<a class="list subject"' | \
+    $(SED) -n 's,.*<a[^>]*>\([0-9][^>]*\)<.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    # wine confuses the cross-compiling detection, so set it explicitly
+    $(SED) 's,cross_compiling=no,cross_compiling=yes,' -i '$(1)/configure'
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --with-libiconv-prefix='$(PREFIX)/$(TARGET)'
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+endef
