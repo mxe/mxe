8cc923257e4a5e745e329f9b9b2972012111422d
Volker Grabsch <vog@notjusthosting.com>
2009-02-27 19:04:14 +0100

    new package: pcre

---
+++ src/pcre.mk
@@ -0,0 +1,29 @@
+# PCRE
+# http://www.pcre.org/
+
+PKG            := pcre
+$(PKG)_VERSION := 7.8
+$(PKG)_SUBDIR  := pcre-$($(PKG)_VERSION)
+$(PKG)_FILE    := pcre-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL     := http://$(SOURCEFORGE_MIRROR)/pcre/$($(PKG)_FILE)
+$(PKG)_DEPS    := gcc
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://sourceforge.net/project/showfiles.php?group_id=10194&package_id=9960' | \
+    grep 'pcre-' | \
+    $(SED) -n 's,.*pcre-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --enable-utf8 \
+        --enable-unicode-properties \
+        --disable-pcregrep-libz \
+        --disable-pcregrep-libbz2 \
+        --disable-pcretest-libreadline
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+endef
