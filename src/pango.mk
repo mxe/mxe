1da1b906b46f02262c51271c132b2ee4296a0cf2
Volker Grabsch <vog@notjusthosting.com>
2009-05-05 12:57:03 +0200

    fix for systems where wine confuses the cross-compiling detection of ./configure

--- src/pango.mk
+++ src/pango.mk
@@ -22,6 +22,8 @@ define $(PKG)_BUILD
     $(SED) 's,enable_static=no,enable_static=yes,' -i '$(1)/configure'
     $(SED) 's,enable_shared=yes,enable_shared=no,' -i '$(1)/configure'
     $(SED) 's,^install-data-local:.*,install-data-local:,' -i '$(1)/modules/Makefile.in'
+    # wine confuses the cross-compiling detection, so set it explicitly
+    $(SED) 's,cross_compiling=no,cross_compiling=yes,' -i '$(1)/configure'
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
         --disable-shared \
