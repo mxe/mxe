7a2aa945a2899988224fa912695621e5cf25675b
Volker Grabsch <vog@notjusthosting.com>
2009-10-10 14:22:39 +0200

    re-enable an important workaround in package qt

--- src/qt.mk
+++ src/qt.mk
@@ -68,6 +68,7 @@ define $(PKG)_BUILD
 
     # rebuild qmake to use "-unix" as default and to use the correct "ar" command
     $(SED) 's,\(Option::TARG_MODE Option::target_mode = Option::TARG_\)[A-Z_]*,\1UNIX_MODE,' -i '$(1)'.native/qmake/option.cpp
+    $(SED) 's,"ar -M,"$(TARGET)-ar -M,' -i '$(1)'.native/qmake/generators/win32/mingw_make.cpp
     $(MAKE) -C '$(1)'.native/qmake -j $(JOBS)
 
     # install the native tools manually
