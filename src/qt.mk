1328479859c8fd51f0c6c45084d53bf4c5594657
Volker Grabsch <vog@notjusthosting.com>
2009-10-10 04:15:53 +0200

    portability fix for MacOS X for package qt

--- src/qt.mk
+++ src/qt.mk
@@ -64,6 +64,11 @@ define $(PKG)_BUILD
     $(MAKE) -C '$(1)'.native -j $(JOBS) sub-rcc
     $(MAKE) -C '$(1)'.native -j $(JOBS) sub-uic
 
+    # rebuild qmake to use "-unix" as default and to use the correct "ar" command
+    $(SED) 's,\(Option::TARG_MODE Option::target_mode = Option::TARG_\)[A-Z_]*,\1UNIX_MODE,' -i '$(1)'.native/qmake/option.cpp
+    $(SED) 's,"ar -M,"$(TARGET)-ar -M,' -i '$(1)'.native/qmake/generators/win32/mingw_make.cpp
+    $(MAKE) -C '$(1)'.native/qmake -j $(JOBS)
+
     # install the native tools manually
     $(INSTALL) -m755 '$(1)'.native/bin/moc   '$(PREFIX)/bin/$(TARGET)-moc'
     $(INSTALL) -m755 '$(1)'.native/bin/rcc   '$(PREFIX)/bin/$(TARGET)-rcc'
