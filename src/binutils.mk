a2686d0a827e0a89eea9b4c2f4aaec3cfb30996e
Volker Grabsch <vog@notjusthosting.com>
2009-11-12 01:24:47 +0100

    portability fix to packages binutils and gcc for MacOS 10.6

--- src/binutils.mk
+++ src/binutils.mk
@@ -47,7 +47,8 @@ define $(PKG)_BUILD
         --with-gnu-ld \
         --with-gnu-as \
         --disable-nls \
-        --disable-shared
+        --disable-shared \
+        --disable-werror
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     $(MAKE) -C '$(1)' -j 1 install
 endef
