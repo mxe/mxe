c0b472f486dd45ea34bef29c4f8a5908e99433b5
Volker Grabsch <vog@notjusthosting.com>
2009-12-20 05:47:19 +0100

    replace tabs with spaces in package zziplib

--- src/zziplib.mk
+++ src/zziplib.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2009  Volker Grabsch
-#		      Gregory Smith
+#                     Gregory Smith
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
