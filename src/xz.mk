bf4cafca34bb512764b1639521cf14981a725eac
Volker Grabsch <vog@notjusthosting.com>
2010-02-07 10:43:17 +0100

    new package: xz  (by Martin Lambers)

---
+++ src/xz.mk
@@ -0,0 +1,29 @@
+# This file is part of mingw-cross-env.
+# See doc/index.html or doc/README for further information.
+
+# XZ
+PKG             := xz
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 4.999.9beta
+$(PKG)_CHECKSUM := 67cd2598b5149da9b3c126f3483fd632e06b3d48
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_WEBSITE  := http://tukaani.org/xz/
+$(PKG)_URL      := http://tukaani.org/xz/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://tukaani.org/xz/' | \
+    $(SED) -n 's,.*xz-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --disable-shared \
+        --disable-dynamic \
+        --disable-nls
+    $(MAKE) -C '$(1)'/src/liblzma -j '$(JOBS)' install
+endef
