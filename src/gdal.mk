d9fefb83b7f7f275f4903fbb8fb5372a14175ef7
Volker Grabsch <vog@notjusthosting.com>
2010-02-15 15:52:04 +0100

    make all dependencies explicit in package gdal

--- src/gdal.mk
+++ src/gdal.mk
@@ -11,7 +11,7 @@ $(PKG)_FILE     := gdal-$($(PKG)_VERSION).tar.gz
 $(PKG)_WEBSITE  := http://www.gdal.org/
 $(PKG)_URL      := http://ftp.remotesensing.org/gdal/$($(PKG)_FILE)
 $(PKG)_URL_2    := http://download.osgeo.org/gdal/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc zlib libpng tiff libgeotiff jpeg giflib expat curl geos
+$(PKG)_DEPS     := gcc zlib libpng tiff libgeotiff jpeg jasper giflib expat sqlite curl geos postgresql libodbc++
 
 define $(PKG)_UPDATE
     wget -q -O- 'http://trac.osgeo.org/gdal/wiki/DownloadSource' | \
@@ -20,23 +20,64 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
+    cd '$(1)' && autoconf
+    # The option '--without-threads' means native win32 threading without pthread.
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
         --disable-shared \
         --prefix='$(PREFIX)/$(TARGET)' \
-        LIBS='-ljpeg' \
-        --with-threads \
+        --with-bsb \
+        --with-grib \
+        --with-ogr \
+        --with-vfk \
+        --with-pam \
+        --without-threads \
         --with-libz='$(PREFIX)/$(TARGET)' \
         --with-png='$(PREFIX)/$(TARGET)' \
         --with-libtiff='$(PREFIX)/$(TARGET)' \
         --with-geotiff='$(PREFIX)/$(TARGET)' \
         --with-jpeg='$(PREFIX)/$(TARGET)' \
+        --with-jasper='$(PREFIX)/$(TARGET)' \
         --with-gif='$(PREFIX)/$(TARGET)' \
         --with-expat='$(PREFIX)/$(TARGET)' \
+        --with-sqlite3='$(PREFIX)/$(TARGET)' \
         --with-curl='$(PREFIX)/$(TARGET)/bin/curl-config' \
         --with-geos='$(PREFIX)/$(TARGET)/bin/geos-config' \
+        --with-pg='$(PREFIX)/$(TARGET)/bin/pg_config' \
+        --with-odbc='$(PREFIX)/$(TARGET)' \
+        --without-static-proj4 \
+        --without-xerces \
+        --without-grass \
+        --without-libgrass \
+        --without-spatialite \
+        --without-cfitsio \
+        --without-pcraster \
+        --without-netcdf \
+        --without-pcidsk \
+        --without-ogdi \
+        --without-fme \
+        --without-hdf4 \
+        --without-hdf5 \
+        --without-ecw \
+        --without-kakadu \
+        --without-mrsid \
+        --without-jp2mrsid \
+        --without-msg \
+        --without-oci \
+        --without-mysql \
+        --without-ingres \
+        --without-dods-root \
+        --without-dwgdirect \
+        --without-dwg-plt \
+        --without-idb \
+        --without-sde \
+        --without-epsilon \
+        --without-perl \
+        --without-php \
+        --without-ruby \
         --without-python \
-        --without-ngpython
+        --without-macosx-framework \
+        LIBS="-ljpeg -lsecur32 `'$(TARGET)-pkg-config' --libs openssl`"
     $(MAKE) -C '$(1)'       -j 1 lib-target
     $(MAKE) -C '$(1)'       -j 1 install-lib
     $(MAKE) -C '$(1)/port'  -j 1 install
