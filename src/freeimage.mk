456d5a9d685e5d82c814ad76ebe6064fae75cd83
Mark Brand <mabrand@mabrand.nl>
2010-02-16 12:22:34 +0100

    new package: freeimage

---
+++ src/freeimage.mk
@@ -0,0 +1,33 @@
+# This file is part of mingw-cross-env.
+# See doc/index.html for further information.
+
+# FreeImage
+PKG             := freeimage
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 3131
+$(PKG)_CHECKSUM := 52ba4453aa9682c57104c3420e58f843aaa6ab61
+$(PKG)_SUBDIR   := FreeImage
+$(PKG)_FILE     := FreeImage$($(PKG)_VERSION).zip
+$(PKG)_WEBSITE  := http://freeimage.sourceforge.net/
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/freeimage/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc
+
+define $(PKG)_UPDATE
+   echo Update not implemented.
+endef
+
+define $(PKG)_BUILD
+    $(MAKE) -C '$(1)' -j '$(JOBS)' -f Makefile.gnu \
+        CXX='$(TARGET)-g++' \
+        CC='$(TARGET)-gcc' \
+        AR='$(TARGET)-ar' \
+        INCDIR='$(PREFIX)/$(TARGET)/include' \
+        INSTALLDIR='$(PREFIX)/$(TARGET)/lib'
+
+    $(MAKE) -C '$(1)' -j '$(JOBS)' -f Makefile.gnu install \
+        CXX='$(TARGET)-g++' \
+        CC='$(TARGET)-gcc' \
+        AR='$(TARGET)-ar' \
+        INCDIR='$(PREFIX)/$(TARGET)/include' \
+        INSTALLDIR='$(PREFIX)/$(TARGET)/lib'
+endef
