f50af1ef5f7b433b3cbcc271fb71a2ff40fade6f
Volker Grabsch <vog@notjusthosting.com>
2010-02-23 23:36:18 +0100

    bugfix: don't use WINAPI in package bzip2

--- src/bzip2.mk
+++ src/bzip2.mk
@@ -21,6 +21,7 @@ endef
 
 define $(PKG)_BUILD
     $(SED) -i 's,sys\\stat\.h,sys/stat.h,g' '$(1)/bzip2.c'
+    $(SED) -i 's,WINAPI,,g'                 '$(1)/bzlib.h'
     $(MAKE) -C '$(1)' -j '$(JOBS)' libbz2.a \
         PREFIX='$(PREFIX)/$(TARGET)' \
         CC='$(TARGET)-gcc' \
