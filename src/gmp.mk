26ac64f318752a5a06e72b9d6d23ec9075a83a30
Volker Grabsch <vog@notjusthosting.com>
2010-04-10 08:16:56 +0200

    new package: gmp

---
+++ src/gmp.mk
@@ -0,0 +1,31 @@
+# This file is part of mingw-cross-env.
+# See doc/index.html for further information.
+
+# GMP
+PKG             := gmp
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 5.0.1
+$(PKG)_CHECKSUM := 6340edc7ceb95f9015a758c7c0d196eb0f441d49
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_WEBSITE  := http://www.gmplib.org/
+$(PKG)_URL      := ftp://ftp.gmplib.org/pub/$($(PKG)_SUBDIR)/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://www.gmplib.org/' | \
+    grep '<a href="' | \
+    $(SED) -n 's,.*gmp-\([0-9][^>]*\)\.tar\.bz2.*,\1,p' | \
+    grep -v '^4\.' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --disable-shared \
+        --without-readline
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+endef
