059874a74e4c2e73b8c83be783d519f556ed77b6
Mark Brand <mabrand@mabrand.nl>
2010-04-14 21:26:59 +0200

    use the ddraw.h from Wine/KDE-Win to support directx in SDL

--- src/sdl.mk
+++ src/sdl.mk
@@ -26,7 +26,7 @@ define $(PKG)_BUILD
         --disable-shared \
         --disable-debug \
         --prefix='$(PREFIX)/$(TARGET)' \
-        --disable-directx
+        --enable-directx
     $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
     $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 
