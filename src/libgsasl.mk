452508167854e75509454d31d0773ea4788f68ca
Volker Grabsch <vog@notjusthosting.com>
2010-04-21 01:03:10 +0200

    avoid beta versions of package libgsasl

--- src/libgsasl.mk
+++ src/libgsasl.mk
@@ -15,7 +15,7 @@ $(PKG)_DEPS     := gcc libiconv libidn libntlm
 define $(PKG)_UPDATE
     wget -q -O- 'http://git.savannah.gnu.org/gitweb/?p=gsasl.git;a=tags' | \
     grep '<a class="list subject"' | \
-    $(SED) -n 's,.*<a[^>]*>\([0-9][^<]*\)<.*,\1,p' | \
+    $(SED) -n 's,.*<a[^>]*>\([0-9]*\.[0-9]*[02468]\.[^<]*\)<.*,\1,p' | \
     head -1
 endef
 
