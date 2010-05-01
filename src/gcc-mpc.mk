50862948013d648b610cbc274da402a4660ebadd
Volker Grabsch <vog@notjusthosting.com>
2010-05-01 15:31:20 +0200

    upgrade package gcc to GCC 4.5, switching back from TDM to MinGW  (by Tony Theodore)

---
+++ src/gcc-mpc.mk
@@ -0,0 +1,19 @@
+# This file is part of mingw-cross-env.
+# See doc/index.html for further information.
+
+# MPC for GCC
+PKG             := gcc-mpc
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 0.8.1
+$(PKG)_CHECKSUM := 5ef03ca7aee134fe7dfecb6c9d048799f0810278
+$(PKG)_SUBDIR   := mpc-$($(PKG)_VERSION)
+$(PKG)_FILE     := mpc-$($(PKG)_VERSION).tar.gz
+$(PKG)_WEBSITE  := http://www.multiprecision.org/
+$(PKG)_URL      := $($(PKG)_WEBSITE)/mpc/download/$($(PKG)_FILE)
+$(PKG)_DEPS     :=
+
+define $(PKG)_UPDATE
+    wget -q -O- 'https://gforge.inria.fr/scm/viewvc.php/tags/?root=mpc&sortby=date' | \
+    $(SED) -n 's,.*<a name="\([0-9][^"]*\)".*,\1,p' | \
+    head -1
+endef
--- src/gcc.mk
+++ src/gcc.mk
@@ -3,15 +3,15 @@
 
 # GCC
 PKG             := gcc
-$(PKG)_IGNORE   := 4.4.3
-$(PKG)_VERSION  := 4.4.0
-$(PKG)_CHECKSUM := 9215af6beb900ca1eef1d6a40c3dabe990203b25
+$(PKG)_IGNORE   := 
+$(PKG)_VERSION  := 4.5.0
+$(PKG)_CHECKSUM := 4beb8366ce1883f37255aa57f0258e7d3cd13a9b
 $(PKG)_SUBDIR   := gcc-$($(PKG)_VERSION)
 $(PKG)_FILE     := gcc-$($(PKG)_VERSION).tar.bz2
 $(PKG)_WEBSITE  := http://gcc.gnu.org/
 $(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/gcc/gcc-$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_URL_2    := ftp://ftp.cs.tu-berlin.de/pub/gnu/gcc/gcc-$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_DEPS     := mingwrt mingwrt-dll w32api binutils gcc-gmp gcc-mpfr gcc-tdm gcc-pthreads
+$(PKG)_DEPS     := mingwrt mingwrt-dll w32api binutils gcc-gmp gcc-mpc gcc-mpfr gcc-pthreads
 
 define $(PKG)_UPDATE
     wget -q -O- 'http://ftp.gnu.org/gnu/gcc/?C=M;O=D' | \
@@ -35,18 +35,11 @@ $(PKG)_CONFIGURE_OPTIONS := \
         --enable-sjlj-exceptions
 
 define $(PKG)_BUILD
-    # unpack TDM-GCC
-    mkdir '$(1)/gcc-tdm'
-    cd    '$(1)/gcc-tdm' && $(call UNPACK_PKG_ARCHIVE,gcc-tdm)
-    # apply TDM-GCC patches
-    cd '$(1)' && \
-        for p in '$(1)'/gcc-tdm/*.patch; do \
-            $(SED) -i 's,\r$$,,' "$$p" || exit 1; \
-            patch -p1 -u < "$$p" || exit 1; \
-        done
     # unpack support libraries
     cd '$(1)' && $(call UNPACK_PKG_ARCHIVE,gcc-gmp)
     mv '$(1)/$(gcc-gmp_SUBDIR)' '$(1)/gmp'
+    cd '$(1)' && $(call UNPACK_PKG_ARCHIVE,gcc-mpc)
+    mv '$(1)/$(gcc-mpc_SUBDIR)' '$(1)/mpc'
     cd '$(1)' && $(call UNPACK_PKG_ARCHIVE,gcc-mpfr)
     mv '$(1)/$(gcc-mpfr_SUBDIR)' '$(1)/mpfr'
     # build everything of GCC except libgomp and libmudflap
