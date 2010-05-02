31917d198b84e6d3c14744fc19980e2c340a73fe
Volker Grabsch <vog@notjusthosting.com>
2010-05-02 03:14:43 +0200

    don't suppress the win32 command line tools of package proj

--- src/proj.mk
+++ src/proj.mk
@@ -23,8 +23,7 @@ define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
         --disable-shared \
-        --prefix='$(PREFIX)/$(TARGET)' \
-        --bindir='$(1)/bindirsink'
+        --prefix='$(PREFIX)/$(TARGET)'
     $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
     $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 endef
