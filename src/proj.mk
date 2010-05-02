e6199107f523264e2cac55f1be74e75c8ec3b652
Volker Grabsch <vog@notjusthosting.com>
2010-05-02 03:40:24 +0200

    use explicitly the --with-mutex configure option in package proj

--- src/proj.mk
+++ src/proj.mk
@@ -23,7 +23,8 @@ define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
         --disable-shared \
-        --prefix='$(PREFIX)/$(TARGET)'
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --with-mutex
     $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
     $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 endef
