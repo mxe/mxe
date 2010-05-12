dee66284ffcf20f5bdde4190640a78d876450fe3
Volker Grabsch <vog@notjusthosting.com>
2010-05-12 19:22:38 +0200

    new package: gdb  (by Mark de Wever)

---
+++ src/gdb.mk
@@ -0,0 +1,28 @@
+# This file is part of mingw-cross-env.
+# See doc/index.html for further information.
+
+# gdb
+PKG             := gdb
+$(PKG)_VERSION  := 7.1
+$(PKG)_CHECKSUM := 417e2e637a296ea0e1cdddf56233311b8708fa19
+$(PKG)_SUBDIR   := gdb-$($(PKG)_VERSION)
+$(PKG)_FILE     := gdb-$($(PKG)_VERSION).tar.bz2
+$(PKG)_WEBSITE  := http://www.gnu.org/software/gdb/
+$(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/$(PKG)/$($(PKG)_FILE)
+$(PKG)_URL_2    := ftp://ftp.cs.tu-berlin.de/pub/gnu/$(PKG)/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc expat libiconv zlib
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://ftp.gnu.org/gnu/gdb/?C=M;O=D' | \
+    $(SED) -n 's,.*<a href="gdb-\([0-9][^"]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)'
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)/gdb' -j 1 install
+endef
