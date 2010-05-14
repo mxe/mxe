ac61fc08bfc6bb6f3b59411e879f590234690902
Volker Grabsch <vog@notjusthosting.com>
2010-05-14 11:37:11 +0200

    actually build the test program of package gtkglext

--- src/gtkglext.mk
+++ src/gtkglext.mk
@@ -38,4 +38,9 @@ define $(PKG)_BUILD
         INFO_DEPS= \
         GDKGLEXT_DEP_CFLAGS='`'$(TARGET)-pkg-config' gtk+-2.0 --cflags`' \
         GTKGLEXT_DEP_CFLAGS='`'$(TARGET)-pkg-config' gtk+-2.0 --cflags`'
+
+    '$(TARGET)-gcc' \
+        -W -Wall -ansi -pedantic \
+        '$(2).c' -o '$(PREFIX)/$(TARGET)/bin/test-gtkglext.exe' \
+        `'$(TARGET)-pkg-config' gtkglext-1.0 --cflags --libs`
 endef
