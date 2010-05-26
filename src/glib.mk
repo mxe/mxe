86410a44443ea3b4ae699d732e3aa45d9160a9ca
Volker Grabsch <vog@notjusthosting.com>
2010-05-26 11:24:19 +0200

    update version recognition of package glib

--- src/glib.mk
+++ src/glib.mk
@@ -15,7 +15,7 @@ $(PKG)_DEPS     := gcc gettext pcre libiconv zlib
 define $(PKG)_UPDATE
     wget -q -O- 'http://git.gnome.org/browse/glib/refs/tags' | \
     grep '<a href=' | \
-    $(SED) -n 's,.*<a[^>]*>\([0-9][^<]*\)<.*,\1,p' | \
+    $(SED) -n 's,.*<a[^>]*>glib-*\([0-9][^<]*\)<.*,\1,p' | \
     grep -v '^2\.24\.' | \
     head -1
 endef
