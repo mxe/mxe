64e4474fa41777f816e209687c5f0a9e1bb223cb
Volker Grabsch <vog@notjusthosting.com>
2010-05-31 11:49:58 +0000

    new package: libgee  (by Matias De la Puente)

---
+++ src/libgee.mk
@@ -0,0 +1,30 @@
+# This file is part of mingw-cross-env.
+# See doc/index.html for further information.
+
+# libgee
+PKG             := libgee
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 0.5.0
+$(PKG)_CHECKSUM := 78d7fbd0668d01bc23e9772211b4885ae7e479cd
+$(PKG)_SUBDIR   := libgee-$($(PKG)_VERSION)
+$(PKG)_FILE     := libgee-$($(PKG)_VERSION).tar.bz2
+$(PKG)_WEBSITE  := http://live.gnome.org/Libgee
+$(PKG)_URL      := http://ftp.gnome.org/pub/gnome/sources/libgee/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc glib
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://git.gnome.org/browse/libgee/refs/tags' | \
+    grep '<a href=' | \
+    $(SED) -n "s,.*<a href='[^']*/tag/?id=LIBGEE_\\([0-9]*_[0-9]*[02468]_[^<]*\\)'.*,\\1,p" | \
+    $(SED) 's,_,.,g' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)'
+    $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+    $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+endef
