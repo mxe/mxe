e6cf0850fdfad8e343378ee01cc9053aea3cb840
Volker Grabsch <vog@notjusthosting.com>
2010-05-31 16:26:25 +0200

    new package: xvidcore

---
+++ src/xvidcore.mk
@@ -0,0 +1,31 @@
+# This file is part of mingw-cross-env.
+# See doc/index.html for further information.
+
+# xvidcore
+PKG             := xvidcore
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.2.2
+$(PKG)_CHECKSUM := d6e73c725b2fe74695f9fd129966c98f81b27fea
+$(PKG)_SUBDIR   := xvidcore/build/generic
+$(PKG)_FILE     := xvidcore-$($(PKG)_VERSION).tar.gz
+$(PKG)_WEBSITE  := http://www.xvid.org/
+$(PKG)_URL      := http://downloads.xvid.org/downloads/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc pthreads
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://www.xvid.org/' | \
+    $(SED) -n 's,.*Xvid \([0-9][^ ]*\) .*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./bootstrap.sh && ./configure \
+        --host='$(TARGET)' \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --disable-assembly
+    $(MAKE) -C '$(1)' -j '$(JOBS)' BUILD_DIR='build' SHARED_LIB=
+    $(INSTALL) -d '$(PREFIX)/$(TARGET)/include'
+    $(INSTALL) -m644 '$(1)/../../src/xvid.h' '$(PREFIX)/$(TARGET)/include/'
+    $(INSTALL) -d '$(PREFIX)/$(TARGET)/lib'
+    $(INSTALL) -m644 '$(1)/build/xvidcore.a' '$(PREFIX)/$(TARGET)/lib/'
+endef
