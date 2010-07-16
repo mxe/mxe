97a45232f6dd9f894157cefc39c4d436eeffb72d
Volker Grabsch <vog@notjusthosting.com>
2010-07-16 17:16:43 +0200

    new package: pfstools  (by Martin Lambers)

---
+++ src/pfstools.mk
@@ -0,0 +1,28 @@
+# This file is part of mingw-cross-env.
+# See doc/index.html for further information.
+
+# pfstools
+PKG             := pfstools
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.8.2
+$(PKG)_CHECKSUM := 6109587b0f8e53d1d963e9e02f7298bf0d197d5d
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_WEBSITE  := http://pfstools.sourceforge.net/
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc
+
+define $(PKG)_UPDATE
+    $(call SOURCEFORGE_FILES,http://sourceforge.net/projects/pfstools/files/pfstools/) | \
+    $(SED) -n 's,.*pfstools-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --disable-shared
+    $(MAKE) -C '$(1)'/src/pfs -j '$(JOBS)'
+    $(MAKE) -C '$(1)'/src/pfs -j 1 install
+endef
