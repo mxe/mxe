34cbc1ebcf55aa57ef9762aa73b03a4f9dde5bb9
Mark Brand <mabrand@mabrand.nl>
2010-09-20 01:40:14 +0200

    upgrade package imagemagick

--- src/imagemagick.mk
+++ src/imagemagick.mk
@@ -4,8 +4,8 @@
 # ImageMagick
 PKG             := imagemagick
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 6.6.4-3
-$(PKG)_CHECKSUM := 74f8ba993b6a93ba4e3c68ca658e37072ad10fbc
+$(PKG)_VERSION  := 6.6.4-4
+$(PKG)_CHECKSUM := 1e36c3c9a9a9b800278c790ba7de781ecba29e14
 $(PKG)_SUBDIR   := ImageMagick-$($(PKG)_VERSION)
 $(PKG)_FILE     := ImageMagick-$($(PKG)_VERSION).tar.bz2
 $(PKG)_WEBSITE  := http://www.imagemagick.org/
