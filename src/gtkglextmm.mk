deff50b40a374e23d7340c9611f1159b07fdbfff
Mark Brand <mabrand@mabrand.nl>
2010-09-30 08:42:37 +0200

    package gtkglextmm: correct test output name

--- src/gtkglextmm.mk
+++ src/gtkglextmm.mk
@@ -33,6 +33,6 @@ define $(PKG)_BUILD
 
     '$(TARGET)-g++' \
         -W -Wall -Werror -pedantic -std=c++0x \
-        '$(2).cpp' -o '$(PREFIX)/$(TARGET)/bin/test-$(TARGET).exe' \
+        '$(2).cpp' -o '$(PREFIX)/$(TARGET)/bin/test-gtkglextmm.exe' \
         `'$(TARGET)-pkg-config' gtkglextmm-1.2 --cflags --libs`
 endef
