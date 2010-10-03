78ae1f81128a383752e6f650f268100e5ddd96a8
Tony Theodore <tonyt@logyst.com>
2010-10-03 22:37:52 +1100

    package xine-lib: correct package name of faad2

--- src/xine-lib.mk
+++ src/xine-lib.mk
@@ -10,7 +10,7 @@ $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_WEBSITE  := http://www.xine-project.org/
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/xine/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc faad ffmpeg flac fontconfig freetype libiconv libmng pthreads sdl speex theora vorbis zlib
+$(PKG)_DEPS     := gcc faad2 ffmpeg flac fontconfig freetype libiconv libmng pthreads sdl speex theora vorbis zlib
 
 define $(PKG)_UPDATE
     wget -q -O- 'http://hg.debian.org/hg/xine-lib/xine-lib/tags' | \
