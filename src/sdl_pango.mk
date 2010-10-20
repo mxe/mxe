37c97de9c1252224c1cfd4b3ef9597acd67d5fba
Volker Grabsch <vog@notjusthosting.com>
2010-10-20 17:12:45 +0200

    fix line endings in SDL_Pango.pc

--- src/sdl_pango.mk
+++ src/sdl_pango.mk
@@ -19,6 +19,7 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
+    $(SED) -i 's,\r$$,,'                        '$(1)/SDL_Pango.pc.in'
     $(SED) -i 's,^\(Requires:.*\),\1 pangoft2,' '$(1)/SDL_Pango.pc.in'
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
