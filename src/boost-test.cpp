a3d5a8e958e9a0ace7f1b9f81062d7d04ccef6b9
Lothar May <lothar.imap@googlemail.com>
2010-10-23 00:04:16 +0200

    improved test program and portability patches for package boost

--- src/boost-test.cpp
+++ src/boost-test.cpp
@@ -3,6 +3,18 @@
 
 #include <iostream>
 #include <boost/archive/xml_oarchive.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/thread/tss.hpp>
+
+boost::thread_specific_ptr<int> ptr;
+
+void test_thread()
+{
+    if (ptr.get() == 0) {
+        ptr.reset(new int(0));
+    }
+    std::cout << "Hello, World! from thread" << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
@@ -12,6 +24,9 @@ int main(int argc, char *argv[])
     boost::archive::xml_oarchive oa(std::cout);
     std::string s = "\n\n    Hello, World!\n\n";
     oa << BOOST_SERIALIZATION_NVP(s);
+
+    boost::thread thrd(test_thread);
+    thrd.join();
 
     return 0;
 }
