4b7e00d7e752bf6d8fb3f62bfcdbe16c484c74d4
Volker Grabsch <vog@notjusthosting.com>
2010-10-22 23:18:43 +0200

    add alternative download URL for package ffmpeg

--- src/ffmpeg.mk
+++ src/ffmpeg.mk
@@ -10,6 +10,7 @@ $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_WEBSITE  := http://www.ffmpeg.org/
 $(PKG)_URL      := http://www.ffmpeg.org/releases/$($(PKG)_FILE)
+$(PKG)_URL_2    := http://launchpad.net/ffmpeg/main/$($(PKG)_VERSION)/+download/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc bzip2 faad2 faac lame libvpx opencore-amr sdl speex theora vorbis x264 xvidcore zlib
 
 define $(PKG)_UPDATE
