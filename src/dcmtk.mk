d89bcd74eccebc3c7a329721cdd4e56e9f157bc9
Volker Grabsch <vog@notjusthosting.com>
2010-11-16 15:09:10 +0100

    improved meta information of package dcmtk

--- src/dcmtk.mk
+++ src/dcmtk.mk
@@ -1,7 +1,7 @@
 # This file is part of mingw-cross-env.
 # See doc/index.html for further information.
 
-# dcmtk
+# DCMTK
 PKG             := dcmtk
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 3.5.4
@@ -9,11 +9,13 @@ $(PKG)_CHECKSUM := 169056874947083e68eb941fcd53faaebc862ae5
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_WEBSITE  := http://dicom.offis.de/dcmtk.php.en
-$(PKG)_URL      := ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/dcmtk354/$($(PKG)_FILE)
+$(PKG)_URL      := ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/dcmtk$(subst .,,$($(PKG)_VERSION))/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc openssl tiff libpng libxml2 zlib
 
 define $(PKG)_UPDATE
-    /bin/false
+    wget -q -O- 'http://dicom.offis.de/download/dcmtk/?C=M;O=D' | \
+    $(SED) -n 's,.*<a href="dcmtk\([0-9]\)\([0-9]\)\([0-9]\)/.*,\1.\2.\3,p' | \
+    head -1
 endef
 
 define $(PKG)_BUILD
