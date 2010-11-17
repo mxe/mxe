cc06f950de7119efa2fc4227f6f3c83abb2e5e3a
Volker Grabsch <vog@notjusthosting.com>
2010-11-17 03:36:25 +0100

    fix website URLs of package: metis

--- src/metis.mk
+++ src/metis.mk
@@ -8,7 +8,7 @@ $(PKG)_VERSION  := 4.0
 $(PKG)_CHECKSUM := 580568308e1fa40e5a7a77cacbf27f865d6c01af
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
-$(PKG)_WEBSITE  := http://glaros.dtc.umn.edu/gkhome/views/metis
+$(PKG)_WEBSITE  := http://glaros.dtc.umn.edu/gkhome/metis/metis/overview
 $(PKG)_URL      := http://glaros.dtc.umn.edu/gkhome/fetch/sw/$(PKG)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
 
