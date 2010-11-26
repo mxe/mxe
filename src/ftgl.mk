c90c27d680a7e0be803cf2dac92d6f1f257f2d53
Volker Grabsch <vog@notjusthosting.com>
2010-11-26 14:05:42 +0100

    cleanup package: ftgl

--- src/ftgl.mk
+++ src/ftgl.mk
@@ -7,19 +7,20 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.1.3-rc5
 $(PKG)_CHECKSUM := 8508f26c84001d7bc949246affa03744fa1fd22e
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_WEBSITE  := http://sourceforge.net/projects/ftgl/
-$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/ftgl/FTGL Source/$(subst -,~,$($(PKG)_VERSION))/$($(PKG)_FILE)
-$(PKG)_DEPS     := freetype
+$(PKG)_FILE     := $($(PKG)_SUBDIR).tar.bz2
+$(PKG)_WEBSITE  := http://sourceforge.net/projects/$(PKG)/
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/FTGL Source/$(subst -,~,$($(PKG)_VERSION))/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc freetype
 
 define $(PKG)_UPDATE
-    $(call SOURCEFORGE_FILES,http://sourceforge.net/projects/ftgl/files/FTGL%20Source/) | \
+    $(call SOURCEFORGE_FILES,http://sourceforge.net/projects/ftgl/files/FTGL Source/) | \
     $(SED) -n 's,.*ftgl-\([0-9][^>]*\)\.tar.*,\1,p' | \
     tail -1
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && autoconf && ./configure \
+    cd '$(1)' && autoconf
+    cd '$(1)' && ./configure \
         --host='$(TARGET)' \
         --disable-shared \
         --disable-freetypetest \
