f7436a8ba33aa6633c9740a30cdc2a8526cbc9ab
Volker Grabsch <vog@notjusthosting.com>
2010-11-26 23:16:08 +0100

    cleanup package: mxml

--- src/mxml.mk
+++ src/mxml.mk
@@ -1,15 +1,15 @@
 # This file is part of mingw-cross-env.
 # See doc/index.html for further information.
 
-# minixml
+# Mini-XML
 PKG             := mxml
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.6
 $(PKG)_CHECKSUM := df180bd2e3890c97fa8a05dd131f9285468cffe1
-$(PKG)_SUBDIR   := mxml-$($(PKG)_VERSION)
-$(PKG)_FILE     := mxml-$($(PKG)_VERSION).tar.gz
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_WEBSITE  := http://www.minixml.org/
-$(PKG)_URL      := http://ftp.easysw.com/pub/mxml/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_URL      := http://ftp.easysw.com/pub/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
