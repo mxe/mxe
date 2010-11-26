ae012ab8bf50c5a5209348ec0a97fb1c6e5b95d8
Volker Grabsch <vog@notjusthosting.com>
2010-11-26 16:53:12 +0100

    cleanup package: bfd

--- src/bfd.mk
+++ src/bfd.mk
@@ -1,17 +1,17 @@
 # This file is part of mingw-cross-env.
 # See doc/index.html for further information.
 
-# Binary file descriptor lib
-PKG             := libbfd
-$(PKG)_IGNORE   := $(binutils_IGNORE)
-$(PKG)_VERSION  := $(binutils_VERSION)
-$(PKG)_CHECKSUM := $(binutils_CHECKSUM)
-$(PKG)_SUBDIR   := $(binutils_SUBDIR)
-$(PKG)_FILE     := $(binutils_FILE)
-$(PKG)_WEBSITE  := $(binutils_WEBSITE)
-$(PKG)_URL      := $(binutils_URL)
-$(PKG)_URL_2    := $(binutils_URL2)
-$(PKG)_DEPS     := binutils
+# Binary File Descriptor library
+PKG             := bfd
+$(PKG)_IGNORE    = $(binutils_IGNORE)
+$(PKG)_VERSION   = $(binutils_VERSION)
+$(PKG)_CHECKSUM  = $(binutils_CHECKSUM)
+$(PKG)_SUBDIR    = $(binutils_SUBDIR)
+$(PKG)_FILE      = $(binutils_FILE)
+$(PKG)_WEBSITE   = $(binutils_WEBSITE)
+$(PKG)_URL       = $(binutils_URL)
+$(PKG)_URL_2     = $(binutils_URL2)
+$(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
     echo $(binutils_VERSION)
