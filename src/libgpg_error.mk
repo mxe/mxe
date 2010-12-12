8761fe839f0949ca1f3c0e69cbd73a11b4740827
Volker Grabsch <vog@notjusthosting.com>
2010-12-12 14:03:35 +0100

    bugfix: build package libgpg_error only for C, not CL

--- src/libgpg_error.mk
+++ src/libgpg_error.mk
@@ -24,8 +24,9 @@ define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
         --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)' \
         --disable-nls \
-        --prefix='$(PREFIX)/$(TARGET)'
+        --disable-languages
     $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
     $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 endef
