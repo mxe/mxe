e2042071bad2ba428b1c713fd4ce1aac1ccc55f1
Volker Grabsch <vog@notjusthosting.com>
2010-12-19 11:22:18 +0100

    provide alternative download location for OpenSceneGraph in case the main site is down

--- src/openscenegraph.mk
+++ src/openscenegraph.mk
@@ -10,6 +10,7 @@ $(PKG)_SUBDIR   := OpenSceneGraph-$($(PKG)_VERSION)
 $(PKG)_FILE     := OpenSceneGraph-$($(PKG)_VERSION).zip
 $(PKG)_WEBSITE  := http://www.openscenegraph.org/
 $(PKG)_URL      := http://www.openscenegraph.org/downloads/stable_releases/OpenSceneGraph-$($(PKG)_VERSION)/source/$($(PKG)_FILE)
+$(PKG)_URL_2    := http://distfiles.macports.org/OpenSceneGraph/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc curl ffmpeg freetype gdal giflib jasper jpeg libpng openexr tiff xine-lib zlib
 
 define $(PKG)_UPDATE
