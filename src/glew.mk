353a604d7716c8428f496f5c720c8a9b23c2c8d2
Martin Lambers <marlam@marlam.de>
2011-01-05 14:30:15 +0100

    package glew: make use of the provided pkg-config file

--- src/glew.mk
+++ src/glew.mk
@@ -22,9 +22,20 @@ define $(PKG)_BUILD
     cd '$(1)' && $(TARGET)-gcc -O2 -DGLEW_STATIC -Iinclude -c -o glew.o src/glew.c
     cd '$(1)' && $(TARGET)-ar cr libGLEW.a glew.o
     $(TARGET)-ranlib '$(1)/libGLEW.a'
+    $(SED) \
+        -e "s|@prefix@|$(PREFIX)/$(TARGET)|g" \
+        -e "s|@libdir@|$(PREFIX)/$(TARGET)/lib|g" \
+        -e "s|@exec_prefix@|$(PREFIX)/$(TARGET)/bin|g" \
+        -e "s|@includedir@|$(PREFIX)/$(TARGET)/include/GL|g" \
+        -e "s|@version@|$(glew_VERSION)|g" \
+        -e "s|Cflags: |Cflags: -DGLEW_STATIC |g" \
+        -e "s|-lGLEW|-lGLEW -lopengl32|g" \
+        < '$(1)'/glew.pc.in > '$(1)'/glew.pc
     $(INSTALL) -d '$(PREFIX)/$(TARGET)/lib'
     $(INSTALL) -m644 '$(1)/libGLEW.a' '$(PREFIX)/$(TARGET)/lib/'
     $(INSTALL) -m644 '$(1)/libGLEW.a' '$(PREFIX)/$(TARGET)/lib/libglew32s.a'
+    $(INSTALL) -d '$(PREFIX)/$(TARGET)/lib/pkgconfig'
+    $(INSTALL) -m644 '$(1)/glew.pc' '$(PREFIX)/$(TARGET)/lib/pkgconfig/'
     $(INSTALL) -d '$(PREFIX)/$(TARGET)/include'
     $(INSTALL) -d '$(PREFIX)/$(TARGET)/include/GL'
     $(INSTALL) -m644 '$(1)/include/GL/glew.h' '$(1)/include/GL/wglew.h' '$(PREFIX)/$(TARGET)/include/GL/'
