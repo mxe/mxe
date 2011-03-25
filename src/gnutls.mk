257ed383f43db9425b6f798bfe3f99bd8909060f
Mark Brand <mabrand@mabrand.nl>
2011-03-25 17:21:46 +0100

    upgrade package gnutls

--- src/gnutls.mk
+++ src/gnutls.mk
@@ -4,8 +4,8 @@
 # GnuTLS
 PKG             := gnutls
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.10.5
-$(PKG)_CHECKSUM := 4530657082a0e754356de89a5529d1ad7a18e777
+$(PKG)_VERSION  := 2.12.0
+$(PKG)_CHECKSUM := 48f27df66c7a721c5ce2b690ab936a3c9162c40a
 $(PKG)_SUBDIR   := gnutls-$($(PKG)_VERSION)
 $(PKG)_FILE     := gnutls-$($(PKG)_VERSION).tar.bz2
 $(PKG)_WEBSITE  := http://www.gnu.org/software/gnutls/
@@ -23,17 +23,19 @@ endef
 
 define $(PKG)_BUILD
     echo '/* DEACTIVATED */' > '$(1)/gl/gai_strerror.c'
+    $(SED) -i 's/^\(SUBDIRS.*\) tests/\1/;' '$(1)/Makefile.in'
+    $(SED) -i 's/^\(SUBDIRS.*\) doc/\1/;' '$(1)/Makefile.in'
     $(SED) -i 's, sed , $(SED) ,g' '$(1)/gl/tests/Makefile.in'
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
+        --enable-static \
         --disable-shared \
         --prefix='$(PREFIX)/$(TARGET)' \
-        --with-libgcrypt-prefix='$(PREFIX)/$(TARGET)' \
         --disable-nls \
-        --with-included-opencdk \
         --with-included-libtasn1 \
         --with-included-libcfg \
-        --with-included-lzo \
+        --with-libgcrypt \
+        --with-lzo \
         LIBS='-lz'
     $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS= defexec_DATA=
 endef
