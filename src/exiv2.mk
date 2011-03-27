d29806fbe30a2d0f6ecd064084bb6966dc22e116
Volker Grabsch <vog@notjusthosting.com>
2011-03-27 15:53:15 +0200

    remove an old GCC workaround which is no longer needed

--- src/exiv2.mk
+++ src/exiv2.mk
@@ -20,8 +20,6 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    # workaround for the missing snprintf() in the <cstdio> of GCC-4.4.0
-    $(SED) -i 's,#include <cstdio>,#include <stdio.h>,' '$(1)/xmpsdk/src/XMPMeta.cpp'
     # wine confuses the cross-compiling detection, so set it explicitly
     $(SED) -i 's,cross_compiling=no,cross_compiling=yes,' '$(1)/configure'
     $(SED) -i 's/ -no-undefined//;' '$(1)/configure'
