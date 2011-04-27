a1fdb3a5513408d8b8bb0e390599060cc9dcecc2
Mark Brand <mabrand@mabrand.nl>
2011-04-27 17:08:26 +0200

    update package ffmpeg

--- src/ffmpeg.mk
+++ src/ffmpeg.mk
@@ -4,8 +4,8 @@
 # ffmpeg
 PKG             := ffmpeg
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.6.90-rc0
-$(PKG)_CHECKSUM := a5c898a220a7f2220a49a37c44d273d1775d081c
+$(PKG)_VERSION  := 0.7-rc1
+$(PKG)_CHECKSUM := d15c005f46483fbfc3b678cb1c89123762b56b82
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_WEBSITE  := http://www.ffmpeg.org/
