055bc0c689500ca573e2d77d103a5b14c7ce87b8
Mark Brand <mabrand@mabrand.nl>
2011-04-29 09:13:21 +0200

    package gcc: fix update macro

--- src/gcc.mk
+++ src/gcc.mk
@@ -16,7 +16,7 @@ $(PKG)_DEPS     := mingwrt w32api binutils gcc-gmp gcc-mpc gcc-mpfr
 define $(PKG)_UPDATE
     wget -q -O- 'http://ftp.gnu.org/gnu/gcc/?C=M;O=D' | \
     $(SED) -n 's,.*<a href="gcc-\([0-9][^"]*\)/".*,\1,p' | \
-    grep -v '^4\.[43]\.' | \
+    grep -v '^4\.[543]\.' | \
     head -1
 endef
 
