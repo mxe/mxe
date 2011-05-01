8d00dc333228261aaaeacfa4840da8ac891538fb
Volker Grabsch <vog@notjusthosting.com>
2011-05-01 17:44:17 +0200

    corrections and improvements for package poco

--- src/poco.mk
+++ src/poco.mk
@@ -4,22 +4,29 @@
 # POCO C++ Libraries
 PKG             := poco
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.4.1
-$(PKG)_PATCHNUM := p1
+$(PKG)_VERSION  := 1.4.1p1
 $(PKG)_CHECKSUM := e9810b8fc14c607626d7d3c74baf60726a61e83c
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)$($(PKG)_PATCHNUM)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION)$($(PKG)_PATCHNUM).tar.gz
-$(PKG)_WEBSITE  := http://sourceforge.net/projects/$(PKG)/
-$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/poco/sources/poco-$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
+$(PKG)_WEBSITE  := http://pocoproject.org/
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/sources/$(PKG)-$(word 1,$(subst p, ,$($(PKG)_VERSION)))/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://pocoproject.org/download/' | \
+    $(SED) -n 's,.*poco-\([0-9][^>/]*\)\.tar.*,\1,p' | \
+    head -1
+endef
 
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         --config=MinGW-CrossEnv \
         --static \
-        --prefix='$(PREFIX)/$(TARGET)' 
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install 
+        --prefix='$(PREFIX)/$(TARGET)'
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install
 
-    '$(TARGET)-g++' -W -Wall -Werror -ansi -pedantic \
-        '$(2).cpp' -o '$(PREFIX)/$(TARGET)/bin/test-poco.exe' -lPocoFoundation
+    '$(TARGET)-g++' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(2).cpp' -o '$(PREFIX)/$(TARGET)/bin/test-poco.exe' \
+        -lPocoFoundation
 endef
