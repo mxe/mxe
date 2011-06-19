08b639beb62654bd54f1780cfb772dab4d512c71
Tony Theodore <tonyt@logyst.com>
2011-06-19 18:22:17 +1000

    package libgsf: gettext/intltool configure patch no longer required

--- src/libgsf.mk
+++ src/libgsf.mk
@@ -23,8 +23,6 @@ endef
 define $(PKG)_BUILD
     $(SED) -i 's,^\(Requires:.*\),\1 gio-2.0,' '$(1)'/libgsf-1.pc.in
     echo 'Libs.private: -lz -lbz2'          >> '$(1)'/libgsf-1.pc.in
-    cd '$(1)' && aclocal -I m4
-    cd '$(1)' && autoconf
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
         --disable-shared \
