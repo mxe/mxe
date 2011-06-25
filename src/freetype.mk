2d1d3f09f5e275591fae3715e0d462f33b37d0f9
Mark Brand <mabrand@mabrand.nl>
2011-06-25 22:18:01 +0200

    update package freetype

--- src/freetype.mk
+++ src/freetype.mk
@@ -4,8 +4,8 @@
 # freetype
 PKG             := freetype
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.4.4
-$(PKG)_CHECKSUM := 1d136cbc51c67b212c91ba04dc5db797f35e64e6
+$(PKG)_VERSION  := 2.4.5
+$(PKG)_CHECKSUM := edc5217f1a0e3bec37f9846bb5203e8d005cf470
 $(PKG)_SUBDIR   := freetype-$($(PKG)_VERSION)
 $(PKG)_FILE     := freetype-$($(PKG)_VERSION).tar.bz2
 $(PKG)_WEBSITE  := http://freetype.sourceforge.net/
