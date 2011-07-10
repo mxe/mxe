d7f47c6f21cea311fa6a5f8a5ec3d9b150cf1fc3
Mark Brand <mabrand@mabrand.nl>
2011-07-10 22:38:36 +0200

    update package openscenegraph

--- src/openscenegraph.mk
+++ src/openscenegraph.mk
@@ -4,14 +4,14 @@
 # OpenSceneGraph
 PKG             := openscenegraph
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.8.5
-$(PKG)_CHECKSUM := 7325504070a262aa6eb952bc36d98843a4ed4cf7
+$(PKG)_VERSION  := 3.0.0
+$(PKG)_CHECKSUM := aa4982bf1502c8ad52b40508221978a2ca48e34e
 $(PKG)_SUBDIR   := OpenSceneGraph-$($(PKG)_VERSION)
 $(PKG)_FILE     := OpenSceneGraph-$($(PKG)_VERSION).zip
 $(PKG)_WEBSITE  := http://www.openscenegraph.org/
-$(PKG)_URL      := http://www.openscenegraph.org/downloads/stable_releases/OpenSceneGraph-$($(PKG)_VERSION)/source/$($(PKG)_FILE)
+$(PKG)_URL      := http://www.openscenegraph.org/downloads/stable_releases/OpenSceneGraph-3.0/source/$($(PKG)_FILE)
 $(PKG)_URL_2    := http://distfiles.macports.org/OpenSceneGraph/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc curl ffmpeg freetype gdal giflib jasper jpeg libpng openexr tiff xine-lib zlib
+$(PKG)_DEPS     := gcc curl ffmpeg freetype gdal giflib jasper jpeg libpng openexr tiff xine-lib zlib dcmtk
 
 define $(PKG)_UPDATE
     wget -q -O- 'http://www.openscenegraph.org/projects/osg/browser/OpenSceneGraph/tags?order=date&desc=1' | \
@@ -30,5 +30,5 @@ define $(PKG)_BUILD
         -DDYNAMIC_OPENSCENEGRAPH=OFF \
         -DBUILD_OSG_APPLICATIONS=OFF \
         -D_OPENTHREADS_ATOMIC_USE_GCC_BUILTINS_EXITCODE=1
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install VERBOSE=1
 endef
