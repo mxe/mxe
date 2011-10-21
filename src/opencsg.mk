1b9c7b13e8e340cfae6c3e87566c0864040889a5
Brad Pitcher <bradpitcher@gmail.com>
2011-10-22 01:27:06 +0200

    new package opencsg

---
+++ src/opencsg.mk
@@ -0,0 +1,30 @@
+# This file is part of mingw-cross-env.
+# See doc/index.html for further information.
+
+# opencsg
+PKG             := opencsg
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.3.1
+$(PKG)_CHECKSUM := d94c22d4e824c6b5e69a97a726b514ecaf9e4596
+$(PKG)_SUBDIR   := OpenCSG-$($(PKG)_VERSION)
+$(PKG)_FILE     := OpenCSG-$($(PKG)_VERSION).tar.gz
+$(PKG)_WEBSITE  := http://www.opencsg.org/
+$(PKG)_URL      := http://www.opencsg.org/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc freeglut glew qt
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://www.opencsg.org/#download' | \
+    grep 'OpenCSG-' | \
+    $(SED) -n 's,.*OpenCSG-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)/src' && $(SED) -i 's/release/release staticlib\nDEFINES += FREEGLUT_STATIC GLEW_STATIC/g' src.pro
+    cd '$(1)/example' && $(SED) -i 's,\(lGLEW\),\1 -lglut -lglu32 -lopengl32 -lwinmm -lgdi32\nDEFINES += FREEGLUT_STATIC GLEW_STATIC,g' example.pro
+    cd '$(1)/example' && $(SED) -i 's,opencsgexample,test-libopencsg\nRelease:DESTDIR = $(PREFIX)/$(TARGET)/bin/,g' example.pro
+    cd '$(1)/src' && '$(TARGET)-qmake' src.pro && $(MAKE) -C '$(1)/src' -j '$(JOBS)'
+    cd '$(1)/example' && '$(TARGET)-qmake' example.pro && $(MAKE) -C '$(1)/example' -j '$(JOBS)'
+    $(INSTALL) -m644 '$(1)/lib/libopencsg.a' '$(PREFIX)/$(TARGET)/lib/'
+    $(INSTALL) -m644 '$(1)/include/opencsg.h' '$(PREFIX)/$(TARGET)/include/'
+endef
