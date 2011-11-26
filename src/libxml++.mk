612e61523e45025ee57b1dc1e220962c58e43629
Tony Theodore <tonyt@logyst.com>
2011-11-26 14:50:42 +1100

    package libxml++: portability fix for bsd make

--- src/libxml++.mk
+++ src/libxml++.mk
@@ -23,7 +23,8 @@ define $(PKG)_BUILD
     cd '$(1)' && CXX="$(TARGET)-g++ -mthreads" ./configure \
         --host='$(TARGET)' \
         --disable-shared \
-        --prefix='$(PREFIX)/$(TARGET)'
+        --prefix='$(PREFIX)/$(TARGET)' \
+        MAKE=$(MAKE)
     $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
     $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 endef
