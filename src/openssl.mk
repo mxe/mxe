c5a2e48f98ac993f345bd17848260ca8b1194268
Mark Brand <mabrand@mabrand.nl>
2012-03-13 03:01:04 +0100

    update package openssl

--- src/openssl.mk
+++ src/openssl.mk
@@ -4,8 +4,8 @@
 # openssl
 PKG             := openssl
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.0g
-$(PKG)_CHECKSUM := 2b517baada2338663c27314cb922f9755e73e07f
+$(PKG)_VERSION  := 1.0.0h
+$(PKG)_CHECKSUM := 6d4587a96817147021e93ca266441daf4bcbf485
 $(PKG)_SUBDIR   := openssl-$($(PKG)_VERSION)
 $(PKG)_FILE     := openssl-$($(PKG)_VERSION).tar.gz
 $(PKG)_WEBSITE  := http://www.openssl.org/
