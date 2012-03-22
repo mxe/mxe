715b83bde15557a6346689d63f0e30207173de84
Volker Grabsch <vog@notjusthosting.com>
2012-03-22 20:21:59 +0100

    New package: libiberty

---
+++ src/libiberty-test.c
@@ -0,0 +1,24 @@
+/* This file is part of mingw-cross-env.       */
+/* See doc/index.html for further information. */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <libiberty.h>
+
+int main(int argc, char *argv[])
+{
+    char *s;
+
+    (void)argc;
+    (void)argv;
+
+    if (asprintf(&s, "Test%i", 123) >= 0) {
+        printf("asprintf output: %s\n", s);
+        free(s);
+        return 0;
+    } else {
+        printf("asprintf() failed!\n");
+        return 1;
+    }
+}
