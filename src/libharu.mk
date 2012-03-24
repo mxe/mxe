0fbb964a5744bfdb89ce5f568e88b20763884017
Tony Theodore <tonyt@logyst.com>
2012-03-24 23:20:54 +1100

    package libharu: fix update macro

--- src/libharu.mk
+++ src/libharu.mk
@@ -13,9 +13,9 @@ $(PKG)_URL      := http://libharu.org/files/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc zlib libpng
 
 define $(PKG)_UPDATE
-    wget -q -O- 'http://libharu.org/files/' | \
-    grep 'libharu-' | \
+    wget -q -O- 'http://libharu.org/files/?C=M;O=D' | \
     $(SED) -n 's,.*libharu-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    grep -v 'rc' | \
     head -1
 endef
 
