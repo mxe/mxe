2f374210cb9e2662325283a7c33f7ecd8a472a2c
LM <lmemsm@gmail.com>
2012-03-25 23:08:30 +1100

    package libffi: fix update macro

--- src/libffi.mk
+++ src/libffi.mk
@@ -13,7 +13,7 @@ $(PKG)_URL      := ftp://sourceware.org/pub/$(PKG)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
-    wget -q --no-check-certificate -O- 'http://github.com/atgreen/libffi/tags' | \
+    wget -q --no-check-certificate -O- 'https://github.com/atgreen/libffi/tags' | \
     grep '<a href="/atgreen/libffi/tarball/' | \
     $(SED) -n 's,.*href="/atgreen/libffi/tarball/v\([0-9][^"]*\)".*,\1,p' | \
     head -1
