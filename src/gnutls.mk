8a1048357a49f5c660d1d7d366e6da66bde91c6c
Mark Brand <mabrand@mabrand.nl>
2012-04-03 14:05:49 +0200

    update package gnutls

--- src/gnutls.mk
+++ src/gnutls.mk
@@ -2,7 +2,7 @@
 # See index.html for further information.
 
 PKG             := gnutls
-$(PKG)_CHECKSUM := 2e2adc41e0c75d8170fdca5955bd007ae93ceada
+$(PKG)_CHECKSUM := 2aeac620a26e6c8f266954110578d8817939b084
 $(PKG)_SUBDIR   := gnutls-$($(PKG)_VERSION)
 $(PKG)_FILE     := gnutls-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := ftp://ftp.gnutls.org/pub/gnutls/$($(PKG)_FILE)
