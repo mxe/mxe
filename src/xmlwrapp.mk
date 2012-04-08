546b1b623c3a28e36a5d08e36b1f52d1eb55b29a
Mark Brand <mabrand@mabrand.nl>
2012-04-08 16:13:59 +0200

    update package xmlwrapp

--- src/xmlwrapp.mk
+++ src/xmlwrapp.mk
@@ -3,11 +3,11 @@
 
 PKG             := xmlwrapp
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := b3ef8bff215bbacd988790615b76379672105928
+$(PKG)_CHECKSUM := 7bd66ecc1204b296580a499fd929d77b79e6e85a
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc libxml2 libxslt
+$(PKG)_DEPS     := gcc boost libxml2 libxslt
 
 define $(PKG)_UPDATE
     wget -q -O- 'http://sourceforge.net/projects/xmlwrapp/files/xmlwrapp/' | \
