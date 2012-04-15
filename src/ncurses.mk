5f0403f7dc5ef0cb85cacfe9365c312692483e22
Tony Theodore <tonyt@logyst.com>
2012-04-16 02:45:25 +1000

    package ncurses:add to index.html

--- src/ncurses.mk
+++ src/ncurses.mk
@@ -4,11 +4,9 @@
 # ncurses
 PKG             := ncurses
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.9
 $(PKG)_CHECKSUM := 3e042e5f2c7223bffdaac9646a533b8c758b65b5
 $(PKG)_SUBDIR   := ncurses-$($(PKG)_VERSION)
 $(PKG)_FILE     := ncurses-$($(PKG)_VERSION).tar.gz
-$(PKG)_WEBSITE  := http://www.gnu.org/software/ncurses/
 $(PKG)_URL      := http://ftp.gnu.org/pub/gnu/ncurses/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
 
