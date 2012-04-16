6171bcb8e714b9d938627998262966868f992ea8
Luis Saavedra <luis94855510@gmail.com>
2012-04-16 22:26:19 +1000

    package wt: add cmake finder install directory

--- src/wt.mk
+++ src/wt.mk
@@ -31,6 +31,7 @@ define $(PKG)_BUILD
         -DGM_PREFIX='$(PREFIX)/$(TARGET)' \
         -DGM_LIBS="`'$(TARGET)-pkg-config' --libs-only-l GraphicsMagick++`" \
         -DPANGO_FT2_LIBS="`'$(TARGET)-pkg-config' --libs-only-l pangoft2`" \
+        -DWT_CMAKE_FINDER_INSTALL_DIR='/lib/wt' \
         -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
         -DCMAKE_BUILD_TYPE:STRING="Release" \
         '$(1)'
