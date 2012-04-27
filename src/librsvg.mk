2d87e7b0f9626c1760454bdeb66f3f91918972d5
Tony Theodore <tonyt@logyst.com>
2012-04-27 21:16:59 +1000

    remove more gtk* x86_64-static-mingw32 build rules

--- src/librsvg.mk
+++ src/librsvg.mk
@@ -33,5 +33,4 @@ define $(PKG)_BUILD
         `'$(TARGET)-pkg-config' librsvg-2.0 --cflags --libs`
 endef
 
-$(PKG)_BUILD_i686-static-mingw32   = $($(PKG)_BUILD)
-$(PKG)_BUILD_x86_64-static-mingw32 = $($(PKG)_BUILD)
+$(PKG)_BUILD_i686-static-mingw32 = $($(PKG)_BUILD)
--- src/plotmm.mk
+++ src/plotmm.mk
@@ -27,5 +27,4 @@ define $(PKG)_BUILD
         INFO_DEPS=
 endef
 
-$(PKG)_BUILD_i686-static-mingw32   = $($(PKG)_BUILD)
-$(PKG)_BUILD_x86_64-static-mingw32 = $($(PKG)_BUILD)
+$(PKG)_BUILD_i686-static-mingw32 = $($(PKG)_BUILD)
