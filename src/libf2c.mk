14b3608e3c357f3177d13129ef31ea4ca7bae305
Tony Theodore <tonyt@logyst.com>
2012-05-04 01:55:07 +1000

    package libf2c: remove runtime test and unnecessary makefile options

--- src/libf2c.mk
+++ src/libf2c.mk
@@ -4,13 +4,13 @@
 PKG             := libf2c
 $(PKG)_IGNORE   :=
 $(PKG)_CHECKSUM := f71066b41695738dec2261de71eaf02a1aaffe8b
-$(PKG)_SUBDIR   := 
+$(PKG)_SUBDIR   :=
 $(PKG)_FILE     := $(PKG).zip
 $(PKG)_URL      := http://www.netlib.org/f2c/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
-    echo 1 
+    echo 1
 endef
 
 define $(PKG)_BUILD
@@ -18,7 +18,8 @@ define $(PKG)_BUILD
         CC=$(TARGET)-gcc \
         AR=$(TARGET)-ar \
         LD=$(TARGET)-ld \
-        RANLIB=$(TARGET)-ranlib
+        RANLIB=$(TARGET)-ranlib \
+        CFLAGS='-O -DUSE_CLOCK'
     $(INSTALL) -m644 '$(1)/libf2c.a' '$(PREFIX)/$(TARGET)/lib'
     $(INSTALL) -m644 '$(1)/f2c.h'    '$(PREFIX)/$(TARGET)/include'
 endef
