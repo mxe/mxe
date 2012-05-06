189d7068c2b15515594079cae360688411151c50
Tony Theodore <tonyt@logyst.com>
2012-05-06 22:58:09 +1000

    package log4cxx: fix reference to i686-pc-mingw32

--- src/log4cxx.mk
+++ src/log4cxx.mk
@@ -33,5 +33,5 @@ define $(PKG)_BUILD
     '$(TARGET)-g++' \
         -W -Wall -Werror -ansi -pedantic \
         '$(2).cpp' -o '$(PREFIX)/$(TARGET)/bin/test-log4cxx.exe' \
-        `i686-pc-mingw32-pkg-config liblog4cxx --libs`
+        `$(TARGET)-pkg-config liblog4cxx --libs`
 endef
