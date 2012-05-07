1213449eec9b4dd5f9ca9950809b90080cf1ba64
Tony Theodore <tonyt@logyst.com>
2012-05-08 00:55:30 +1000

    package mingw-w64: add update macro, fix url, and update

--- src/mingw-w64.mk
+++ src/mingw-w64.mk
@@ -4,15 +4,18 @@
 # Mingw-w64
 PKG             := mingw-w64
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 700c3d0517e65141f9719d38418549205bd69cef
+$(PKG)_CHECKSUM := bdbe8e33c630b82c14e4280e8bcfb8dc4b247f18
 $(PKG)_SUBDIR   := $(PKG)-v$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-v$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := http://sourceforge.net/projects/$(PKG)/files/$(PKG)/$(PKG)-release/$($(PKG)_FILE)/download
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$(PKG)-release/$($(PKG)_FILE)
 $(PKG)_DEPS     :=
 
 define $(PKG)_UPDATE
-    echo 'Warning: Updates are temporarily disabled for package mingw-w64.' >&2;
-    echo $(mingw-w64_VERSION)
+    wget -q -O- 'http://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/' | \
+    $(SED) -n 's,.*mingw-w64-v\([0-9][^>]*\)\.tar.*,\1,p' | \
+    grep -v [a-zA-Z] | \
+    sort | \
+    tail -1
 endef
 
 $(PKG)_BUILD_i686-static-mingw32    =
