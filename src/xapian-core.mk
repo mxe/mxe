9082339ba4cffbb7577028e3957fc43aa9572ccc
Richard Boulton <richard@tartarus.org>
2012-05-10 04:41:24 +0100

    new package: xapian-core

---
+++ src/xapian-core.mk
@@ -0,0 +1,18 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := xapian-core
+$(PKG)_IGNORE   :=
+$(PKG)_CHECKSUM := 1be1896ab11a3a66c6c0ade962c700d96678116e
+$(PKG)_SUBDIR   := xapian-core-$($(PKG)_VERSION)
+$(PKG)_FILE     := xapian-core-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://oligarchy.co.uk/xapian/$($(PKG)_VERSION)/xapian-core-$($(PKG)_VERSION).tar.gz
+$(PKG)_DEPS     := gcc zlib
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --enable-static
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+endef
