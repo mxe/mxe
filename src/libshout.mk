1faa9ded41f02572a106cfcaa84f6a049afded11
Mark Brand <mabrand@mabrand.nl>
2012-05-20 23:13:14 +0200

    update package libshout

--- src/libshout.mk
+++ src/libshout.mk
@@ -3,7 +3,7 @@
 
 PKG             := libshout
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := cabc409e63f55383f4d85fac26d3056bf0365aac
+$(PKG)_CHECKSUM := a6f26441ec27b6f9b55fba38b99bd1d7ca17fecf
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://downloads.us.xiph.org/releases/$(PKG)/$($(PKG)_FILE)
@@ -18,9 +18,9 @@ endef
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
+        --build="`config.guess`" \
         --prefix='$(PREFIX)/$(TARGET)' \
         --disable-shared \
-        --disable-debug \
         --disable-thread \
         --infodir='$(1)/sink' \
         --mandir='$(1)/sink'
