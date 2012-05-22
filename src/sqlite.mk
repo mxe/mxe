46df151e98a0174dd61ee4ea254bdc184647c2fb
Mark Brand <mabrand@mabrand.nl>
2012-05-22 14:29:06 +0200

    update package sqlite

--- src/sqlite.mk
+++ src/sqlite.mk
@@ -3,7 +3,7 @@
 
 PKG             := sqlite
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 30e6b0912f074cff5563697367c2ba9608d83bd3
+$(PKG)_CHECKSUM := 295b9acdf9c0452017ca1af392c0e5db019e6874
 $(PKG)_SUBDIR   := $(PKG)-autoconf-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-autoconf-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.sqlite.org/$($(PKG)_FILE)
