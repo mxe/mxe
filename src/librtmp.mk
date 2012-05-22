2162a16a0e294af67bf040a2b77aa028a8fb215a
Matthias Gehre <M.Gehre@gmx.de>
2012-05-22 23:08:41 +0300

    Add package for librtmp (version 2.3)

---
+++ src/librtmp.mk
@@ -0,0 +1,21 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := librtmp
+$(PKG)_IGNORE   :=
+$(PKG)_CHECKSUM := b65ce7708ae79adb51d1f43dd0b6d987076d7c42
+$(PKG)_SUBDIR   := rtmpdump-$($(PKG)_VERSION)
+$(PKG)_FILE     := rtmpdump-$($(PKG)_VERSION).tgz
+$(PKG)_URL      := http://rtmpdump.mplayerhq.hu/download/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc openssl
+
+define $(PKG)_UPDATE
+	wget -q -O- 'http://rtmpdump.mplayerhq.hu/download/' | \
+    $(SED) -n 's,.*rtmpdump-\([0-9.]*\)\.tgz.*,\1,ip' | \
+    sort -r | \
+	head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && make SYS=mingw CROSS_COMPILE='$(TARGET)-' prefix='$(PREFIX)/$(TARGET)' -j '$(JOBS)' install
+endef
