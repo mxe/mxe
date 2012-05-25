caefb69845cf32452de7e7f384e2c762b29ad651
Mark Brand <mabrand@mabrand.nl>
2012-05-25 16:09:09 +0200

    package libpaper: add alternate URL

--- src/libpaper.mk
+++ src/libpaper.mk
@@ -7,6 +7,7 @@ $(PKG)_CHECKSUM := 1f7a810a433a5a68b099aa92777cc2d0b3d03b42
 $(PKG)_SUBDIR   := libpaper-$($(PKG)_VERSION)
 $(PKG)_FILE     := libpaper_$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://ftp.debian.org/debian/pool/main/libp/$(PKG)/$($(PKG)_FILE)
+$(PKG)_URL_2    := http://linux.mirrors.es.net/pub/ubuntu/pool/main/libp/$(PKG)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
