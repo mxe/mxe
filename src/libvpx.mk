2613e25a915baf2f62a2341790383b22c94090dd
Mark Brand <mabrand@mabrand.nl>
2012-05-27 22:12:59 +0200

    package libvpx: add missing pthreads DEP

--- src/libvpx.mk
+++ src/libvpx.mk
@@ -7,7 +7,7 @@ $(PKG)_CHECKSUM := 356af5f770c50cd021c60863203d8f30164f6021
 $(PKG)_SUBDIR   := $(PKG)-v$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-v$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://webm.googlecode.com/files/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc
+$(PKG)_DEPS     := gcc pthreads
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://code.google.com/p/webm/downloads/list?sort=-uploaded' | \
