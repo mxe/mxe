eefeffc853ae73c6e149f7a0d6f96f2c314740bb
William <r.3@libertysurf.fr>
2012-06-27 15:55:54 +0200

    add package ocaml-lablgtk2
    patch submitted upstream : http://forge.ocamlcore.org/tracker/index.php?func=detail&aid=1178&group_id=220&atid=1033

---
+++ src/ocaml-lablgtk2-test.ml
@@ -0,0 +1,10 @@
+open GlGtk
+
+let destroy () = GMain.Main.quit ()
+
+let _ =
+  let _ = GtkMain.Main.init () in
+  let window = GWindow.window () in
+  let _ = window#connect#destroy ~callback:destroy in
+  window#show ();
+  GMain.Main.main ()
