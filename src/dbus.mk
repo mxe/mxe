f3b077d0c28c17edb356e1364d347e843fe77218
Mark Brand <mabrand@mabrand.nl>
2012-06-27 22:29:24 +0200

    update package dbus

--- src/dbus.mk
+++ src/dbus.mk
@@ -3,7 +3,7 @@
 
 PKG             := dbus
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 9bfc971ff4cafa2741050d53a98cc7bfdc56880a
+$(PKG)_CHECKSUM := 09d55ac4ea4ccf0d233070909494028f5a4e1e63
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://$(PKG).freedesktop.org/releases/$(PKG)/$($(PKG)_FILE)
