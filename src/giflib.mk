015ee5634616eb6a60d313cec0d91d21bf92883f
Mark Brand <mabrand@mabrand.nl>
2012-07-04 09:01:39 +0200

    update package giflib

--- src/giflib.mk
+++ src/giflib.mk
@@ -3,15 +3,19 @@
 
 PKG             := giflib
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := bc942711f75de7d8539f79be34d69c0d53c381c1
+$(PKG)_CHECKSUM := 0b032c2104a0956e2f91b6c11ff8c5515421cd8b
 $(PKG)_SUBDIR   := giflib-$($(PKG)_VERSION)
 $(PKG)_FILE     := giflib-$($(PKG)_VERSION).tar.bz2
-$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/giflib/giflib-4.x/$($(PKG)_FILE)
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/giflib/giflib-5.x/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://sourceforge.net/projects/giflib/files/giflib 4.x/' | \
-    $(SED) -n 's,.*/giflib-\([0-9][^"]*\)/".*,\1,p' | \
+    $(WGET) -q -O- 'http://giflib.git.sourceforge.net/git/gitweb.cgi?p=giflib/giflib;a=tags' | \
+    grep '<a class="list name"' | \
+    $(SED) -n 's,.*<a[^>]*>\([0-9][^<]*\)<.*,\1,p' | \
+    grep -v alpha | \
+    grep -v beta | \
+    grep -v rc | \
     head -1
 endef
 
@@ -23,5 +27,6 @@ define $(PKG)_BUILD
         --disable-shared \
         --without-x \
         CPPFLAGS='-D_OPEN_BINARY'
+    echo 'all:' > '$(1)/doc/Makefile'
     $(MAKE) -C '$(1)/lib' -j '$(JOBS)' install
 endef
