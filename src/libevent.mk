82fe6a17c9fbb6d432eb22848ad9dc12fa9ea435
Mark Brand <mabrand@mabrand.nl>
2012-08-25 11:32:18 +0200

    update package libevent

--- src/libevent.mk
+++ src/libevent.mk
@@ -3,7 +3,7 @@
 
 PKG             := libevent
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 28c109190345ce5469add8cf3f45c5dd57fe2a85
+$(PKG)_CHECKSUM := 20bb4a1a296ac93c08dfc32ae19ab874cab67a0c
 $(PKG)_SUBDIR   := libevent-$($(PKG)_VERSION)-stable
 $(PKG)_FILE     := libevent-$($(PKG)_VERSION)-stable.tar.gz
 $(PKG)_URL      := https://github.com/downloads/$(PKG)/$(PKG)/$($(PKG)_FILE)
