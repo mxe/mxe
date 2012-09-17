cd7ada78a6a85d93e79fe305c4155a5a2bb9868f
Tony Theodore <tonyt@logyst.com>
2012-09-17 21:31:16 +1000

    package suitesparse: add secondary url

--- src/suitesparse.mk
+++ src/suitesparse.mk
@@ -6,6 +6,7 @@ $(PKG)_CHECKSUM := 46b24a28eef4b040ea5a02d2c43e82e28b7d6195
 $(PKG)_SUBDIR   := SuiteSparse
 $(PKG)_FILE     := SuiteSparse-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.cise.ufl.edu/research/sparse/SuiteSparse/$($(PKG)_FILE)
+$(PKG)_URL_2    := https://distfiles.macports.org/SuiteSparse/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc blas lapack
 
 define $(PKG)_UPDATE
