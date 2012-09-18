22b2cce94157688b22c8a02c6f8259623355d8f5
Mark Brand <mabrand@mabrand.nl>
2012-09-18 10:31:39 +0200

    update package mdbtools

--- src/mdbtools.mk
+++ src/mdbtools.mk
@@ -4,21 +4,25 @@
 # mdbtools
 PKG             := mdbtools
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 37a50d623a444ec690d2677b12b59c2f11e497c0
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_CHECKSUM := 62fe0703fd8691e4536e1012317406bdb72594cf
+$(PKG)_SUBDIR   := brianb-mdbtools-004cc9f
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_URL      := http://github.com/brianb/$(PKG)/tarball/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc glib
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://sourceforge.net/projects/mdbtools/files/mdbtools/' | \
-    $(SED) -n 's,.*/\([0-9][^"]*\)/".*,\1,p' | \
+    $(WGET) -q -O- 'https://github.com/brianb/mdbtools/tags' | \
+    grep '<a href="/brianb/mdbtools/tarball/' | \
+    $(SED) -n 's,.*href="/brianb/mdbtools/tarball/\([0-9][^"_]*\)".*,\1,p' | \
     head -1
 endef
 
 define $(PKG)_BUILD
+    '$(SED)' -i 's/libtooloze/libtoolize/g;' '$(1)/autogen.sh'
+    cd '$(1)' && NOCONFIGURE=1 ./autogen.sh
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
+        --build="`config.guess`" \
         --disable-shared \
         --prefix='$(PREFIX)/$(TARGET)' \
         PKG_CONFIG='$(PREFIX)/bin/$(TARGET)-pkg-config'
