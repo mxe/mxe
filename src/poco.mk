a27ba32baa62d5bebdba5db94c2590f814c9c6e6
Mark Brand <mabrand@mabrand.nl>
2012-10-12 11:56:54 +0200

    update package poco

--- src/poco.mk
+++ src/poco.mk
@@ -3,7 +3,7 @@
 
 PKG             := poco
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 9e1ef31d369c67eef2d3ce1ca8f2b6ac5cc38ec4
+$(PKG)_CHECKSUM := 22ee6a217eb3f5e0f48c85893af128fe8955eb4f
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/sources/$(PKG)-$(word 1,$(subst p, ,$($(PKG)_VERSION)))/$($(PKG)_FILE)
