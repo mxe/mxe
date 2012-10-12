025889f1a1e70e4118fbaaec1d19283c973303aa
Mark Brand <mabrand@mabrand.nl>
2012-10-12 11:50:52 +0200

    update package xapian-core

--- src/xapian-core.mk
+++ src/xapian-core.mk
@@ -3,7 +3,7 @@
 
 PKG             := xapian-core
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 1be1896ab11a3a66c6c0ade962c700d96678116e
+$(PKG)_CHECKSUM := 2b96800280fee41eed767289620172f5226c9c4f
 $(PKG)_SUBDIR   := xapian-core-$($(PKG)_VERSION)
 $(PKG)_FILE     := xapian-core-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://oligarchy.co.uk/xapian/$($(PKG)_VERSION)/xapian-core-$($(PKG)_VERSION).tar.gz
