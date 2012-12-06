d1eef209b29bd54d2cec52b25f77a45779b8e1aa
Mark Brand <mabrand@mabrand.nl>
2012-12-06 15:04:38 +0100

    update package harfbuzz

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -3,7 +3,7 @@
 
 PKG             := harfbuzz
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 50124f84e769f24fd4fb9c4ab8aeda5afb7fdba2
+$(PKG)_CHECKSUM := a3db0aac6d4f80b4c76050007de8e1135c4ff17f
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.freedesktop.org/software/$(PKG)/release/$($(PKG)_FILE)
