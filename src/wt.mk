d0fa988bd3e79bf3feb0b84afddd9bd788d2d6ef
Tony Theodore <tonyt@logyst.com>
2012-12-12 23:17:33 +1100

    package wt: fix update script

--- src/wt.mk
+++ src/wt.mk
@@ -10,7 +10,7 @@ $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/witty/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc boost openssl libharu graphicsmagick pango postgresql sqlite
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://sourceforge.net/projects/witty/files/witty/' | \
+    $(WGET) -q -O- 'http://sourceforge.net/projects/witty/files/wt/' | \
     $(SED) -n 's,.*wt-\([0-9][^>]*\)\.tar.*,\1,p' | \
     tail -1
 endef
