3ea130d93568fe07bd4da7a6c7012778e8d420b6
Sylvain Beucler <beuc@beuc.net>
2012-12-16 21:18:29 +0100

    Add build file for libzip
    - Build against latest stable release
    
    - Cross-compilation broke somewhere between 0.9 and 0.10, we could
      recommend
        http://www.sourceware.org/autobook/autobook/autobook_255.html
      to upstream so they fix 'zip.h'.
      Provided quick patch meanwhile.
    
    - Added 'Requires: zlib' in the pkg-config file, we could suggest the
      improvement to upstream
    
    - _UPDATE rule implemented
    
    - Test case included
    
    Coded while hacking GNU FreeDink :)

---
+++ src/libzip-test.c
@@ -0,0 +1,42 @@
+/*
+ * This file is part of MXE.
+ * See index.html for further information.
+ */
+
+#include <zip.h>
+#include <stdlib.h>
+
+/* Adapted from freedink/src/SDL_rwops_libzip.c */
+int main(int argc, char* argv[])
+{
+    struct zip* zarchive;
+    int errorp = 0;
+
+    (void)argc;
+    (void)argv;
+
+    zarchive = zip_open("idontexist.zip", ZIP_CHECKCONS, &errorp);
+    if (errorp != 0)
+        {
+            char *errorbuf = NULL;
+            int len = 1;
+            errorbuf = malloc(len);
+            len = zip_error_to_str(errorbuf, len, errorp, errno);
+            errorbuf = realloc(errorbuf, len + 1);
+            len = zip_error_to_str(errorbuf, len, errorp, errno);
+            fprintf(stderr, "zip_open: %s\n", errorbuf);
+            free(errorbuf);
+        }
+    else
+        {
+            struct zip_file* zfile;
+            zfile = zip_fopen(zarchive, "fichier.txt", 0);
+            if (zfile == NULL)
+                {
+                    fprintf(stderr, "zip_open: %s\n", zip_strerror(zarchive));
+                    zip_close(zarchive);
+                }
+        }
+
+    return 0;
+}
---
+++ src/libzip.mk
@@ -0,0 +1,32 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := libzip
+$(PKG)_IGNORE   :=
+$(PKG)_CHECKSUM := 04be811a1919e1063a1f5210671181b7b5416d45
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := http://www.nih.at/libzip/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc zlib
+
+define $(PKG)_UPDATE
+    wget -q -O- 'http://www.nih.at/libzip/' | \
+    $(SED) -n 's,.*libzip-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+# TODO: send a patch upstream
+    echo 'Requires: zlib' >> '$(1)/libzip.pc.in'
+
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --disable-shared
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+
+    '$(TARGET)-gcc' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(2).c' -o '$(PREFIX)/$(TARGET)/bin/test-libzip.exe' \
+        `'$(TARGET)-pkg-config' libzip --cflags --libs`
+endef
