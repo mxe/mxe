5db1e81e7585b535124a70e80f28d09c0407372a
Tony Theodore <tonyt@logyst.com>
2012-12-27 23:59:35 +1100

    qt5 packages: add multi-target rules (x86_64-static-mingw32 tested)

--- src/qwt.mk
+++ src/qwt.mk
@@ -28,3 +28,8 @@ define $(PKG)_BUILD
     # install
     $(INSTALL) -m755 '$(1)/examples/bin/sinusplot.exe' '$(PREFIX)/$(TARGET)/bin/test-qwt.exe'
 endef
+
+$(PKG)_BUILD_i686-static-mingw32    = $($(PKG)_BUILD)
+$(PKG)_BUILD_x86_64-static-mingw32  = $($(PKG)_BUILD)
+$(PKG)_BUILD_i686-dynamic-mingw32   = $($(PKG)_BUILD)
+$(PKG)_BUILD_x86_64-dynamic-mingw32 = $($(PKG)_BUILD)
