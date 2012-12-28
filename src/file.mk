ccbc036c698ffabfc7c1fe1f2663e1e20ab2534f
Tony Theodore <tonyt@logyst.com>
2012-12-28 14:02:24 +1100

    remove failing x86_64 build rules

--- src/file.mk
+++ src/file.mk
@@ -41,6 +41,6 @@ define $(PKG)_BUILD
 endef
 
 $(PKG)_BUILD_i686-static-mingw32    = $($(PKG)_BUILD)
-$(PKG)_BUILD_x86_64-static-mingw32  = $($(PKG)_BUILD)
+$(PKG)_BUILD_x86_64-static-mingw32  =
 $(PKG)_BUILD_i686-dynamic-mingw32   = $($(PKG)_BUILD)
 $(PKG)_BUILD_x86_64-dynamic-mingw32 = $($(PKG)_BUILD)
