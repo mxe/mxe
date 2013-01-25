5cae6217d05d0fde3ce94adaf865a68640a7af75
Martin Lambers <marlam@marlam.de>
2013-01-25 23:17:26 +0100

    update package matio

--- src/matio.mk
+++ src/matio.mk
@@ -3,11 +3,11 @@
 
 PKG             := matio
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 721b9b57f2437850dd114acca1f422622e2c47e2
+$(PKG)_CHECKSUM := b2e4f5524a9d7ce1fb268a67bb5b1a7427d047d4
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc zlib
+$(PKG)_DEPS     := gcc zlib hdf5
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://sourceforge.net/projects/matio/files/matio/' | \
@@ -19,9 +19,7 @@ define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
         --prefix='$(PREFIX)/$(TARGET)' \
-        --disable-shared \
-        --disable-test \
-        --disable-docs
+        --disable-shared
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     $(MAKE) -C '$(1)' -j 1 install
 endef
