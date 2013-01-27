6b8cd9095d22d7e13105f1e1a4ec315318c3ed84
Alois Schlögl <alois.schloegl@ist.ac.at>
2013-01-27 21:54:46 +0100

    NEW: add package libbiosig v1.4.3 (including biosig-tools, biosig4c++)

---
+++ src/libbiosig.mk
@@ -0,0 +1,39 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := libbiosig
+$(PKG)_IGNORE   := 
+$(PKG)_CHECKSUM := f1fbae9c000c1faa25dafcd7ae94241a14ff2e2c
+$(PKG)_SUBDIR   := biosig4c++-$($(PKG)_VERSION)
+$(PKG)_FILE     := biosig4c++-$($(PKG)_VERSION).src.tar.gz
+$(PKG)_URL      := https://sourceforge.net/projects/biosig/files/BioSig%20for%20C_C%2B%2B/src/$($(PKG)_FILE)/download
+$(PKG)_DEPS     := gcc suitesparse zlib libgomp libiberty libiconv
+
+define $(PKG)_UPDATE
+#    wget -q -O- 'http://biosig.sourceforge.net/download.html' | \
+#    $(SED) -n 's_.*>libbiosig, version \([0-9]\.[0-9]\.[0-9]\).*tar.gz_\1_ip' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+
+
+    # make sure NDEBUG is defined 
+    $(SED) -i '/NDEBUG/ s|^#*||g' '$(1)'/Makefile 
+
+    TARGET='$(TARGET)' $(MAKE) -C '$(1)' -j '$(JOBS)' io.h libbiosig.a libbiosig2.a libgdf.a save2gdf libphysicalunits.a
+
+    $(INSTALL) -m644 '$(1)'/save2gdf           '$(PREFIX)/$(TARGET)/bin/save2gdf.exe'
+
+    $(INSTALL) -m644 '$(1)/biosig.h'           '$(PREFIX)/$(TARGET)/include/'
+    $(INSTALL) -m644 '$(1)/libbiosig.a'        '$(PREFIX)/$(TARGET)/lib/'
+    $(INSTALL) -m644 '$(1)/libgdf.a'           '$(PREFIX)/$(TARGET)/lib/'
+
+    $(INSTALL) -m644 '$(1)/biosig2.h'          '$(PREFIX)/$(TARGET)/include/'
+    $(INSTALL) -m644 '$(1)/libbiosig2.a'       '$(PREFIX)/$(TARGET)/lib/'
+
+    $(INSTALL) -m644 '$(1)/physicalunits.h'    '$(PREFIX)/$(TARGET)/include/'
+    $(INSTALL) -m644 '$(1)/libphysicalunits.a' '$(PREFIX)/$(TARGET)/lib/'
+
+endef
+
