3350b6d88e6098fd70c658c68becf83883138d8c
Martin Lambers <marlam@marlam.de>
2013-01-30 18:02:44 +0100

    package imagemagick: fix runtime errors due to threading bugs
    Runtime tests have revealed that libMagick++ crashes in pthread-related code.
    Therefore, this patch disables threading in the package. Unfortunately the
    configure switch is not enough; the resulting config header needs to be tweaked.

--- src/imagemagick.mk
+++ src/imagemagick.mk
@@ -25,7 +25,9 @@ define $(PKG)_BUILD
         --with-x=no \
         --without-zlib \
         --disable-largefile \
-	--with-freetype='$(PREFIX)/$(TARGET)/bin/freetype-config'
+        --without-threads \
+        --with-freetype='$(PREFIX)/$(TARGET)/bin/freetype-config'
+    $(SED) -i 's/#define MAGICKCORE_HAVE_PTHREAD 1//g' '$(1)/magick/magick-baseconfig.h'
     $(SED) -i 's/#define MAGICKCORE_ZLIB_DELEGATE 1//g' '$(1)/magick/magick-config.h'
     $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS=
     $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS=
