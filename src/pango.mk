ec45affda61db2669a3e6ec67d0cfcf6ecc9f52c
Mark Brand <mabrand@mabrand.nl>
2013-03-24 11:04:30 +0100

    update package pango

--- src/pango.mk
+++ src/pango.mk
@@ -3,7 +3,7 @@
 
 PKG             := pango
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := dc55a67a2df363943ca8fa3b936783c8817b01bf
+$(PKG)_CHECKSUM := ef26b2f4eae1860dcbcfc8d3d47fedf0f0d1427b
 $(PKG)_SUBDIR   := pango-$($(PKG)_VERSION)
 $(PKG)_FILE     := pango-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://ftp.gnome.org/pub/gnome/sources/pango/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
