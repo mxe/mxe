f785c6c315cd5e1a783f84d428876cf58d480a4e
Niels Kristian Bech Jensen <nkbj1970@hotmail.com>
2013-03-25 16:55:59 +1100

    add package liblqr-1 (Liquid Rescale Library)

---
+++ src/liblqr-1.mk
@@ -0,0 +1,25 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := liblqr-1
+$(PKG)_IGNORE   :=
+$(PKG)_CHECKSUM := 69639f7dc56a084f59a3198f3a8d72e4a73ff927
+$(PKG)_SUBDIR   := liblqr-1-$($(PKG)_VERSION)
+$(PKG)_FILE     := liblqr-1-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := http://liblqr.wdfiles.com/local--files/en:download-page/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc glib
+
+define $(PKG)_UPDATE
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --disable-shared \
+        --enable-static \
+        --disable-declspec \
+        --disable-install-man
+    $(MAKE) -C '$(1)' -j
+    $(MAKE) -C '$(1)' -j 1 install
+endef
