5a6eca8904e892d1fe599213d48759e25ed6615f
Tony Theodore <tonyt@logyst.com>
2013-03-31 17:04:34 +1100

    gtk2 dependencies: disable mingw-w64 builds

--- src/librsvg.mk
+++ src/librsvg.mk
@@ -32,3 +32,6 @@ define $(PKG)_BUILD
         '$(2).c' -o '$(PREFIX)/$(TARGET)/bin/test-librsvg.exe' \
         `'$(TARGET)-pkg-config' librsvg-2.0 --cflags --libs`
 endef
+
+$(PKG)_BUILD_i686-w64-mingw32 =
+$(PKG)_BUILD_x86_64-w64-mingw32 =
