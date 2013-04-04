6d0b5f1848083bcadb2bfa8414241d0c3a946224
Alois Schlögl <alois.schloegl@ist.ac.at>
2013-04-04 13:41:43 +0200

    package octave: add some tricks from mxe-octave; compilation seems to finish ; link step fails with many 'undefined references'

--- src/octave.mk
+++ src/octave.mk
@@ -7,8 +7,8 @@ $(PKG)_CHECKSUM := 3cc9366b6dbbd336eaf90fe70ad16e63705d82c4
 $(PKG)_SUBDIR   := octave-$($(PKG)_VERSION)
 $(PKG)_FILE     := octave-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := ftp://ftp.gnu.org/gnu/$(PKG)/$($(PKG)_FILE)
-$(PKG)_DEPS     := blas curl fftw fltk freetype gcc glpk gnuplot graphicsmagick hdf5 lapack libgomp pcre pthreads qhull qrupdate qt readline suitesparse texinfo zlib
-## arpack fontconfig pstoedit qscintilla ### FIXME: these are dependencies for Octave, but do not build yet
+$(PKG)_DEPS     := blas curl fftw fltk freetype gcc glpk graphicsmagick hdf5 lapack libgomp pcre pthreads qrupdate readline suitesparse texinfo zlib
+## arpack fontconfig gnuplot pstoedit qhull qscintilla qt ### FIXME: these are dependencies for Octave, but do not build yet
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://ftp.gnu.org/gnu/octave/?C=M;O=D' | \
@@ -18,23 +18,36 @@ endef
 
 
 define $(PKG)_BUILD
+    if [ $(BUILD_SHARED) = yes ]; then \
+      $(INSTALL) -d '$(PREFIX)/$(TARGET)/bin'; \
+      $(MAKE_SHARED_FROM_STATIC) --ar '$(TARGET)-ar' --ld '$(TARGET)-gcc' '$(PREFIX)/$(TARGET)/lib/libuuid.a'; \
+      $(INSTALL) -m755 '$(PREFIX)/$(TARGET)/lib/libuuid.dll.a' '$(PREFIX)/$(TARGET)/lib/libuuid.dll.a'; \
+      $(INSTALL) -m755 '$(PREFIX)/$(TARGET)/lib/libuuid.dll' '$(PREFIX)/$(TARGET)/bin/libuuid.dll'; \
+      rm -f '$(PREFIX)/$(TARGET)/lib/libuuid.dll'; \
+    fi
+
+    mkdir '$(1)/.build'
+    cd '$(1)' && autoreconf --force -W none
 
     ### make sure pcre-config of host is used
-    $(SED) -i 's,pcre-config,$(PREFIX)/$(TARGET)/bin/pcre-config,g' '$(1)'/configure
+    $(SED) -i 's,pcre-config,$(PREFIX)/$(TARGET)/bin/pcre-config,g' '$(1)/configure'
 
     # configure
-    cd    '$(1)' && ./configure \
+    cd '$(1)/.build' && '$(1)/configure' \
         --host='$(TARGET)' \
         --build="`config.guess`" \
         --prefix='$(PREFIX)/$(TARGET)' \
         --enable-static \
-	--enable-openmp
+        --enable-openmp \
+        FLTK_CONFIG="$(PREFIX)/bin/$(TARGET)-fltk-config" \
+        gl_cv_func_gettimeofday_clobber=no
 
-    # make
-    $(MAKE) -C '$(1)'	
 
-    # install 
-    $(MAKE) -C '$(1)' install	
+    ## We want both of these install steps so that we install in the
+    ## location set by the configure --prefix option, and the other
+    ## in a directory tree that will have just Octave files.
+    $(MAKE) -C '$(1)/.build' -j '$(JOBS)' install
+    $(MAKE) -C '$(1)/.build' -j '$(JOBS)' DESTDIR=$(PREFIX)/../octave install
 
 endef
 
