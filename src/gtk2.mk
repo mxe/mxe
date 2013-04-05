1a5a7742404f8efbb005d9052ca98bb1b848cdbf
Tony Theodore <tonyt@logyst.com>
2013-04-05 18:29:02 +1100

    Revert "package gtk2: disable mingw-w64 builds"
    This reverts commit f4535d20215fa96cee1b857414b7c1769cd3f3a4.

--- src/gtk2.mk
+++ src/gtk2.mk
@@ -40,6 +40,3 @@ define $(PKG)_BUILD
         '$(2).c' -o '$(PREFIX)/$(TARGET)/bin/test-gtk2.exe' \
         `'$(TARGET)-pkg-config' gtk+-2.0 --cflags --libs`
 endef
-
-$(PKG)_BUILD_i686-w64-mingw32 =
-$(PKG)_BUILD_x86_64-w64-mingw32 =
