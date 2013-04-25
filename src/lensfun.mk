0ee360f02d8463f9c19872eb01113da3d03c6099
Tony Theodore <tonyt@logyst.com>
2013-04-25 21:49:09 +1000

    package lensfun: disable mingw-w64 builds

--- src/lensfun.mk
+++ src/lensfun.mk
@@ -43,3 +43,7 @@ define $(PKG)_BUILD
         '$(2).c' -o '$(PREFIX)/$(TARGET)/bin/test-lensfun.exe' \
         `'$(TARGET)-pkg-config' lensfun --cflags --libs`
 endef
+
+#depends on libgnurx
+$(PKG)_BUILD_x86_64-w64-mingw32 =
+$(PKG)_BUILD_i686-w64-mingw32 =
