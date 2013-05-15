83de0295ff97f4f90052acc233d75d401787f73a
Mark Brand <mabrand@mabrand.nl>
2013-05-15 06:23:34 +0200

    package gsoap: update checksum

--- src/gsoap.mk
+++ src/gsoap.mk
@@ -3,7 +3,7 @@
 
 PKG             := gsoap
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 42abbf0ff0ba813f6ca46231a521095370fa9181
+$(PKG)_CHECKSUM := 3238ea70fcd3356bd2e7cd3d1e2ef91c1040b81a
 $(PKG)_SUBDIR   := gsoap-$(call SHORT_PKG_VERSION,$(PKG))
 $(PKG)_FILE     := gsoap_$($(PKG)_VERSION).zip
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/gsoap2/gSOAP/$($(PKG)_FILE)
