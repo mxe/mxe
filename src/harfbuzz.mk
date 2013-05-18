c3330ee8ee100b8be55b98e564564ece2129278c
Tony Theodore <tonyt@logyst.com>
2013-05-19 00:25:40 +1000

    package harfbuzz: add enable icu support

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -7,7 +7,7 @@ $(PKG)_CHECKSUM := 2f33c388a0be3d07fda58201890d8a9f54a9e7ee
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.freedesktop.org/software/$(PKG)/release/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc glib cairo freetype
+$(PKG)_DEPS     := gcc glib cairo freetype icu4c
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://cgit.freedesktop.org/harfbuzz/refs/tags' | \
@@ -22,7 +22,11 @@ define $(PKG)_BUILD
         --build="`config.guess`" \
         --prefix='$(PREFIX)/$(TARGET)' \
         --disable-shared \
-        --enable-static \
-        ICU_CONFIG=/bin/false
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+        --enable-static
+    $(MAKE) -C '$(1)/src'      -j '$(JOBS)'
+    # some of these tests want to be linked with g++
+    # but there's no easy way to bypass the am__v_CCLD... logic
+    $(MAKE) -C '$(1)/test/api' -j '$(JOBS)' test-blob.exe test-buffer.exe test-common.exe test-ot-tag.exe test-set.exe test-version.exe
+    $(MAKE) -C '$(1)/test/api' -j '$(JOBS)' || $(MAKE) -C '$(1)/test/api' -j '$(JOBS)' CC=$(TARGET)-g++
+    $(MAKE) -C '$(1)' install
 endef
