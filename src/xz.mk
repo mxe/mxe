e3b57405d20d6eda15f8da82947b06f990c359ce
Mark Brand <mabrand@mabrand.nl>
2013-07-02 10:34:51 +0200

    update package xz

--- src/xz.mk
+++ src/xz.mk
@@ -3,7 +3,7 @@
 
 PKG             := xz
 $(PKG)_IGNORE   :=
-$(PKG)_CHECKSUM := 3e976d7715fde43422572c70f927bfdae56a94c3
+$(PKG)_CHECKSUM := 26fec2c1e409f736e77a85e4ab314dc74987def0
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://tukaani.org/xz/$($(PKG)_FILE)
