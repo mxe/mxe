7f5a1115b78dca22f3b2bf904debf7a3654bd68c
Tony Theodore <tonyt@logyst.com>
2013-07-13 20:22:45 +1000

    package x264: build native version of yasm assembler

--- src/x264.mk
+++ src/x264.mk
@@ -7,13 +7,23 @@ $(PKG)_CHECKSUM := 768008db411c03afbd74ea808da5a1f57a77fed4
 $(PKG)_SUBDIR   := $(PKG)-snapshot-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-snapshot-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://download.videolan.org/pub/videolan/$(PKG)/snapshots/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc
+$(PKG)_DEPS     := gcc yasm
 
 define $(PKG)_UPDATE
     $(DATE) -d yesterday +%Y%m%d-2245
 endef
 
 define $(PKG)_BUILD
+    # native build of yasm
+    mkdir '$(1).native'
+    cd '$(1).native' && $(call UNPACK_PKG_ARCHIVE,yasm)
+    cd '$(1).native/$(yasm_SUBDIR)' && './configure' \
+        --disable-nls \
+        --disable-python
+    $(MAKE) -C '$(1).native/$(yasm_SUBDIR)' -j '$(JOBS)' yasm
+
+    # cross build with newly compiled yasm
+    $(SED) -i 's,yasm,$(1).native/$(yasm_SUBDIR)/yasm,g' '$(1)/configure'
     cd '$(1)' && ./configure \
         --cross-prefix='$(TARGET)'- \
         --host='$(TARGET)' \
