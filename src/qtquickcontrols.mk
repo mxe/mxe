6d7fc718cff282e5a49507d2edf3e8c655d4fcbc
Tony Theodore <tonyt@logyst.com>
2013-07-15 02:25:35 +1000

    package qtquickcontrols: add qtbase dependency

--- src/qtquickcontrols.mk
+++ src/qtquickcontrols.mk
@@ -7,7 +7,7 @@ $(PKG)_CHECKSUM := e7d178b8927dc0356023ae1fca3909dd509757f0
 $(PKG)_SUBDIR   := $(PKG)-opensource-src-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-opensource-src-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://download.qt-project.org/official_releases/qt/5.1/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc qtdeclarative
+$(PKG)_DEPS     := gcc qtbase qtdeclarative
 
 define $(PKG)_UPDATE
     echo $(qtbase_VERSION)
