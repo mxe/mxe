4b0a0f1441b7997a290728ea25af161bb36771d2
Martin Gerhardy <martin.gerhardy@gmail.com>
2013-07-27 10:49:42 +0200

    new package sdl2_net (release candidate)

---
+++ src/sdl2_net-test.c
@@ -0,0 +1,19 @@
+/*
+ * This file is part of MXE.
+ * See index.html for further information.
+ */
+
+#include <SDL_net.h>
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) return 1;
+    if (SDLNet_Init() < 0) return 1;
+
+    SDLNet_Quit();
+    SDL_Quit();
+    return 0;
+}
---
+++ src/sdl2_net.mk
@@ -0,0 +1,36 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := sdl2_net
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.0.0
+$(PKG)_CHECKSUM := 96ba08cb2ab5bf25d7a427f208297137aa531306
+$(PKG)_SUBDIR   := SDL2_net-$($(PKG)_VERSION)
+$(PKG)_FILE     := SDL2_net-$($(PKG)_VERSION).tar.gz
+#$(PKG)_URL      := http://www.libsdl.org/projects/SDL_net/release/$($(PKG)_FILE)
+$(PKG)_URL      := http://www.libsdl.org/tmp/SDL_net/release/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc sdl
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://www.libsdl.org/projects/SDL_net/release/?C=M;O=D' | \
+    $(SED) -n 's,.*SDL_net-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --with-sdl-prefix='$(PREFIX)/$(TARGET)' \
+        --disable-sdltest \
+        --disable-gui
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+
+    '$(TARGET)-gcc' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(2).c' -o '$(PREFIX)/$(TARGET)/bin/test-sdl2_net.exe' \
+        `'$(TARGET)-pkg-config' SDL2_net --cflags --libs`
+endef
+
+$(PKG)_BUILD_i686-pc-mingw32 =
