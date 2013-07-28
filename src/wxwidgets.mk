a5663fa1c7e1837af88c02c95c9f829d60042d9c
Tony Theodore <tonyt@logyst.com>
2013-07-28 22:48:08 +1000

    package wxwidgets: enable mingw-w64 builds

--- src/wxwidgets.mk
+++ src/wxwidgets.mk
@@ -105,5 +105,11 @@ define $(PKG)_BUILD
     $($(PKG)_TEST)
 endef
 
-$(PKG)_BUILD_i686-w64-mingw32 =
-$(PKG)_BUILD_x86_64-w64-mingw32 =
+define $(PKG)_BUILD_UNICODE_ONLY
+    $($(PKG)_PRE_CONFIGURE)
+    $($(PKG)_BUILD_UNICODE)
+    $($(PKG)_TEST)
+endef
+
+$(PKG)_BUILD_i686-w64-mingw32   = $($(PKG)_BUILD_UNICODE_ONLY)
+$(PKG)_BUILD_x86_64-w64-mingw32 = $($(PKG)_BUILD_UNICODE_ONLY)
