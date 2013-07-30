0758529a653b7a748151c61c68bc399e19b6cb33
Alois Schloegl <alois.schloegl@ist.ac.at>
2013-07-30 07:13:41 +0200

    add initial packages: vlc phonon phonon-backend-vlc minitube

---
+++ src/automoc4.mk
@@ -0,0 +1,32 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+# Qwt - Qt widgets for technical applications
+PKG             := automoc4
+$(PKG)_VERSION  := 0.9.88
+$(PKG)_CHECKSUM := d864c3dda99d8b5f625b9267acfa1d88ff617e3a
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_WEBSITE  := http://www.kde.org/
+$(PKG)_URL      := http://mirrors.isc.org/pub/kde/stable/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc qt
+
+define $(PKG)_UPDATE
+#    $(WGET) -q -O- 'http://sourceforge.net/projects/qwt/files/qwt/' | \
+#    $(SED) -n 's,.*/\([0-9][^"]*\)/".*,\1,p' | \
+#    head -1
+     echo 1
+endef
+
+define $(PKG)_BUILD
+	mkdir '$(1)'/build
+
+	cd '$(1)'/build && cmake \
+		-DCMAKE_INSTALL_PREFIX=$(PREFIX)/$(TARGET)/qt/ \
+        	-DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+		..
+
+	$(MAKE) -C '$(1)/build' -j $(JOBS) VERBOSE=1
+#	$(MAKE) -C '$(1)/build' -j $(JOBS) install
+
+endef
---
+++ src/vlc.mk
@@ -0,0 +1,29 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+# Qwt - Qt widgets for technical applications
+PKG             := vlc
+$(PKG)_VERSION  := 2.0.6
+$(PKG)_CHECKSUM := 6d33a52367f7a82498ba27812bec4e15de005534
+$(PKG)_SUBDIR   := $(PKG)
+$(PKG)_FILE     := $(PKG).tar.gz
+$(PKG)_WEBSITE  := http://videolan.org
+$(PKG)_URL      := ftp://ftp.videolan.org/pub/videolan/vlc/2.0.6/vlc-2.0.6.tar.xz
+$(PKG)_DEPS     := gcc qt dbus lua libgcrypt gnutls ffmpeg
+
+define $(PKG)_UPDATE
+#    $(WGET) -q -O- 'http://sourceforge.net/projects/qwt/files/qwt/' | \
+#    $(SED) -n 's,.*/\([0-9][^"]*\)/".*,\1,p' | \
+#    head -1
+     echo 1
+endef
+
+define $(PKG)_BUILD
+
+    cd '$(1)' && $(PREFIX)/bin/$(TARGET)-qmake
+
+    $(MAKE) -C '$(1)'  -j '$(JOBS)'
+
+    $(MAKE) -C '$(1)'  -j '$(JOBS)' install
+
+endef
