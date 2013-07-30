365adf39535bd9ad020e731d6f759c6c0c236de5
Tony Theodore <tonyt@logyst.com>
2013-07-30 14:54:28 +1000

    package mpc: add cross build for target

---
+++ src/mpc.mk
@@ -0,0 +1,30 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := mpc
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.0.1
+$(PKG)_CHECKSUM := 8c7e19ad0dd9b3b5cc652273403423d6cf0c5edf
+$(PKG)_SUBDIR   := mpc-$($(PKG)_VERSION)
+$(PKG)_FILE     := mpc-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://www.multiprecision.org/mpc/download/$($(PKG)_FILE)
+$(PKG)_URL_2    := http://ftp.debian.org/debian/pool/main/m/mpclib/mpclib_$($(PKG)_VERSION).orig.tar.gz
+$(PKG)_DEPS     := gcc gmp mpfr
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'https://gforge.inria.fr/scm/viewvc.php/tags/?root=mpc&sortby=date' | \
+    $(SED) -n 's,.*<a name="\([0-9][^"]*\)".*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --enable-static \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --with-gmp='$(PREFIX)/$(TARGET)/' \
+        --with-mpfr='$(PREFIX)/$(TARGET)/'
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+endef
