fa1f7d80827e3915fe45749b404e589f7c3d5fb0
Tony Theodore <tonyt@logyst.com>
2013-08-01 00:31:58 +1000

    package ffmpeg: enable libxvid for x86_64-w64-mingw32 build

--- src/ffmpeg.mk
+++ src/ffmpeg.mk
@@ -50,5 +50,3 @@ define $(PKG)_BUILD
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     $(MAKE) -C '$(1)' -j 1 install
 endef
-
-$(PKG)_BUILD_x86_64-w64-mingw32 = $(subst enable-libxvid,disable-libxvid,$($(PKG)_BUILD))
