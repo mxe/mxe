8ec99c2923a5bd2aaae5d9e7becdf89b73c52b16
Alois Schlögl <alois.schloegl@ist.ac.at>
2013-08-06 11:53:14 +0200

    pkg pstoedit: prepare for use with libemf

--- src/pstoedit.mk
+++ src/pstoedit.mk
@@ -9,6 +9,7 @@ $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://sourceforge.net/projects/pstoedit/files/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)/download
 $(PKG)_DEPS     := gcc 
+### libemf libming ###
 
 define $(PKG)_UPDATE
     echo 'Warning: Updates are temporarily disabled for package pstoedit.' >&2;
@@ -16,13 +17,26 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
+    #cd '$(1)' && tar xvf '$(1)'/../../pkg/libemf-1.0.7.src.tar.gz
+
     cd '$(1)' && autoreconf
     mkdir '$(1)/.build'
-    cd '$(1)/.build' && '$(1)/configure' \
+
+    cd '$(1)/.build' && '../configure' \
         --host='$(TARGET)' \
         --build="`config.guess`" \
         --prefix='$(PREFIX)/$(TARGET)' \
-        --enable-static 
+        --enable-static --disable-shared \
+         --without-emf
+
+	#--without-libplot --without-swf --without-magick
+
+        #--with-libemf-src=DIR \
+        #--with-libemf-src=DIR
+
 
     $(MAKE) -C '$(1)/.build' -j '$(JOBS)' install
 endef
+
+$(PKG)_BUILD_x86_64-w64-mingw32 =
+
