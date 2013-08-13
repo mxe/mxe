c927b1424b74e7eac06d2bd7b16985b9af8f9772
Tony Theodore <tonyt@logyst.com>
2013-08-13 22:45:35 +1000

    packages mingw-utils pthreads-w32 winpthreads: use target specific deps

--- src/mingw-utils.mk
+++ src/mingw-utils.mk
@@ -8,7 +8,9 @@ $(PKG)_CHECKSUM := 716f51d7622b36448fc1e92d2c69d8f41b1cc2df
 $(PKG)_SUBDIR   := $(PKG)-$(word 1,$(subst -, ,$($(PKG)_VERSION)))
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION)-mingw32-src.tar.lzma
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/mingw/MinGW/Extension/$(PKG)/$(PKG)-$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc
+$(PKG)_DEPS     :=
+
+$(PKG)_DEPS_i686-pc-mingw32 := gcc
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://sourceforge.net/projects/mingw/files/MinGW/Extension/mingw-utils/' | \
