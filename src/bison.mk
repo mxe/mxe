d7f48a7140a4faf9079e6934564f9961a1d3f600
Tony Theodore <tonyt@logyst.com>
2013-08-25 20:40:34 +1000

    packages bison flex: enable native builds

--- src/bison.mk
+++ src/bison.mk
@@ -17,7 +17,7 @@ define $(PKG)_UPDATE
     tail -1
 endef
 
-define $(PKG)_BUILD_NONE
+define $(PKG)_BUILD_$(BUILD)
     mkdir '$(1).build'
     cd    '$(1).build' && '$(1)/configure' \
         --prefix='$(PREFIX)/$(TARGET)'
