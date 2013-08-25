6c15e6e00c482f321e80fa42a6fd75c3b38405c7
Tony Theodore <tonyt@logyst.com>
2013-08-25 19:09:00 +1000

    package libtool: separate binary from library (new package libltdl)

---
+++ src/libltdl.mk
@@ -0,0 +1,25 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := libltdl
+$(PKG)_IGNORE    = $(libtool_IGNORE)
+$(PKG)_VERSION   = $(libtool_VERSION)
+$(PKG)_CHECKSUM  = $(libtool_CHECKSUM)
+$(PKG)_SUBDIR    = $(libtool_SUBDIR)
+$(PKG)_FILE      = $(libtool_FILE)
+$(PKG)_URL       = $(libtool_URL)
+$(PKG)_DEPS     := gcc
+
+define $(PKG)_UPDATE
+    echo $(libtool_VERSION)
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)/libltdl' && ./configure \
+        --host='$(TARGET)' \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --disable-shared \
+        --enable-ltdl-install
+    $(MAKE) -C '$(1)/libltdl' -j '$(JOBS)'
+    $(MAKE) -C '$(1)/libltdl' -j 1 install
+endef
--- src/libtool.mk
+++ src/libtool.mk
@@ -8,9 +8,7 @@ $(PKG)_CHECKSUM := 22b71a8b5ce3ad86e1094e7285981cae10e6ff88
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://ftp.gnu.org/gnu/$(PKG)/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc
-
-$(PKG)_DEPS_NATIVE := autoconf automake
+$(PKG)_DEPS     := autoconf automake
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://ftp.gnu.org/gnu/libtool/?C=M;O=D' | \
@@ -18,20 +16,10 @@ define $(PKG)_UPDATE
     head -1
 endef
 
-define $(PKG)_BUILD_NATIVE
+define $(PKG)_BUILD_$(BUILD)
     mkdir '$(1).build'
     cd    '$(1).build' && '$(1)/configure' \
-        --prefix='$(PREFIX)'
+        --prefix='$(PREFIX)/$(TARGET)'
     $(MAKE) -C '$(1).build' -j '$(JOBS)'
     $(MAKE) -C '$(1).build' -j 1 install
-endef
-
-define $(PKG)_BUILD
-    cd '$(1)/libltdl' && ./configure \
-        --host='$(TARGET)' \
-        --prefix='$(PREFIX)/$(TARGET)' \
-        --disable-shared \
-        --enable-ltdl-install
-    $(MAKE) -C '$(1)/libltdl' -j '$(JOBS)'
-    $(MAKE) -C '$(1)/libltdl' -j 1 install
 endef
