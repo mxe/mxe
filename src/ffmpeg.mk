f5626edd3546c50b08b94803103aca76c4f550ba
Timothy Gu <timothygu99@gmail.com>
2013-09-01 20:03:28 -0700

    Add vo-aacenc package and use it for ffmpeg

--- src/ffmpeg.mk
+++ src/ffmpeg.mk
@@ -9,7 +9,7 @@ $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.ffmpeg.org/releases/$($(PKG)_FILE)
 $(PKG)_URL_2    := http://launchpad.net/ffmpeg/main/$($(PKG)_VERSION)/+download/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc bzip2 lame libass libvpx opencore-amr opus sdl speex theora vorbis x264 xvidcore zlib
+$(PKG)_DEPS     := gcc bzip2 lame libass libvpx opencore-amr opus sdl speex theora vo-aacenc vorbis x264 xvidcore zlib
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://www.ffmpeg.org/download.html' | \
@@ -43,6 +43,7 @@ define $(PKG)_BUILD
         --enable-libopus \
         --enable-libspeex \
         --enable-libtheora \
+        --enable-libvo-aacenc \
         --enable-libvorbis \
         --enable-libvpx \
         --enable-libx264 \
---
+++ src/vo-aacenc.mk
@@ -0,0 +1,26 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := vo-aacenc
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 0.1.3
+$(PKG)_CHECKSUM := 6f8eea8e13f369e3568d9cb33d78e0ac28b7eaee
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/opencore-amr/$(PKG)/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://sourceforge.net/projects/opencore-amr/files/$(PKG)/' | \
+    $(SED) -n 's,.*vo-aacenc-\([0-9][^"]*\)/".*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --disable-shared
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+endef
