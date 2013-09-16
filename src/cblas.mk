a51cf3f2a25835506230b4a8f071005c32b1766c
Tony Theodore <tonyt@logyst.com>
2013-09-16 14:36:04 +1000

    package cblas: tidy up build, add missing blas dependency and build test programs

--- src/cblas.mk
+++ src/cblas.mk
@@ -9,26 +9,36 @@ $(PKG)_SUBDIR   := CBLAS
 $(PKG)_FILE     := $(PKG).tgz
 $(PKG)_URL      := http://www.netlib.org/blas/blast-forum/$($(PKG)_FILE)
 $(PKG)_URL_2    := ftp://ftp.eq.uc.pt/pub/software/math/netlib/blas/blast-forum/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc
+$(PKG)_DEPS     := gcc blas
 
 define $(PKG)_UPDATE
     echo 1
 endef
 
 define $(PKG)_BUILD
-    cp '$(1)/Makefile.LINUX' '$(1)/Makefile.MINGW32'
-    $(SED) -i 's,CBDIR =.*,CBDIR = $(1),g'         '$(1)/Makefile.MINGW32'
-    $(SED) -i 's,FC =.*,FC = $(TARGET)-gfortran,g' '$(1)/Makefile.MINGW32'
-    $(SED) -i 's, make , $(MAKE) ,g'               '$(1)/Makefile'
-    rm '$(1)/Makefile.in'
-    ln -sf '$(1)/Makefile.MINGW32' '$(1)/Makefile.in'
-    mkdir '$(1)/MINGW32'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' alllib
-    cd '$(1)' && $(TARGET)-ar cr libcblas.a src/*.o
+    $(SED) -i 's, make , $(MAKE) ,g' '$(1)/Makefile'
+    $(MAKE) -C '$(1)' -j '$(JOBS)' \
+        CBDIR='$(1)' \
+        CBLIB='$(1)/lib/libcblas.a' \
+        CC='$(TARGET)-gcc' \
+        FC='$(TARGET)-gfortran' \
+        ARCH='$(TARGET)-ar' \
+        RANLIB='$(TARGET)-ranlib' \
+        alllib
 
-    $(INSTALL) -d                           '$(PREFIX)/$(TARGET)/lib'
-    $(INSTALL) -m644 '$(1)/libcblas.a'      '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -d                           '$(PREFIX)/$(TARGET)/include'
+    $(INSTALL) -d                               '$(PREFIX)/$(TARGET)/lib'
+    $(INSTALL) -m644 '$(1)/lib/libcblas.a'      '$(PREFIX)/$(TARGET)/lib/'
+    $(INSTALL) -d                               '$(PREFIX)/$(TARGET)/include'
     $(INSTALL) -m644 '$(1)/include/cblas.h'     '$(PREFIX)/$(TARGET)/include/'
     $(INSTALL) -m644 '$(1)/include/cblas_f77.h' '$(PREFIX)/$(TARGET)/include/'
+
+    '$(TARGET)-gcc' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(1)/examples/cblas_example1.c' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
+        -lcblas -lblas -lgfortran
+
+    '$(TARGET)-gcc' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(1)/examples/cblas_example2.c' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG)-F77.exe' \
+        -lcblas -lblas -lgfortran -DADD_
 endef
