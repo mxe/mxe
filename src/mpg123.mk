f9c5501f9685bc29f4949aea67f6125ea9ca5c2d
Timothy Gu <timothygu99@gmail.com>
2013-09-22 16:32:19 -0700

    Add package mpg123
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

---
+++ src/mpg123.mk
@@ -0,0 +1,29 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := mpg123
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.15.4
+$(PKG)_CHECKSUM := f39d927bcf7abf4f9d857f10cd97c8ceccaffbfa
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/mpg123/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc sdl
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://sourceforge.net/projects/mpg123/files/mpg123/' | \
+    $(SED) -n 's,.*/\([0-9][^"]*\)/".*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --host='$(TARGET)' \
+        --disable-shared \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --with-default-audio=win32 \
+        --with-audio=win32,sdl,dummy \
+        --enable-modules=no
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+endef
