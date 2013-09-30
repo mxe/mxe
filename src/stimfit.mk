536391e457254f778968dff3726a7c7ffa07ffa3
Alois Schloegl <alois.schloegl@ist.ac.at>
2013-09-30 22:00:37 +0200

    pkg stimfit: workaround to checksum test - because downloads from googlecode/archive do not provide the same checksum over time

--- src/stimfit.mk
+++ src/stimfit.mk
@@ -7,13 +7,19 @@ $(PKG)_VERSION  := 0.13.2linux
 $(PKG)_CHECKSUM := 21a4e45a68a2d296c6ca8ee22fc53a92c0ee1531
 $(PKG)_SUBDIR   := stimfit-efab850f4f2d
 $(PKG)_FILE     := $($(PKG)_VERSION).zip
-$(PKG)_URL      := http://stimfit.googlecode.com/archive/$($(PKG)_FILE)
+$(PKG)_URL      := https://stimfit.googlecode.com/archive/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc biosig wxwidgets hdf5 boost fftw
 
 define $(PKG)_UPDATE
     wget -q -O- 'http://code.google.com/p/stimfit/downloads/list' | \
     $(SED) -n 's_.*name=stimfit-\([0-9\.]*\)\.tar\.gz.*_\1_ip' | \
     head -1
+endef
+
+## hack: because download from googlecode/archive/*.zip has varying checksums 
+##    so we use the file size as a poor man's check
+define CHECK_PKG_ARCHIVE
+     [[ `du -b pkg/$($(PKG)_FILE) | cut -f 1` = 14401795 ]]
 endef
 
 define $(PKG)_BUILD
