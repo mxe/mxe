29008ee2bd913a2555e017bc1d858727b0d116c7
Tony Theodore <tonyt@logyst.com>
2013-10-20 17:02:10 +1100

    update package gd

--- src/gd.mk
+++ src/gd.mk
@@ -3,44 +3,32 @@
 
 PKG             := gd
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.0.35
-$(PKG)_CHECKSUM := ccf34a610abff2dbf133a20c4d2a4aa94939018a
-$(PKG)_SUBDIR   := gd-$($(PKG)_VERSION)
-$(PKG)_FILE     := gd-$($(PKG)_VERSION).tar.bz2
-$(PKG)_URL      := http://www.libgd.org/releases/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc freetype libpng jpeg libxml2 pthreads
+$(PKG)_VERSION  := 2.1.0
+$(PKG)_CHECKSUM := 66c56fc07246b66ba649c83e996fd2085ea2f9e2
+$(PKG)_SUBDIR   := libgd-$($(PKG)_VERSION)
+$(PKG)_FILE     := libgd-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://bitbucket.org/libgd/gd-libgd/downloads/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc fontconfig freetype jpeg libpng libvpx pthreads tiff
 
 define $(PKG)_UPDATE
-    echo 'Warning: Updates are temporarily disabled for package gd.' >&2;
-    echo $(gd_VERSION)
-endef
-define $(PKG)_UPDATE_orig
-    $(WGET) -q -O- 'http://www.libgd.org/releases/' | \
-    $(SED) -n 's,.*gd-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    $(WGET) -q -O- 'https://bitbucket.org/libgd/gd-libgd/downloads/' | \
+    $(SED) -n 's,.*libgd-\([0-9.]*\)\.tar.*,\1,p' | \
     head -1
 endef
 
 define $(PKG)_BUILD
-    touch '$(1)/aclocal.m4'
-    touch '$(1)/config.hin'
-    touch '$(1)/Makefile.in'
-    $(SED) -i 's,-I@includedir@,-I@includedir@ -DNONDLL,' '$(1)/config/gdlib-config.in'
-    $(SED) -i 's,-lX11 ,,g'     '$(1)/configure'
-    $(SED) -i 's,png12,png16,g' '$(1)/configure'
+    $(SED) -i 's,-I@includedir@,-I@includedir@ -DNONDLL -DBGDWIN32,' '$(1)/config/gdlib-config.in'
     cd '$(1)' && ./configure \
         --host='$(TARGET)' \
         --disable-shared \
         --prefix='$(PREFIX)/$(TARGET)' \
         --with-freetype='$(PREFIX)/$(TARGET)' \
         --without-x \
-        LIBPNG_CONFIG='$(PREFIX)/$(TARGET)/bin/libpng-config' \
-        CFLAGS='-DNONDLL -DXMD_H -L$(PREFIX)/$(TARGET)/lib' \
-        LIBS="`$(PREFIX)/$(TARGET)/bin/xml2-config --libs`"
+        CFLAGS='-DNONDLL'
     $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 
     '$(TARGET)-gcc' \
         -W -Wall -Werror -ansi -pedantic \
         '$(2).c' -o '$(PREFIX)/$(TARGET)/bin/test-gd.exe' \
-        `'$(PREFIX)/$(TARGET)/bin/gdlib-config' --cflags` \
-        -lgd `'$(PREFIX)/$(TARGET)/bin/gdlib-config' --libs`
+        `'$(PREFIX)/$(TARGET)/bin/gdlib-config' --cflags --libs`
 endef
