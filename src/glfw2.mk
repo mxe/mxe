dd60b2f8178502417471ba931b7dc030a38731ae
Daniele Di Proietto <daniele.di.proietto@gmail.com>
2013-11-02 13:08:26 +0100

    Added glfw (version 2) package

---
+++ src/glfw2.mk
@@ -0,0 +1,26 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := glfw2
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.7.9
+$(PKG)_CHECKSUM := b189922e9804062a0014a3799b4dc35431034623
+$(PKG)_SUBDIR   := glfw-$($(PKG)_VERSION)
+$(PKG)_FILE     := glfw-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/glfw/glfw/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc
+
+define $(PKG)_UPDATE
+    echo 'TODO: write update script for $(PKG).' >&2;
+    echo $($(PKG)_VERSION)
+endef
+
+define $(PKG)_BUILD
+    $(MAKE) -C '$(1)' -j '$(JOBS)' cross-mgw-install TARGET=$(TARGET)- PREFIX='$(PREFIX)/$(TARGET)'
+    
+    #Test
+    '$(TARGET)-gcc' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(2).c' -o '$(PREFIX)/$(TARGET)/bin/test-glfw.exe' \
+        `'$(TARGET)-pkg-config' libglfw --cflags --libs`
+endef
