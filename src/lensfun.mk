190d2368b8ee53c1d839d53efe06488f10434223
Niels Kristian Bech Jensen <nkbj1970@hotmail.com>
2013-11-09 14:18:10 +1100

    update package lensfun

--- src/lensfun.mk
+++ src/lensfun.mk
@@ -3,11 +3,11 @@
 
 PKG             := lensfun
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.2.7
-$(PKG)_CHECKSUM := f26121093dfee85d6371c2c79dae22e6d1b8d0d6
+$(PKG)_VERSION  := 0.2.8
+$(PKG)_CHECKSUM := 0e85eb7692620668d27e2303687492ad68c90eb4
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/lensfun.berlios/$($(PKG)_FILE)
+$(PKG)_URL      := http://download.berlios.de/lensfun/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc libpng glib libgnurx
 
 define $(PKG)_UPDATE
