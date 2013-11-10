b986cc25af281ac4864346ef7826c558a78cc230
Tony Theodore <tonyt@logyst.com>
2013-11-10 20:01:17 +1100

    package ocaml-lablgtk2: revert previous update

--- src/ocaml-lablgtk2.mk
+++ src/ocaml-lablgtk2.mk
@@ -3,11 +3,8 @@
 
 PKG             := ocaml-lablgtk2
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.18.0
+$(PKG)_VERSION  := 2.16.0
 $(PKG)_CHECKSUM := 3dec411a410fbb38d6e2e5a43a4ebfb2e407e7e6
-# $(PKG)_VERSION 2.18.0 is shipped with $(PKG)_SUBDIR lablgtk-2.16.0
-#$(PKG)_SUBDIR   := lablgtk-$($(PKG)_VERSION)
-$(PKG)_SUBDIR   := lablgtk-2.16.0
 $(PKG)_FILE     := lablgtk-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://forge.ocamlcore.org/frs/download.php/979/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc ocaml-findlib libglade gtkglarea ocaml-lablgl gtk2
