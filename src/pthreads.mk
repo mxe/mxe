0660b6af8416e509565377fc6f16a21ccfade4c1
Tony Theodore <tonyt@logyst.com>
2013-11-12 00:54:33 +1100

    package pthreads: add pkg-config file and use for test

--- src/pthreads.mk
+++ src/pthreads.mk
@@ -15,7 +15,13 @@ define $(PKG)_UPDATE
 endef
 
 PTHREADS_TEST = \
+    $(INSTALL) -d '$(PREFIX)/$(TARGET)/lib/pkgconfig' && \
+    (echo 'Name: pthreads'; \
+     echo 'Version: $($(PKG)_VERSION)'; \
+     echo 'Description: Posix Threads ($(PKG))'; \
+     echo 'Libs: -lpthread -lws2_32';) \
+     > '$(PREFIX)/$(TARGET)/lib/pkgconfig/pthreads.pc' && \
     '$(TARGET)-gcc' \
         -W -Wall -Werror -ansi -pedantic \
-        '$(TOP_DIR)/src/pthreads-test.c' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
-        -lpthread -lws2_32
+        '$(TOP_DIR)/src/pthreads-test.c' -o '$(PREFIX)/$(TARGET)/bin/test-pthreads.exe' \
+        `'$(TARGET)-pkg-config' --libs pthreads`
