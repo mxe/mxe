bd8c94f0d576a0ca261eaf0cd6b4ceb5b7eae619
Mark Brand <mabrand@mabrand.nl>
2013-11-17 15:08:07 +0100

    update packge wavpack

--- src/wavpack.mk
+++ src/wavpack.mk
@@ -3,8 +3,8 @@
 
 PKG             := wavpack
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.60.1
-$(PKG)_CHECKSUM := 003c65cb4e29c55011cf8e7b10d69120df5e7f30
+$(PKG)_VERSION  := 4.70.0
+$(PKG)_CHECKSUM := 7bf2022c988c19067196ee1fdadc919baacf46d1
 $(PKG)_SUBDIR   := wavpack-$($(PKG)_VERSION)
 $(PKG)_FILE     := wavpack-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.wavpack.com/$($(PKG)_FILE)
