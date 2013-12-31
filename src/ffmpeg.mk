f5b1de8d586f033181c18d69abb7ad4fc19cf76a
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2013-12-31 16:01:47 +0200

    package ffmpeg: Updated configure to support shared in future

--- src/ffmpeg.mk
+++ src/ffmpeg.mk
@@ -11,6 +11,35 @@ $(PKG)_URL      := http://www.ffmpeg.org/releases/$($(PKG)_FILE)
 $(PKG)_URL_2    := http://launchpad.net/ffmpeg/main/$($(PKG)_VERSION)/+download/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc bzip2 lame libass libnut libvpx opencore-amr opus sdl speex theora vo-aacenc vo-amrwbenc vorbis x264 xvidcore zlib
 
+$(PKG)_CONFS    := --enable-cross-compile \
+                   --target-os=mingw32 \
+                   --disable-debug \
+                   --enable-memalign-hack \
+                   --disable-pthreads \
+                   --enable-w32threads \
+                   --disable-doc \
+                   --enable-gpl \
+                   --enable-version3 \
+                   --disable-nonfree \
+                   --enable-avisynth \
+                   --enable-libass \
+                   --disable-libfaac \
+                   --enable-libmp3lame \
+                   --enable-libnut \
+                   --enable-libopencore-amrnb \
+                   --enable-libopencore-amrwb \
+                   --enable-libopus \
+                   --enable-libspeex \
+                   --enable-libtheora \
+                   --enable-libvo-aacenc \
+                   --enable-libvo-amrwbenc \
+                   --enable-libvorbis \
+                   --enable-libvpx \
+                   --enable-libx264 \
+                   --enable-libxvid \
+                   --enable-postproc
+
+
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://www.ffmpeg.org/download.html' | \
     $(SED) -n 's,.*ffmpeg-\([0-9][^>]*\)\.tar.*,\1,p' | \
@@ -23,36 +52,10 @@ define $(PKG)_BUILD
     '$(SED)' -i "s^[-]lvpx^`'$(TARGET)'-pkg-config --libs-only-l vpx`^g;" $(1)/configure
     cd '$(1)' && ./configure \
         --cross-prefix='$(TARGET)'- \
-        --enable-cross-compile \
         --arch=$(patsubst -%,,$(TARGET)) \
-        --target-os=mingw32 \
         --prefix='$(PREFIX)/$(TARGET)' \
         --disable-shared \
-        --disable-debug \
-        --enable-memalign-hack \
-        --disable-pthreads \
-        --enable-w32threads \
-        --disable-doc \
-        --enable-gpl \
-        --enable-version3 \
-        --disable-nonfree \
-        --enable-avisynth \
-        --enable-libass \
-        --disable-libfaac \
-        --enable-libmp3lame \
-        --enable-libnut \
-        --enable-libopencore-amrnb \
-        --enable-libopencore-amrwb \
-        --enable-libopus \
-        --enable-libspeex \
-        --enable-libtheora \
-        --enable-libvo-aacenc \
-        --enable-libvo-amrwbenc \
-        --enable-libvorbis \
-        --enable-libvpx \
-        --enable-libx264 \
-        --enable-libxvid \
-        --enable-postproc
+        $($(PKG)_CONFS) 
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     $(MAKE) -C '$(1)' -j 1 install
 endef
