c19c99a8b1cbb80429f5eaa30b007ed2998ee94e
Alois Schlögl <alois.schloegl@ist.ac.at>
2014-01-25 22:57:02 +0100

    pkg stimfit: upgrade to v0.13.10windows

--- src/stimfit.mk
+++ src/stimfit.mk
@@ -3,23 +3,22 @@
 
 PKG             := stimfit
 $(PKG)_IGNORE   := 
-$(PKG)_VERSION  := 0.13.8
-$(PKG)_CHECKSUM := bb24c14ae820cd96265739c4b155312ed2b1adeb
+$(PKG)_VERSION  := 0.13.10windows
+$(PKG)_CHECKSUM := caf318ea945ee55daf79ba49ecdb39caf2e52c5c
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := http://stimfit.googlecode.com/files/$($(PKG)_FILE)
+$(PKG)_URL      := https://github.com/neurodroid/$(PKG)/archive/$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := gcc biosig wxwidgets hdf5 boost fftw
 
 define $(PKG)_UPDATE
-    wget -q -O- 'http://code.google.com/p/stimfit/downloads/list' | \
-    $(SED) -n 's_.*name=stimfit-\([0-9\.]*\)\.tar\.gz.*_\1_ip' | \
+    wget -q -O- 'https://github.com/neurodroid/stimfit/releases' | \
+    $(SED) -n 's_.*<a href="/neurodroid/stimfit/tree/\([0-9\.]*\)\.tar\.gz.*_\1_ip' | \
     head -1
 endef
 
 define $(PKG)_BUILD
 
-    cd '$(1)' && ./configure --enable-python --with-biosig --with-pslope
-
+    cd '$(1)' && ./autogen.sh && ./configure --enable-python --with-biosig --with-pslope
     WXCONF='$(PREFIX)/bin/$(TARGET)-wx-config' $(MAKE) -C '$(1)' -f Makefile.static -j '$(JOBS)'
 
     $(INSTALL) '$(1)/stimfit.exe' '$(PREFIX)/$(TARGET)/bin/'
