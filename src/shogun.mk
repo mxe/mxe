127ddf93be89f73a6a5b4eef6653548cf0bc7177
Michael McGarrah <mcgarrah@gmail.com>
2014-02-04 18:21:38 -0500

    Update Shogun build information

--- src/shogun.mk
+++ src/shogun.mk
@@ -8,7 +8,7 @@ $(PKG)_CHECKSUM := c1048ab21e8548457d3f7fec2800a65db959f202
 $(PKG)_SUBDIR   := $(PKG)-$(PKG)_$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)_$($(PKG)_VERSION).zip
 $(PKG)_URL      := https://github.com/$(PKG)-toolbox/$(PKG)/archive/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc
+$(PKG)_DEPS     := gcc blas boost curl bzip2 eigen lapack libgomp libxml2 lzo pcre xz protobuf ccache glpk ColPack nlopt arprec arpack-ng szip hdf5
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://github.com/shogun-toolbox/shogun/tags' | \
@@ -19,8 +19,11 @@ endef
 define $(PKG)_BUILD
     cd '$(1)' && mkdir build && cd build && cmake .. \
         -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+        -DGLPK_ROOT_DIR='$(PREFIX)/$(TARGET)' \
         -DCMAKE_BUILD_TYPE=Release \
-        -DEIGEN_BUILD_PKGCONFIG=ON \
-        -Drun_res=1 -Drun_res__TRYRUN_OUTPUT=""
-    $(MAKE) -C '$(1)'/build -j '$(JOBS)' install VERBOSE=1
+        -DCSharpModular=OFF \
+        -DCmdLineStatic=ON \
+        -DBUILD_EXAMPLES=OFF
+    $(MAKE) -C '$(1)'/build -j '$(JOBS)'
+    $(MAKE) -C '$(1)'/build -j '$(JOBS)' install
 endef
