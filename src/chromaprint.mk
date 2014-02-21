e6edb821b0f9593e82ad2393435c678e38e276db
Tony Theodore <tonyt@logyst.com>
2014-02-22 02:49:19 +1100

    package chromaprint: enable static and shared builds

--- src/chromaprint.mk
+++ src/chromaprint.mk
@@ -8,17 +8,16 @@ $(PKG)_CHECKSUM := 5a250f761761d2ce08e2591b9daa909393552939
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://bitbucket.org/acoustid/chromaprint/downloads/$(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_DEPS     := gcc fftw
+$(PKG)_DEPS     := gcc ffmpeg
 
 define $(PKG)_UPDATE
-     echo $($(PKG)_VERSION)
+    echo 'TODO: Updates for package chromaprint need to be written.' >&2;
+    echo $(chromaprint_VERSION)
 endef
 
 define $(PKG)_BUILD
     cd '$(1)' && cmake \
-        -DBUILD_STATIC=ON \
-        -DBUILD_SHARED=OFF \
-        -DBUILD_EXAMPLES=OFF \
-        -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)'
+        -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+        -DBUILD_SHARED_LIBS=$(if $(BUILD_SHARED),ON,OFF)
     $(MAKE) -C '$(1)' -j 1 install
 endef
