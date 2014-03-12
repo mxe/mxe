1cc196a50c37685a3ad2edd9fbdfe883c3282e52
Tobias Gruetzmacher <tobias-git@23.gs>
2014-03-12 21:43:30 +0100

    Package libcroco: Enable shared build.

--- src/libcroco.mk
+++ src/libcroco.mk
@@ -19,9 +19,7 @@ endef
 
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
-        --host='$(TARGET)' \
-        --disable-shared \
-        --prefix='$(PREFIX)/$(TARGET)' \
+        $(MXE_CONFIGURE_OPTS) \
         --disable-gtk-doc
     $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 endef
