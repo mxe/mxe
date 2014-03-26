b2896a198d41391ffee5385306126853ea267b1c
Mark Brand <mabrand@mabrand.nl>
2014-03-26 09:10:31 +0100

    update package gmp

--- src/gmp.mk
+++ src/gmp.mk
@@ -4,9 +4,9 @@
 PKG             := gmp
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 6.0.0
-$(PKG)_CHECKSUM := c4976716a277b1d3b595171143f52f8c1a788284
+$(PKG)_CHECKSUM := 360802e3541a3da08ab4b55268c80f799939fddc
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION)a.tar.bz2
 $(PKG)_URL      := ftp://ftp.gmplib.org/pub/$(PKG)-$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_URL_2    := ftp://ftp.cs.tu-berlin.de/pub/gnu/$(PKG)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
