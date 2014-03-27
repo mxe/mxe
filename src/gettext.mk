0f90669139af671909abee90c3fdc0004eaddf40
Mark Brand <mabrand@mabrand.nl>
2014-03-27 08:59:07 +0100

    update package gettext

--- src/gettext.mk
+++ src/gettext.mk
@@ -3,8 +3,8 @@
 
 PKG             := gettext
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.18.3.1
-$(PKG)_CHECKSUM := a32c19a6e39450748f6e56d2ac6b8b0966a5ab05
+$(PKG)_VERSION  := 0.18.3.2
+$(PKG)_CHECKSUM := a2dc46d788edb0078ab20da7bd194bdb6da2f0d9
 $(PKG)_SUBDIR   := gettext-$($(PKG)_VERSION)
 $(PKG)_FILE     := gettext-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/gettext/$($(PKG)_FILE)
