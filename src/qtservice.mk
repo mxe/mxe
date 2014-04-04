f4cafd0edbefc2b12f511be5009605be18c38e23
Mark Brand <mabrand@mabrand.nl>
2014-04-04 17:58:11 +0200

    update package qtservice

--- src/qtservice.mk
+++ src/qtservice.mk
@@ -2,8 +2,8 @@
 # See index.html for further information.
 PKG             := qtservice
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4ac977553f2b04a54feb74ebcbc90ef255d38895
-$(PKG)_CHECKSUM := b3a185696066dc8f1792de83aa8d7187fb416580
+$(PKG)_VERSION  := fd22bee22274975c56f1c10d87ee9fd2c0818f83
+$(PKG)_CHECKSUM := ecf7563a56c3ac7a812950c6e28d214e4b01b7b2
 $(PKG)_SUBDIR   := qt-solutions-qt-solutions
 $(PKG)_FILE     := $($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://qt.gitorious.org/qt-solutions/qt-solutions/archive/$($(PKG)_FILE)
