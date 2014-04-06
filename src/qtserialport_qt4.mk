a1a97358aad33650b20433025e169a597f35d0c7
Tony Theodore <tonyt@logyst.com>
2014-04-06 19:37:14 +1000

    package qtserialport: add Qt4 version

---
+++ src/qtserialport_qt4.mk
@@ -0,0 +1,21 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := qtserialport_qt4
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION   = $(qtbase_VERSION)
+$(PKG)_CHECKSUM := 929b27f7d67a44cd7f9fc138f0c1300145d7e517
+$(PKG)_SUBDIR    = $(subst qtbase,qtserialport,$(qtbase_SUBDIR))
+$(PKG)_FILE      = $(subst qtbase,qtserialport,$(qtbase_FILE))
+$(PKG)_URL       = $(subst qtbase,qtserialport,$(qtbase_URL))
+$(PKG)_DEPS     := gcc qt
+
+define $(PKG)_UPDATE
+    echo $(qtbase_VERSION)
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && '$(PREFIX)/$(TARGET)/qt/bin/qmake'
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+endef
