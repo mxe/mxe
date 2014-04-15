27c0ab09f6433ce68289765b9db8d1c7daf1ac54
Timothy Gu <timothygu99@gmail.com>
2014-04-14 19:09:26 -0700

    mpg123: add shared support
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/mpg123.mk
+++ src/mpg123.mk
@@ -18,14 +18,10 @@ endef
 
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
-        --host='$(TARGET)' \
-        --disable-shared \
-        --prefix='$(PREFIX)/$(TARGET)' \
+        $(MXE_CONFIGURE_OPTS) \
         --with-default-audio=win32 \
         --with-audio=win32,sdl,dummy \
         --enable-modules=no
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     $(MAKE) -C '$(1)' -j 1 install
 endef
-
-$(PKG)_BUILD_SHARED =
