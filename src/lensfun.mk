5a29aa3f2fd490b40330fddb7c1befd2b86cb491
Niels Kristian Bech Jensen <nkbj1970@hotmail.com>
2014-04-19 16:44:25 -0700

    Lensfun has moved to sourceforge.net
    The lensfun project has moved to sourceforge.net pending the close-down of
    berlios.de by the end of April. This patch updates lensfun.mk accordingly.
    
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/lensfun.mk
+++ src/lensfun.mk
@@ -7,14 +7,14 @@ $(PKG)_VERSION  := 0.2.7
 $(PKG)_CHECKSUM := f26121093dfee85d6371c2c79dae22e6d1b8d0d6
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/lensfun.berlios/$($(PKG)_FILE)
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/lensfun/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc libpng glib libgnurx
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- "http://developer.berlios.de/project/showfiles.php?group_id=9034" | \
-    grep -i 'lensfun.*tar' | \
-    $(SED) -n 's,.*lensfun-\([0-9][^>]*\)\.tar.*,\1,p' | \
-    head -1
+    $(WGET) -q -O- 'http://sourceforge.net/projects/lensfun/files/' | \
+    $(SED) -n 's,.*/\([0-9][^"]*\)/".*,\1,p' | \
+    sort -V | \
+    tail -1
 endef
 
 define $(PKG)_BUILD
