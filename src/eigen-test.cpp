e4bf5a3a9f4d5ea9e85556d353cffb4e77062237
Brad Pitcher <bradpitcher@gmail.com>
2014-04-22 08:31:47 -0700

    add eigen test

---
+++ src/eigen-test.cpp
@@ -0,0 +1,19 @@
+/*
+ * This file is part of MXE.
+ * See index.html for further information.
+ */
+
+#include <iostream>
+#include <Eigen/Dense>
+
+using Eigen::MatrixXd;
+
+int main()
+{
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    return 0;
+}
