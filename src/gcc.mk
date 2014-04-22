4f2a84496f1d00ca010eab56d85cd167dcd5375f
Mark Brand <mabrand@mabrand.nl>
2014-04-22 14:10:46 +0200

    update package gcc

--- src/gcc.mk
+++ src/gcc.mk
@@ -3,8 +3,8 @@
 
 PKG             := gcc
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.8.2
-$(PKG)_CHECKSUM := 810fb70bd721e1d9f446b6503afe0a9088b62986
+$(PKG)_VERSION  := 4.9.0
+$(PKG)_CHECKSUM := fbde8eb49f2b9e6961a870887cf7337d31cd4917
 $(PKG)_SUBDIR   := gcc-$($(PKG)_VERSION)
 $(PKG)_FILE     := gcc-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/gcc/gcc-$($(PKG)_VERSION)/$($(PKG)_FILE)
