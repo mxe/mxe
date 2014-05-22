d8ff66ccc58b698fb508f957932e09b74668576c
Roy Storey <roy.storey@plantandfood.co.nz>
2014-05-22 13:44:07 +1200

    remove old comments

--- src/tk.mk
+++ src/tk.mk
@@ -16,19 +16,11 @@ define $(PKG)_UPDATE
     head -1
 endef
 
-## tk physically depends on the build dir of tcl
-## here we rely on the tcl*-src.tar.gz still existing - it's a dependency
-## tclConfig.sh (installed by tcl) is sourced to find build location and 
-## therefore location of tclInt.h for its inclusion.
 define $(PKG)_BUILD
-    ## tcl now make install-private-headers negating this requirement
-    ## mkdir -p $(call TMP_DIR,tcl-$(TARGET))
-    ## cd $(call TMP_DIR,tcl) && $(call UNPACK_ARCHIVE,$(PKG_DIR)/$(tcl_FILE))
     cd '$(1)/win' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
         --enable-threads \
 	$(if $(findstring 64,$(TARGET)), --enable-64bit) \
 	CFLAGS=-D__MINGW_EXCPT_DEFINE_PSDK
     $(MAKE) -C '$(1)/win' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
-    ## rm -rfv $(call TMP_DIR,tcl-$(TARGET))
 endef
