ff52639735c7b1bd4b0219022e34e6454b9db5e8
Mark Brand <mabrand@mabrand.nl>
2014-06-19 14:40:03 +0200

    update package vmime

--- src/vmime.mk
+++ src/vmime.mk
@@ -3,8 +3,8 @@
 
 PKG             := vmime
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 7302731
-$(PKG)_CHECKSUM := 30b25d230c78f13ab8a8a351c84a673333b62b14
+$(PKG)_VERSION  := 0863f50
+$(PKG)_CHECKSUM := 4ea261f3d632ee9ab19b56f5e23c8e615abd6a70
 $(PKG)_SUBDIR   := kisli-vmime-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/kisli/vmime/tarball/$($(PKG)_VERSION)/$($(PKG)_FILE)
