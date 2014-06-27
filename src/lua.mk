8e1be0a98218868f0aafa04763add2a58de52c68
Timothy Gu <timothygu99@gmail.com>
2014-06-27 16:58:02 -0700

    lua: enable shared
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/lua.mk
+++ src/lua.mk
@@ -4,6 +4,8 @@
 PKG             := lua
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 5.2.3
+# Shared version
+$(PKG)_SOVERS   := 52
 $(PKG)_CHECKSUM := 926b7907bc8d274e063d42804666b40a3f3c124c
 $(PKG)_SUBDIR   := lua-$($(PKG)_VERSION)
 $(PKG)_FILE     := lua-$($(PKG)_VERSION).tar.gz
@@ -16,21 +18,7 @@ define $(PKG)_UPDATE
     head -1
 endef
 
-define $(PKG)_BUILD
-    $(MAKE) -C '$(1)/src' -j '$(JOBS)' \
-        INSTALL_TOP='$(PREFIX)/$(TARGET)' \
-        CC='$(TARGET)-gcc' \
-        AR='$(TARGET)-ar rcu' \
-        RANLIB='$(TARGET)-ranlib' \
-        a
-    $(MAKE) -C '$(1)' -j 1 \
-        INSTALL_TOP='$(PREFIX)/$(TARGET)' \
-        INSTALL_BIN='$(1)/noinstall' \
-        INSTALL_MAN='$(1)/noinstall' \
-        TO_BIN='lua.h' \
-        INSTALL='$(INSTALL)' \
-        install
-
+define $(PKG)_BUILD_COMMON
     #pkg-config file
     (echo 'Name: $(PKG)'; \
      echo 'Version: $($(PKG)_VERSION)'; \
@@ -44,4 +32,37 @@ define $(PKG)_BUILD
         `$(TARGET)-pkg-config --libs lua`
 endef
 
-$(PKG)_BUILD_SHARED =
+define $(PKG)_BUILD
+    $(MAKE) -C '$(1)/src' -j '$(JOBS)' \
+        INSTALL_TOP='$(PREFIX)/$(TARGET)' \
+        CC='$(TARGET)-gcc' \
+        AR='$(TARGET)-ar rcu' \
+        RANLIB='$(TARGET)-ranlib' \
+        a
+    $(MAKE) -C '$(1)' -j 1 \
+        INSTALL_TOP='$(PREFIX)/$(TARGET)' \
+        INSTALL_BIN='$(1)/noinstall' \
+        INSTALL_MAN='$(1)/noinstall' \
+        INSTALL='$(INSTALL)' \
+        install
+    $($(PKG)_BUILD_COMMON)
+endef
+
+define $(PKG)_BUILD_SHARED
+    $(MAKE) -C '$(1)/src' -j '$(JOBS)' \
+        INSTALL_TOP='$(PREFIX)/$(TARGET)' \
+        CC='$(TARGET)-gcc' \
+        AR='$(TARGET)-gcc -Wl,--out-implib,liblua.dll.a -shared -o' \
+        RANLIB='echo skipped ranlib' \
+        SYSCFLAGS='-DLUA_BUILD_AS_DLL' \
+        LUA_A=lua$($(PKG)_SOVERS).dll \
+        a
+    $(MAKE) -C '$(1)' -j 1 \
+        INSTALL_TOP='$(PREFIX)/$(TARGET)' \
+        INSTALL_MAN='$(1)/noinstall' \
+        TO_BIN='lua$($(PKG)_SOVERS).dll' \
+        INSTALL='$(INSTALL)' \
+        TO_LIB='liblua.dll.a' \
+        install
+    $($(PKG)_BUILD_COMMON)
+endef
