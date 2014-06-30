2b42e831c9939f8d681c4e9e2564b1cec8fd2953
Timothy Gu <timothygu99@gmail.com>
2014-06-29 19:34:33 -0700

    openssl: update
    Again, a security update, including the fix for Heartbleed.
    
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/openssl.mk
+++ src/openssl.mk
@@ -3,8 +3,8 @@
 
 PKG             := openssl
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.1e
-$(PKG)_CHECKSUM := 3f1b1223c9e8189bfe4e186d86449775bd903460
+$(PKG)_VERSION  := 1.0.1h
+$(PKG)_CHECKSUM := b2239599c8bf8f7fc48590a55205c26abe560bf8
 $(PKG)_SUBDIR   := openssl-$($(PKG)_VERSION)
 $(PKG)_FILE     := openssl-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.openssl.org/source/$($(PKG)_FILE)
