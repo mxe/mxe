39f524cb1176088f72bcc177ce9ec80f02561784
Timothy Gu <timothygu99@gmail.com>
2014-08-01 20:29:22 -0700

    ncurses: update to development version and enable shared build
    Dev is fairly stable, and has fixes for mingw shared build.
    
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/ncurses.mk
+++ src/ncurses.mk
@@ -3,18 +3,24 @@
 
 PKG             := ncurses
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.9
-$(PKG)_CHECKSUM := 3e042e5f2c7223bffdaac9646a533b8c758b65b5
-$(PKG)_SUBDIR   := ncurses-$($(PKG)_VERSION)
-$(PKG)_FILE     := ncurses-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := http://ftp.gnu.org/pub/gnu/ncurses/$($(PKG)_FILE)
+$(PKG)_VERSION  := c6f5464
+$(PKG)_CHECKSUM := 5a40e0906139f877be083976f80bf0262dc95ad2
+# $(PKG)_VERSION  := 5.9
+# $(PKG)_SUBDIR   := ncurses-$($(PKG)_VERSION)
+# $(PKG)_FILE     := ncurses-$($(PKG)_VERSION).tar.gz
+# $(PKG)_URL      := http://ftp.gnu.org/pub/gnu/ncurses/$($(PKG)_FILE)
+$(PKG)_SUBDIR   := mirror-$(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://github.com/mirror/$(PKG)/tarball/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc libgnurx
 
-define $(PKG)_UPDATE
+define $(PKG)_UPDATE_RELEASE
     $(WGET) -q -O- 'http://ftp.gnu.org/pub/gnu/ncurses/?C=M;O=D' | \
     $(SED) -n 's,.*<a href="ncurses-\([0-9][^"]*\)\.tar.*,\1,p' | \
     head -1
 endef
+
+$(PKG)_UPDATE = $(call MXE_GET_GITHUB_SHA, mirror/ncurses, master)
 
 define $(PKG)_BUILD
     # native build of terminfo compiler
@@ -40,10 +46,9 @@ define $(PKG)_BUILD
         --without-tests \
         --enable-pc-files \
         PKG_CONFIG_LIBDIR='$(PREFIX)/$(TARGET)/lib/pkgconfig' \
-        --with-normal \
-        --without-shared
+        $(if $(BUILD_STATIC), \
+            --with-normal    --without-shared --with-static, \
+            --without-normal --without-static --with-shared)
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     $(MAKE) -C '$(1)' -j 1 install TIC_PATH='$(1).native/progs/tic'
 endef
-
-$(PKG)_BUILD_SHARED =
