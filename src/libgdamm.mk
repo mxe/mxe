8e1be39ff0de4e0785bdd58c31cddcfc72ed9169
Timothy Gu <timothygu99@gmail.com>
2014-08-10 10:46:25 -0700

    libgdamm: add update macro
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/libgdamm.mk
+++ src/libgdamm.mk
@@ -11,8 +11,10 @@ $(PKG)_URL      := http://ftp.gnome.org/pub/gnome/sources/libgdamm/$(call SHORT_
 $(PKG)_DEPS     := gcc libgda glibmm
 
 define $(PKG)_UPDATE
-    echo 'TODO: Updates for package libgdamm need to be fixed.' >&2;
-    echo $(libgdamm_VERSION)
+    $(WGET) -q -O- 'http://git.gnome.org/browse/libgdamm/refs/tags' | \
+    grep '<a href=' | \
+    $(SED) -n 's,.*<a[^>]*>\([0-9][^<]*\)<.*,\1,p' | \
+    head -1
 endef
 
 define $(PKG)_BUILD
