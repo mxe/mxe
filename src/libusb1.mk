e0e90761d59e1dcaecf1edfc857d14c19e3bb368
Uwe Hermann <uwe@hermann-uwe.de>
2014-08-11 20:52:23 +0200

    libusb1: Fix update macro

--- src/libusb1.mk
+++ src/libusb1.mk
@@ -11,8 +11,9 @@ $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/libusb/libusb-1.0/libusb
 $(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://sourceforge.net/projects/libusb/files/releases/' | \
-    $(SED) -n 's,.*/\([0-9][^"]*\)/".*,\1,p' | \
+    $(WGET) -q -O- 'http://sourceforge.net/projects/libusb/files/libusb-1.0/' | \
+    grep -i 'libusb/files/libusb-1.0' | \
+    $(SED) -n 's,.*/libusb-1.0/libusb-\([0-9\.]*\)/.*,\1,p' | \
     head -1
 endef
 
