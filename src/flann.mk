4202b74cc1bf60f7488b8ded803741004317c11b
Timothy Gu <timothygu99@gmail.com>
2014-08-11 11:25:40 -0700

    flann: enable OpenMP
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/flann.mk
+++ src/flann.mk
@@ -8,7 +8,7 @@ $(PKG)_CHECKSUM := e03d9d458757f70f6af1d330ff453e3621550a4f
 $(PKG)_SUBDIR   := flann-$($(PKG)_VERSION)-src
 $(PKG)_FILE     := flann-$($(PKG)_VERSION)-src.zip
 $(PKG)_URL      := http://people.cs.ubc.ca/~mariusm/uploads/FLANN/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc
+$(PKG)_DEPS     := gcc libgomp
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://people.cs.ubc.ca/~mariusm/index.php/FLANN/Changelog' | \
@@ -27,6 +27,6 @@ define $(PKG)_BUILD
         -DBUILD_CUDA_LIB=OFF \
         -DBUILD_MATLAB_BINDINGS=OFF \
         -DBUILD_PYTHON_BINDINGS=OFF  \
-        -DUSE_OPENMP=OFF
+        -DUSE_OPENMP=ON
     $(MAKE) -C '$(1)' -j '$(JOBS)' install VERBOSE=1
 endef
