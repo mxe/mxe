59f59f2fde4702b7a68d210725e577c6ec838c51
Timothy Gu <timothygu99@gmail.com>
2014-08-19 08:24:34 -0700

    glib: Clarify the glib-* tools building process

--- src/glib.mk
+++ src/glib.mk
@@ -64,12 +64,19 @@ endef
 define $(PKG)_BUILD
     cd '$(1)' && NOCONFIGURE=true ./autogen.sh
     rm -f '$(PREFIX)/$(TARGET)/bin/glib-*'
+
+    # Detecting if these GLib tools are already available on host machine,
+    # either because of a host package installation or from an earlier MXE
+    # installation of GLib.
+	# If it is installed, we symlink it into the MXE bin/.
+	# If not, we build it.
     $(if $(findstring y,\
             $(shell [ -x "`which glib-genmarshal`" ] && \
                     [ -x "`which glib-compile-schemas`" ] && \
                     [ -x "`which glib-compile-resources`" ] && echo y)), \
         $($(PKG)_SYMLINK), \
         $($(PKG)_NATIVE_BUILD))
+
     # cross build
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
