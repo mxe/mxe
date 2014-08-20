414a585650476428fc7001fcc96fcfb6ec1509ce
Guilherme Bernal <lb-guilherme@live.com>
2014-08-19 23:23:32 -0300

    qtservice, qtsystems: fix checksums (again)

--- src/qtsystems.mk
+++ src/qtsystems.mk
@@ -3,7 +3,7 @@
 PKG             := qtsystems
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 341352b4bb1e729f384a3af1ca966ebd123f16e3
-$(PKG)_CHECKSUM := 4f79f8950f5a52b45a222d9c7bdae7fbdf13832a
+$(PKG)_CHECKSUM := 8fcc4d3329b388432e46c63a79acf0c0c7741a42
 $(PKG)_SUBDIR   := qt-$(PKG)
 $(PKG)_FILE     := $($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://qt.gitorious.org/qt/qtsystems/archive/$($(PKG)_FILE)
