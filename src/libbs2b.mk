23a469cd69246dcef6fe4f15e806960f7c62504d
Timothy Gu <timothygu99@gmail.com>
2014-08-31 14:34:31 -0700

    Add package libbs2b
    The libbs2b.mk file was incorrectly added in 262aa647f04db4aac. Sorry about
    that.
    
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/libbs2b.mk
+++ src/libbs2b.mk
@@ -3,12 +3,12 @@
 
 PKG             := libbs2b
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.7
-$(PKG)_CHECKSUM := 80eaaa5cc576c35dd28863767b795c50cbcc0511
+$(PKG)_VERSION  := 3.1.0
+$(PKG)_CHECKSUM := 353180e0f260b074508c6ddb34259b0d08a12dd7
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/bs2b/libbs2b/$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc libsndfile
+$(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://sourceforge.net/projects/bs2b/files/libbs2b/' | \
@@ -17,8 +17,12 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
+    cd '$(1)' && autoreconf -fi
     cd '$(1)' && ./configure \
-        $(MXE_CONFIGURE_OPTS)
-    $(MAKE) -C '$(1)' -j '$(JOBS)' LDFLAGS='-no-undefined'
-    $(MAKE) -C '$(1)' -j 1 install LDFLAGS='-no-undefined'
+        $(MXE_CONFIGURE_OPTS) \
+        ac_cv_func_malloc_0_nonnull=yes
+    # The ac_cv_func_malloc_0_nonnull=yes is needed because the configure
+    # check tries to run a program.
+    $(MAKE) -C '$(1)' -j '$(JOBS)' LDFLAGS='-no-undefined' $(MXE_DISABLE_CRUFT)
+    $(MAKE) -C '$(1)' -j 1 install LDFLAGS='-no-undefined' $(MXE_DISABLE_CRUFT)
 endef
