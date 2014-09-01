ab4016a29308fdcb1edc2a5010b4c9aa27b81735
Timothy Gu <timothygu99@gmail.com>
2014-09-01 11:53:03 -0700

    libsndfile: Use MXE_DISABLE_CRUFT

--- src/libsndfile.mk
+++ src/libsndfile.mk
@@ -27,5 +27,5 @@ define $(PKG)_BUILD
         --disable-alsa \
         --disable-shave \
         LIBS="`$(TARGET)-pkg-config --libs vorbis ogg`"
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS= man_MANS= html_DATA=
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install $(MXE_DISABLE_CRUFT)
 endef
