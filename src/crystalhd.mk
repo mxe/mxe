31318fc69c280a8949adf06ddd858e248e7847d0
Timothy Gu <timothygu99@gmail.com>
2014-09-03 21:37:01 -0700

    crystalhd: Fix update macro

--- src/crystalhd.mk
+++ src/crystalhd.mk
@@ -6,12 +6,12 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1
 $(PKG)_CHECKSUM := 2ee046b1a20485abb750244d4c71f733673496f7
 $(PKG)_SUBDIR   := .
-$(PKG)_FILE     := crystalhd_lgpl_includes_v1.zip
+$(PKG)_FILE     := crystalhd_lgpl_includes_v$($(PKG)_VERSION).zip
 $(PKG)_URL      := http://www.broadcom.com/docs/support/$(PKG)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
-    echo "TODO: crystalhd update script" >&2
+    echo "TODO: crystalhd update script" >&2;
     echo $(crystalhd_VERSION)
 endef
 
