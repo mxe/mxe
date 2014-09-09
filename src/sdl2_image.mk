c41829f89e23817ec91ac27946efc5279ef1e74a
Timothy Gu <timothygu99@gmail.com>
2014-09-09 07:23:01 -0700

    sdl2_image: require libwebp as a dependency
    See previous commit.
    
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/sdl2_image.mk
+++ src/sdl2_image.mk
@@ -8,7 +8,7 @@ $(PKG)_CHECKSUM := 20b1b0db9dd540d6d5e40c7da8a39c6a81248865
 $(PKG)_SUBDIR   := SDL2_image-$($(PKG)_VERSION)
 $(PKG)_FILE     := SDL2_image-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.libsdl.org/projects/SDL_image/release/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc sdl2 jpeg libpng tiff
+$(PKG)_DEPS     := gcc sdl2 jpeg libpng libwebp tiff
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://hg.libsdl.org/SDL_image/tags' | \
@@ -17,7 +17,7 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    $(SED) -i 's,^\(Requires:.*\),\1\nRequires.private: libtiff-4 libpng,' '$(1)/SDL2_image.pc.in'
+    $(SED) -i 's,^\(Requires:.*\),\1\nRequires.private: libtiff-4 libpng libwebp,' '$(1)/SDL2_image.pc.in'
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
         --with-sdl-prefix='$(PREFIX)/$(TARGET)' \
