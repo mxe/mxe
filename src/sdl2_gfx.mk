b4f2887860ee2197e211d2edab0c16d01387ffc7
Luis Saavedra <luis94855510@gmail.com>
2014-09-15 17:23:34 -0700

    sdl2_gfx: fix syntax error near unexpected token `) '
    Also use `` instead of $() and use $(SED) instead of sed (completely my
    fault).
    
    Fixes #506.
    
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/sdl2_gfx.mk
+++ src/sdl2_gfx.mk
@@ -21,8 +21,8 @@ define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
         --with-sdl-prefix='$(PREFIX)/$(TARGET)' \
-        SDL_LIBS=$($(TARGET)-pkg-config --libs sdl2 | sed -e 's/-lmingw32//' -e 's/-lSDL2main//') \
-        SDL_CFLAGS=$($(TARGET)-pkg-config --cflags sdl2 | sed 's/-Dmain=SDL_main//'))
+        SDL_LIBS="`$(TARGET)-pkg-config --libs sdl2 | $(SED) -e 's/-lmingw32//' -e 's/-lSDL2main//'`" \
+        SDL_CFLAGS="`$(TARGET)-pkg-config --cflags sdl2 | $(SED) 's/-Dmain=SDL_main//'`"
     $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 
     '$(TARGET)-gcc' \
