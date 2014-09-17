3299da02ee942fb77ba8c7f7df62845680c17366
Mark Brand <mabrand@mabrand.nl>
2014-09-17 08:44:22 +0200

    update package dbus

--- src/dbus.mk
+++ src/dbus.mk
@@ -3,8 +3,8 @@
 
 PKG             := dbus
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.8.6
-$(PKG)_CHECKSUM := ad7cb87cdce66533479a9d7c1c956bdb0243ad87
+$(PKG)_VERSION  := 1.8.8
+$(PKG)_CHECKSUM := e0d10e8b4494383c7e366ac80a942ba45a705a96
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://$(PKG).freedesktop.org/releases/$(PKG)/$($(PKG)_FILE)
