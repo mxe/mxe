7e51f06b81206274a6eb84f5fab2e236728980de
Timothy Gu <timothygu99@gmail.com>
2014-09-30 17:24:31 -0700

    ffmpeg: update
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/ffmpeg.mk
+++ src/ffmpeg.mk
@@ -3,8 +3,8 @@
 
 PKG             := ffmpeg
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.3.3
-$(PKG)_CHECKSUM := 012afcbc57ecdf23f71b9901087cd5dbc8056756
+$(PKG)_VERSION  := 2.4.1
+$(PKG)_CHECKSUM := 8cb1c03af61001edbd0d9643b2a01b7aa097a4b3
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.ffmpeg.org/releases/$($(PKG)_FILE)
