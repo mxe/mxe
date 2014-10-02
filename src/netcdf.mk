90f8971e32ee0f006a7ab8d8012597fef38301cb
Tony Theodore <tonyt@logyst.com>
2014-10-03 02:15:07 +1000

    netcdf: enable i686-w64-mingw32 (see #492)

--- src/netcdf.mk
+++ src/netcdf.mk
@@ -39,6 +39,7 @@ define $(PKG)_BUILD
 endef
 
 $(PKG)_BUILD_x86_64-w64-mingw32 =
-$(PKG)_BUILD_i686-w64-mingw32 =
+$(PKG)_BUILD_i686-w64-mingw32 = $(subst --enable-hdf4, --disable-hdf4,\
+                                $(subst -lmfhdf -ldf,,$($(PKG)_BUILD)))
 
 $(PKG)_BUILD_SHARED =
