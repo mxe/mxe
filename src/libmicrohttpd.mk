5805dd15b0ac53d548ca2ce3647bb318273c0dd2
Tony Theodore <tonyt@logyst.com>
2014-10-03 01:42:40 +1000

    plibc & libmicrohttpd: enable i686-w64-mingw32 (see #492)

--- src/libmicrohttpd.mk
+++ src/libmicrohttpd.mk
@@ -37,7 +37,6 @@ define $(PKG)_BUILD
         `'$(TARGET)-pkg-config' --cflags --libs libmicrohttpd`
 endef
 
-$(PKG)_BUILD_i686-w64-mingw32 =
 $(PKG)_BUILD_x86_64-w64-mingw32 =
 
 $(PKG)_BUILD_SHARED =
