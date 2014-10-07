bc82d593ce273507936edf30b769b0718ee4eac9
Rashad M <mohammedrashadkm@gmail.com>
2014-10-07 17:33:30 +0530

    using offical ossim tar ball and updated its PKG_UPDATE and PKG_BUILD

--- src/ossim.mk
+++ src/ossim.mk
@@ -3,15 +3,16 @@
 PKG             := ossim
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.8.18
-$(PKG)_CHECKSUM := 64a1018148d3ea4e53b47940e24aba2bff3c9185
+$(PKG)_CHECKSUM := 50a925a4304a5df4af19a60ee41201e26a62a125
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := http://sourceforge.net/projects/mxedeps/files/$($(PKG)_FILE)
+$(PKG)_URL      := http://download.osgeo.org/ossim/source/$($(PKG)_SUBDIR)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc zlib jpeg tiff proj libpng geos openthreads
 
 define $(PKG)_UPDATE
-    echo 'TODO: Updates for package OSSIM need to be written.' >&2;
-    echo $(itk_VERSION)
+    $(WGET) -q -O- 'http://download.osgeo.org/ossim/source/latest/' | \
+    $(SED) -n 's,.*ossim-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    head -1
 endef
 
 define $(PKG)_BUILD
@@ -19,7 +20,7 @@ define $(PKG)_BUILD
     cd '$(1).build' && cmake \
         -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
         -DBUILD_SHARED_LIBS=ON \
-	-DCMAKE_MODULE_PATH='$(1)/CMakeModukes' \
-        '$(1)'
+	-DCMAKE_MODULE_PATH='$(1)/ossim_package_support/cmake/CMakeModules' \
+        '$(1)/ossim'
   $(MAKE) -C '$(1).build' -j '$(JOBS)' install 
 endef
