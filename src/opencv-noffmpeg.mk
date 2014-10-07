82efcbc42beaf233075113edb840eff24a53fcca
Rashad M <mohammedrashadkm@gmail.com>
2014-10-07 17:37:51 +0530

    REVIEW: opencv without ffmpeg support.

---
+++ src/opencv-noffmpeg.mk
@@ -0,0 +1,66 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := opencv-noffmpeg
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.4.9
+$(PKG)_CHECKSUM := 4f5166e2bd22bd6167cb56dd04f2c6ed68148b2c
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := opencv-$($(PKG)_VERSION).zip
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/opencvlibrary/opencv-unix/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_URL_2    := http://distfiles.macports.org/opencv/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc eigen jasper jpeg lcms1 libpng openexr tiff xz zlib
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/' | \
+    $(SED) -n 's,.*/\([0-9][^"]*\)/".*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    # build
+    mkdir '$(1).build'
+    cd '$(1).build' && cmake \
+      -DWITH_QT=OFF \
+      -DWITH_OPENGL=ON \
+      -DWITH_GSTREAMER=OFF \
+      -DWITH_GTK=OFF \
+      -DWITH_VIDEOINPUT=OFF \
+      -DWITH_XINE=OFF \
+      -DBUILD_SHARED_LIBS=ON \
+      -DBUILD_opencv_apps=OFF \
+      -DBUILD_DOCS=OFF \
+      -DBUILD_EXAMPLES=OFF \
+      -DBUILD_PACKAGE=OFF \
+      -DBUILD_PERF_TESTS=OFF \
+      -DBUILD_TESTS=OFF \
+      -DBUILD_WITH_DEBUG_INFO=OFF \
+      -DBUILD_FAT_JAVA_LIB=OFF \
+      -DBUILD_ZLIB=OFF \
+      -DBUILD_TIFF=OFF \
+      -DBUILD_JASPER=OFF \
+      -DBUILD_JPEG=OFF \
+      -DBUILD_PNG=OFF \
+      -DBUILD_OPENEXR=OFF \
+      -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+      -DCMAKE_CXX_FLAGS='-D_WIN32_WINNT=0x0500' \
+      '$(1)'
+
+    # install
+    $(MAKE) -C '$(1).build' -j '$(JOBS)' install ##VERBOSE=1
+
+    # fixup and install pkg-config file
+    # openexr isn't available on x86_64-w64-mingw32
+    # opencv builds it's own libIlmImf.a
+    $(if $(findstring x86_64-w64-mingw32,$(TARGET)),\
+        $(SED) -i 's/OpenEXR//' '$(1).build/unix-install/opencv.pc')
+    $(SED) -i 's,share/OpenCV/3rdparty/,,g' '$(1).build/unix-install/opencv.pc'
+    $(SED) -i 's/dll/dll.a/g' '$(1).build/unix-install/opencv.pc'
+    $(INSTALL) -m755 '$(1).build/unix-install/opencv.pc' '$(PREFIX)/$(TARGET)/lib/pkgconfig'
+    $(SED) -i 's/CMAKE_OPENCV_GCC_TARGET_MACHINE/OPENCV_GCC_TARGET_MACHINE/g' '$(PREFIX)/$(TARGET)/OpenCVConfig.cmake'
+
+    '$(TARGET)-g++' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(1)/samples/c/fback_c.c' -o '$(PREFIX)/$(TARGET)/bin/test-opencv.exe' \
+        `'$(TARGET)-pkg-config' opencv --cflags --libs`
+endef
