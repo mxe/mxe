10d10facd7414e72485e03a080596b393eaf6f45
Tobias Gruetzmacher <tobias-git@23.gs>
2014-10-09 19:59:30 +0200

    Enable shared build for smpeg2.

--- src/smpeg2.mk
+++ src/smpeg2.mk
@@ -11,8 +11,8 @@ $(PKG)_URL      := http://www.libsdl.org/projects/smpeg/release/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc sdl2
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://packages.debian.org/unstable/source/smpeg' | \
-    $(SED) -n 's,.*smpeg2_\([0-9][^>]*\)\.orig\.tar.*,\1,p' | \
+    $(WGET) -q -O- 'http://www.libsdl.org/projects/smpeg/release' | \
+    $(SED) -n 's,.*smpeg2-\([0-9][^>]*\)\.tar.*,\1,p' | \
     head -1
 endef
 
@@ -21,23 +21,19 @@ define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         AR='$(TARGET)-ar' \
         NM='$(TARGET)-nm' \
-        --host='$(TARGET)' \
-        --disable-shared \
+        $(MXE_CONFIGURE_OPTS) \
         --disable-debug \
-        --prefix='$(PREFIX)/$(TARGET)' \
         --with-sdl-prefix='$(PREFIX)/$(TARGET)' \
         --disable-sdltest \
         --disable-gtk-player \
         --disable-opengl-player \
         CFLAGS='-ffriend-injection'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+    $(MAKE) -C '$(1)' -j '$(JOBS)' install $(MXE_DISABLE_CRUFT)
 
-#    '$(TARGET)-gcc' \
-#        -W -Wall -Werror -std=c99 -pedantic \
-#        '$(2).c' -o '$(PREFIX)/$(TARGET)/bin/test-smpeg2.exe' \
-#        `'$(PREFIX)/$(TARGET)/bin/smpeg2-config' --cflags --libs`
+    '$(TARGET)-gcc' \
+        -W -Wall -Werror -std=c99 -pedantic \
+        '$(TOP_DIR)/src/smpeg-test.c' -o '$(PREFIX)/$(TARGET)/bin/test-smpeg2.exe' \
+        `'$(PREFIX)/$(TARGET)/bin/smpeg2-config' --cflags --libs`
 endef
 
 $(PKG)_BUILD_i686-pc-mingw32 =
-
-$(PKG)_BUILD_SHARED =
