78db25a3b8aef652ba92583765fa0165e8d3ba86
Martin Müllenhaupt <mm@netlair.de>
2014-10-09 10:05:11 +0200

    more CMake FindSDL.. replacements

---
+++ src/cmake/FindSDL_mixer.cmake
@@ -0,0 +1,13 @@
+if(NOT PKG_CONFIG_FOUND)
+  find_package(PkgConfig REQUIRED)
+endif()
+
+pkg_check_modules(SDL_MIXER SDL_mixer)
+
+#compatiblity
+set(SDL_MIXER_VERSION_STRING ${SDL_MIXER_VERSION})
+
+# for backward compatiblity
+set(SDLMIXER_LIBRARY ${SDL_MIXER_LIBRARIES})
+set(SDLMIXER_INCLUDE_DIR ${SDL_MIXER_INCLUDE_DIRS})
+set(SDLMIXER_FOUND ${SDL_MIXER_FOUND})
