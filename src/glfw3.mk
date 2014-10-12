05c7f490a9aa15e2ced65901222d7bbb2bbe8cad
Tobias Gruetzmacher <tobias-git@23.gs>
2014-10-12 21:52:30 +0200

    glfw3: Update, enable shared & refresh patch.

--- src/glfw3.mk
+++ src/glfw3.mk
@@ -3,8 +3,8 @@
 
 PKG             := glfw3
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.0.3
-$(PKG)_CHECKSUM := 95d0d2a250dc4e9d612cdd1a7433de464db16d89
+$(PKG)_VERSION  := 3.0.4
+$(PKG)_CHECKSUM := 9b04309418ccbc74b2115d11198b7912669814ee
 $(PKG)_SUBDIR   := glfw-$($(PKG)_VERSION)
 $(PKG)_FILE     := glfw-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/glfw/glfw/$($(PKG)_VERSION)/$($(PKG)_FILE)
@@ -22,11 +22,18 @@ define $(PKG)_BUILD
     mkdir '$(1).build'
     cd    '$(1).build' && cmake '$(1)' \
         -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+        -DBUILD_SHARED_LIBS=$(if $(BUILD_STATIC),FALSE,TRUE) \
         -DGLFW_BUILD_EXAMPLES=FALSE \
         -DGLFW_BUILD_TESTS=FALSE \
+        -DGLFW_BUILD_DOCS=FALSE \
         -DGLFW_INSTALL_PKG_CONFIG=TRUE \
         -DGLFW_PKG_LIBS='-lopengl32 -lgdi32'
         $(MAKE) -C '$(1).build' -j '$(JOBS)' install
+
+    # Windows convention: DLLs in bin/, not in lib/, import library is called "libglfw3.dll.a"
+    $(if $(BUILD_SHARED),
+        mv -fv $(PREFIX)/$(TARGET)/lib/glfw3.dll '$(PREFIX)/$(TARGET)/bin/'; \
+        mv -fv $(PREFIX)/$(TARGET)/lib/glfw3dll.a '$(PREFIX)/$(TARGET)/lib/libglfw3.dll.a')
 
     '$(TARGET)-gcc' \
         -W -Wall -Werror -ansi -pedantic \
@@ -34,4 +41,3 @@ define $(PKG)_BUILD
         `'$(TARGET)-pkg-config' glfw3 --cflags --libs`
 endef
 
-$(PKG)_BUILD_SHARED =
