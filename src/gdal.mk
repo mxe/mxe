6fdc87f2bffeed15dc55656279819abd19169611
Rashad M <mohammedrashadkm@gmail.com>
2014-10-19 16:51:01 +0530

    GDAL: enable shared library for mingw64 with as much as drivers

--- src/gdal.mk
+++ src/gdal.mk
@@ -9,7 +9,7 @@ $(PKG)_SUBDIR   := gdal-$($(PKG)_VERSION)
 $(PKG)_FILE     := gdal-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://download.osgeo.org/gdal/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_URL_2    := ftp://ftp.remotesensing.org/gdal/$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc proj zlib libpng tiff libgeotiff jpeg jasper giflib expat sqlite curl geos postgresql gta hdf4 hdf5 json-c netcdf
+$(PKG)_DEPS     := gcc proj zlib libpng tiff libgeotiff jpeg jasper giflib expat sqlite curl geos postgresql gta hdf5 openjpeg libxml2
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://trac.osgeo.org/gdal/wiki/DownloadSource' | \
@@ -20,17 +20,12 @@ endef
 define $(PKG)_CONFIGURE
     # The option '--without-threads' means native win32 threading without pthread.
     cd '$(1)' && ./configure \
-        --host='$(TARGET)' \
-        --build='$(BUILD)' \
-        --enable-static \
-        --disable-shared \
-        --prefix='$(PREFIX)/$(TARGET)' \
+	$(MXE_CONFIGURE_OPTS) \
         --with-bsb \
         --with-grib \
         --with-ogr \
         --with-pam \
         --without-threads \
-        --with-static-proj4 \
         --with-libz='$(PREFIX)/$(TARGET)' \
         --with-png='$(PREFIX)/$(TARGET)' \
         --with-libtiff='$(PREFIX)/$(TARGET)' \
@@ -45,8 +40,9 @@ define $(PKG)_CONFIGURE
         --with-pg='$(PREFIX)/bin/$(TARGET)-pg_config' \
         --with-gta='$(PREFIX)/$(TARGET)' \
         --with-hdf5='$(PREFIX)/$(TARGET)' \
-        --with-libjson-c='$(PREFIX)/$(TARGET)' \
-        --without-odbc \
+	--with-openjpeg='$(PREFIX)/$(TARGET)' \
+	--with-xml2='$(PREFIX)/$(TARGET)/bin/xml2-config' \
+	--without-odbc \
         --without-xerces \
         --without-grass \
         --without-libgrass \
@@ -82,16 +78,15 @@ define $(PKG)_MAKE
     $(MAKE) -C '$(1)/gcore' -j '$(JOBS)' install
     $(MAKE) -C '$(1)/frmts' -j '$(JOBS)' install
     $(MAKE) -C '$(1)/alg'   -j '$(JOBS)' install
-    $(MAKE) -C '$(1)/ogr'   -j '$(JOBS)' install OGR_ENABLED=
-    $(MAKE) -C '$(1)/apps'  -j '$(JOBS)' install BIN_LIST=
+    $(MAKE) -C '$(1)/ogr'   -j '$(JOBS)' install #OGR_ENABLED=
+    $(MAKE) -C '$(1)/apps'  -j '$(JOBS)' install
+    $(MAKE) -C '$(1)'       -j '$(JOBS)' install
     ln -sf '$(PREFIX)/$(TARGET)/bin/gdal-config' '$(PREFIX)/bin/$(TARGET)-gdal-config'
 endef
 
 define $(PKG)_BUILD
     $($(PKG)_CONFIGURE)\
-        --with-hdf4='$(PREFIX)/$(TARGET)' \
-        --with-netcdf='$(PREFIX)/$(TARGET)' \
-        LIBS="-ljpeg -lsecur32 -lportablexdr `'$(TARGET)-pkg-config' --libs openssl libtiff-4`"
+        LIBS="-liconv `'$(TARGET)-pkg-config' --libs openssl libtiff-4`"
     $($(PKG)_MAKE)
 endef
 
@@ -102,11 +97,12 @@ define $(PKG)_BUILD_x86_64-w64-mingw32
 endef
 
 define $(PKG)_BUILD_i686-w64-mingw32
+#for i686-w64-mingw32.shared target netcdf and portablexdr must be disabled.
     $($(PKG)_CONFIGURE) \
-        --with-netcdf='$(PREFIX)/$(TARGET)' \
-        LIBS="-ljpeg -lsecur32 -lportablexdr `'$(TARGET)-pkg-config' --libs openssl libtiff-4`"
+	$(if $(BUILD_STATIC),\
+	 --with-netcdf='$(PREFIX)/$(TARGET)' \
+        LIBS="-ljpeg -lsecur32 -lportablexdr `'$(TARGET)-pkg-config' --libs openssl libtiff-4`")
+	$(if $(BUILD_SHARED),\
+        LIBS="-ljpeg -lsecur32`'$(TARGET)-pkg-config' --libs openssl libtiff-4`")
     $($(PKG)_MAKE)
 endef
-
-$(PKG)_BUILD_i686-w64-mingw32.shared =
-$(PKG)_BUILD_x86_64-w64-mingw32.shared =
