1f599c1e1fa8cafd92793c019bb274676c1c2018
Rashad M <mohammedrashadkm@gmail.com>
2014-10-19 19:36:33 +0530

    REVIEW: qwt_qt4 downgrade to 5.2

--- src/qwt_qt4.mk
+++ src/qwt_qt4.mk
@@ -2,30 +2,34 @@
 # See index.html for further information.
 
 PKG             := qwt_qt4
-$(PKG)_VERSION   = $(qwt_VERSION)
-$(PKG)_CHECKSUM  = $(qwt_CHECKSUM)
-$(PKG)_SUBDIR    = $(qwt_SUBDIR)
-$(PKG)_FILE      = $(qwt_FILE)
+$(PKG)_VERSION  := 5.2
+$(PKG)_CHECKSUM := 01a40259134864bc36a335807c1d2f1157347d36
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_WEBSITE   = $(qwt_WEBSITE)
-$(PKG)_URL       = $(qwt_URL)
+$(PKG)_URL      := http://sourceforge.net/projects/mxedeps/files/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc qt
 
 define $(PKG)_UPDATE
-    echo $(qwt_VERSION)
+    echo 'TODO: Updates for package qwt_qt4 need to be written.' >&2;
 endef
 
 define $(PKG)_BUILD
+    $(SED) -i '/\INSTALLBASE /s%\INSTALLBASE .*%INSTALLBASE=$(PREFIX)/$(TARGET)/qwt%g' '$(1)/qwtconfig.pri'
     $(if $(BUILD_STATIC),\
         echo "QWT_CONFIG -= QwtDll" >> '$(1)/qwtconfig.pri')
-
-    # build
+ # build
     cd '$(1)/src' && $(PREFIX)/$(TARGET)/qt/bin/qmake
     $(MAKE) -C '$(1)/src' -f 'Makefile.Release' -j '$(JOBS)' install
 
+    $(if $(BUILD_SHARED),\
+        rm -f '$(PREFIX)/$(TARGET)/qwt/lib/libqwt5.a')
+
     #build sinusplot example to test linkage
-    cd '$(1)/examples/sinusplot' && $(PREFIX)/$(TARGET)/qt/bin/qmake
-    $(MAKE) -C '$(1)/examples/sinusplot' -f 'Makefile.Release' -j '$(JOBS)'
+ #   cd '$(1)/examples/simple_plot' && $(PREFIX)/$(TARGET)/qt/bin/qmake
+  #  $(MAKE) -C '$(1)/examples/simple_plot' -f 'Makefile.Release' -j '$(JOBS)'
 
     # install
-    $(INSTALL) -m755 '$(1)/examples/bin/sinusplot.exe' '$(PREFIX)/$(TARGET)/bin/test-qwt-qt4.exe'
+#    $(INSTALL) -m755 '$(1)/examples/bin/simple_plot.exe' '$(PREFIX)/$(TARGET)/bin/test-qwt-qt4.exe'
+
 endef
