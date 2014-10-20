aa7d1d1e16ba62ce1d8228788fa1b3cd2d1c60d0
Timothy Gu <timothygu99@gmail.com>
2014-10-20 00:16:56 -0400

    armadillo: Update and use out-of-tree builds
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/armadillo.mk
+++ src/armadillo.mk
@@ -3,8 +3,8 @@
 
 PKG             := armadillo
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.930.2
-$(PKG)_CHECKSUM := cbd8b2b65d5af4af1e2ab8d93896cd641c27023c
+$(PKG)_VERSION  := 4.450.4
+$(PKG)_CHECKSUM := a8b4ecba8161aee4d252da9f6de40eb78895f329
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/arma/$($(PKG)_FILE)
@@ -17,8 +17,12 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && cmake . -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install VERBOSE=1
+    mkdir '$(1)/build'
+    cd '$(1)/build' && cmake .. \
+        -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+        -DBUILD_SHARED_LIBS=$(if $(BUILD_SHARED),YES,NO) \
+        -DARMA_USE_WRAPPER=false
+    $(MAKE) -C '$(1)/build' -j '$(JOBS)' install VERBOSE=1
 
     # note: don't use -Werror with GCC 4.7.0 and .1
     '$(TARGET)-g++' \
