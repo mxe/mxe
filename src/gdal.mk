mxe/mxe

commit 0e39fbcb46b13d0822a5162186416611338de4f4

gdal: Remove remnants of i686-pc-mingw32
Signed-off-by: Timothy Gu <timothygu99@gmail.com>

src/gdal.mk
@@ -87,14 +87,6 @@ define $(PKG)_MAKE
     ln -sf '$(PREFIX)/$(TARGET)/bin/gdal-config' '$(PREFIX)/bin/$(TARGET)-gdal-config'
 endef
 
-define $(PKG)_BUILD
-    $($(PKG)_CONFIGURE)\
-        --with-hdf4='$(PREFIX)/$(TARGET)' \
-        --with-netcdf='$(PREFIX)/$(TARGET)' \
-        LIBS="-ljpeg -lsecur32 -lportablexdr `'$(TARGET)-pkg-config' --libs openssl libtiff-4`"
-    $($(PKG)_MAKE)
-endef
-
 define $(PKG)_BUILD_x86_64-w64-mingw32
     $($(PKG)_CONFIGURE) \
         LIBS="-ljpeg -lsecur32 `'$(TARGET)-pkg-config' --libs openssl libtiff-4`"
@@ -108,5 +100,4 @@ define $(PKG)_BUILD_i686-w64-mingw32
     $($(PKG)_MAKE)
 endef
 
-$(PKG)_BUILD_i686-w64-mingw32.shared =
-$(PKG)_BUILD_x86_64-w64-mingw32.shared =
+$(PKG)_BUILD_SHARED =

commit 7b7db80b32a6c29f670197a4573cbcd8714fe63e

gdal: Avoid $(PKG)_BUILD_SHARED
Regression from 0e39fbcb46b13d0822a5162186416611338de4f4.

Fixes #535.

Signed-off-by: Timothy Gu <timothygu99@gmail.com>

src/gdal.mk
@@ -100,4 +100,7 @@ define $(PKG)_BUILD_i686-w64-mingw32
     $($(PKG)_MAKE)
 endef
 
-$(PKG)_BUILD_SHARED =
+# Can't use $(PKG)_BUILD_SHARED here as $(PKG)_BUILD_i686-w64-mingw32 has a
+# higher precedence.
+$(PKG)_BUILD_i686-w64-mingw32.shared =
+$(PKG)_BUILD_x86_64-w64-mingw32.shared =