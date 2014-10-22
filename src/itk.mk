1df14d23720998f0594f3be36a54c096b18ee7aa
Rashad M <mohammedrashadkm@gmail.com>
2014-10-22 09:41:30 +0530

    upgrade itk to 4.6 and fixed PKG_UPDATE

--- src/itk.mk
+++ src/itk.mk
@@ -3,16 +3,17 @@
 
 PKG             := itk
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.4.1
-$(PKG)_CHECKSUM := 9272a15323e9c1d44f598fca02d2bb0c16478bb5
+$(PKG)_VERSION  := 4.6.0
+$(PKG)_CHECKSUM := 490d221bd7441ef1d151092a72082998c597308e
 $(PKG)_SUBDIR   := InsightToolkit-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.xz
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc hdf5
+$(PKG)_DEPS     := gcc hdf5 libpng tiff jpeg expat
 
 define $(PKG)_UPDATE
-    echo 'TODO: Updates for package ITK need to be written.' >&2;
-    echo $(itk_VERSION)
+    $(WGET) -q -O- 'https://sourceforge.net/projects/itk/files/itk/' | \
+    $(SED) -n 's,.*/\([0-9][^"]*\)/".*,\1,p' | \
+    head -1
 endef
 
 define $(PKG)_BUILD
@@ -20,13 +21,20 @@ define $(PKG)_BUILD
     cd '$(1).build' && cmake \
         -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
         -C '$(1)/TryRunResults.cmake'\
-        -DBUILD_SHARED_LIBS=FALSE \
-        -DCMAKE_VERBOSE_MAKEFILE=TRUE \
+	$(if $(BUILD_SHARED),\
+        -DBUILD_SHARED_LIBS=TRUE ) \
         -DBUILD_TESTING=FALSE \
         -DBUILD_EXAMPLES=FALSE \
         -DITK_USE_SYSTEM_HDF5=TRUE \
+        -DITK_USE_SYSTEM_TIFF=TRUE \
+        -DITK_USE_SYSTEM_PNG=TRUE \
+        -DITK_USE_SYSTEM_JPEG=TRUE \
+        -DITK_USE_SYSTEM_EXPAT=TRUE \
+	-DVCL_CHAR_IS_SIGNED=1 \
         '$(1)'
     $(MAKE) -C '$(1).build' -j '$(JOBS)' install VERBOSE=1
-endef
 
-$(PKG)_BUILD_SHARED =
+    # install
+    $(INSTALL) -m755 '$(1).build/bin/itkTestDriver.exe' '$(PREFIX)/$(TARGET)/bin/test-itk.exe'
+
+endef
