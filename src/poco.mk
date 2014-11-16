2cbc2a658e3374df05d617f8be96e97733c7a69c
Timothy Gu <timothygu99@gmail.com>
2014-11-16 09:19:53 -0800

    poco: Build as far as possible with parallel building
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/poco.mk
+++ src/poco.mk
@@ -25,7 +25,7 @@ define $(PKG)_BUILD
     $(if $(BUILD_STATIC), \
         $(SED) -i 's:// #define POCO_STATIC:#define POCO_STATIC:' \
             '$(1)/Foundation/include/Poco/Config.h')
-    $(MAKE) -C '$(1)' -j '$(JOBS)' CROSSENV=$(TARGET) || $(MAKE) -C '$(1)' -j 1 CROSSENV=$(TARGET)
+    $(MAKE) -C '$(1)' -j '$(JOBS)' -k CROSSENV=$(TARGET) || $(MAKE) -C '$(1)' -j 1 CROSSENV=$(TARGET)
     $(MAKE) -C '$(1)' -j 1 install CROSSENV=$(TARGET)
 
     '$(TARGET)-g++' \
