b83b3dbb29119aa47cbf4c48768c8c4557613ea2
Timothy Gu <timothygu99@gmail.com>
2014-11-16 09:21:04 -0800

    poco: Update
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- src/poco.mk
+++ src/poco.mk
@@ -3,8 +3,8 @@
 
 PKG             := poco
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.4.6p2
-$(PKG)_CHECKSUM := 90042349faf1790b5167bad0e84e1713bfd46046
+$(PKG)_VERSION  := 1.4.7
+$(PKG)_CHECKSUM := 7cd3122662afe31ea9eccab002aefd96c6de91c4
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := http://pocoproject.org/releases/$(PKG)-$(word 1,$(subst p, ,$($(PKG)_VERSION)))/$($(PKG)_FILE)
