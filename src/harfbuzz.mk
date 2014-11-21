579bddf71354f027f1d4a6fe6b3fd72945390718
Mark Brand <mabrand@mabrand.nl>
2014-11-21 19:30:43 +0100

    harfbuzz: update

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -3,8 +3,8 @@
 
 PKG             := harfbuzz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.9.35
-$(PKG)_CHECKSUM := 6f4401af396069214be2ba15b884361ef540e501
+$(PKG)_VERSION  := 0.9.36
+$(PKG)_CHECKSUM := 237e869d9dafc30c37cb6ab9886a616ffee6d71e
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.freedesktop.org/software/$(PKG)/release/$($(PKG)_FILE)
