mxe/mxe

commit 839eaf513b0068589a531e9a7a5525dc206860bd

qtbase: build sql drivers as plugins
Since Qt 5.3.0, static plugin linking is taken care of automaticallly
by qmake, which removes the reason for building the sql drivers
into QtSql instead of as plugins. For shared builds, plugins is the
sensible choice too.

src/qtbase.mk
@@ -39,10 +39,10 @@ define $(PKG)_BUILD
             -nomake examples \
             -nomake tests \
             -no-sql-mysql \
-            -qt-sql-sqlite \
-            -qt-sql-odbc \
-            -qt-sql-psql \
-            -qt-sql-tds -D Q_USE_SYBASE \
+            -plugin-sql-sqlite \
+            -plugin-sql-odbc \
+            -plugin-sql-psql \
+            -plugin-sql-tds -D Q_USE_SYBASE \
             -system-zlib \
             -system-libpng \
             -system-libjpeg \
@@ -86,5 +86,4 @@ endef
 
 
 $(PKG)_BUILD_SHARED = $(subst -static ,-shared ,\
-                      $(subst -qt-sql-,-plugin-sql-,\
                       $($(PKG)_BUILD)))

commit 61cebef64520a443342fc79254da7b04e39821b8

qtbase: fix shared build
Typo in 839eaf513b0068589a531e9a7a5525dc206860bd broke it.

src/qtbase.mk
@@ -86,4 +86,4 @@ endef
 
 
 $(PKG)_BUILD_SHARED = $(subst -static ,-shared ,\
-                      $($(PKG)_BUILD)))
+                      $($(PKG)_BUILD))