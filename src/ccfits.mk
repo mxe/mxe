db8326927a79bd9807d533aee45199acdcd34529
Stefano Balzan <stefano.balzan@gmail.com>
2014-12-10 20:34:35 +0100

    Added CCfits library
    TODO: add an example program and updade instructions in ccfits.mk

---
+++ src/ccfits.mk
@@ -0,0 +1,27 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := ccfits
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.4
+$(PKG)_CHECKSUM := 3de2a6379bc1024300befae95cfdf33645a7b64a
+$(PKG)_SUBDIR   := CCfits
+$(PKG)_FILE     := CCfits-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://heasarc.gsfc.nasa.gov/fitsio/CCfits/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc cfitsio
+
+define $(PKG)_UPDATE
+    echo 'TODO: write update script for $(PKG).' >&2;
+    echo $($(PKG)_VERSION)
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        $(MXE_CONFIGURE_OPTS) \
+        --with-cfitsio='$(PREFIX)/$(TARGET)'
+
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+endef
+
+$(PKG)_BUILD_SHARED =
