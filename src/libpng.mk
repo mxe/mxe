d588c1d5a688914828ef961fd3bbd77c0fc6805c
Alois Schlögl <alois.schloegl@ist.ac.at>
2015-01-06 23:16:00 +0100

    pkg libpng: upgrade to version 1.6.16

--- src/libpng.mk
+++ src/libpng.mk
@@ -3,11 +3,11 @@
 
 PKG             := libpng
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.6.12
-$(PKG)_CHECKSUM := c3d54f9ab683d63218361487269380fb8e0cf3b6
+$(PKG)_VERSION  := 1.6.16
+$(PKG)_CHECKSUM := 31855a8438ae795d249574b0da15b34eb0922e13
 $(PKG)_SUBDIR   := libpng-$($(PKG)_VERSION)
 $(PKG)_FILE     := libpng-$($(PKG)_VERSION).tar.xz
-$(PKG)_URL      := http://downloads.sourceforge.net/project/libpng/libpng16/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/libpng/libpng16/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_URL_2    := ftp://ftp.simplesystems.org/pub/$(PKG)/png/src/libpng16/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc zlib
 
