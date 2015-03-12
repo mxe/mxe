c761464b6c814b9246455a09a5acb1eb90617cbb
0xACE <0xACE@users.noreply.github.com>
2015-03-12 22:39:08 +0100

    Fixed errors in mman-win32.mk
    This has been tested and works fine now.

--- src/mman-win32.mk
+++ src/mman-win32.mk
@@ -4,8 +4,8 @@
 PKG             := mman-win32
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 378ed6b69bb7220511dd9cd0973c22b3f6773ce7
-$(PKG)_CHECKSUM := f2e54393f530b35d1736fa98a5a29d7d3a0ce76b
-$(PKG)_SUBDIR   := mman-win32-master
+$(PKG)_CHECKSUM := 489969397e78c99e0ca89652e3c6f3981d676b79
+$(PKG)_SUBDIR   := mman-win32-$($(PKG)_VERSION)
 $(PKG)_FILE     := mman-win32-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/witwall/mman-win32/archive/$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := gcc
