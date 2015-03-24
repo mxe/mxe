e87c874f729aee7e88eb9c613812d73f31097471
Alois Schlögl <alois.schloegl@ist.ac.at>
2015-03-24 20:27:59 +0100

    pkg stimfit: upgrade to 0.14.7.windows-1

--- src/stimfit.mk
+++ src/stimfit.mk
@@ -3,8 +3,8 @@
 
 PKG             := stimfit
 $(PKG)_IGNORE   := 
-$(PKG)_VERSION  := 0.14.5windows
-$(PKG)_CHECKSUM := b4484ff4bde473b89f74b55114fa2bec3accf95a
+$(PKG)_VERSION  := 0.14.7windows-1
+$(PKG)_CHECKSUM := ba67b2d0e31d29b4ecd0272ca86deae66f49242f
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := v$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/neurodroid/$(PKG)/archive/$($(PKG)_FILE)
@@ -18,18 +18,10 @@ endef
 
 define $(PKG)_BUILD
 
-    -WXCONF='$(PREFIX)/bin/$(TARGET)-wx-config' $(MAKE) -C '$(1)' -f Makefile.static clean
-
     cd '$(1)' && ./autogen.sh && ./configure --enable-python --with-biosig --with-pslope
     WXCONF='$(PREFIX)/bin/$(TARGET)-wx-config' $(MAKE) -C '$(1)' -f Makefile.static -j '$(JOBS)'
 
     $(INSTALL) '$(1)/stimfit.exe' '$(PREFIX)/$(TARGET)/bin/'
-
-    -$(INSTALL) '$(1)/stimfit.exe' /fs3/group/jonasgrp/Software/Stimfit/stimfit.$(TARGET).$(shell date +%Y%m%d).exe
-    -(cd /fs3/group/jonasgrp/Software/Stimfit && ln -sf stimfit.$(TARGET).$(shell date +%Y%m%d).exe stimfit.$(TARGET).LATEST.exe)
-
-    #-cd /fs3/group/jonasgrp/Software/Stimfit && ln -sf stimfit.$(TARGET).$(shell date +%Y%m%d).exe stimfit.$(shell date +%Y%m%d).exe
-
 
 endef
 
