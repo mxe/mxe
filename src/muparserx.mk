f3f7cd641885acc8ef1a43ed2d8a9dfd7aeca351
Rashad <mohammedrashadkm@gmail.com>
2015-04-27 19:54:45 +0200

    muparserx: update to 3.0.5

--- src/muparserx.mk
+++ src/muparserx.mk
@@ -3,11 +3,11 @@
 
 PKG             := muparserx
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := v3_0_3
-$(PKG)_CHECKSUM := d476899a024b6b720591484f615464dc1eb25b23
-$(PKG)_SUBDIR   :=
-$(PKG)_FILE     := $(PKG)_v3_0_2.zip
-$(PKG)_URL      := http://muparserx.googlecode.com/svn/archives/$($(PKG)_FILE)
+$(PKG)_VERSION  := 3.0.5
+$(PKG)_CHECKSUM := d8f646aac4099973b70ae84e1c7cb411abb6fe0b
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
+$(PKG)_URL      := http://sourceforge.net/projects/mxedeps/files/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
@@ -15,11 +15,14 @@ echo "TODO: write update for muparserx"
 endef
 
 define $(PKG)_BUILD
-    mkdir '$(1)$(PKG)_$($(PKG)_VERSION).build'
-    cd '$(1)$(PKG)_$($(PKG)_VERSION).build' && cmake \
-        -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
-        -DBUILD_SHARED_LIBS=$(if $(BUILD_STATIC),FALSE,TRUE) \
-    -DCMAKE_CXX_FLAGS='-std=c++11' \
-        '$(1)$(PKG)_$($(PKG)_VERSION)'
-    $(MAKE) -C '$(1)$(PKG)_$($(PKG)_VERSION).build' install
+    mkdir '$(1).build'
+    cd '$(1).build' && cmake \
+	-DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+	-DBUILD_SHARED_LIBS=$(if $(BUILD_STATIC),FALSE,TRUE) \
+	-DCMAKE_CXX_FLAGS='-std=c++11' \
+        '$(1)'	
+	$(MAKE) -C '$(1).build' install
+
+    rm -f $(PREFIX)/$(TARGET)/include/mpCompat.h
+
 endef
