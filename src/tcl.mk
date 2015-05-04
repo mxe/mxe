dbe181a01dc796a9ca19b6c0611a2130d86a4f4b
Russell Standish <hpcoder@hpcoders.com.au>
2015-05-04 16:31:03 +1000

    disable TCL's dependency on sqlite

--- src/tcl.mk
+++ src/tcl.mk
@@ -8,7 +8,8 @@ $(PKG)_CHECKSUM := 5c83d44152cc0496cc0847a2495f659502a30e40
 $(PKG)_SUBDIR   := tcl$($(PKG)_VERSION)
 $(PKG)_FILE     := tcl$($(PKG)_VERSION)-src.tar.gz
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/tcl/Tcl/$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc sqlite
+#$(PKG)_DEPS     := gcc sqlite
+$(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://sourceforge.net/projects/tcl/files/Tcl/' | \
@@ -19,7 +20,7 @@ endef
 define $(PKG)_BUILD
     cd '$(1)/win' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
-        --enable-threads \
+        --enable-threads --without-sqlite\
         $(if $(findstring 64,$(TARGET)), --enable-64bit) \
         CFLAGS='-D__MINGW_EXCPT_DEFINE_PSDK'
     $(MAKE) -C '$(1)/win' install install-private-headers $(MXE_DISABLE_PROGRAMS)
@@ -27,4 +28,4 @@ endef
 
 # tcl doesn't compile on i686-pc-mingw32. See
 # https://github.com/mxe/mxe/issues/508
-$(PKG)_BUILD_i686-pc-mingw32 :=
+#$(PKG)_BUILD_i686-pc-mingw32 :=
