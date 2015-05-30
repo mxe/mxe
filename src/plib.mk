794ade764ef3809bbd2e366f793e96b776689979
Hamza Alloush <hamzaalloush@live.com>
2015-05-30 15:02:20 +0300

    plib: add $(MXE_CONFIGURE_OPTS) to allow for shared support

--- src/plib.mk
+++ src/plib.mk
@@ -19,9 +19,7 @@ endef
 
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
-        --host='$(TARGET)' \
-        --disable-shared \
-        --prefix='$(PREFIX)/$(TARGET)' \
+	$(MXE_CONFIGURE_OPTS) \
         PKG_CONFIG='$(TARGET)-pkg-config'
     $(MAKE) -C '$(1)' -j '$(JOBS)' install \
         bin_PROGRAMS= \
