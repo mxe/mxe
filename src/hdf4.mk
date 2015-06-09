f37f99010ff024397b6a008f5ad05f1f5bba19bd
Rashad <mohammedrashadkm@gmail.com>
2015-06-09 03:39:44 +0200

    make cleanup-style

--- src/hdf4.mk
+++ src/hdf4.mk
@@ -25,17 +25,17 @@ define $(PKG)_BUILD
         --disable-fortran \
         --disable-netcdf \
         AR='$(TARGET)-ar' \
-	    $(if $(BUILD_SHARED), \
-	        LIBS="-lportablexdr -lws2_32" CPPFLAGS="-DH4_F77_FUNC\(name,NAME\)=NAME -DH4_BUILT_AS_DYNAMIC_LIB=1 -DBIG_LONGS", \
-	        CPPFLAGS="-DH4_F77_FUNC\(name,NAME\)=NAME -DH4_BUILT_AS_STATIC_LIB=1")
+        $(if $(BUILD_SHARED), \
+            LIBS="-lportablexdr -lws2_32" CPPFLAGS="-DH4_F77_FUNC\(name,NAME\)=NAME -DH4_BUILT_AS_DYNAMIC_LIB=1 -DBIG_LONGS", \
+            CPPFLAGS="-DH4_F77_FUNC\(name,NAME\)=NAME -DH4_BUILT_AS_STATIC_LIB=1")
     $(MAKE) -C '$(1)'/mfhdf/xdr -j '$(JOBS)' \
-	    LDFLAGS=-no-undefined
+        LDFLAGS=-no-undefined
 
     $(MAKE) -C '$(1)'/hdf/src -j '$(JOBS)' \
-	    LDFLAGS=-no-undefined
+        LDFLAGS=-no-undefined
     $(MAKE) -C '$(1)'/hdf/src -j 1 install
 
     $(MAKE) -C '$(1)'/mfhdf/libsrc -j '$(JOBS)' \
-	    LDFLAGS="-no-undefined -ldf"
+        LDFLAGS="-no-undefined -ldf"
     $(MAKE) -C '$(1)'/mfhdf/libsrc -j 1 install
 endef
