3d9c254ac15903cb42b279b5bf870a74955899f8
Timothy Gu <timothygu99@gmail.com>
2015-07-27 08:00:53 +0800

    libsvm: Fix indentation

--- src/libsvm.mk
+++ src/libsvm.mk
@@ -19,9 +19,9 @@ endef
 define $(PKG)_BUILD
     mkdir '$(1).build'
     cd '$(1).build' && cmake \
-    -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
-    -DBUILD_SHARED_LIBS=$(if $(BUILD_STATIC),FALSE,TRUE) \
-    '$(1)'
+        -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+        -DBUILD_SHARED_LIBS=$(if $(BUILD_STATIC),FALSE,TRUE) \
+        '$(1)'
 
     $(MAKE) -C '$(1).build' install
 
