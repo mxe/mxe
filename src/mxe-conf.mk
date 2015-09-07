93eaa16faeac34042813fbbdc254594f6fadf126
Tony Theodore <tonyt@logyst.com>
2015-09-08 00:18:03 +1000

    mxe-conf: create prefixed cmake wrapper script

--- src/mxe-conf.mk
+++ src/mxe-conf.mk
@@ -44,6 +44,12 @@ define $(PKG)_BUILD_COMMON
      echo '    include($${mxe_cmake_file})'; \
      echo 'endforeach()'; \
      ) > '$(CMAKE_TOOLCHAIN_FILE)'
+
+    #create prefixed cmake wrapper script
+    (echo '#!/bin/sh'; \
+     echo 'exec cmake -DCMAKE_TOOLCHAIN_FILE="$(CMAKE_TOOLCHAIN_FILE)" "$$@"') \
+             > '$(PREFIX)/bin/$(TARGET)-cmake'
+    chmod 0755 '$(PREFIX)/bin/$(TARGET)-cmake'
 endef
 
 define $(PKG)_BUILD
