31b19374edd997e7d9e4455a3f11dc89841b8ea2
Mark Brand <mabrand@mabrand.nl>
2015-09-08 09:50:01 +0200

    libgcrypt: update

--- src/libgcrypt.mk
+++ src/libgcrypt.mk
@@ -3,8 +3,8 @@
 
 PKG             := libgcrypt
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.6.3
-$(PKG)_CHECKSUM := 9456e7b64db9df8360a1407a38c8c958da80bbf1
+$(PKG)_VERSION  := 1.6.4
+$(PKG)_CHECKSUM := ed52add1ce635deeb2f5c6650e52667debd4ec70
 $(PKG)_SUBDIR   := libgcrypt-$($(PKG)_VERSION)
 $(PKG)_FILE     := libgcrypt-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://mirrors.dotsrc.org/gcrypt/libgcrypt/$($(PKG)_FILE)
