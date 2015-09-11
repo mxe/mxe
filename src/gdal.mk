8afc49f77d2e483a3b23642a4625afa4fde54fef
Tony Theodore <tonyt@logyst.com>
2015-09-11 14:41:09 +1000

    gdal:unify build rule - all deps now x86_64 enabled

--- src/gdal.mk
+++ src/gdal.mk
@@ -19,7 +19,7 @@ define $(PKG)_UPDATE
     head -1
 endef
 
-define $(PKG)_CONFIGURE
+define $(PKG)_BUILD
     cd '$(1)' && autoreconf -fi
     # The option '--without-threads' means native win32 threading without pthread.
     cd '$(1)' && ./configure \
@@ -73,10 +73,9 @@ define $(PKG)_CONFIGURE
         --without-perl \
         --without-php \
         --without-ruby \
-        --without-python
-endef
+        --without-python \
+        LIBS="-ljpeg -lsecur32 -lportablexdr `'$(TARGET)-pkg-config' --libs openssl libtiff-4`"
 
-define $(PKG)_MAKE
     $(MAKE) -C '$(1)'       -j '$(JOBS)' lib-target
     $(MAKE) -C '$(1)'       -j '$(JOBS)' install-lib
     $(MAKE) -C '$(1)/port'  -j '$(JOBS)' install
@@ -86,17 +85,4 @@ define $(PKG)_MAKE
     $(MAKE) -C '$(1)/ogr'   -j '$(JOBS)' install OGR_ENABLED=
     $(MAKE) -C '$(1)/apps'  -j '$(JOBS)' install
     ln -sf '$(PREFIX)/$(TARGET)/bin/gdal-config' '$(PREFIX)/bin/$(TARGET)-gdal-config'
-endef
-
-define $(PKG)_BUILD_x86_64-w64-mingw32
-    $($(PKG)_CONFIGURE) \
-        LIBS="-ljpeg -lsecur32 `'$(TARGET)-pkg-config' --libs openssl libtiff-4`"
-    $($(PKG)_MAKE)
-endef
-
-define $(PKG)_BUILD_i686-w64-mingw32
-    $($(PKG)_CONFIGURE) \
-        --with-netcdf='$(PREFIX)/$(TARGET)' \
-        LIBS="-ljpeg -lsecur32 -lportablexdr `'$(TARGET)-pkg-config' --libs openssl libtiff-4`"
-    $($(PKG)_MAKE)
 endef
