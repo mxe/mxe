cdaeec0c6d9f10689016342b48284ac19efd479e
Tony Theodore <tonyt@logyst.com>
2015-10-06 18:06:45 +1100

    qt-test.cmake: 2.8 series compatibility
    closes #906

--- src/qt-test.cmake
+++ src/qt-test.cmake
@@ -7,15 +7,18 @@ set(TGT test-${PKG}-cmake)
 
 enable_language(CXX)
 
-set(CMAKE_INCLUDE_CURRENT_DIR ON)
-set(CMAKE_AUTOMOC ON)
-set(CMAKE_AUTORCC ON)
-set(CMAKE_AUTOUIC ON)
-
 find_package(Qt4 ${PKG_VERSION} EXACT REQUIRED)
 include(${QT_USE_FILE})
 
-add_executable(${TGT} WIN32 ${CMAKE_CURRENT_LIST_DIR}/${PKG}-test.cpp)
+include_directories("${PROJECT_BINARY_DIR}")
+
+file(GLOB sources "${CMAKE_CURRENT_LIST_DIR}/qt-test.cpp")
+
+QT4_WRAP_UI(UISrcs "${CMAKE_CURRENT_LIST_DIR}/qt-test.ui")
+QT4_WRAP_CPP(MOCrcs "${CMAKE_CURRENT_LIST_DIR}/qt-test.hpp")
+QT4_ADD_RESOURCES(RCSrcs "${CMAKE_CURRENT_LIST_DIR}/qt-test.qrc")
+
+add_executable(${TGT} WIN32 ${sources} ${UISrcs} ${MOCrcs} ${RCSrcs})
 target_link_libraries(${TGT} ${QT_LIBRARIES})
 
 install(TARGETS ${TGT} DESTINATION bin)
