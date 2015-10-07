f1dd70802a4e20c0f8c849aa44f24651895c7cfc
Tony Theodore <tonyt@logyst.com>
2015-10-08 09:18:47 +1100

    boost pthreads qt sdl: disable cmake tests
    see #909

--- src/sdl.mk
+++ src/sdl.mk
@@ -35,10 +35,11 @@ define $(PKG)_BUILD
         `'$(TARGET)-pkg-config' sdl --cflags --libs`
 
     # test cmake
+    $(and $(ENABLE_CMAKE_TESTS),
     mkdir '$(1).test-cmake'
     cd '$(1).test-cmake' && '$(TARGET)-cmake' \
         -DPKG=$(PKG) \
         -DPKG_VERSION=$($(PKG)_VERSION) \
         '$(PWD)/src/cmake/test'
-    $(MAKE) -C '$(1).test-cmake' -j 1 install
+    $(MAKE) -C '$(1).test-cmake' -j 1 install)
 endef
