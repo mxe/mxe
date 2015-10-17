106222714e4d3ba0928618dd35c8bc883b556d3f
Mark Brand <mabrand@mabrand.nl>
2015-10-17 14:37:08 +0200

    sqlite: update

--- src/sqlite.mk
+++ src/sqlite.mk
@@ -3,8 +3,8 @@
 
 PKG             := sqlite
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3090000
-$(PKG)_CHECKSUM := a324143f4cc35cd7e9605a0a8dec9f9e4861d0be8305f3642e7d05008b77e60d
+$(PKG)_VERSION  := 3090100
+$(PKG)_CHECKSUM := d89d0e1d93deafebd0aa067d20d9029d9914ccde244920d9dd09ea51944e1c6c
 $(PKG)_SUBDIR   := $(PKG)-autoconf-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-autoconf-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.sqlite.org/2015/$($(PKG)_FILE)
