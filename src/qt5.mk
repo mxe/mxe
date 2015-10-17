069b03d5112f12afb513e70e68d0bbf13e87bf34
Tony Theodore <tonyt@logyst.com>
2015-10-17 13:46:20 +1100

    qt5: remove download related variables
    in rare cases, it would try downloading qtbase again

--- src/qt5.mk
+++ src/qt5.mk
@@ -2,17 +2,9 @@
 # See index.html for further information.
 
 PKG             := qt5
-$(PKG)_IGNORE    = $(qtbase_IGNORE)
 $(PKG)_VERSION   = $(qtbase_VERSION)
-$(PKG)_CHECKSUM  = $(qtbase_CHECKSUM)
-$(PKG)_SUBDIR    = $(qtbase_SUBDIR)
-$(PKG)_FILE      = $(qtbase_FILE)
-$(PKG)_URL       = $(qtbase_URL)
+$(PKG)_UPDATE    = echo $(qtbase_VERSION)
 $(PKG)_DEPS     := $(patsubst $(TOP_DIR)/src/%.mk,%,\
                         $(shell grep -l 'DEPS.*qtbase' \
                                 $(TOP_DIR)/src/qt*.mk \
                                 --exclude '$(TOP_DIR)/src/qt5.mk'))
-
-define $(PKG)_UPDATE
-    echo $(qtbase_VERSION)
-endef
