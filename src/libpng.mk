9b90e8a6f95fb7c8486bcaace84688d65184f02a
Mark Brand <mabrand@mabrand.nl>
2015-11-14 14:54:12 +0100

    libpng: update

--- src/libpng.mk
+++ src/libpng.mk
@@ -3,8 +3,8 @@
 
 PKG             := libpng
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.6.18
-$(PKG)_CHECKSUM := 2e10c13b7949883ac961db6177c516d778184432d440317e9f0391305c360963
+$(PKG)_VERSION  := 1.6.19
+$(PKG)_CHECKSUM := 311c5657f53516986c67713c946f616483e3cdb52b8b2ee26711be74e8ac35e8
 $(PKG)_SUBDIR   := libpng-$($(PKG)_VERSION)
 $(PKG)_FILE     := libpng-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/libpng/libpng16/$($(PKG)_VERSION)/$($(PKG)_FILE)
