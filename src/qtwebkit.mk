19d0acd474e39fbc2c2f7f136f9abaafdfcdc337
Tony Theodore <tonyt@logyst.com>
2015-11-26 22:27:06 +1100

    qtwebkit: re-enable build

--- src/qtwebkit.mk
+++ src/qtwebkit.mk
@@ -10,17 +10,11 @@ $(PKG)_FILE      = $(subst qtbase,qtwebkit,$(qtbase_FILE))
 $(PKG)_URL       = $(subst qtbase,qtwebkit,$(qtbase_URL))
 $(PKG)_DEPS     := gcc qtbase qtmultimedia qtquick1 sqlite
 
-$(PKG)_MESSAGE  :=*** qtwebkit is deprecated in Qt 5.5 ***
-
 define $(PKG)_UPDATE
     echo $(qtbase_VERSION)
 endef
 
 define $(PKG)_BUILD_SHARED
-    echo qtwebkit is deprecated in Qt 5.5
-endef
-
-define $(PKG)_BUILD_SHARED_DISABLED
     # looks for build tools with .exe suffix and tries to use win_flex
     $(SED) -i 's,\.exe,,' '$(1)/Tools/qmake/mkspecs/features/functions.prf'
     # invoke qmake with removed debug options as a workaround for
