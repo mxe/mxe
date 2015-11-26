188a7757d9f546dffc5ae90de07d73a9f01a5438
Tony Theodore <tonyt@logyst.com>
2015-11-26 14:28:00 +1100

    coin: fix update and detection of -ldl

--- src/coin.mk
+++ src/coin.mk
@@ -8,13 +8,13 @@ $(PKG)_CHECKSUM := 583478c581317862aa03a19f14c527c3888478a06284b9a46a0155fa5886d
 $(PKG)_SUBDIR   := Coin-$($(PKG)_VERSION)
 $(PKG)_FILE     := Coin-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://bitbucket.org/Coin3D/coin/downloads/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc
+$(PKG)_DEPS     := gcc dlfcn-win32
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://bitbucket.org/Coin3D/coin/downloads' | \
     $(SED) -n 's,.*Coin-\([0-9.]*\).tar.gz.*,\1,p' | \
     $(SORT) -V | \
-    $(TAIL) -n1
+    tail -1
 endef
 
 define $(PKG)_BUILD
@@ -27,7 +27,7 @@ define $(PKG)_BUILD
         COIN_STATIC=$(if $(BUILD_STATIC),true,false)
 
     # libtool misses some dependency libs and there's no lt_cv* etc. options
-    $(SED) -i 's,^postdeps="-,postdeps="-lopengl32 -lgdi32 -lwinmm -,g' '$(1)/libtool'
+    $(SED) -i 's,^postdeps="-,postdeps="-ldl -lopengl32 -lgdi32 -lwinmm -,g' '$(1)/libtool'
 
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     $(MAKE) -C '$(1)' -j 1 install
