702b214c8316c347fb2ee75e07d36af70a7e0c4e
Tony Theodore <tonyt@logyst.com>
2015-11-29 01:56:57 +1100

    hdf5: fix for static cmake test

--- src/hdf5-test.cmake
+++ src/hdf5-test.cmake
@@ -8,7 +8,7 @@ set(TGT test-${PKG}-cmake)
 enable_language(CXX)
 add_executable(${TGT} ${CMAKE_CURRENT_LIST_DIR}/${PKG}-test.cpp)
 
-find_package(HDF5 ${PKG_VERSION} EXACT REQUIRED)
+find_package(HDF5 ${PKG_VERSION} EXACT REQUIRED COMPONENTS C HL)
 target_link_libraries(${TGT} ${HDF5_LIBRARIES})
 
 install(TARGETS ${TGT} DESTINATION bin)
