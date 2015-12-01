068e20243d2b531c032f04fcff07bd613594d9ce
Tony Theodore <tonyt@logyst.com>
2015-12-02 04:03:59 +1100

    libxml2: add missing zlib dependency

--- src/libxml2.mk
+++ src/libxml2.mk
@@ -9,7 +9,7 @@ $(PKG)_SUBDIR   := libxml2-$($(PKG)_VERSION)
 $(PKG)_FILE     := libxml2-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://xmlsoft.org/sources/$($(PKG)_FILE)
 $(PKG)_URL_2    := ftp://xmlsoft.org/libxml2/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc xz
+$(PKG)_DEPS     := gcc xz zlib
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://git.gnome.org/browse/libxml2/refs/tags' | \
