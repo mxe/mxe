cca04023d126a33f344cd64d5d377b43da5c750f
Tony Theodore <tonyt@logyst.com>
2015-12-02 13:09:23 +1100

    opus: update 1.1 --> 1.1.1

--- src/opus.mk
+++ src/opus.mk
@@ -3,8 +3,8 @@
 
 PKG             := opus
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.1
-$(PKG)_CHECKSUM := b9727015a58affcf3db527322bf8c4d2fcf39f5f6b8f15dbceca20206cbe1d95
+$(PKG)_VERSION  := 1.1.1
+$(PKG)_CHECKSUM := 9b84ff56bd7720d5554103c557664efac2b8b18acc4bbcc234cb881ab9a3371e
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://downloads.xiph.org/releases/$(PKG)/$($(PKG)_FILE)
