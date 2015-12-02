b7e8885624c45fc55abdf78b00ad39288c6754c9
Mark Brand <mabrand@mabrand.nl>
2015-12-02 11:35:11 +0100

    dbus: update

--- src/dbus.mk
+++ src/dbus.mk
@@ -3,8 +3,8 @@
 
 PKG             := dbus
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.10.4
-$(PKG)_CHECKSUM := ad7dcad73ad9b0ff55819985d354eacfffe07e2eb8c763e155efc21d6001084b
+$(PKG)_VERSION  := 1.10.6
+$(PKG)_CHECKSUM := b5fefa08a77edd76cd64d872db949eebc02cf6f3f8be82e4bbc641742af5d35f
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://$(PKG).freedesktop.org/releases/$(PKG)/$($(PKG)_FILE)
