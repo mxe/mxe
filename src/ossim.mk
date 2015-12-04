3dbae68a7494e8b35661c5feb593381ce472c371
Tony Theodore <tonyt@logyst.com>
2015-12-04 16:03:37 +1100

    ossim: fix SUBDIR and disable apps

--- src/ossim.mk
+++ src/ossim.mk
@@ -5,9 +5,9 @@ PKG             := ossim
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.8.20
 $(PKG)_CHECKSUM := a9148cbc7eebaed1d09d139e68c038592edcf74318ec2623f21494aa56879f52
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)-1
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION)-1.tar.gz
-$(PKG)_URL      := http://download.osgeo.org/ossim/source/$($(PKG)_SUBDIR)/$($(PKG)_FILE)
+$(PKG)_URL      := http://download.osgeo.org/ossim/source/$(PKG)-$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc freetype geos jpeg libpng openthreads proj tiff zlib
 
 define $(PKG)_UPDATE
@@ -24,6 +24,9 @@ define $(PKG)_BUILD
         -DPKG_CONFIG_EXECUTABLE='$(PREFIX)/bin/$(TARGET)-pkg-config' \
         -DCMAKE_MODULE_PATH='$(1)/ossim_package_support/cmake/CMakeModules' \
         -DBUILD_OSSIM_FREETYPE_SUPPORT=ON \
+        -DBUILD_OSSIM_APPS=OFF \
+        -DBUILD_OSSIM_CURL_APPS=OFF \
+        -DBUILD_OSSIM_TEST_APPS=OFF \
         -DCMAKE_CXX_FLAGS='-DGEOS_INLINE=1' \
         '$(1)/ossim'
 
