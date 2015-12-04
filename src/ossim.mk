a3269c05fead2e61cc305202a92491f2f00ee0e7
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-12-04 12:36:59 +0100

    ossim: fixes subdir and build

--- src/ossim.mk
+++ src/ossim.mk
@@ -6,7 +6,7 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.8.20
 $(PKG)_CHECKSUM := a9148cbc7eebaed1d09d139e68c038592edcf74318ec2623f21494aa56879f52
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)-1
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION)-1.tar.gz
+$(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := http://download.osgeo.org/ossim/source/$(PKG)-$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc freetype geos jpeg libpng openthreads proj tiff zlib
 
@@ -23,10 +23,9 @@ define $(PKG)_BUILD
         -DCMAKE_VERBOSE_MAKEFILE=TRUE \
         -DPKG_CONFIG_EXECUTABLE='$(PREFIX)/bin/$(TARGET)-pkg-config' \
         -DCMAKE_MODULE_PATH='$(1)/ossim_package_support/cmake/CMakeModules' \
-        -DBUILD_OSSIM_FREETYPE_SUPPORT=ON \
-        -DBUILD_OSSIM_APPS=OFF \
-        -DBUILD_OSSIM_CURL_APPS=OFF \
-        -DBUILD_OSSIM_TEST_APPS=OFF \
+        -DBUILD_OSSIM_FREETYPE_SUPPORT=TRUE \
+        -DBUILD_OSSIM_CURL_APPS=FALSE \
+        -DBUILD_OSSIM_TEST_APPS=FALSE \
         -DCMAKE_CXX_FLAGS='-DGEOS_INLINE=1' \
         '$(1)/ossim'
 
