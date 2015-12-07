6ebda0e8118f0195f3d4c6f5a63b158a9272f312
Tony Theodore <tonyt@logyst.com>
2015-12-08 10:33:18 +1100

    openssl: change second url
    primary http and ftp sites only keep the latest version so use secondary
    http `.../source/old` in case we haven't yet updated.
    
    fixes #1043

--- src/openssl.mk
+++ src/openssl.mk
@@ -8,7 +8,7 @@ $(PKG)_CHECKSUM := 671c36487785628a703374c652ad2cebea45fa920ae5681515df25d9f2c9a
 $(PKG)_SUBDIR   := openssl-$($(PKG)_VERSION)
 $(PKG)_FILE     := openssl-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.openssl.org/source/$($(PKG)_FILE)
-$(PKG)_URL_2    := ftp://ftp.openssl.org/source/$($(PKG)_FILE)
+$(PKG)_URL_2    := http://www.openssl.org/source/old/$(call tr,$([a-z]),,$($(PKG)_VERSION))/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc libgcrypt zlib
 
 define $(PKG)_UPDATE
