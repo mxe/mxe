cb32c57c3334a8b502edaaf12fc4e578620c3604
Pavel Vatagin <pavelvat@gmail.com>
2016-01-11 20:03:36 +0300

    fixes libechonest

--- src/libechonest.mk
+++ src/libechonest.mk
@@ -7,22 +7,32 @@ $(PKG)_VERSION  := 2.3.1
 $(PKG)_CHECKSUM := ab961ab952df30c5234b548031594d7e281e7c9f2a9d1ce91fe5421ddde85e7c
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/lfranchi/libechonest/archive/$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://github.com/lfranchi/$(PKG)/archive/$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := gcc qt qjson
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://github.com/lfranchi/libechonest/archive/' | \
-    $(SED) -n 's,.*/\([0-9][^"]*\)/"\.tar.*,\1,p' | \
-    sort | uniq | \
-    head -1
+    $(call MXE_GET_GITHUB_TAGS, lfranchi/libechonest)
 endef
 
 define $(PKG)_BUILD
-    mkdir '$(1)/build'
-    cd '$(1)/build' && cmake .. \
-        -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)'
+    mkdir '$(1).build'
+    cd '$(1).build' && '$(TARGET)-cmake' '$(1)'
+    $(MAKE) -C '$(1).build' -j '$(JOBS)'
+    $(MAKE) -C '$(1).build' -j 1 install
 
-    $(MAKE) -C '$(1)/build' -j '$(JOBS)' install
+    # create pkg-config file
+    $(INSTALL) -d '$(PREFIX)/$(TARGET)/lib/pkgconfig'
+    (echo 'Name: $(PKG)'; \
+     echo 'Version: $($(PKG)_VERSION)'; \
+     echo 'Description: $(PKG)'; \
+     echo 'Requires: QtCore QtNetwork'; \
+     echo 'Libs: -lechonest';) \
+     > '$(PREFIX)/$(TARGET)/lib/pkgconfig/$(PKG).pc'
+
+    '$(TARGET)-g++' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(2).cpp' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
+        `'$(TARGET)-pkg-config' libechonest --cflags --libs`
 endef
 
 $(PKG)_BUILD_STATIC =
