9d09e67443f318f54e0f93a4018e008743e4c994
Boris Nagaev <bnagaev@gmail.com>
2016-01-17 15:47:54 +0300

    qt5: add test that cmake works with QUiLoader
    See https://github.com/mxe/mxe/issues/1185

---
+++ src/qttools-test/mxe-cmake-qtuitools.cpp
@@ -0,0 +1,11 @@
+// This file is part of MXE.
+// See index.html for further information.
+
+// Source: https://github.com/mxe/mxe/issues/1185
+
+#include <QUiLoader>
+
+int main() {
+    QUiLoader l;
+    return 0;
+}
