3e172fdee73dd6598a9970d77d8163ad49864775
Alois Schlögl <alois.schloegl@ist.ac.at>
2016-01-18 06:54:16 +0100

    pkg stimfit: upgrade to v0.14.13, use of autoconf

--- src/stimfit.mk
+++ src/stimfit.mk
@@ -3,12 +3,12 @@
 
 PKG             := stimfit
 $(PKG)_IGNORE   := 
-$(PKG)_VERSION  := 0.14.7windows-1
-$(PKG)_CHECKSUM := ba67b2d0e31d29b4ecd0272ca86deae66f49242f
+$(PKG)_VERSION  := 0.14.13windows
+$(PKG)_CHECKSUM := 065d348165b743c09c5c2ddab9e199746061cda6ee5bb2a507b95af769f1b24f
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := v$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/neurodroid/$(PKG)/archive/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc biosig wxwidgets hdf5 boost fftw
+$(PKG)_DEPS     := gcc biosig wxwidgets hdf5 boost fftw libtool
 
 define $(PKG)_UPDATE
     wget -q -O- 'https://github.com/neurodroid/stimfit/releases' | \
@@ -18,10 +18,25 @@ endef
 
 define $(PKG)_BUILD
 
-    cd '$(1)' && ./autogen.sh && ./configure --enable-python --with-biosig --with-pslope
-    WXCONF='$(PREFIX)/bin/$(TARGET)-wx-config' $(MAKE) -C '$(1)' -f Makefile.static -j '$(JOBS)'
+    rm -rf '$(1)'
+    #cp -r ~/src/stimfit '$(1)'
+    rsync -av  ~/src/stimfit/ '$(1)/'
 
-    $(INSTALL) '$(1)/stimfit.exe' '$(PREFIX)/$(TARGET)/bin/'
+    cd '$(1)' && ./autogen.sh && \
+	CPPFLAGS="-std=gnu++11" \
+	./configure --disable-python --with-biosig --with-pslope \
+		--with-hdf5-prefix=$(PREFIX)/$(TARGET) \
+                --with-wx-config=$(PREFIX)/$(TARGET)/bin/wx-config \
+		--with-sysroot=$(PREFIX)/$(TARGET)/bin \
+		--host='$(TARGET)' \
+		--build="`config.guess`" \
+		--prefix='$(PREFIX)/$(TARGET)' \
+		--enable-static \
+		--disable-shared
+
+    WXCONF='$(PREFIX)/bin/$(TARGET)-wx-config' make -C '$(1)'
+
+    -$(INSTALL) '$(1)/stimfit.exe' '$(PREFIX)/$(TARGET)/bin/'
 
 endef
 
