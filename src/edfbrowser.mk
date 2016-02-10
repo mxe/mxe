5042e226d6685eab0ec25d5d3879dd0cd9fb8e34
Alois Schloegl <alois.schloegl@ist.ac.at>
2016-02-10 22:45:27 +0100

    pkg edfbrowser: upgrade to 1.57; fix compilation

--- src/edfbrowser.mk
+++ src/edfbrowser.mk
@@ -3,9 +3,9 @@
 
 PKG             := edfbrowser
 $(PKG)_IGNORE   := 
-$(PKG)_VERSION  := 1.56
-$(PKG)_CHECKSUM := 06b9360ceb1dcddd0b00370c23b3708a019f2b60872533e7776b49a5b4484d01
-$(PKG)_SUBDIR   := edfbrowser_156_source
+$(PKG)_VERSION  := 1.57
+$(PKG)_CHECKSUM := e84c6de28f294d5a02078080bf03c6c2f8548a4b8656c87850101933311e8b71
+$(PKG)_SUBDIR   := edfbrowser_157_source
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := http://www.teuniz.net/edfbrowser/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc qt
