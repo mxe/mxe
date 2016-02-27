b481c36d87755e8b503d28c4d8cb070f69981d57
Mark Brand <mabrand@mabrand.nl>
2016-02-27 22:39:20 +0100

    mingw-w64: update

--- src/mingw-w64.mk
+++ src/mingw-w64.mk
@@ -3,8 +3,8 @@
 
 PKG             := mingw-w64
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.0.4
-$(PKG)_CHECKSUM := 89356a0aa8cf9f8b9dc8d92bc8dd01a131d4750c3acb30c6350a406316c42199
+$(PKG)_VERSION  := 4.0.5
+$(PKG)_CHECKSUM := d4775e381202c5ecea7dbb21a1f247e4b3506509cb7d8b01bee6d83ee538e62c
 $(PKG)_SUBDIR   := $(PKG)-v$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-v$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$(PKG)-release/$($(PKG)_FILE)
