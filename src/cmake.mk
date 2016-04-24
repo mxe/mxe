96ae6b546f33ef9f01868900bda359aa678ba2d4
Boris Pek <tehnick-8@yandex.ru>
2016-04-24 15:08:41 +0300

    cmake: update to 3.5.2

--- src/cmake.mk
+++ src/cmake.mk
@@ -3,8 +3,8 @@
 
 PKG             := cmake
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.5.1
-$(PKG)_CHECKSUM := 93d651a754bcf6f0124669646391dd5774c0fc4d407c384e3ae76ef9a60477e8
+$(PKG)_VERSION  := 3.5.2
+$(PKG)_CHECKSUM := 92d8410d3d981bb881dfff2aed466da55a58d34c7390d50449aa59b32bb5e62a
 $(PKG)_SUBDIR   := cmake-$($(PKG)_VERSION)
 $(PKG)_FILE     := cmake-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.cmake.org/files/v$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
