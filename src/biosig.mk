d16e8def16aab024ce0c6502eafcca0683ebeb34
Alois Schlögl <alois.schloegl@ist.ac.at>
2016-04-28 01:58:59 +0200

    pkg biosig: split pkg into libbiosig and biosig(-tools)

--- src/biosig.mk
+++ src/biosig.mk
@@ -8,7 +8,7 @@ $(PKG)_CHECKSUM := b5574b5491964416853b21680c7259d511d5e17908e7d972f14ca6e5609b9
 $(PKG)_SUBDIR   := biosig4c++-$($(PKG)_VERSION)
 $(PKG)_FILE     := biosig4c++-$($(PKG)_VERSION).src.tar.gz
 $(PKG)_URL      := https://sourceforge.net/projects/biosig/files/BioSig%20for%20C_C%2B%2B/src/$($(PKG)_FILE)/download
-$(PKG)_DEPS     := gcc suitesparse zlib libiberty libiconv pthreads
+$(PKG)_DEPS     := gcc libbiosig
 
 define $(PKG)_UPDATE
 #    wget -q -O- 'http://biosig.sourceforge.net/download.html' | \
@@ -18,6 +18,12 @@ endef
 
 define $(PKG)_BUILD_PRE
 
+
+    rm -rf '$(1)'
+    #cp -r ~/src/stimfit '$(1)'
+    rsync -avL  ~/src/biosig-code/biosig4c++/* '$(1)/'
+
+
     # make sure NDEBUG is defined
     $(SED) -i '/NDEBUG/ s|#||g' '$(1)'/Makefile
 
@@ -26,69 +32,13 @@ define $(PKG)_BUILD_PRE
     #$(SED) -i 's| -lssp | |g' '$(1)'/Makefile
 
     TARGET='$(TARGET)' $(MAKE) -C '$(1)' clean
-    TARGET='$(TARGET)' $(MAKE) -C '$(1)' -j '$(JOBS)' io.h \
-		libbiosig.a libbiosig2.a libgdf.a  libphysicalunits.a \
-		libbiosig.def libbiosig2.def libgdf.def libphysicalunits.def \
-		libbiosig
+    TARGET='$(TARGET)' $(MAKE) -C '$(1)' -j '$(JOBS)' io.h tools
 
 endef
 
 define $(PKG)_BUILD_POST
 
-    $(INSTALL) -m644 '$(1)/biosig.h'             '$(PREFIX)/$(TARGET)/include/'
-    $(INSTALL) -m644 '$(1)/gdftime.h'            '$(PREFIX)/$(TARGET)/include/'
-    $(INSTALL) -m644 '$(1)/biosig-dev.h'         '$(PREFIX)/$(TARGET)/include/'
-    $(INSTALL) -m644 '$(1)/libbiosig.a'          '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -m644 '$(1)/libbiosig.def' 	 '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -m644 '$(1)/libbiosig.dll.a' 	 '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -m644 '$(1)/libbiosig.dll' 	 '$(PREFIX)/$(TARGET)/lib/'
-
-    $(INSTALL) -m644 '$(1)/libgdf.a'             '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -m644 '$(1)/libgdf.def' 		 '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -m644 '$(1)/libgdf.dll.a' 	 '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -m644 '$(1)/libgdf.dll'	 	 '$(PREFIX)/$(TARGET)/lib/'
-
-    $(INSTALL) -m644 '$(1)/biosig2.h'            '$(PREFIX)/$(TARGET)/include/'
-    $(INSTALL) -m644 '$(1)/libbiosig2.a'         '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -m644 '$(1)/libbiosig2.def' 	 '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -m644 '$(1)/libbiosig2.dll.a' 	 '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -m644 '$(1)/libbiosig2.dll'	 '$(PREFIX)/$(TARGET)/lib/'
-
-    $(INSTALL) -m644 '$(1)/physicalunits.h'      '$(PREFIX)/$(TARGET)/include/'
-    $(INSTALL) -m644 '$(1)/libphysicalunits.a'   '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -m644 '$(1)/libphysicalunits.def' '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -m644 '$(1)/libphysicalunits.dll.a' '$(PREFIX)/$(TARGET)/lib/'
-    $(INSTALL) -m644 '$(1)/libphysicalunits.dll' '$(PREFIX)/$(TARGET)/lib/'
-
-    $(INSTALL) -m644 '$(1)/libbiosig.pc'         '$(PREFIX)/$(TARGET)/lib/pkgconfig/'
-
-    ### make release file
-    rm -f $(PREFIX)/$($(PKG)_SUBDIR).$(TARGET).zip
-    cd $(PREFIX)/$(TARGET) && zip $(PREFIX)/$($(PKG)_SUBDIR).$(TARGET).zip \
-		include/biosig.h include/biosig-dev.h include/biosig2.h include/gdftime.h include/io.h \
-		lib/libbiosig.a lib/libbiosig.def lib/libbiosig.dll lib/libbiosig.dll.a \
-		lib/libbiosig2.a lib/libbiosig2.def lib/libbiosig2.dll lib/libbiosig2.dll.a \
-		lib/libgdf.a lib/libgdf.def lib/libgdf.dll lib/libgdf.dll.a \
-		lib/libz.a lib/libcholmod.a lib/liblapack.a lib/libiconv.a lib/libiberty.a  \
-		include/libiberty/*.h include/iconv.h \
-		include/physicalunits.h \
-		lib/libphysicalunits.a lib/libphysicalunits.def lib/libphysicalunits.dll lib/libphysicalunits.dll.a
-
-    mkdir -p $(PREFIX)/release/$(TARGET)/include/
-    cd $(PREFIX)/$(TARGET) && cp -r \
-		include/biosig.h include/biosig-dev.h include/biosig2.h include/gdftime.h include/io.h \
-		include/libiberty include/iconv.h \
-		include/physicalunits.h \
-		$(PREFIX)/release/$(TARGET)/include/
-
-    mkdir -p $(PREFIX)/release/$(TARGET)/lib/
-    cd $(PREFIX)/$(TARGET) && cp -r \
-		lib/libbiosig.a lib/libbiosig.def lib/libbiosig.dll lib/libbiosig.dll.a \
-		lib/libbiosig2.a lib/libbiosig2.def lib/libbiosig2.dll lib/libbiosig2.dll.a \
-		lib/libgdf.a lib/libgdf.def lib/libgdf.dll lib/libgdf.dll.a \
-		lib/libz.a lib/libcholmod.a lib/liblapack.a lib/libiconv.a lib/libiberty.a  \
-		lib/libphysicalunits.a lib/libphysicalunits.def lib/libphysicalunits.dll lib/libphysicalunits.dll.a \
-		$(PREFIX)/release/$(TARGET)/lib/
+    TARGET='$(TARGET)' $(MAKE) -C '$(1)' -j '$(JOBS)' install_tools
 
     mkdir -p $(PREFIX)/release/$(TARGET)/bin/
     -cp $(PREFIX)/$(TARGET)/bin/save2gdf.exe $(PREFIX)/release/$(TARGET)/bin/
@@ -97,13 +47,6 @@ define $(PKG)_BUILD_POST
     -cp $(1)/mex/mex* $(PREFIX)/release/matlab/
 
     cd '$(1)/win32' && zip $(PREFIX)/$($(PKG)_SUBDIR).$(TARGET).zip *.bat README
-
-    #exit -1
-    ### these cause problems when compiling stimfit
-    rm -rf '$(PREFIX)/$(TARGET)/lib/libphysicalunits.dll.a' \
-	'$(PREFIX)/$(TARGET)/lib/libbiosig.dll.a' \
-	'$(PREFIX)/$(TARGET)/lib/libbiosig2.dll.a' \
-	'$(PREFIX)/$(TARGET)/lib/libgdf.dll.a'
 
 endef
 
@@ -116,12 +59,13 @@ endef
 
 define $(PKG)_BUILD_i686-w64-mingw32
 	$($(PKG)_BUILD_PRE)
+	TARGET=$(TARGET) $(MAKE) -C '$(1)' mexw32 
 	$($(PKG)_BUILD_POST)
 endef
 
 define $(PKG)_BUILD_x86_64-w64-mingw32
 	$($(PKG)_BUILD_PRE)
-	#HOME=/home/as  TARGET=$(TARGET) $(MAKE) -C '$(1)' mexw64
+	TARGET=$(TARGET) $(MAKE) -C '$(1)' mexw64 
 	$($(PKG)_BUILD_POST)
 endef
 
