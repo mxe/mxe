cb06170667a0d9ce974f48ff798d25411ed808f7
Alois Schlögl <alois.schloegl@ist.ac.at>
2016-04-28 20:31:27 +0200

    pkg opencv3: add new package

---
+++ src/opencv3.mk
@@ -0,0 +1,64 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := opencv3
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 3.1.0
+$(PKG)_CHECKSUM := 1f6990249fdb82804fff40e96fa6d99949023ab0e3277eae4bd459b374e622a4
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := opencv-$($(PKG)_VERSION).zip
+$(PKG)_URL      := https://github.com/Itseez/opencv/archive/$($(PKG)_VERSION).zip
+$(PKG)_DEPS     := gcc eigen ffmpeg jasper jpeg lcms1 libpng openexr tiff xz zlib
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/' | \
+    $(SED) -n 's,.*/\([0-9][^"]*\)/".*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    # build
+    mkdir '$(1).build'
+    cd '$(1).build' && cmake \
+      -DWITH_QT=OFF \
+      -DWITH_OPENGL=ON \
+      -DWITH_GSTREAMER=OFF \
+      -DWITH_GTK=OFF \
+      -DWITH_VIDEOINPUT=ON \
+      -DWITH_XINE=OFF \
+      -DBUILD_SHARED_LIBS=$(if $(BUILD_STATIC),OFF,ON) \
+      -DBUILD_opencv_apps=OFF \
+      -DBUILD_DOCS=OFF \
+      -DBUILD_EXAMPLES=OFF \
+      -DBUILD_PACKAGE=OFF \
+      -DBUILD_PERF_TESTS=OFF \
+      -DBUILD_TESTS=OFF \
+      -DBUILD_WITH_DEBUG_INFO=OFF \
+      -DBUILD_FAT_JAVA_LIB=OFF \
+      -DBUILD_ZLIB=OFF \
+      -DBUILD_TIFF=OFF \
+      -DBUILD_JASPER=OFF \
+      -DBUILD_JPEG=OFF \
+      -DBUILD_PNG=OFF \
+      -DBUILD_OPENEXR=OFF \
+      -DCMAKE_VERBOSE=ON \
+      -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+      -DCMAKE_CXX_FLAGS='-D_WIN32_WINNT=0x0500' \
+      '$(1)'
+
+    # install
+    $(MAKE) -C '$(1).build' -j '$(JOBS)' install VERBOSE=1
+
+    # fixup and install pkg-config file
+    # openexr isn't available on x86_64-w64-mingw32
+    # opencv builds it's own libIlmImf.a
+    $(if $(findstring x86_64-w64-mingw32,$(TARGET)),\
+        $(SED) -i 's/OpenEXR//' '$(1).build/unix-install/opencv.pc')
+    $(SED) -i 's,share/OpenCV/3rdparty/,,g' '$(1).build/unix-install/opencv.pc'
+    $(INSTALL) -m755 '$(1).build/unix-install/opencv.pc' '$(PREFIX)/$(TARGET)/lib/pkgconfig'
+
+    '$(TARGET)-g++' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(1)/samples/c/fback_c.c' -o '$(PREFIX)/$(TARGET)/bin/test-opencv.exe' \
+        `'$(TARGET)-pkg-config' opencv --cflags --libs`
+endef
