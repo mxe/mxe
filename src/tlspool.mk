f95346840ef7f69d21517a91c0c62f7b13229d79
Henri Manson <hfmanson@gmail.com>
2016-06-06 16:22:14 +0200

    tlspool

---
+++ src/tlspool.mk
@@ -0,0 +1,28 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := tlspool
+$(PKG)_VERSION  := 0.4
+$(PKG)_CHECKSUM := fff4c133ce9c927dfc3e36f48437671c56f1f9a4c2769768c474a0973e412387
+$(PKG)_SUBDIR   := tlspool-$($(PKG)_VERSION)
+$(PKG)_FILE     := $($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://github.com/hfmanson/$(PKG)/archive/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc 
+
+define $(PKG)_UPDATE
+    echo 'TODO: write update script for $(PKG).' >&2;
+    echo $($(PKG)_VERSION)
+endef
+
+$(PKG)_MAKE_OPTS = \
+        PREFIX='$(PREFIX)/$(TARGET)' \
+        CROSS_SUFFIX='$(TARGET)-' \
+        CC='$(TARGET)-gcc' \
+		PKG_CONFIG='$(TARGET)-pkg-config' \
+        HOSTCC='$(BUILD_CC)' \
+        WINVER=0x0600 \
+		SBIN=bin
+
+define $(PKG)_BUILD
+    $(MAKE)  -C '$(1)/src' -j '$(JOBS)' install $($(PKG)_MAKE_OPTS)
+endef
