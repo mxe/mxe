d138f9cb28dbee4c53cb28ea53eab80c88a1e5ab
Henri Manson <hfmanson@gmail.com>
2016-06-12 11:46:14 +0200

    tlspool daemon and libtlspool library

--- src/tlspool.mk
+++ src/tlspool.mk
@@ -2,10 +2,10 @@
 # See index.html for further information.
 
 PKG             := tlspool
-$(PKG)_VERSION  := 0.4
-$(PKG)_CHECKSUM := fff4c133ce9c927dfc3e36f48437671c56f1f9a4c2769768c474a0973e412387
-$(PKG)_SUBDIR   := tlspool-$($(PKG)_VERSION)
-$(PKG)_FILE     := $($(PKG)_VERSION).tar.gz
+$(PKG)_VERSION  := 0.11
+$(PKG)_CHECKSUM := 8decc8c9c459c901fe718eecb38107a958d491c93c7673315eef2a17b4083813
+$(PKG)_SUBDIR   := $(PKG)-$(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/hfmanson/$(PKG)/archive/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc pthreads gnutls p11-kit libtasn1 db quick-der
 
@@ -19,10 +19,12 @@ $(PKG)_MAKE_OPTS = \
         CROSS_SUFFIX='$(TARGET)-' \
         CC='$(TARGET)-gcc' \
 		PKG_CONFIG='$(TARGET)-pkg-config' \
+		DLLTOOL='$(TARGET)-dlltool' \
         HOSTCC='$(BUILD_CC)' \
         WINVER=0x0600 \
 		SBIN=bin
 
 define $(PKG)_BUILD
     $(MAKE)  -C '$(1)/src' -j '$(JOBS)' install $($(PKG)_MAKE_OPTS)
+    $(MAKE)  -C '$(1)/lib' -j '$(JOBS)' install $($(PKG)_MAKE_OPTS)
 endef
