b3402ae06bdd9780ce4938578422d196963db2f1
Boris Nagaev <bnagaev@gmail.com>
2016-08-24 21:44:48 +0300

    hyperscan: use MXE ragel

--- src/hyperscan.mk
+++ src/hyperscan.mk
@@ -9,6 +9,7 @@ $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := https://github.com/01org/$(PKG)/archive/v$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := gcc boost
+# $(PKG)_NATIVE_DEPS := ragel
 
 define $(PKG)_UPDATE
     $(call MXE_GET_GITHUB_TAGS, 01org/hyperscan, v)
@@ -20,6 +21,7 @@ define $(PKG)_BUILD
     # -DCMAKE_C_FLAGS="-march=core2" -DCMAKE_CXX_FLAGS="-march=core2"
     cd '$(1).build' && '$(TARGET)-cmake' \
         -DBUILD_SHARED_LIBS=$(if $(BUILD_STATIC),OFF,ON) \
+        -DRAGEL='$(PREFIX)/$(BUILD)/bin/ragel' \
         '$(1)'
     $(MAKE) -C '$(1).build' -j '$(JOBS)'
     $(MAKE) -C '$(1).build' -j 1 install
