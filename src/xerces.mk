dea607ff0fcc283651de7168cb2c268f31c56b95
Boris Nagaev <bnagaev@gmail.com>
2016-09-13 21:34:44 +0300

    xerces: update
    Version 3.1.2 is not available in upstream.
    
    see https://github.com/mxe/mxe/issues/1519

--- src/xerces.mk
+++ src/xerces.mk
@@ -2,8 +2,8 @@
 
 PKG             := xerces
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.1.2
-$(PKG)_CHECKSUM := 743bd0a029bf8de56a587c270d97031e0099fe2b7142cef03e0da16e282655a0
+$(PKG)_VERSION  := 3.1.4
+$(PKG)_CHECKSUM := c98eedac4cf8a73b09366ad349cb3ef30640e7a3089d360d40a3dde93f66ecf6
 $(PKG)_SUBDIR   := xerces-c-$($(PKG)_VERSION)
 $(PKG)_FILE     := xerces-c-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.apache.org/dist/xerces/c/$(word 1,$(subst ., ,$($(PKG)_VERSION)))/sources/$($(PKG)_FILE)
