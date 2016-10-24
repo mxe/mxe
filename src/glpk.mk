fa8a48634441d3cc8f4e2e78d1cbc86a0993f235
Alois Schloegl <alois.schloegl@ist.ac.at>
2016-10-24 02:24:43 +0200

    pkg vlc and glpk: fix previous merge

--- src/glpk.mk
+++ src/glpk.mk
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := glpk
@@ -40,32 +39,4 @@ define $(PKG)_BUILD
         -W -Wall -Werror -ansi -pedantic \
         '$(SOURCE_DIR)/examples/netgen.c' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
         `'$(TARGET)-pkg-config' $(PKG) --cflags --libs`
-=======
-# This file is part of MXE.
-# See index.html for further information.
-
-PKG             := glpk
-$(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.47
-$(PKG)_CHECKSUM := 35e16d3167389b6bc75eb51b4b48590db59f789c
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := glpk-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := ftp://ftp.gnu.org/gnu/glpk/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc
-
-define $(PKG)_UPDATE
-    echo 'Warning: Updates are temporarily disabled for package glpk.' >&2;
-    echo $(glpk_VERSION)
-endef
-
-define $(PKG)_BUILD
-    cd '$(1)' && aclocal && libtoolize && autoreconf
-    mkdir '$(1)/.build'
-    cd '$(1)/.build' && '$(1)/configure' \
-        --host='$(TARGET)' \
-        --build="`config.guess`" \
-        $(ENABLE_SHARED_OR_STATIC) \
-        --prefix='$(PREFIX)/$(TARGET)'
-    $(MAKE) -C '$(1)/.build' -j '$(JOBS)' install
->>>>>>> ffebf9b1f1bc25ebcefd165224db1ee70f0cfa78
 endef
