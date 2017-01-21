2a92ee34324326b4988fb7899a02b59553c25329
Tony Theodore <tonyt@logyst.com>
2017-01-21 15:17:40 +1100

    hyperscan: fix build with gcc54 on darwin

--- src/hyperscan.mk
+++ src/hyperscan.mk
@@ -21,7 +21,7 @@ define $(PKG)_BUILD
     # Add the following options to run on (virtual) machine without AVX2
     # -DCMAKE_C_FLAGS="-march=core2" -DCMAKE_CXX_FLAGS="-march=core2"
     cd '$(1).build' && '$(TARGET)-cmake' \
-        -DCMAKE_C_FLAGS="-mssse4" -DCMAKE_CXX_FLAGS="-mssse4" \
+        -DCMAKE_C_FLAGS="-msse4" -DCMAKE_CXX_FLAGS="-msse4" \
         -DRAGEL='$(PREFIX)/$(BUILD)/bin/ragel' \
         '$(1)'
     $(MAKE) -C '$(1).build' -j '$(JOBS)'
