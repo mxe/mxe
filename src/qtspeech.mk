3e3ed0efed5cd1b9eac272ec87049aed0d79e4ed
Tony Theodore <tonyt@logyst.com>
2017-02-08 12:18:04 +1100

    add qtspeech module

---
+++ src/qtspeech.mk
@@ -0,0 +1,22 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := qtspeech
+$(PKG)_WEBSITE   = $(qtbase_WEBSITE)
+$(PKG)_DESCR    := Qt
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION   = $(qtbase_VERSION)
+$(PKG)_CHECKSUM := 94dd2871278ba69c70de690b7351f463f86e49f5c45be0b990c9c244e4c04735
+$(PKG)_SUBDIR    = $(subst qtbase,qtspeech,$(qtbase_SUBDIR))
+$(PKG)_FILE      = $(subst qtbase,qtspeech,$(qtbase_FILE))
+$(PKG)_URL       = $(subst qtbase,qtspeech,$(qtbase_URL))
+$(PKG)_DEPS     := gcc qtbase
+
+define $(PKG)_UPDATE
+    echo $(qtbase_VERSION)
+endef
+
+define $(PKG)_BUILD
+    cd '$(BUILD_DIR)' && '$(PREFIX)/$(TARGET)/qt5/bin/qmake' '$(SOURCE_DIR)'
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
+endef
