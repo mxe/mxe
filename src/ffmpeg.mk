a36df67ccf20e1d66e2e81ee71e471f2204cf778
Boris Pek <tehnick-8@yandex.ru>
2017-02-17 02:09:12 +0300

    ffmpeg: update from 3.2.1 to 3.2.4

--- src/ffmpeg.mk
+++ src/ffmpeg.mk
@@ -3,8 +3,8 @@
 PKG             := ffmpeg
 $(PKG)_WEBSITE  := http://www.ffmpeg.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.2.1
-$(PKG)_CHECKSUM := 72abc55bea5ff5397ac82320fa5c4843a05f527d0d7912d66784c92fdfbd12fb
+$(PKG)_VERSION  := 3.2.4
+$(PKG)_CHECKSUM := c0fa3593a2e9e96ace3c1757900094437ad96d1d6ca19f057c378b5f394496a4
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://www.ffmpeg.org/releases/$($(PKG)_FILE)
