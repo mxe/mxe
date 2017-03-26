68f5df67445b142c2948f7b013402b8ec4939aee
Tony Theodore <tonyt@logyst.com>
2017-03-26 15:59:20 +1100

    qca: use GH_CONF

--- src/qca.mk
+++ src/qca.mk
@@ -6,16 +6,8 @@ $(PKG)_DESCR    := Qt Cryptographic Architecture
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.1.3
 $(PKG)_CHECKSUM := a5135ffb0250a40e9c361eb10cd3fe28293f0cf4e5c69d3761481eafd7968067
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/KDE/qca/archive/v$($(PKG)_VERSION).tar.gz
+$(PKG)_GH_CONF  := KDE/qca,v
 $(PKG)_DEPS     := gcc qtbase
-
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://github.com/KDE/qca/tags' | \
-    $(SED) -n 's,.*/KDE/qca/archive/v\([0-9][^>]*\)\.tar\.gz.*,\1,p' | \
-    head -1
-endef
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
