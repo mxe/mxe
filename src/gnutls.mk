49fee79736a186f3301deabe0b818137a9e15afd
Mark Brand <mabrand@mabrand.nl>
2017-05-08 00:22:46 +0200

    let gnutls use libidn2
    Gnutls is supposed to be able to detect and use libidn2, but it
    tries to do this without pkg-config, in a way that doesn't
    consider transitive dependencies. Using pkg-config with libidn2.pc
    seems to work okay with MXE's version of libidn2.

--- src/gnutls.mk
+++ src/gnutls.mk
@@ -9,7 +9,7 @@ $(PKG)_SUBDIR   := gnutls-$($(PKG)_VERSION)
 $(PKG)_FILE     := gnutls-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://gnupg.org/ftp/gcrypt/gnutls/v3.5/$($(PKG)_FILE)
 $(PKG)_URL_2    := ftp://ftp.gnutls.org/gcrypt/gnutls/v3.5/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc gettext gmp libgnurx libidn libunistring nettle zlib
+$(PKG)_DEPS     := gcc gettext gmp libgnurx libidn2 libunistring nettle zlib
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- https://gnupg.org/ftp/gcrypt/gnutls/v3.5/ | \
@@ -21,7 +21,7 @@ endef
 define $(PKG)_BUILD
     # AI_ADDRCONFIG referenced by src/serv.c but not provided by mingw.
     # Value taken from https://msdn.microsoft.com/en-us/library/windows/desktop/ms737530%28v=vs.85%29.aspx
-    cd '$(1)' && ./configure \
+    cd '$(1)' && autoreconf -fi && ./configure \
         $(MXE_CONFIGURE_OPTS) \
         --disable-rpath \
         --disable-nls \
