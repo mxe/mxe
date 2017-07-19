9f793b72dce2430eb9cf8af055701ca2de5c8232
Tony Theodore <tonyt@logyst.com>
2017-07-19 15:22:57 +1000

    add qt5 freeze example plugin (e.g. winxp support)

---
+++ plugins/examples/qt5-freeze/qtwebsockets.mk
@@ -0,0 +1,23 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := qtwebsockets
+$(PKG)_WEBSITE  := http://qt-project.org/
+$(PKG)_DESCR    := Qt
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION   = $(qtbase_VERSION)
+$(PKG)_CHECKSUM := 5c2a75b68e7f2e98530659b33bb08edee83013832dbf99cc5b40afc8a90652d1
+$(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/$(PKG)-[0-9]*.patch)))
+$(PKG)_SUBDIR    = $(subst qtbase,qtwebsockets,$(qtbase_SUBDIR))
+$(PKG)_FILE      = $(subst qtbase,qtwebsockets,$(qtbase_FILE))
+$(PKG)_URL       = $(subst qtbase,qtwebsockets,$(qtbase_URL))
+$(PKG)_DEPS     := gcc qtbase
+
+define $(PKG)_UPDATE
+    echo $(qtbase_VERSION)
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && '$(PREFIX)/$(TARGET)/qt5/bin/qmake'
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+endef
