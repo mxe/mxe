c8770bd85ff6249670eb265b31370b3bd7fa4caf
Tony Theodore <tonyt@logyst.com>
2017-07-22 18:03:19 +1000

    libharu: use new-style GH_CONF and SOURCE_DIR etc

--- src/libharu.mk
+++ src/libharu.mk
@@ -4,28 +4,19 @@ PKG             := libharu
 $(PKG)_WEBSITE  := http://libharu.org/
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.3.0
-$(PKG)_CHECKSUM := 34de0ed4e994d9c704339292740b8019c33e8538814e104d882508a33517d1a8
-$(PKG)_SUBDIR   := libharu-RELEASE_$(subst .,_,$($(PKG)_VERSION))
-$(PKG)_FILE     := $($(PKG)_SUBDIR).zip
-$(PKG)_URL      := https://github.com/$(PKG)/$(PKG)/archive/RELEASE_$(subst .,_,$($(PKG)_VERSION)).zip
+$(PKG)_CHECKSUM := 8f9e68cc5d5f7d53d1bc61a1ed876add1faf4f91070dbc360d8b259f46d9a4d2
+$(PKG)_GH_CONF  := libharu/libharu,RELEASE_,,,_
 $(PKG)_DEPS     := gcc libpng zlib
 
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://github.com/libharu/libharu/tags' | \
-    $(SED) -n 's,.*/archive/RELEASE_\([0-9][^"]*\)\.tar.*,\1,p' | \
-    $(SED) 's,_,.,g' | \
-    grep -v 'RC' | \
-    head -1
-endef
-
 define $(PKG)_BUILD
-    mkdir '$(1)/build'
-    cd '$(1)/build' && '$(TARGET)-cmake' .. \
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' '$(SOURCE_DIR)' \
         -DCMAKE_C_FLAGS=$(if $(BUILD_STATIC),,-DHPDF_DLL_MAKE) \
-        -DLIBHPDF_SHARED=$(if $(BUILD_STATIC),FALSE,TRUE) \
-        -DLIBHPDF_STATIC=$(if $(BUILD_STATIC),TRUE,FALSE) \
+        -DLIBHPDF_SHARED=$(CMAKE_SHARED_BOOL) \
+        -DLIBHPDF_STATIC=$(CMAKE_STATIC_BOOL) \
         -DDEVPAK=ON
-    $(MAKE) -C '$(1)/build' -j '$(JOBS)' install VERBOSE=1
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1
+
     $(TARGET)-gcc -o $(PREFIX)/$(TARGET)/bin/test-$(PKG).exe \
         $(if $(BUILD_STATIC),,-DHPDF_DLL) \
         '$(1)/demo/slide_show_demo.c' \
