f35618ef6da3d553e73ec10cec47a0cfd20e285d
Tony Theodore <tonyt@logyst.com>
2017-08-04 12:17:12 +1000

    qtwebkit: add test

--- src/qtwebkit.mk
+++ src/qtwebkit.mk
@@ -19,4 +19,18 @@ define $(PKG)_BUILD_SHARED
     cd '$(BUILD_DIR)' && '$(PREFIX)/$(TARGET)/qt5/bin/qmake' '$(SOURCE_DIR)'
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
+
+    # build test manually
+    # add $(BUILD_TYPE_SUFFIX) for debug builds - see qtbase.mk
+    $(TARGET)-g++ \
+        -W -Wall -std=c++11 \
+        '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
+        `$(TARGET)-pkg-config Qt5WebKitWidgets --cflags --libs`
+
+    # batch file to run test programs
+    (printf 'set PATH=..\\lib;..\\qt5\\bin;..\\qt5\\lib;%%PATH%%\r\n'; \
+     printf 'set QT_QPA_PLATFORM_PLUGIN_PATH=..\\qt5\\plugins\r\n'; \
+     printf 'test-$(PKG).exe\r\n'; \
+     printf 'cmd\r\n';) \
+     > '$(PREFIX)/$(TARGET)/bin/test-$(PKG).bat'
 endef
