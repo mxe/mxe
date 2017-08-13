909c82b99e98aac84cbc346ee263f574f564a6f8
Tony Theodore <tonyt@logyst.com>
2017-08-13 13:20:13 +1000

    libarchive: use nettle instead of bcrypt for CNG(Crypto Next Generation)
    fixes #1876
    closes #1877

--- src/libarchive.mk
+++ src/libarchive.mk
@@ -9,7 +9,7 @@ $(PKG)_CHECKSUM := ed2dbd6954792b2c054ccf8ec4b330a54b85904a80cef477a1c74643ddafa
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://www.libarchive.org/downloads/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc bzip2 libiconv libxml2 openssl xz zlib
+$(PKG)_DEPS     := gcc bzip2 libiconv libxml2 nettle openssl xz zlib
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://www.libarchive.org/downloads/' | \
@@ -22,11 +22,15 @@ define $(PKG)_BUILD
     $(if $(BUILD_STATIC),\
         $(SED) -i '1i#ifndef LIBARCHIVE_STATIC\n#define LIBARCHIVE_STATIC\n#endif' -i '$(1)/libarchive/archive.h'
         $(SED) -i '1i#ifndef LIBARCHIVE_STATIC\n#define LIBARCHIVE_STATIC\n#endif' -i '$(1)/libarchive/archive_entry.h')
+
+    # use nettle instead of bcrypt for CNG(Crypto Next Generation)
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
         --disable-bsdtar \
         --disable-bsdcpio \
         --disable-bsdcat \
+        --without-cng \
+        --with-nettle \
         XML2_CONFIG='$(PREFIX)/$(TARGET)'/bin/xml2-config
     $(MAKE) -C '$(1)' -j '$(JOBS)' man_MANS=
     $(MAKE) -C '$(1)' -j 1 install man_MANS=
