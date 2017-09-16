b8599ed14b051b88203ba6db32dd327946e7d809
Viktor Szakats <vszakats@users.noreply.github.com>
2017-09-16 03:25:30 +0000

    secure/update URLs
    * change libssh2/apache/xiph URLs to canonical ones
    * resubmit patch to remove unnecessary
      --no-check-certificate option. The URLs
      are non-HTTPS anyway
    * blas: restore URL and point to versioned archive

--- src/libssh2.mk
+++ src/libssh2.mk
@@ -1,17 +1,17 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := libssh2
-$(PKG)_WEBSITE  := https://www.libssh2.org/
+$(PKG)_WEBSITE  := https://libssh2.org/
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.8.0
 $(PKG)_CHECKSUM := 39f34e2f6835f4b992cafe8625073a88e5a28ba78f83e8099610a7b3af4676d4
 $(PKG)_SUBDIR   := libssh2-$($(PKG)_VERSION)
 $(PKG)_FILE     := libssh2-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://www.libssh2.org/download/$($(PKG)_FILE)
+$(PKG)_URL      := https://libssh2.org/download/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc libgcrypt zlib
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://www.libssh2.org/download/?C=M;O=D' | \
+    $(WGET) -q -O- 'https://libssh2.org/download/?C=M;O=D' | \
     grep 'libssh2-' | \
     $(SED) -n 's,.*libssh2-\([0-9][^>]*\)\.tar.*,\1,p' | \
     head -1
