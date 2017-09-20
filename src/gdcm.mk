8999c23c29121d8a45d4650db09f869e92a541ed
Alois Schlögl <alois.schloegl@ist.ac.at>
2017-09-21 00:02:26 +0200

    package gdcm: fix compilation by disabling docbook

--- src/gdcm.mk
+++ src/gdcm.mk
@@ -31,6 +31,7 @@ define $(PKG)_BUILD
         -DGDCM_BUILD_SHARED_LIBS:BOOL=TRUE \
         -DGDCM_USE_SYSTEM_ZLIB:BOOL=TRUE \
 	-DGDCM_USE_SYSTEM_EXPAT:BOOL=TRUE \
+        -DGDCM_BUILD_DOCBOOK_MANPAGES:BOOL=OFF \
         ../$($(PKG)_SUBDIR)
     cd '$(1)/../.build' && \
         env -u MAKE -u MAKEFLAGS nmake && \
@@ -43,6 +44,7 @@ define $(PKG)_BUILD
         $($(PKG)_CMAKE_OPTS) \
         -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)'  \
         -DGDCM_BUILD_SHARED_LIBS:BOOL=TRUE \
+        -DGDCM_BUILD_DOCBOOK_MANPAGES:BOOL=OFF \
         ../$($(PKG)_SUBDIR)
     make -C $(1)/../.build -j $(JOBS) 
     make -C $(1)/../.build -j 1 install
