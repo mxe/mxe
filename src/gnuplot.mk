5144a773a1e50814419187a4f42165161e06f017
Alois Schlögl <alois.schloegl@ist.ac.at>
2017-09-23 21:15:18 +0200

    pkg gnuplot: update to v5.2

--- src/gnuplot.mk
+++ src/gnuplot.mk
@@ -3,9 +3,8 @@
 
 PKG             := gnuplot
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.6.4
-$(PKG)_CHECKSUM := 54ee5ce9a0a2698b046064bd275e772673350013
-#f01e417dc9504a05fd5cc2595b05ccb58bcea5b2
+$(PKG)_VERSION  := 5.2.0
+$(PKG)_CHECKSUM := 7dfe6425a1a6b9349b1fb42dae46b2e52833b13e807a78a613024d6a99541e43
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://sourceforge.net/projects/gnuplot/files/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
