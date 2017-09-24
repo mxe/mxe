ef5aa69c022fc3446e8149f3d39fbba3b8e60cf7
Alois Schlögl <alois.schloegl@ist.ac.at>
2017-09-24 13:57:14 +0200

    pkg libass: update to v0.13.7

--- src/libass.mk
+++ src/libass.mk
@@ -3,10 +3,8 @@
 PKG             := libass
 $(PKG)_WEBSITE  := https://code.google.com/p/libass/
 $(PKG)_IGNORE   :=
-# remove autoreconf step after 0.13.1
-# https://github.com/libass/libass/issues/209
-$(PKG)_VERSION  := 0.13.1
-$(PKG)_CHECKSUM := 4aa36b1876a61cab46fc9284fee84224b9e2840fe7b3e63d96a8d32574343fe7
+$(PKG)_VERSION  := 0.13.7
+$(PKG)_CHECKSUM := 7065e5f5fb76e46f2042a62e7c68d81e5482dbeeda24644db1bd066e44da7e9d
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.xz
 $(PKG)_URL      := https://github.com/libass/libass/releases/download/$($(PKG)_VERSION)/$($(PKG)_FILE)
@@ -14,14 +12,13 @@ $(PKG)_DEPS     := gcc fontconfig freetype fribidi harfbuzz
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- "https://api.github.com/repos/libass/libass/releases" | \
-    grep 'tag_name' | \
-    $(SED) -n 's,.*tag_name": "\([0-9][^>]*\)".*,\1,p' | \
+    $(SED) -n '/tag_name/ s,.*tag_name": "\([0-9][^>]*\)".*,\1,p' | \
     $(SORT) -Vr | \
     head -1
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && $(LIBTOOLIZE) && autoreconf -fi
+    cd '$(1)' && $(LIBTOOLIZE)
     # fontconfig is only required for legacy XP support
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
