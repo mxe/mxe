0b486c2dac49b2c45dfcbf74b997488b4593eec6
Alois Schlögl <alois.schloegl@ist.ac.at>
2017-09-25 18:03:38 +0200

    pkg edfbrowser: update to v1.60; improve update method

--- src/edfbrowser.mk
+++ src/edfbrowser.mk
@@ -2,19 +2,18 @@
 # See index.html for further information.
 
 PKG             := edfbrowser
+$(PKG)_WEBSITE  := https://www.teuniz.net/edfbrowser/
+$(PKG)_DESCR    := EDFbrowser
 $(PKG)_IGNORE   := 
 $(PKG)_VERSION  := 1.60
 $(PKG)_CHECKSUM := beeed235bb87abcd28df865f4dc60e25b119b1306fb07faf616d2ad91fa43b1e
 $(PKG)_SUBDIR   := edfbrowser_160_source
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
-$(PKG)_URL      := https://www.teuniz.net/edfbrowser/$($(PKG)_FILE)
+$(PKG)_URL      := https://github.com/Teuniz/EDFbrowser/releases
+                   https://www.teuniz.net/edfbrowser/$($(PKG)_FILE)
+$(PKG)_GH_CONF  := Teuniz/EDFbrowser/releases v
 $(PKG)_DEPS     := gcc qt
 
-define $(PKG)_UPDATE
-    wget -q -O- 'http://www.teuniz.net/edfbrowser/version.txt' | \
-    $(SED) -n 's_^version \([0-9.\]\.[0-9][0-9]\).*_\1_ip' | \
-    head -1
-endef
 
 define $(PKG)_BUILD
 
