32925791d8b4ac0fa6d3db6669123e9310f72ae4
Alois Schloegl <alois.schloegl@ist.ac.at>
2017-09-29 22:33:33 +0200

    pkg graphicsmagick: update to v1.3.26

--- src/graphicsmagick.mk
+++ src/graphicsmagick.mk
@@ -1,18 +1,20 @@
-# This file is part of MXE. See LICENSE.md for licensing information.
+# This file is part of MXE.
+# See index.html for further information.
 
 PKG             := graphicsmagick
-$(PKG)_WEBSITE  := http://www.graphicsmagick.org/
-$(PKG)_DESCR    := GraphicsMagick
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.3.21
-$(PKG)_CHECKSUM := 9045304d991776b6a37e1b45b9b6ef152593ada0d49bc744263565617cbf3c1f
+$(PKG)_VERSION  := 1.3.26
+$(PKG)_CHECKSUM := fba015f3d5e5d5f17e57db663f1aa9d338e7b62f1d415b85d13ee366927e5f88
 $(PKG)_SUBDIR   := GraphicsMagick-$($(PKG)_VERSION)
 $(PKG)_FILE     := GraphicsMagick-$($(PKG)_VERSION).tar.xz
-$(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc bzip2 freetype jasper jpeg lcms libltdl libpng libxml2 pthreads tiff zlib
+$(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_DEPS     := zlib bzip2 jpeg jasper lcms libpng tiff freetype libxml2
+ifneq ($(MXE_SYSTEM),msvc)
+    $(PKG)_DEPS += pthreads libtool
+endif
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://sourceforge.net/projects/graphicsmagick/files/graphicsmagick/' | \
+    $(WGET) -q -O- 'http://sourceforge.net/projects/graphicsmagick/files/graphicsmagick/' | \
     $(SED) -n 's,.*/\([0-9][^"]*\)/".*,\1,p' | \
     head -1
 endef
@@ -21,7 +23,11 @@ define $(PKG)_BUILD
     # This can be removed once the patch "graphicsmagick-1-fix-xml2-config.patch" is accepted by upstream
     cd '$(1)' && autoconf
     cd '$(1)' && ./configure \
-         $(MXE_CONFIGURE_OPTS) \
+        $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
+        $(ENABLE_SHARED_OR_STATIC) \
+         $(CONFIGURE_CPPFLAGS) $(CONFIGURE_LDFLAGS) \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --disable-openmp \
         --without-modules \
         --with-threads \
         --with-magick-plus-plus \
@@ -42,14 +48,14 @@ define $(PKG)_BUILD
         --with-xml \
         --with-zlib \
         --without-x \
+       --with-quantum-depth=16 \
         ac_cv_prog_xml2_config='$(PREFIX)/$(TARGET)/bin/xml2-config' \
         ac_cv_path_xml2_config='$(PREFIX)/$(TARGET)/bin/xml2-config' \
-        LIBS='-lgomp -fopenmp'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS=
-    $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS=
 
-    '$(TARGET)-g++' \
-        -W -Wall -Werror -pedantic -std=gnu++0x \
-        '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-graphicsmagick.exe' \
-        `'$(TARGET)-pkg-config' GraphicsMagick++ --cflags --libs` -llzma
+    $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS=
+    $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS= DESTDIR='$(3)'
+
+    if [ "$(ENABLE_DEP_DOCS)" == "no" ]; then \
+      rm -rf "$(3)$(PREFIX)/$(TARGET)/share/doc/GraphicsMagick"; \
+    fi
 endef
