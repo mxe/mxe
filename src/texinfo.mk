8004b9ed370a3a6df9f6ef2599ba158a482de588
Alois Schloegl <alois.schloegl@ist.ac.at>
2017-09-29 23:12:09 +0200

    pkg texinfo: checksum is now sha256

--- src/texinfo.mk
+++ src/texinfo.mk
@@ -4,7 +4,7 @@
 PKG             := texinfo
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 4.13a
-$(PKG)_CHECKSUM := a1533cf8e03ea4fa6c443b73f4c85e4da04dead0
+$(PKG)_CHECKSUM := 1303e91a1c752b69a32666a407e9fbdd6e936def4b09bc7de30f416301530d68
 $(PKG)_SUBDIR   := $(PKG)-4.13
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := ftp://ftp.gnu.org/gnu/texinfo/$($(PKG)_FILE)
