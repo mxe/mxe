8620d26cbd49bb4c74d991522ce54b46a861cb3b
Alois Schloegl <alois.schloegl@ist.ac.at>
2017-10-01 00:48:44 +0200

    pkg libsodium: update to v1.0.14

--- src/libsodium.mk
+++ src/libsodium.mk
@@ -3,8 +3,8 @@
 PKG             := libsodium
 $(PKG)_WEBSITE  := https://download.libsodium.org/doc/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.6
-$(PKG)_CHECKSUM := 940d03ea7d2caa7940e24564bf6d9f66d6edd1df1e0111ff8e3655f3b864fb59
+$(PKG)_VERSION  := 1.0.14
+$(PKG)_CHECKSUM := 3cfc84d097fdc891b40d291f2ac2c3f99f71a87e36b20cc755c6fa0e97a77ee7
 $(PKG)_SUBDIR   := libsodium-$($(PKG)_VERSION)
 $(PKG)_FILE     := libsodium-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://download.libsodium.org/libsodium/releases/$($(PKG)_FILE)
