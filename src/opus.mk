29f9d00f2ee24df4bf6d5e4f6589cd1ecde104e4
Alois Schloegl <alois.schloegl@ist.ac.at>
2017-10-01 01:11:01 +0200

    pkg opus: update to v1.2.1

--- src/opus.mk
+++ src/opus.mk
@@ -3,8 +3,8 @@
 PKG             := opus
 $(PKG)_WEBSITE  := https://opus-codec.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.1.1
-$(PKG)_CHECKSUM := 9b84ff56bd7720d5554103c557664efac2b8b18acc4bbcc234cb881ab9a3371e
+$(PKG)_VERSION  := 1.2.1
+$(PKG)_CHECKSUM := cfafd339ccd9c5ef8d6ab15d7e1a412c054bf4cb4ecbbbcc78c12ef2def70732
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://archive.mozilla.org/pub/$(PKG)/$($(PKG)_FILE)
