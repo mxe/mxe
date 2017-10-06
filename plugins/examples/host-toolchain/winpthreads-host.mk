a6351242b673d25d6ff1812cbacf635121610b62
Tony Theodore <tonyt@logyst.com>
2017-10-06 22:51:10 +1100

    winpthreads-host: use mingw-w64 patches

--- plugins/examples/host-toolchain/winpthreads-host.mk
+++ plugins/examples/host-toolchain/winpthreads-host.mk
@@ -6,6 +6,7 @@ $(PKG)_VERSION   = $(mingw-w64_VERSION)
 $(PKG)_CHECKSUM  = $(mingw-w64_CHECKSUM)
 $(PKG)_SUBDIR    = $(mingw-w64_SUBDIR)
 $(PKG)_FILE      = $(mingw-w64_FILE)
+$(PKG)_PATCHES   = $(mingw-w64_PATCHES)
 $(PKG)_URL       = $(mingw-w64_URL)
 $(PKG)_URL_2     = $(mingw-w64_URL_2)
 $(PKG)_DEPS     := gcc
