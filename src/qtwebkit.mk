3d2369f71dfdab78a4ff36ef24b513fb81cc78d4
Mark Brand <mabrand@mabrand.nl>
2017-10-07 11:53:04 +0200

    qt5: update to 5.9.2
    qtwebkit is still at version 5.9.1 since 5.9.2 is not published.

--- src/qtwebkit.mk
+++ src/qtwebkit.mk
@@ -4,11 +4,11 @@ PKG             := qtwebkit
 $(PKG)_WEBSITE  := https://www.qt.io/
 $(PKG)_DESCR    := Qt
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION   = $(qtbase_VERSION)
+$(PKG)_VERSION  := 5.9.1
 $(PKG)_CHECKSUM := 28a560becd800a4229bfac317c2e5407cd3cc95308bc4c3ca90dba2577b052cf
-$(PKG)_SUBDIR    = $(subst qtbase,qtwebkit,$(qtbase_SUBDIR))
-$(PKG)_FILE      = $(subst qtbase,qtwebkit,$(qtbase_FILE))
-$(PKG)_URL       = $(subst qtbase,qtwebkit,$(qtbase_URL))
+$(PKG)_SUBDIR   := $(PKG)-opensource-src-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-opensource-src-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://download.qt.io/official_releases/qt/5.9/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc qtbase qtmultimedia qtquickcontrols sqlite
 
 define $(PKG)_UPDATE
