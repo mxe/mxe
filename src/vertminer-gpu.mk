272c8bf2f7cf895ce428b90e536243ba5fcb35a1
Tony Theodore <tonyt@logyst.com>
2017-10-23 00:37:42 +1100

    add vertminer-gpu (to test opencl)

---
+++ src/vertminer-gpu.mk
@@ -0,0 +1,24 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := vertminer-gpu
+$(PKG)_WEBSITE  := https://github.com/Bufius/vertminer-gpu
+$(PKG)_DESCR    := GPU miner for vertcoin
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 6419989
+$(PKG)_CHECKSUM := 5552fc5bb41adcc2701196281c997b728f2653b065b961596e476bb16aaf36a1
+$(PKG)_GH_CONF  := Bufius/vertminer-gpu/master
+$(PKG)_DEPS     := gcc curl opencl-icd pthreads
+
+define $(PKG)_BUILD
+    # doesn't seem to support out-of-source builds
+    cd '$(SOURCE_DIR)' && autoreconf -fi
+    cd '$(SOURCE_DIR)' && ./configure \
+        $(MXE_CONFIGURE_OPTS) \
+        --enable-scrypt \
+        --disable-adl \
+        --without-curses \
+        --enable-opencl \
+        CFLAGS="`'$(TARGET)-pkg-config' libcurl --cflags`"
+    $(MAKE) -C '$(SOURCE_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(SOURCE_DIR)' -j 1 install
+endef
