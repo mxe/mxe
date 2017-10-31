4803dbce95c0efef37f9517a6173219ba085a26a
Tony Theodore <tonyt@logyst.com>
2017-10-31 17:50:14 +1100

    openmp-validation: switch to github master and tidy up
    http://web.cs.uh.edu/ no longer exists, see:
    https://github.com/mxe/mxe/pull/1900#issuecomment-329972938

--- src/openmp-validation.mk
+++ src/openmp-validation.mk
@@ -4,21 +4,13 @@ PKG             := openmp-validation
 $(PKG)_WEBSITE  := https://github.com/uhhpctools/omp-validation
 $(PKG)_DESCR    := OpenMP Validation Suite
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.1
-$(PKG)_CHECKSUM := 9dbfe3cf49ab6187def83c85b57e133e11fbb667aa111f0fd70775166254dbff
-$(PKG)_SUBDIR   := OpenMP$($(PKG)_VERSION)_Validation
-$(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
-$(PKG)_URL      := http://web.cs.uh.edu/~openuh/download/packages/$($(PKG)_FILE)
-$(PKG)_URL_2    :=
+$(PKG)_VERSION  := ff8cf0c
+$(PKG)_CHECKSUM := 9a86e52c2901488c5af87a83bb771818ea1650c3aec79cb4b5aa5b8f6888533f
+$(PKG)_GH_CONF  := uhhpctools/omp-validation/master
 $(PKG)_DEPS     := gcc
 
-define $(PKG)_UPDATE
-    echo 'TODO: Updates for package openmp-validation need to be written.' >&2;
-    echo $(openmp-validation_VERSION)
-endef
-
 define $(PKG)_BUILD
-    $(MAKE) -C '$(1)' -j '$(JOBS)' \
+    $(MAKE) -C '$(SOURCE_DIR)' -j '$(JOBS)' \
         RUNTEST_OPTIONS='--norun' \
         EXE_SUFFIX='.exe' \
         CC='$(TARGET)-gcc' \
@@ -27,9 +19,9 @@ define $(PKG)_BUILD
 
     # execute validation tests on host - perl testsuite doesn't
     # work on windows (perl runtest.pl --nocompile --lang=c testlist-c.txt)
-    # so run $(PREFIX)/$(TARGET)/bin/$(PKG)-tests/all-tests-openmp-validation.bat
+    # so run $(PREFIX)/$(TARGET)/bin/openmp-validation-tests/all-tests-openmp-validation.bat
 
     mkdir -p '$(PREFIX)/$(TARGET)/bin'
     rm -rf '$(PREFIX)/$(TARGET)/bin/$(PKG)-tests'
-    cp -rv '$(1)' '$(PREFIX)/$(TARGET)/bin/$(PKG)-tests'
+    cp -rv '$(SOURCE_DIR)' '$(PREFIX)/$(TARGET)/bin/$(PKG)-tests'
 endef
