d27d22904160007bca36c39b1df10e80558e61e3
Tony Theodore <tonyt@logyst.com>
2017-11-04 20:50:18 +1100

    gnutls: enable new deps and use BUILD_DIR

--- src/gnutls.mk
+++ src/gnutls.mk
@@ -9,7 +9,7 @@ $(PKG)_SUBDIR   := gnutls-$($(PKG)_VERSION)
 $(PKG)_FILE     := gnutls-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://gnupg.org/ftp/gcrypt/gnutls/v3.5/$($(PKG)_FILE)
 $(PKG)_URL_2    := https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnutls/v3.5/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc gettext gmp libgnurx libidn2 libunistring nettle zlib
+$(PKG)_DEPS     := gcc gettext gmp libgnurx libidn2 libtasn1 libunistring nettle p11-kit unbound zlib
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- https://gnupg.org/ftp/gcrypt/gnutls/v3.5/ | \
@@ -21,22 +21,24 @@ endef
 define $(PKG)_BUILD
     # AI_ADDRCONFIG referenced by src/serv.c but not provided by mingw.
     # Value taken from https://msdn.microsoft.com/library/windows/desktop/ms737530
-    cd '$(1)' && autoreconf -fi && ./configure \
+    cd '$(SOURCE_DIR)' && autoreconf -fi
+    cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
         $(MXE_CONFIGURE_OPTS) \
-        --disable-rpath \
-        --disable-nls \
-        --disable-guile \
         --disable-doc \
+        --disable-guile \
+        --disable-libdane \
+        --disable-nls \
+        --disable-rpath \
+        --disable-silent-rules \
         --disable-tests \
         --enable-local-libopts \
-        --with-included-libtasn1 \
         --with-libregex-libs="-lgnurx" \
-        --without-p11-kit \
-        --disable-silent-rules \
+        --with$(if $(BUILD_STATIC),out)-p11-kit \
         CPPFLAGS='-DWINVER=0x0501 -DAI_ADDRCONFIG=0x0400 -DIPV6_V6ONLY=27' \
         LIBS='-lws2_32' \
         ac_cv_prog_AR='$(TARGET)-ar'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 
     '$(TARGET)-gcc' \
         -W -Wall -Werror -ansi -pedantic \
