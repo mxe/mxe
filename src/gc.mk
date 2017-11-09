1cb70e441afbf8085e81d834d5b82d8bb57de91b
Tony Theodore <tonyt@logyst.com>
2017-11-09 17:10:16 +1100

    gc: use GH_CONF and downgrade 7.6.0 to 7.4.6
    7.6.0 is actually a pre-release and shouldn't have been updated
    
    Fixes #1967

--- src/gc.mk
+++ src/gc.mk
@@ -1,23 +1,14 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := gc
-$(PKG)_WEBSITE  := http://www.hpl.hp.com/personal/Hans_Boehm/gc/
+$(PKG)_WEBSITE  := https://github.com/ivmai/bdwgc
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 7.6.0
-$(PKG)_CHECKSUM := a14a28b1129be90e55cd6f71127ffc5594e1091d5d54131528c24cd0c03b7d90
+$(PKG)_VERSION  := 7.4.6
+$(PKG)_CHECKSUM := 8c9b91cc2facc7d0ec3829fe9ab29179c1879adafecb4474a827aa2a18596533
+$(PKG)_GH_CONF  := ivmai/bdwgc/releases/latest,v
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://hboehm.info/$(PKG)/$(PKG)_source/$($(PKG)_FILE)
+$(PKG)_URL      := https://github.com/ivmai/bdwgc/releases/download/v$($(PKG)_VERSION)/$($(PKG)_SUBDIR).tar.gz
 $(PKG)_DEPS     := gcc libatomic_ops
-
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://hboehm.info/gc/gc_source/' | \
-    grep '<a href="gc-' | \
-    $(SED) -n 's,.*<a href="gc-\([0-9][^"]*\)\.tar.*,\1,p' | \
-    grep -v 'alpha' | \
-    $(SORT) -Vr | \
-    head -1
-endef
 
 define $(PKG)_BUILD
     # build and install the library
