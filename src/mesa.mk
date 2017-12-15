ec4d0925a6a00d49abc73af1a8a89251df6bcd5d
Robert Manner <robert.manner@balabit.com>
2017-12-15 16:59:51 +0100

    src/mesa: add recipe for Mesa, needed for qtbase
    We are going to enable the dynamic OpenGL selection in qtbase, and for
    that we also need the headers from Mesa.

---
+++ src/mesa.mk
@@ -0,0 +1,32 @@
+PKG             := mesa
+$(PKG)_VERSION  := 18.3.6
+$(PKG)_CHECKSUM := aaf17638dcf5a90b93b6389e152fdc9ef147768b09598f24d2c5cf482fcfc705
+$(PKG)_SUBDIR   := mesa-$($(PKG)_VERSION)
+$(PKG)_FILE     := mesa-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := ftp://ftp.freedesktop.org/pub/mesa/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc scons-local
+
+ifeq (,$(findstring x86_64,$(TARGET)))
+    MACHINE=x86_64
+else
+    MACHINE=x86
+endif
+
+define $(PKG)_BUILD
+    mkdir -p '$(BUILD_DIR).scons'
+    $(call PREPARE_PKG_SOURCE,scons-local,'$(BUILD_DIR).scons')
+    cd '$(1)' && \
+    MINGW_PREFIX='$(TARGET)-' $(SCONS_LOCAL) \
+        platform=windows \
+        toolchain=crossmingw \
+        machine=$(MACHINE) \
+        verbose=1 \
+        build=release \
+        libgl-gdi
+
+    for i in EGL GLES GLES2 GLES3 KHR; do \
+        $(INSTALL) -d "$(PREFIX)/$(TARGET)/include/$$i"; \
+        $(INSTALL) -m 644 "$(1)/include/$$i/"* "$(PREFIX)/$(TARGET)/include/$$i/"; \
+    done
+    $(INSTALL) -m 755 '$(1)/build/windows-$(MACHINE)/gallium/targets/libgl-gdi/opengl32.dll' '$(PREFIX)/$(TARGET)/bin/'
+endef
