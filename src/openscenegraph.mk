d767af30bd2543a940daf87614f3a778e1fb6367
Christoph Weiss <weiss@wsoptics.de>
2018-01-26 15:22:21 +0100

    update OpenSceneGraph to 3.5.9

--- src/openscenegraph.mk
+++ src/openscenegraph.mk
@@ -4,22 +4,14 @@ PKG             := openscenegraph
 $(PKG)_WEBSITE  := http://www.openscenegraph.org/
 $(PKG)_DESCR    := OpenSceneGraph
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.4.1
-$(PKG)_CHECKSUM := 930eb46f05781a76883ec16c5f49cfb29a059421db131005d75bec4d78401fd5
-#$(PKG)_GH_CONF  := openscenegraph/OpenSceneGraph/tags, OpenSceneGraph-
+$(PKG)_VERSION  := 3.5.9
+$(PKG)_CHECKSUM := 01cdfa34c525be76bb3c5e3e2c0f91346a6f6287b05ddfb6b9d15f96fd0c812e
+$(PKG)_FILE     := OpenSceneGraph-$($(PKG)_VERSION).zip
 $(PKG)_SUBDIR   := OpenSceneGraph-OpenSceneGraph-$($(PKG)_VERSION)
-$(PKG)_FILE     := OpenSceneGraph-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/openscenegraph/OpenSceneGraph/archive/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc boost curl dcmtk freetype gdal giflib gstreamer \
                    gta jasper jpeg libpng openal openexr openthreads poppler \
                    qtbase tiff zlib
-
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://www.openscenegraph.org/index.php/download-section/stable-releases' | \
-    $(SED) -n 's,.*OpenSceneGraph/tree/OpenSceneGraph-\([0-9]*\.[0-9]*[02468]\.[^<]*\)">.*,\1,p' | \
-    $(SORT) -V | \
-    tail -1
-endef
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && '$(TARGET)-cmake' '$(SOURCE_DIR)' \
