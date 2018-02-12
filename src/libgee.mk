951f507ce74ae8fcf8cc8d35c4ea59bc65f75694
Igor Zhukov <fsb4000@yandex.ru>
2018-02-13 01:54:13 +0700

    update libgee

--- src/libgee.mk
+++ src/libgee.mk
@@ -3,10 +3,10 @@
 PKG             := libgee
 $(PKG)_WEBSITE  := https://wiki.gnome.org/Projects/Libgee
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.5.0
-$(PKG)_CHECKSUM := aa6a2563867d3e3d56921bd1f7a7869d24599e1b5beb70e83f55b718fdddff51
+$(PKG)_VERSION  := 0.20.1
+$(PKG)_CHECKSUM := bb2802d29a518e8c6d2992884691f06ccfcc25792a5686178575c7111fea4630
 $(PKG)_SUBDIR   := libgee-$($(PKG)_VERSION)
-$(PKG)_FILE     := libgee-$($(PKG)_VERSION).tar.bz2
+$(PKG)_FILE     := libgee-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.gnome.org/sources/libgee/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc glib
 
@@ -20,11 +20,14 @@ endef
 
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
-        --host='$(TARGET)' \
-        --disable-shared \
-        --prefix='$(PREFIX)/$(TARGET)'
+        $(MXE_CONFIGURE_OPTS) \
+        MAKE=$(MAKE)
     $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
     $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+
+    '$(TARGET)-gcc' \
+        -W -Wall -Werror -ansi \
+        '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-libgee.exe' \
+        `'$(TARGET)-pkg-config' gee-0.8 gio-2.0 glib-2.0 --cflags --libs`
 endef
 
-$(PKG)_BUILD_SHARED =
