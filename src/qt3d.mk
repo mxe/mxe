6cca41d7b57337527d1714e7825d7e185b09c337
Mark Brand <mabrand@mabrand.nl>
2018-02-15 09:34:10 +0100

    qt5: update to 5.10.1

--- src/qt3d.mk
+++ src/qt3d.mk
@@ -5,11 +5,11 @@ $(PKG)_WEBSITE  := https://www.qt.io/
 $(PKG)_DESCR    := Qt
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION   = $(qtbase_VERSION)
-$(PKG)_CHECKSUM := 12457e7227c793274329371c60b9028ab3e44db837e875e2c460367c1dbd34ab
+$(PKG)_CHECKSUM := 041fb42536a72bbf9be17a6f52d4b73ce93fb98b456fd63503cc47d80d196b3b
 $(PKG)_SUBDIR    = $(subst qtbase,qt3d,$(qtbase_SUBDIR))
 $(PKG)_FILE      = $(subst qtbase,qt3d,$(qtbase_FILE))
 $(PKG)_URL       = $(subst qtbase,qt3d,$(qtbase_URL))
-$(PKG)_DEPS     := cc qtbase qtdeclarative
+$(PKG)_DEPS     := cc qtbase qtdeclarative qtgamepad
 
 define $(PKG)_UPDATE
     echo $(qtbase_VERSION)
