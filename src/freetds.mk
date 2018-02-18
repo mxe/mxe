e28ae5862512f3f6dc3bd99de1ee054e41a83f4f
Mark Brand <mabrand@mabrand.nl>
2018-02-18 16:39:39 +0100

    freetds: switch to openssl

--- src/freetds.mk
+++ src/freetds.mk
@@ -10,7 +10,7 @@ $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := ftp://ftp.freetds.org/pub/$(PKG)/stable/$($(PKG)_FILE)
 $(PKG)_URL_2    := https://fossies.org/linux/privat/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc gnutls libiconv
+$(PKG)_DEPS     := cc openssl libiconv
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'ftp://ftp.freetds.org/pub/freetds/stable/' | \
@@ -29,7 +29,8 @@ define $(PKG)_BUILD
         --enable-sspi \
         --disable-threadsafe \
         --with-tdsver=7.2 \
-        --with-gnutls \
-        PKG_CONFIG='$(TARGET)-pkg-config'
+        --with-openssl \
+        PKG_CONFIG='$(TARGET)-pkg-config' \
+	CFLAGS=-D_WIN32_WINNT=0x0600
     $(MAKE) -C '$(1)' -j '$(JOBS)' install man_MANS=
 endef
