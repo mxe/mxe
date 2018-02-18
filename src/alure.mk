3632f0927f915fb77505f050d275c5a4e2b9c2d6
Tony Theodore <tonyt@logyst.com>
2018-02-18 13:17:27 +1100

    alure: fix pkg-config detection and enable shared

--- src/alure.mk
+++ src/alure.mk
@@ -18,15 +18,13 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)/build' && '$(TARGET)-cmake' \
-        -DDYNLOAD=OFF \
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
+        -DDYNLOAD=$(CMAKE_SHARED_BOOL) \
+        -DBUILD_SHARED=$(CMAKE_SHARED_BOOL) \
+        -DBUILD_STATIC=$(CMAKE_STATIC_BOOL) \
         -DBUILD_EXAMPLES=OFF \
         -DFLUIDSYNTH=OFF \
-        -DCMAKE_C_FLAGS="-DAL_LIBTYPE_STATIC -DALURE_STATIC_LIBRARY" \
-        -DCMAKE_CXX_FLAGS="-DAL_LIBTYPE_STATIC -DALURE_STATIC_LIBRARY" \
-        ..
-    $(MAKE) -C '$(1)/build' -j $(JOBS) VERBOSE=1
-    $(MAKE) -C '$(1)/build' -j $(JOBS) install
+        -DSTATIC_CFLAGS="-DAL_LIBTYPE_STATIC"
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
-
-$(PKG)_BUILD_SHARED =
