ed91582f2326be9b570bf64ab1f49f74625ee754
Tony Theodore <tonyt@logyst.com>
2018-02-18 11:46:30 +1100

    ocaml-camlimages: use pkg in filename

--- src/ocaml-camlimages.mk
+++ src/ocaml-camlimages.mk
@@ -7,7 +7,7 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 4.0.1
 $(PKG)_CHECKSUM := b40237c1505487049799a7af296eb3996b3fa08eab94415546f46d61355747c4
 $(PKG)_SUBDIR   := camlspotter-camlimages-c803efa9d5d3
-$(PKG)_FILE     := v$($(PKG)_VERSION).tar.gz
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://bitbucket.org/camlspotter/camlimages/get/v$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := cc freetype giflib libpng ocaml-findlib ocaml-lablgtk2 tiff
 
