58dcc4a512fbf22bf75308f3e9dad9faf5bac9eb
ZachBacon <zachbacon@budgiemedia.org>
2018-02-19 06:25:27 -0800

    Fix filename

--- plugins/apps/filezilla.mk
+++ plugins/apps/filezilla.mk
@@ -1,11 +1,11 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
-PKG             := filezilla
+PKG             := FileZilla
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 3.30.0
 $(PKG)_CHECKSUM := 910b676dd2b558a7c96cdbe77c2fb82df3cffd88a1b99b98b475e9c5d8c41ff0
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_FILE     := $(PKG)_$($(PKG)_VERSION)_src.tar.bz2
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$(PKG)-$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_WEBSITE  := https://filezilla-project.org
 $(PKG)_OWNER    := https://filezilla-project.org/sourcecode.php
