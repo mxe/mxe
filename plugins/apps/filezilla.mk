34f28beb8dfa1b88471892e2d74a4a0d919d5408
ZachBacon <zachbacon@budgiemedia.org>
2018-02-19 05:50:33 -0800

    Some fixups

--- plugins/apps/filezilla.mk
+++ plugins/apps/filezilla.mk
@@ -1,21 +1,21 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
-PKG             := qbittorrent
+PKG             := filezilla
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.3.4
-$(PKG)_CHECKSUM := c0d0d4b72c240f113b59a061146803bc1b7926d3d7f39b06b50a4d26f5ad91b8
+$(PKG)_VERSION  := 3.30.0
+$(PKG)_CHECKSUM := 910b676dd2b558a7c96cdbe77c2fb82df3cffd88a1b99b98b475e9c5d8c41ff0
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.xz
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$(PKG)-$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_WEBSITE  := https://qbittorrent.org/
-$(PKG)_OWNER    := https://github.com/starius
+$(PKG)_WEBSITE  := https://filezilla-project.org
+$(PKG)_OWNER    := https://filezilla-project.org/sourcecode.php
 $(PKG)_DEPS     := cc gnutls libfilezilla libidn sqlite wxwidgets 
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://www.qbittorrent.org/download.php' | \
     $(SED) -n 's,.*qbittorrent-\([0-9][^"]*\)\.tar.*,\1,p' | \
     head -1
-endef
+ndef
 
 define $(PKG)_BUILD
     cd '$(1)' && \
