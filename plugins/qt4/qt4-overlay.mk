242ffa5f69069c54d6e8c53c64fef3ef8290be2f
Tony Theodore <tonyt@logyst.com>
2018-03-01 17:50:32 +1100

    qt4 plugin: sort packages

--- plugins/qt4/qt4-overlay.mk
+++ plugins/qt4/qt4-overlay.mk
@@ -1,17 +1,17 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
-# override relevant vtk and qwt variables to build against Qt 4 instead of 5
+# override relevant package variables to build against Qt 4 instead of 5
 
-PKG               := vtk
-$(PKG)_DEPS       := cc hdf5 qt libpng expat libxml2 jsoncpp tiff freetype lz4 hdf5 libharu glew
-$(PKG)_QT_VERSION := 4
+PKG               := openscenegraph
+$(PKG)_DEPS       := $(filter-out qtbase ,$($(PKG)_DEPS)) qt
+
+PKG               := poppler
+$(PKG)_DEPS       := $(filter-out qtbase ,$($(PKG)_DEPS)) qt
 
 PKG               := qwt
 $(PKG)_DEPS       := cc qt
 $(PKG)_QT_DIR     := qt
 
-PKG               := poppler
-$(PKG)_DEPS       := $(filter-out qtbase ,$($(PKG)_DEPS)) qt
-
-PKG               := openscenegraph
-$(PKG)_DEPS       := $(filter-out qtbase ,$($(PKG)_DEPS)) qt
+PKG               := vtk
+$(PKG)_DEPS       := cc hdf5 qt libpng expat libxml2 jsoncpp tiff freetype lz4 hdf5 libharu glew
+$(PKG)_QT_VERSION := 4
