50ba42cce4f27263d90832a83563d39a983171bd
Tony Theodore <tonyt@logyst.com>
2018-03-01 15:54:57 +1100

    bison flex: update and build on darwin

--- plugins/native/bison.mk
+++ plugins/native/bison.mk
@@ -1,9 +1,9 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := bison
-$(PKG)_IGNORE   := 3%
-$(PKG)_VERSION  := 2.7.1
-$(PKG)_CHECKSUM := b409adcbf245baadb68d2f66accf6fdca5e282cafec1b865f4b5e963ba8ea7fb
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 3.0.4
+$(PKG)_CHECKSUM := a72428c7917bdf9fa93cb8181c971b6e22834125848cf1d03ce10b1bb0716fe1
 $(PKG)_SUBDIR   := bison-$($(PKG)_VERSION)
 $(PKG)_FILE     := bison-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/bison/$($(PKG)_FILE)
@@ -13,6 +13,9 @@ $(PKG)_OWNER    := https://github.com/tonytheodore
 $(PKG)_TARGETS  := $(BUILD)
 $(PKG)_DEPS     := flex
 
+# recursive variable so always use literal instead of $(PKG)
+MXE_REQS_PKGS   += $(BUILD)~bison
+
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://ftp.gnu.org/gnu/bison/?C=M;O=D' | \
     $(SED) -n 's,.*<a href="bison-\([0-9][^"]*\)\.tar.*,\1,p' | \
@@ -21,9 +24,5 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD_$(BUILD)
-    mkdir '$(1).build'
-    cd    '$(1).build' && '$(1)/configure' \
-        --prefix='$(PREFIX)/$(TARGET)'
-    $(MAKE) -C '$(1).build' -j '$(JOBS)'
-    $(MAKE) -C '$(1).build' -j 1 install
+    $(AUTOTOOLS_BUILD)
 endef
