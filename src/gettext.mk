e30dd374299d3d6065039e1be6cf22847bf09201
Tony Theodore <tonyt@logyst.com>
2018-03-01 15:56:55 +1100

    gettext: install bins to std location and remove from reqs

--- src/gettext.mk
+++ src/gettext.mk
@@ -42,10 +42,11 @@ endef
 
 define $(PKG)_BUILD_DARWIN
     # causes issues with other packages so use different prefix
-    # but install *.m4 files to standard /share
+    # but install *.m4 files and bins to standard location
     cd '$(BUILD_DIR)' && $(SOURCE_DIR)/configure \
         $(MXE_CONFIGURE_OPTS) \
         --prefix='$(PREFIX)/$(TARGET).gnu' \
+        --bindir='$(PREFIX)/$(TARGET)/bin' \
         --datarootdir='$(PREFIX)/$(TARGET)/share'
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_DOCS)
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_DOCS)
