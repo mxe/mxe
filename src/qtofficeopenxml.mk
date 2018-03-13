ef0369e6a40944b3041e6c944034fdc06bc401ca
Tony Theodore <tonyt@logyst.com>
2018-03-14 02:32:12 +1100

    qtofficeopenxml qtsparkle qtxlsxwriter: new-style options, updates, etc

--- src/qtofficeopenxml.mk
+++ src/qtofficeopenxml.mk
@@ -1,16 +1,13 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
+
 PKG             := qtofficeopenxml
 $(PKG)_WEBSITE  := https://github.com/dbzhang800/QtOfficeOpenXml/
 $(PKG)_DESCR    := QtOfficeOpenXml
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 02dda4a46f92a843eaba5f5a021952860eadfe01
-$(PKG)_CHECKSUM := b8b7a452df6aaa1322863f47c27b63b95f528af2c7a70a4baca975fc89d36d13
-$(PKG)_SUBDIR   := QtOfficeOpenXml-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/dbzhang800/QtOfficeOpenXml/archive/$($(PKG)_VERSION).tar.gz
+$(PKG)_VERSION  := 02dda4a
+$(PKG)_CHECKSUM := 02927da4386d5e5f1ed4679f089db3029c4ff8dfebc448c93fa1bacf1828d8d9
+$(PKG)_GH_CONF  := dbzhang800/QtOfficeOpenXml/branches/master
 $(PKG)_DEPS     := cc qtbase
-
-$(PKG)_UPDATE    = $(call MXE_GET_GITHUB_SHA, dbzhang800/QtOfficeOpenXml, master)
 
 define $(PKG)_BUILD
     cd '$(1)' && '$(PREFIX)/$(TARGET)/qt5/bin/qmake'
