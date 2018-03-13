3d30f9ec47e381a66459ff28a53c3c6481b32ea5
Tony Theodore <tonyt@logyst.com>
2018-03-13 21:11:56 +1100

    libgnurx: use GH_CONF

--- src/libgnurx.mk
+++ src/libgnurx.mk
@@ -5,23 +5,18 @@ $(PKG)_WEBSITE  := https://sourceforge.net/projects/mingw/files/UserContributed/
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.6.1
 $(PKG)_CHECKSUM := ee6edc110c6b63d0469f4f05ef187564b310cc8a88b6566310a4aebd48b612c7
+$(PKG)_GH_CONF  := TimothyGu/libgnurx/releases/latest,libgnurx-
 $(PKG)_SUBDIR   := mingw-libgnurx-$($(PKG)_VERSION)
 $(PKG)_FILE     := mingw-libgnurx-$($(PKG)_VERSION)-src.tar.gz
 $(PKG)_URL      := https://github.com/TimothyGu/libgnurx/releases/download/libgnurx-$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc
 
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://api.github.com/repos/TimothyGu/libgnurx/git/refs/tags/' \
-    | $(SED) -n 's#.*"ref": "refs/tags/libgnurx-\([^"]*\).*#\1#p' \
-    | $(SORT) -V \
-    | tail -1
-endef
-
 define $(PKG)_BUILD
-    cd '$(1)' && ./configure \
+    # custom makefile so build in source
+    cd '$(SOURCE_DIR)' && ./configure \
         --host='$(TARGET)' \
         --prefix='$(PREFIX)/$(TARGET)'
-    $(MAKE) -C '$(1)' -f Makefile.mxe -j '$(JOBS)' \
+    $(MAKE) -C '$(SOURCE_DIR)' -f Makefile.mxe -j '$(JOBS)' \
         $(if $(BUILD_STATIC),install-static,install-shared) \
         TARGET=$(TARGET) bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 endef
