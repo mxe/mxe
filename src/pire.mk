adc19c37e8637ac56604490498d5896c61645a25
Tony Theodore <tonyt@logyst.com>
2018-03-14 01:08:58 +1100

    mman picomodel pire: use new-style options

--- src/pire.mk
+++ src/pire.mk
@@ -6,26 +6,20 @@ $(PKG)_DESCR    := PIRE
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 0.0.5
 $(PKG)_CHECKSUM := 85a9bd66fff568554826e4aff9b188ed6124e3ea0530cc561723b36aea2a58e3
-$(PKG)_SUBDIR   := pire-release-$($(PKG)_VERSION)
-$(PKG)_FILE     := pire-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/yandex/pire/archive/release-$($(PKG)_VERSION).tar.gz
+$(PKG)_GH_CONF  := yandex/pire/tags,release-
 $(PKG)_DEPS     := cc
 
-define $(PKG)_UPDATE
-    $(call MXE_GET_GITHUB_TAGS, yandex/pire, release-)
-endef
-
 define $(PKG)_BUILD
-    cd '$(1)' && autoreconf -fi
-    cd '$(1)' && ./configure \
+    cd '$(SOURCE_DIR)' && autoreconf -fi
+    cd '$(BUILD_DIR)' && $(SOURCE_DIR)/configure \
         $(MXE_CONFIGURE_OPTS) \
         --enable-extra \
         ac_cv_func_malloc_0_nonnull=yes
-    $(MAKE) -C '$(1)/pire' -j '$(JOBS)' bin_PROGRAMS= LDFLAGS='-no-undefined'
-    $(MAKE) -C '$(1)/pire' -j 1 install bin_PROGRAMS=
+    $(MAKE) -C '$(BUILD_DIR)/pire' -j '$(JOBS)' bin_PROGRAMS= LDFLAGS='-no-undefined'
+    $(MAKE) -C '$(BUILD_DIR)/pire' -j 1 install bin_PROGRAMS=
 
     '$(TARGET)-g++' \
         -W -Wall -Werror \
-        '$(1)/samples/pigrep/pigrep.cpp' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
+        '$(SOURCE_DIR)/samples/pigrep/pigrep.cpp' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
         -lpire
 endef
