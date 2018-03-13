7e3453b34a159b1ea3ca23c0775c071d94f6ec4e
Tony Theodore <tonyt@logyst.com>
2018-03-13 21:12:19 +1100

    libical: use GH_CONF

--- src/libical.mk
+++ src/libical.mk
@@ -1,17 +1,11 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := libical
-$(PKG)_WEBSITE  := https://freeassociation.sourceforge.io/
+$(PKG)_WEBSITE  := https://libical.github.io/libical/
 $(PKG)_VERSION  := 2.0.0
 $(PKG)_CHECKSUM := 654c11f759c19237be39f6ad401d917e5a05f36f1736385ed958e60cf21456da
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/$(PKG)/$(PKG)/releases/download/v$($(PKG)_VERSION)//$($(PKG)_FILE)
+$(PKG)_GH_CONF  := libical/libical/releases/latest, v
 $(PKG)_DEPS     := cc icu4c
-
-define $(PKG)_UPDATE
-    $(call MXE_GET_GITHUB_TAGS, libical/libical, v)
-endef
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && '$(TARGET)-cmake' \
