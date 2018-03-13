1c31b6d032c44cbe0826a8679274d5e58071ddee
Tony Theodore <tonyt@logyst.com>
2018-03-13 21:14:30 +1100

    openjpeg: use new-style options

--- src/openjpeg.mk
+++ src/openjpeg.mk
@@ -7,22 +7,13 @@ $(PKG)_DESCR    := OpenJPEG
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.3.0
 $(PKG)_CHECKSUM := 3dc787c1bb6023ba846c2a0d9b1f6e179f1cd255172bde9eb75b01f1e6c7d71a
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/uclouvain/openjpeg/archive/v$($(PKG)_VERSION).tar.gz
+$(PKG)_GH_CONF  := uclouvain/openjpeg/tags,v,,version
 $(PKG)_DEPS     := cc lcms libpng tiff zlib
 
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://api.github.com/repos/uclouvain/openjpeg/git/refs/tags/' | \
-    $(SED) -n 's#.*"ref": "refs/tags/v\([0-9,.]*\).*#\1#p' | \
-    $(SORT) -V | \
-    tail -1
-endef
-
 define $(PKG)_BUILD
-    mkdir '$(1).build'
-    cd '$(1).build' && '$(TARGET)-cmake' \
-        -DBUILD_TESTING=FALSE \
-        '$(1)'
-    $(MAKE) -C '$(1).build' install
+    # build and install the library
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
+        -DBUILD_TESTING=FALSE
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
