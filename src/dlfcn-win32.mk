0301ae256fcf1b405f7485167968a3056af984bd
Tony Theodore <tonyt@logyst.com>
2018-03-13 21:07:24 +1100

    dlfc-win32: use GH_CONF etc.

--- src/dlfcn-win32.mk
+++ src/dlfcn-win32.mk
@@ -6,23 +6,22 @@ $(PKG)_DESCR    := POSIX dlfcn wrapper for Windows
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := e19bf07
 $(PKG)_CHECKSUM := 6b31a8547547af27e5dfc092df1ea2c6ac562ce47b7ec08a0a4da4ed0b002767
-$(PKG)_SUBDIR   := $(PKG)-$(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/$(PKG)/$(PKG)/tarball/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_GH_CONF  := dlfcn-win32/dlfcn-win32/branches/master
 $(PKG)_DEPS     := cc
 
-$(PKG)_UPDATE    = $(call MXE_GET_GITHUB_SHA, dlfcn-win32/dlfcn-win32, master) | $(SED) 's/^\(.......\).*/\1/;'
-
 define $(PKG)_BUILD
-    cd '$(1)' && ./configure \
+    # can't use $(MXE_CONFIGURE_OPTS), handwritten ./configure
+    #   - rejects unknown options
+    #   - no support for out-of-source build
+    cd '$(SOURCE_DIR)' && ./configure \
         --prefix='$(PREFIX)/$(TARGET)' \
         --cross-prefix='$(TARGET)-' \
         $(if $(BUILD_STATIC), \
             --enable-static --disable-shared , \
             --disable-static --enable-shared )
-    $(MAKE) -C '$(1)' -j '$(JOBS)'
-    $(MAKE) -C '$(1)' -j 1 install
-    $(MAKE) -C '$(1)' -j '$(JOBS)' test.exe testdll.dll
+    $(MAKE) -C '$(SOURCE_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(SOURCE_DIR)' -j 1 install
+    $(MAKE) -C '$(SOURCE_DIR)' -j '$(JOBS)' test.exe testdll.dll
 
     # create pkg-config file - mostly for psapi dependency
     mkdir -p '$(PREFIX)/$(TARGET)/lib/pkgconfig'
