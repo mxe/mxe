e06a870f1ea49d9a0746146f360bdf64fe8cd9be
Tony Theodore <tonyt@logyst.com>
2018-03-15 17:26:09 +1100

    libplist: update 1.12 --> 2.0.0

--- src/libplist.mk
+++ src/libplist.mk
@@ -3,22 +3,16 @@
 PKG             := libplist
 $(PKG)_WEBSITE  := https://github.com/libimobiledevice/libplist
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.12
-$(PKG)_CHECKSUM := b8e860ef2e01154e79242438252b2a7ed185df351f02c167147a8a602a0aa63e
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/libimobiledevice/libplist/archive/$($(PKG)_VERSION).tar.gz
+$(PKG)_VERSION  := 2.0.0
+$(PKG)_CHECKSUM := 187eb8a7aca2d5abcdfd81c42ac12e67ebe49c434299df1881a942e64b7c7978
+$(PKG)_GH_CONF  := libimobiledevice/libplist/tags
 $(PKG)_DEPS     := cc libxml2
 
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://github.com/libimobiledevice/libplist/archive/' | \
-    $(SED) -n 's,.*/\([0-9][^"]*\)/"\.tar.*,\1,p' | \
-    head -1
-endef
-
 define $(PKG)_BUILD
-    cd '$(1)' && $(SHELL) ./autogen.sh \
+    cd '$(SOURCE_DIR)' && NOCONFIGURE=true $(SHELL) ./autogen.sh
+    cd '$(BUILD_DIR)' && $(SOURCE_DIR)/configure \
         $(MXE_CONFIGURE_OPTS) \
         --without-cython
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
