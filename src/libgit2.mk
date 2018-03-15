a17f43d78cd7b377f2cc640a90756f3c6cacb6ab
Tony Theodore <tonyt@logyst.com>
2018-03-15 14:07:18 +1100

    libgit2: update 0.23.2 --> 0.26.3

--- src/libgit2.mk
+++ src/libgit2.mk
@@ -3,18 +3,14 @@
 PKG             := libgit2
 $(PKG)_WEBSITE  := https://libgit2.github.com/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.23.2
-$(PKG)_CHECKSUM := 6cff6c84d30138358069d29ccf6f3e79c06438a3fda7274d5b1fe14c37f82f08
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/libgit2/libgit2/archive/v$($(PKG)_VERSION).tar.gz
+$(PKG)_VERSION  := 0.26.3
+$(PKG)_CHECKSUM := 0da4e211dfb63c22e5f43f2a4a5373e86a140afa88a25ca6ba3cc2cae58263d2
+$(PKG)_GH_CONF  := libgit2/libgit2/releases/latest,v
 $(PKG)_DEPS     := cc libssh2
 
 define $(PKG)_BUILD
-    mkdir '$(1).build'
-    cd '$(1).build' && '$(TARGET)-cmake' \
-        -DDLLTOOL='$(PREFIX)/bin/$(TARGET)-dlltool' \
-        '$(1)'
-    $(MAKE) -C '$(1).build' -j '$(JOBS)' VERBOSE=1 || $(MAKE) -C '$(1).build' -j 1 VERBOSE=1
-    $(MAKE) -C '$(1).build' -j 1 install VERBOSE=1
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
+        -DDLLTOOL='$(PREFIX)/bin/$(TARGET)-dlltool'
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1 || $(MAKE) -C '$(BUILD_DIR)' -j 1 VERBOSE=1
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1
 endef
