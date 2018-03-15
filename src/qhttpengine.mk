d43808314639173bcd091d941e3dbe6862eb3a61
Tony Theodore <tonyt@logyst.com>
2018-03-15 19:32:48 +1100

    qhttpengine: update 0.1.0 --> 1.0.1

--- src/qhttpengine.mk
+++ src/qhttpengine.mk
@@ -3,16 +3,19 @@
 PKG             := qhttpengine
 $(PKG)_WEBSITE  := https://github.com/nitroshare/qhttpengine
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.1.0
-$(PKG)_CHECKSUM := 6df0e2f303eb5fb80995e0322903c2991b398a0b89fb483dae7c24bdefa1eaf1
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/nitroshare/qhttpengine/archive/$($(PKG)_VERSION).tar.gz
+$(PKG)_VERSION  := 1.0.1
+$(PKG)_CHECKSUM := 6505cf889909dc29bab4069116656e7ca5a9e879f04935139439c5691a76c55e
+$(PKG)_GH_CONF  := nitroshare/qhttpengine/tags
 $(PKG)_DEPS     := cc qtbase
 
 define $(PKG)_BUILD
-    mkdir '$(1)/build'
-    cd '$(1)/build' && '$(TARGET)-cmake' ..
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)'
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 
-    $(MAKE) -C '$(1)/build' -j '$(JOBS)' install
+    # compile test
+    '$(TARGET)-g++' \
+        -W -Wall -Werror -std=c++11 \
+        '$(SOURCE_DIR)/examples/auth/server.cpp' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
+        `'$(TARGET)-pkg-config' $(PKG) --cflags --libs`
 endef
