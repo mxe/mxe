d1fc3155ae146bd2057b14207759395092db3f8b
Boris Pek <tehnick-8@yandex.ru>
2018-03-22 00:40:20 +0300

    update osgearth

--- src/osgearth.mk
+++ src/osgearth.mk
@@ -4,10 +4,10 @@ PKG             := osgearth
 $(PKG)_WEBSITE  := http://osgearth.org/
 $(PKG)_DESCR    := Geospatial SDK for OpenSceneGraph
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.8
-$(PKG)_CHECKSUM := 5570dc5b62f6f9e28954f5cbd7946a9b35767c06b375eff1c4cc40561e7f1655
-$(PKG)_GH_CONF  := gwaldron/osgearth/tags, osgearth-
-$(PKG)_DEPS     := curl gcc gdal openscenegraph sqlite tinyxml2 zlib
+$(PKG)_VERSION  := 2.9
+$(PKG)_CHECKSUM := 22aeef42bb700c1e669d9ba57ce4155c0668caf86cc738750a6b9d34c1eaf2a4
+$(PKG)_GH_CONF  := gwaldron/osgearth/releases/latest, osgearth-
+$(PKG)_DEPS     := curl cc gdal openscenegraph sqlite tinyxml2 zlib
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
