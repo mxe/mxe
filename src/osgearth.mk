5132a846692730308a36cbe3276cb6da520f13d3
Boris Pek <tehnick-8@yandex.ru>
2018-03-23 00:15:41 +0300

    osgearth: use embedded version of tinyxml

--- src/osgearth.mk
+++ src/osgearth.mk
@@ -7,12 +7,12 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.9
 $(PKG)_CHECKSUM := 22aeef42bb700c1e669d9ba57ce4155c0668caf86cc738750a6b9d34c1eaf2a4
 $(PKG)_GH_CONF  := gwaldron/osgearth/releases/latest, osgearth-
-$(PKG)_DEPS     := curl cc gdal openscenegraph poco sqlite tinyxml2 zlib
+$(PKG)_DEPS     := curl cc gdal openscenegraph poco sqlite zlib
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
-        -DWITH_EXTERNAL_TINYXML=ON \
         -DDYNAMIC_OSGEARTH=$(CMAKE_SHARED_BOOL) \
+        -DWITH_EXTERNAL_TINYXML=OFF \
         -DBUILD_OSGEARTH_EXAMPLES=OFF \
         -DBUILD_APPLICATIONS=OFF \
         -DBUILD_TESTS=OFF
