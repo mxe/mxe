80f8a77232e9f7b9697927865ce478bc46c4e0a4
Mark Brand <mabrand@mabrand.nl>
2018-05-22 18:16:04 +0200

    qtwebview: add missing dependency

--- src/qtwebview.mk
+++ src/qtwebview.mk
@@ -9,7 +9,7 @@ $(PKG)_CHECKSUM := 842ea99770a660dbfd05178b5d1d7c6458a02b715a410ff95167dd222bda0
 $(PKG)_SUBDIR    = $(subst qtbase,qtwebview,$(qtbase_SUBDIR))
 $(PKG)_FILE      = $(subst qtbase,qtwebview,$(qtbase_FILE))
 $(PKG)_URL       = $(subst qtbase,qtwebview,$(qtbase_URL))
-$(PKG)_DEPS     := cc qtbase
+$(PKG)_DEPS     := cc qtbase qtdeclarative
 
 define $(PKG)_UPDATE
     echo $(qtbase_VERSION)
