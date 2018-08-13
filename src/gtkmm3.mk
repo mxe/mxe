cc0191215559b244cef171ef96fdac6d4f0be854
Tony Theodore <tonyt@logyst.com>
2018-08-14 01:36:18 +1000

    gtkmm2/3: fix gcc7 build

--- src/gtkmm3.mk
+++ src/gtkmm3.mk
@@ -28,6 +28,7 @@ define $(PKG)_BUILD
 
     '$(TARGET)-g++' \
         -W -Wall -Wno-deprecated-declarations -Werror -pedantic -std=c++11 \
+        -Wno-error=deprecated \
         '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-gtkmm3.exe' \
         `'$(TARGET)-pkg-config' gtkmm-3.0 --cflags --libs`
 endef
