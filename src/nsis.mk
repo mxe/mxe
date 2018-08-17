eb9093a37215f529a737b5fac3eec4b3b5e8f757
don bright <hmbright@fastmail.com>
2018-08-16 21:35:29 -0500

    use XGCC_W32_PREFIX for newer nsis
    new versions of NSIS require XGCC_W32_PREFIX not MINGW_CROSS_PREFIX, see http://nsis.sourceforge.net/Docs/AppendixG.html

--- src/nsis.mk
+++ src/nsis.mk
@@ -23,6 +23,7 @@ define $(PKG)_BUILD
         $(SED) -i 's/m_target_type=TARGET_X86ANSI/m_target_type=TARGET_AMD64/' '$(1)/Source/build.cpp')
     cd '$(1)' && scons \
         MINGW_CROSS_PREFIX='$(TARGET)-' \
+        XGCC_W32_PREFIX='$(TARGET)-' \
         PREFIX='$(PREFIX)/$(TARGET)' \
         `[ -d /usr/local/include ] && echo APPEND_CPPPATH=/usr/local/include` \
         `[ -d /usr/local/lib ]     && echo APPEND_LIBPATH=/usr/local/lib` \
