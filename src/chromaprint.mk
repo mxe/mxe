522e8d487a5a1876533f58422692caafd0142037
Jonas Kvinge <jonas@jkvinge.net>
2018-09-02 11:58:30 +0200

    Fix chromaprint to use GH_CONF

--- src/chromaprint.mk
+++ src/chromaprint.mk
@@ -5,16 +5,11 @@ $(PKG)_WEBSITE  := https://acoustid.org/chromaprint
 $(PKG)_DESCR    := Chromaprint
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.4.3
-$(PKG)_CHECKSUM := ea18608b76fb88e0203b7d3e1833fb125ce9bb61efe22c6e169a50c52c457f82
-$(PKG)_SUBDIR   := $(PKG)-v$($(PKG)_VERSION)
+$(PKG)_CHECKSUM := d4ae6596283aad7a015a5b0445012054c634a4b9329ecb23000cd354b40a283b
+$(PKG)_GH_CONF  := acoustid/chromaprint/tags, v
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/acoustid/chromaprint/releases/download/v$($(PKG)_VERSION)/$(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := cc fftw
-
-define $(PKG)_UPDATE
-    echo 'TODO: Updates for package chromaprint need to be written.' >&2;
-    echo $(chromaprint_VERSION)
-endef
 
 define $(PKG)_BUILD
     cd '$(1)' && '$(TARGET)-cmake'
