239ff09753c26a68280b90ae9a311a5dc3b0862a
Jonas Kvinge <jonas@jkvinge.net>
2018-09-23 23:14:59 +0200

    Update opusfile

--- src/opusfile.mk
+++ src/opusfile.mk
@@ -3,8 +3,8 @@
 PKG             := opusfile
 $(PKG)_WEBSITE  := https://opus-codec.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.6
-$(PKG)_CHECKSUM := 2428717b356e139f18ed2fdb5ad990b5654a238907a0058200b39c46a7d03ea6
+$(PKG)_VERSION  := 0.11
+$(PKG)_CHECKSUM := 74ce9b6cf4da103133e7b5c95df810ceb7195471e1162ed57af415fabf5603bf
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://archive.mozilla.org/pub/opus/$($(PKG)_FILE)
