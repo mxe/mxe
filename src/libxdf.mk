1f5682c3dcd000627a0a8e89485d30be8782b424
Alois SCHLOEGL <alois.schloegl@ist.ac.at>
2018-10-26 23:06:22 +0200

    pkg libxdf: fix compilation

--- src/libxdf.mk
+++ src/libxdf.mk
@@ -10,7 +10,7 @@ $(PKG)_CHECKSUM := 0c3791474f3658fca54e62b16db9e6d82b3eb68e92b77fd37d349263d3fe1
 $(PKG)_SUBDIR   := libxdf-0.98
 $(PKG)_FILE     := libxdf-0.98.zip
 $(PKG)_URL      := https://github.com/Yida-Lin/libxdf/archive/v0.98.zip
-$(PKG)_DEPS     := cc
+$(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
 #    wget -q -O- 'http://biosig.sourceforge.net/download.html' | \
@@ -19,17 +19,14 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-
-	mkdir '$(1)/native_build' 
-	cd '$(1)/native_build' && cmake \
+	mkdir '$(1)/native_build'
+	cd '$(1)/native_build' && $(TARGET)-cmake \
 		-DCMAKE_BUILD_TYPE="Release" \
+		-DSHARED_LIBS=$(if $(BUILD_STATIC),OFF,ON) \
 		'$(1)'
 
 	$(MAKE) -C '$(1)/native_build'
-    
+
 	$(INSTALL) '$(1)/xdf.h'    '$(PREFIX)/$(TARGET)/include/'
-	#$(INSTALL) '$(1)/native_build/'libxdf.so' '$(PREFIX)/$(TARGET)/lib/'
-	$(INSTALL) '$(1)/native_build/libxdf.a' '$(PREFIX)/$(TARGET)/lib/'
-
+	$(INSTALL) '$(1)'/native_build/libxdf.$(if $(BUILD_STATIC),"a","dll*") '$(PREFIX)/$(TARGET)/lib/'
 endef
-
