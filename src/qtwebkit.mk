7a7b04a01fcbbd8e62e50718d493ac94cd2c1465
Dirk Hohndel <dirk@hohndel.org>
2018-11-14 04:23:04 -0800

    Update QtWebKit to 5.212
    All of the work was done by aquiles2k - all I did was fix the install prefix
    and create the pull request so that people can start discussing this proposed
    change.
    
    This has only been tested for shared builds and requires posix threads.
    x86_64-w64-mingw32.shared.posix.dw2 and i686-w64-mingw32.shared.posix.dw2
    appear to work.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/qtwebkit.mk
+++ src/qtwebkit.mk
@@ -4,33 +4,32 @@ PKG             := qtwebkit
 $(PKG)_WEBSITE  := https://www.qt.io/
 $(PKG)_DESCR    := Qt
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.9.1
-$(PKG)_CHECKSUM := 28a560becd800a4229bfac317c2e5407cd3cc95308bc4c3ca90dba2577b052cf
-$(PKG)_SUBDIR   := $(PKG)-opensource-src-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-opensource-src-$($(PKG)_VERSION).tar.xz
-$(PKG)_URL      := https://download.qt.io/official_releases/qt/5.9/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc qtbase qtmultimedia qtquickcontrols sqlite
+$(PKG)_VERSION  := 5.212
+$(PKG)_CHECKSUM := 283b907ea324a2c734e3983c73fc27dbd8b33e2383c583de41842ee84d648a3e
+$(PKG)_SUBDIR   := qtwebkit-everywhere-src-$($(PKG)_VERSION)
+$(PKG)_FILE     := qtwebkit-everywhere-src-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://download.qt.io/snapshots/ci/qtwebkit/$($(PKG)_VERSION)/latest/src/submodules/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc libxslt qtdeclarative
 
 define $(PKG)_UPDATE
     echo $(qtbase_VERSION)
 endef
 
 define $(PKG)_BUILD_SHARED
-    cd '$(BUILD_DIR)' && '$(PREFIX)/$(TARGET)/qt5/bin/qmake' '$(SOURCE_DIR)'
+
+cd '$(BUILD_DIR)' && $(TARGET)-cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DPORT=Qt \
+    -DCMAKE_SYSTEM_PROCESSOR=x86_64 \
+    -DENABLE_INSPECTOR_UI=OFF \
+    -DENABLE_DEVICE_ORIENTATION=OFF \
+    -DUSE_QT_MULTIMEDIA=OFF \
+    -DENABLE_VIDEO=OFF \
+    -DENABLE_QT_WEBCHANNEL=OFF \
+    -DENABLE_GEOLOCATION=OFF \
+    -DCMAKE_INSTALL_PREFIX=$(PREFIX)/$(TARGET)/qt5 \
+    '$(SOURCE_DIR)'
+
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
-
-    # build test manually
-    # add $(BUILD_TYPE_SUFFIX) for debug builds - see qtbase.mk
-    $(TARGET)-g++ \
-        -W -Wall -std=c++11 \
-        '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
-        `$(TARGET)-pkg-config Qt5WebKitWidgets --cflags --libs`
-
-    # batch file to run test programs
-    (printf 'set PATH=..\\lib;..\\qt5\\bin;..\\qt5\\lib;%%PATH%%\r\n'; \
-     printf 'set QT_QPA_PLATFORM_PLUGIN_PATH=..\\qt5\\plugins\r\n'; \
-     printf 'test-$(PKG).exe\r\n'; \
-     printf 'cmd\r\n';) \
-     > '$(PREFIX)/$(TARGET)/bin/test-$(PKG).bat'
 endef
