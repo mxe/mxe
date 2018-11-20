3e0327ba44e7181496d10afa81a748e1efc9f5fb
Jonas Kvinge <jonas@jkvinge.net>
2018-11-20 02:16:03 +0100

    Update gstreamer

--- src/gst-plugins-bad.mk
+++ src/gst-plugins-bad.mk
@@ -3,8 +3,8 @@
 PKG             := gst-plugins-bad
 $(PKG)_WEBSITE  := https://gstreamer.freedesktop.org/modules/gst-plugins-bad.html
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.14.3
-$(PKG)_CHECKSUM := b2224e5d9c1b85ad51233f6135524bb9e16a9172d395edc79c73b89094659fd5
+$(PKG)_VERSION  := 1.14.4
+$(PKG)_CHECKSUM := 910b4e0e2e897e8b6d06767af1779d70057c309f67292f485ff988d087aa0de5
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://gstreamer.freedesktop.org/src/$(PKG)/$($(PKG)_FILE)
