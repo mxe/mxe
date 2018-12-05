a6bc11deb588f73c44eaf21059458eb650b2795d
Mark Brand <mabrand@mabrand.nl>
2018-12-06 00:25:39 +0100

    new qt5 module: qtremoteobjects

---
+++ src/qtremoteobjects.mk
@@ -0,0 +1,22 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := qtremoteobjects
+$(PKG)_WEBSITE  := https://www.qt.io/
+$(PKG)_DESCR    := Qt
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION   = $(qtbase_VERSION)
+$(PKG)_CHECKSUM := f70306a78f440516eaacd6908140bc469ebd101a61c724b9590f4aefbafe72b4
+$(PKG)_SUBDIR    = $(subst qtbase,qtremoteobjects,$(qtbase_SUBDIR))
+$(PKG)_FILE      = $(subst qtbase,qtremoteobjects,$(qtbase_FILE))
+$(PKG)_URL       = $(subst qtbase,qtremoteobjects,$(qtbase_URL))
+$(PKG)_DEPS     := cc qtbase qtdeclarative
+
+define $(PKG)_UPDATE
+    echo $(qtbase_VERSION)
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && '$(PREFIX)/$(TARGET)/qt5/bin/qmake'
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+endef
