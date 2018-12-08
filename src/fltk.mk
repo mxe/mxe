f5a4a6969540a9184fff2bda98f5c83b53b59ce2
Viktor Szakats <commit@vszakats.net>
2018-12-08 13:02:00 +0000

    secure URLs

--- src/fltk.mk
+++ src/fltk.mk
@@ -1,7 +1,7 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := fltk
-$(PKG)_WEBSITE  := http://www.fltk.org/
+$(PKG)_WEBSITE  := https://www.fltk.org/
 $(PKG)_DESCR    := FLTK
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.3.4-2
@@ -9,11 +9,11 @@ $(PKG)_CHECKSUM := 25d349c18c99508737d48f225a2eb26a43338f9247551cab72a317fa42cda
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_MAJOR    := $(word 1,$(subst -, ,$($(PKG)_VERSION)))
 $(PKG)_FILE     := $($(PKG)_SUBDIR)-source.tar.gz
-$(PKG)_URL      := http://fltk.org/pub/fltk/$($(PKG)_MAJOR)/$($(PKG)_FILE)
+$(PKG)_URL      := https://fltk.org/pub/fltk/$($(PKG)_MAJOR)/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc jpeg libpng pthreads zlib
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://www.fltk.org/software.php' | \
+    $(WGET) -q -O- 'https://www.fltk.org/software.php' | \
     $(SED) -n 's,.*>fltk-\([0-9]\+\([\.\-][0-9]\+\)\+\)-source\.tar\.gz<.*,\1,p' | \
     grep -v '^1\.1\.' | \
     head -1
--- src/openal.mk
+++ src/openal.mk
@@ -1,17 +1,17 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := openal
-$(PKG)_WEBSITE  := http://kcat.strangesoft.net/openal.html
+$(PKG)_WEBSITE  := https://openal-soft.org/
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.16.0
 $(PKG)_CHECKSUM := 2f3dcd313fe26391284fbf8596863723f99c65d6c6846dccb48e79cadaf40d5f
 $(PKG)_SUBDIR   := openal-soft-$($(PKG)_VERSION)
 $(PKG)_FILE     := openal-soft-$($(PKG)_VERSION).tar.bz2
-$(PKG)_URL      := http://kcat.strangesoft.net/openal-releases/$($(PKG)_FILE)
+$(PKG)_URL      := https://openal-soft.org/openal-releases/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc portaudio
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://kcat.strangesoft.net/openal-releases/?C=M;O=D' | \
+    $(WGET) -q -O- 'https://openal-soft.org/openal-releases/?C=M;O=D' | \
     $(SED) -n 's,.*"openal-soft-\([0-9][^"]*\)\.tar.*,\1,p' | \
     $(SORT) -V | \
     tail -1
