47c9c445a6fe5ffaa7950873f0b6e4f2d89941f8
Tony Theodore <tonyt@logyst.com>
2018-12-09 13:30:00 +1100

    librtmp: add explicit zlib dep
    was previously part of gnutls deps

--- src/librtmp.mk
+++ src/librtmp.mk
@@ -6,7 +6,7 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := fa8646d
 $(PKG)_CHECKSUM := 301cb9e93a7d2bf2da6b784ee6a9e45a04e2a9e3d322080d46c5d66576a792ec
 $(PKG)_GH_CONF  := mirror/rtmpdump/branches/master
-$(PKG)_DEPS     := cc gnutls
+$(PKG)_DEPS     := cc gnutls zlib
 
 define $(PKG)_BUILD
     $(MAKE) -C '$(SOURCE_DIR)' \
@@ -16,6 +16,7 @@ define $(PKG)_BUILD
         CRYPTO=GNUTLS \
         $(if $(BUILD_STATIC),\
             SHARED=no \
-            LIB_GNUTLS="`$(TARGET)-pkg-config --libs-only-l gnutls`",) \
+            LIB_GNUTLS="`$(TARGET)-pkg-config --libs-only-l gnutls`" \
+            XLIBS="`$(TARGET)-pkg-config --libs-only-l zlib`",) \
         -j '$(JOBS)' install
 endef
