38b333f3ae9fa44596a9ba4c696b4181824eca9c
Mark Brand <mabrand@mabrand.nl>
2019-02-02 16:36:59 +0100

    update: qtxlsxwriter

--- src/qtxlsxwriter.mk
+++ src/qtxlsxwriter.mk
@@ -1,11 +1,11 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := qtxlsxwriter
-$(PKG)_WEBSITE  := https://github.com/dbzhang800/QtXlsxWriter/
+$(PKG)_WEBSITE  := https://github.com/VSRonin/QtXlsxWriter/
 $(PKG)_DESCR    := QtXlsxWriter
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := cd33e43
-$(PKG)_CHECKSUM := d1e1c31b5ce0f76d49eb87a8228edb356302f4ffd6a2df847084e8d118bb4435
+$(PKG)_VERSION  := 5b685b5
+$(PKG)_CHECKSUM := 5f0abd0a63d69b119bac8e4df98484476ec707f8d395ed9bd4c59c59e817b2f2
 $(PKG)_GH_CONF  := VSRonin/QtXlsxWriter/branches/master
 $(PKG)_DEPS     := cc qtbase
 
