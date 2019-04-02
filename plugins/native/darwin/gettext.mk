ae3ebf05e657d337f1989a048e7a2e728c02a067
Tony Theodore <tonyt@logyst.com>
2019-04-02 16:21:33 +1100

    gettext: use plugin for darwin overrides

---
+++ plugins/native/darwin/gettext.mk
@@ -0,0 +1,16 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG := gettext
+
+define $(PKG)_BUILD_$(BUILD)
+    # causes issues with other packages so use different prefix
+    # but install *.m4 files and bins to standard location
+    cd '$(BUILD_DIR)' && $(SOURCE_DIR)/configure \
+        $(MXE_CONFIGURE_OPTS) \
+        --with-included-libcroco \
+        --prefix='$(PREFIX)/$(TARGET).gnu' \
+        --bindir='$(PREFIX)/$(TARGET)/bin' \
+        --datarootdir='$(PREFIX)/$(TARGET)/share'
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_DOCS)
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_DOCS)
+endef
