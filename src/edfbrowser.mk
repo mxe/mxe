453a1a5ab4d6ab128f7972e08c28f16c7a2bc8fe
Alois Schloegl <alois.schloegl@ist.ac.at>
2019-04-14 20:37:04 +0200

    pkg edfbrowser: update v1.67

--- src/edfbrowser.mk
+++ src/edfbrowser.mk
@@ -5,12 +5,12 @@ PKG             := edfbrowser
 $(PKG)_WEBSITE  := https://www.teuniz.net/edfbrowser/
 $(PKG)_DESCR    := EDFbrowser
 $(PKG)_IGNORE   := 
-$(PKG)_VERSION  := 1.66
-$(PKG)_CHECKSUM := 8cab954cc195a721500a78ec699d4459d6e2242ef1ac3bade8ee405557c5602f
-$(PKG)_SUBDIR   := edfbrowser_166_source
+$(PKG)_VERSION  := 1.67
+$(PKG)_CHECKSUM := fd3e1fbf5926817403ac3bef41f77cddfd921bc6c2fd63de23962f00f51128ed
+$(PKG)_SUBDIR   := edfbrowser_167_source
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
-$(PKG)_URL      := https://github.com/Teuniz/EDFbrowser/releases
-                   https://www.teuniz.net/edfbrowser/$($(PKG)_FILE)
+$(PKG)_URL      := https://www.teuniz.net/edfbrowser/$($(PKG)_FILE)
+                   https://gitlab.com/Teuniz/EDFbrowser/releases
 $(PKG)_GH_CONF  := Teuniz/EDFbrowser/releases v
 $(PKG)_QT_DIR   := qt5
 $(PKG)_DEPS     := cc qtbase
