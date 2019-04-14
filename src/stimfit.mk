f8a39ef6038634eba61b0952b97649a68af53168
Alois Schloegl <alois.schloegl@ist.ac.at>
2019-04-14 10:49:49 +0200

    pkg stimfit: upgrade to v0.15.8

--- src/stimfit.mk
+++ src/stimfit.mk
@@ -3,23 +3,24 @@
 
 PKG             := stimfit
 $(PKG)_IGNORE   := 
-$(PKG)_VERSION  := 0.15.5windows
-$(PKG)_CHECKSUM := 2a2199606eb806f40318e4d0b6c6d05e58cd8cfdf51d584cd7822b04729f769f
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := v$($(PKG)_VERSION).tar.gz
+$(PKG)_VERSION  := 0.15.8
+$(PKG)_CHECKSUM := 8a5330612245d3f442ed640b0df91028aa4798301bb6844eaf1cf9b463dfc466
+$(PKG)_SUBDIR   := stimfit-$($(PKG)_VERSION)windows
+$(PKG)_FILE     := stimfit-$($(PKG)_VERSION)windows.tar.gz
 $(PKG)_URL      := https://github.com/neurodroid/$(PKG)/archive/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc libbiosig wxwidgets hdf5 boost fftw libtool levmar
+$(PKG)_DEPS     := cc libbiosig wxwidgets hdf5 boost fftw levmar openblas
 
 define $(PKG)_UPDATE
-    wget -q -O- 'https://github.com/neurodroid/stimfit/releases' | \
-    $(SED) -n 's_.*<a href="/neurodroid/stimfit/tree/\([0-9\.]*\)\.tar\.gz.*_\1_ip' | \
+    $(WGET) -q -O- 'https://github.com/neurodroid/stimfit/releases' | \
+    $(SED) -n 's_.*<a href="/neurodroid/stimfit/archive/\([0-9\.]*\)windows.tar.gz" rel="nofollow">.*_\1_ip' \
     head -1
 endef
 
 define $(PKG)_BUILD
 
-    cd '$(1)' && ./autogen.sh && \
+    cd '$(1)' && ./autogen.sh && CPPFLAGS="-std=gnu++11" \
 	./configure --disable-python --with-biosig2 --with-pslope \
+		--with-hdf5-prefix=$(PREFIX)/$(TARGET) \
 		--with-wx-config=$(PREFIX)/$(TARGET)/bin/wx-config \
 		--with-sysroot=$(PREFIX)/$(TARGET)/bin \
 		--host='$(TARGET)' \
@@ -36,6 +37,10 @@ define $(PKG)_BUILD
 	make -C '$(1)' -f Makefile.static -j '$(JOBS)'
 
     $(INSTALL) '$(1)/stimfit.exe' '$(PREFIX)/$(TARGET)/bin/'
+
+   -$(INSTALL) '$(1)'/stimfit.exe /fs3/group/jonasgrp/Software/Stimfit/stimfit.$(TARGET).$(shell date +%Y%m%d).exe
+
+   -(cd /fs3/group/jonasgrp/Software/Stimfit/ && ln sf stimfit.$(TARGET).$(shell date +%Y%m%d).exe stimfit.$(TARGET).LATEST.exe)
 
 endef
 
