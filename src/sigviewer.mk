89e023d2668679695f19d44be9dfe12b4fca8c72
Alois Schloegl <alois.schloegl@ist.ac.at>
2019-04-23 23:03:52 +0200

    pkg sigviewer: update to v0.6.4

--- src/sigviewer.mk
+++ src/sigviewer.mk
@@ -3,9 +3,8 @@
 
 PKG             := sigviewer
 $(PKG)_IGNORE   := 
-$(PKG)_VERSION  := 0.6.3
-$(PKG)_CHECKSUM := 5fb5dfb84574920fc8bbdfd9d6c30b136e501cfd5a9f71a8790d6fac49ebac3c
-#ddbe6a96802af73c0cee8dfc80d3ba4ca47f9bce9492713cf6da6aa049244b09
+$(PKG)_VERSION  := 0.6.4
+$(PKG)_CHECKSUM := e64516b0d5a2ac65b1ef496a6666cdac8919b67eecd8d5eb6b7cbf2493314367
 $(PKG)_SUBDIR   := sigviewer-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := https://github.com/cbrnr/$(PKG)/archive/v$($(PKG)_VERSION).tar.gz
@@ -13,8 +12,8 @@ $(PKG)_QT_DIR   := qt5
 $(PKG)_DEPS     := libbiosig libxdf qtbase
 
 define $(PKG)_UPDATE
-#    wget -q -O- 'http://biosig.sourceforge.net/download.html' | \
-#    $(SED) -n 's_.*>libbiosig, version \([0-9]\.[0-9]\.[0-9]\).*tar.gz_\1_ip'
+    wget -q -O- 'http://biosig.sourceforge.net/download.html' | \
+    $(SED) -n 's_.*>libbiosig, version \([0-9]\.[0-9]\.[0-9]\).*tar.gz_\1_ip'
     head -1
 endef
 
