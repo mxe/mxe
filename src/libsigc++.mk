9610bbd9b0444457c62241d44141bdaaacbe6c49
Mark Brand <mabrand@mabrand.nl>
2019-05-14 08:50:50 +0200

    fix update macro: libsigc++

--- src/libsigc++.mk
+++ src/libsigc++.mk
@@ -5,17 +5,8 @@ $(PKG)_WEBSITE  := https://libsigc.sourceforge.io/
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.4.0
 $(PKG)_CHECKSUM := 7593d5fa9187bbad7c6868dce375ce3079a805f3f1e74236143bceb15a37cd30
-$(PKG)_SUBDIR   := libsigc++-$($(PKG)_VERSION)
-$(PKG)_FILE     := libsigc++-$($(PKG)_VERSION).tar.xz
-$(PKG)_URL      := https://download.gnome.org/sources/libsigc++/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
+$(PKG)_GH_CONF  := libsigcplusplus/libsigcplusplus/tags
 $(PKG)_DEPS     := cc
-
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://git.gnome.org/browse/libsigc++2/refs/tags' | \
-    grep '<a href=' | \
-    $(SED) -n 's,.*<a[^>]*>\([0-9][^<]*\)<.*,\1,p' | \
-    head -1
-endef
 
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
