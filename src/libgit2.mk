414d3f8b7a39bfee637648182eaad4d93c3c9978
Mark Brand <mabrand@mabrand.nl>
2019-05-21 20:39:40 +0200

    update libgit2

--- src/libgit2.mk
+++ src/libgit2.mk
@@ -3,8 +3,8 @@
 PKG             := libgit2
 $(PKG)_WEBSITE  := https://libgit2.github.com/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.28.1
-$(PKG)_CHECKSUM := 0ca11048795b0d6338f2e57717370208c2c97ad66c6d5eac0c97a8827d13936b
+$(PKG)_VERSION  := 0.28.2
+$(PKG)_CHECKSUM := 42b5f1e9b9159d66d86fff0394215c5733b6ef8f9b9d054cdd8c73ad47177fc3
 $(PKG)_GH_CONF  := libgit2/libgit2/releases/latest,v
 $(PKG)_DEPS     := cc libssh2
 
