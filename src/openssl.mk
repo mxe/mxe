5da6b84c7a99aaab6f03f54688fd916baddf4023
Mark Brand <mabrand@mabrand.nl>
2019-05-29 21:36:19 +0200

    update openssl

--- src/openssl.mk
+++ src/openssl.mk
@@ -3,8 +3,8 @@
 PKG             := openssl
 $(PKG)_WEBSITE  := https://www.openssl.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.1.1b
-$(PKG)_CHECKSUM := 5c557b023230413dfb0756f3137a13e6d726838ccd1430888ad15bfb2b43ea4b
+$(PKG)_VERSION  := 1.1.1c
+$(PKG)_CHECKSUM := f6fb3079ad15076154eda9413fed42877d668e7069d9b87396d0804fdb3f4c90
 $(PKG)_SUBDIR   := openssl-$($(PKG)_VERSION)
 $(PKG)_FILE     := openssl-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://www.openssl.org/source/$($(PKG)_FILE)
