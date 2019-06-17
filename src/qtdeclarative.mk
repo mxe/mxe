0567f17d34463abda1790957812c54e8c0cf59fe
Mark Brand <mabrand@mabrand.nl>
2019-06-17 11:39:55 +0200

    qt5: update to 5.12.4

--- src/qtdeclarative.mk
+++ src/qtdeclarative.mk
@@ -5,11 +5,11 @@ $(PKG)_WEBSITE  := https://www.qt.io/
 $(PKG)_DESCR    := Qt
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION   = $(qtbase_VERSION)
-$(PKG)_CHECKSUM := 839881cd6996e35c351bc7d560372ebb91e61f3688957c33248c4f31ea007fa7
+$(PKG)_CHECKSUM := 614105ed73079d67d81b34fef31c9934c5e751342e4b2e0297128c8c301acda7
 $(PKG)_SUBDIR    = $(subst qtbase,qtdeclarative,$(qtbase_SUBDIR))
 $(PKG)_FILE      = $(subst qtbase,qtdeclarative,$(qtbase_FILE))
 $(PKG)_URL       = $(subst qtbase,qtdeclarative,$(qtbase_URL))
-$(PKG)_DEPS     := cc qtbase qtsvg qtxmlpatterns
+$(PKG)_DEPS     := cc qtbase qtimageformats qtsvg qtxmlpatterns
 $(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
 
 define $(PKG)_UPDATE
