2798e5004f0d2a7b918143fb224b69777deef214
Tony Theodore <tonyt@logyst.com>
2019-06-28 18:49:33 +1000

    m4: update 1.4.17 --> 1.4.18

--- plugins/native/m4.mk
+++ plugins/native/m4.mk
@@ -2,8 +2,8 @@
 
 PKG             := m4
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.4.17
-$(PKG)_CHECKSUM := f0543c3beb51fa6b3337d8025331591e0e18d8ec2886ed391f1aade43477d508
+$(PKG)_VERSION  := 1.4.18
+$(PKG)_CHECKSUM := f2c1e86ca0a404ff281631bdc8377638992744b175afb806e25871a24a934e07
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/m4/$($(PKG)_FILE)
@@ -22,10 +22,9 @@ endef
 
 define $(PKG)_BUILD_$(BUILD)
     # gets has been removed from recent glibc
-    $(SED) -i -e '/gets is a/d' '$(1)/lib/stdio.in.h'
-    mkdir '$(1).build'
-    cd    '$(1).build' && '$(1)/configure' \
+    $(SED) -i -e '/gets is a/d' '$(SOURCE_DIR)/lib/stdio.in.h'
+    cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
         --prefix='$(PREFIX)/$(TARGET)'
-    $(MAKE) -C '$(1).build' -j '$(JOBS)'
-    $(MAKE) -C '$(1).build' -j 1 install
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
