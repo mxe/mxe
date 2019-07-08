e045e5c198ed4516a31f5e62ceb319174f663a0f
Mark Brand <mabrand@mabrand.nl>
2019-07-08 12:23:24 +0200

    update vmime

--- src/vmime.mk
+++ src/vmime.mk
@@ -4,8 +4,8 @@ PKG             := vmime
 $(PKG)_WEBSITE  := https://www.vmime.org/
 $(PKG)_DESCR    := VMime
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 8f4db13
-$(PKG)_CHECKSUM := 45d9b9ea70442455ffd146eca4511019cb2625033fde43e6bdcd60389f662b86
+$(PKG)_VERSION  := 523aacb
+$(PKG)_CHECKSUM := eeb9002c8f4061f4efbe851b40217d2cbec4ac067444a552903eaa60240f37c2
 $(PKG)_GH_CONF  := kisli/vmime/branches/master
 $(PKG)_DEPS     := cc gnutls libgsasl libiconv pthreads zlib
 
