64175fb420847f7143e8190cd5731c712e6f8eb2
Jonas Kvinge <jonas@jkvinge.net>
2019-07-19 22:36:29 +0200

    Update cmake

--- src/cmake.mk
+++ src/cmake.mk
@@ -3,8 +3,8 @@
 PKG             := cmake
 $(PKG)_WEBSITE  := https://www.cmake.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.10.0
-$(PKG)_CHECKSUM := b3345c17609ea0f039960ef470aa099de9942135990930a57c14575aae884987
+$(PKG)_VERSION  := 3.14.5
+$(PKG)_CHECKSUM := 505ae49ebe3c63c595fa5f814975d8b72848447ee13b6613b0f8b96ebda18c06
 $(PKG)_SUBDIR   := cmake-$($(PKG)_VERSION)
 $(PKG)_FILE     := cmake-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://www.cmake.org/files/v$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
