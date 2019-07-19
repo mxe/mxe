e88b0d9fb4e8abb1dc58f2521527a898d97a56d0
Jonas Kvinge <jonas@jkvinge.net>
2019-07-19 22:38:20 +0200

    Update boost

--- src/boost-test.cpp
+++ src/boost-test.cpp
@@ -3,6 +3,8 @@
  */
 
 #include <iostream>
+#include <tuple>
+
 #include <boost/archive/xml_oarchive.hpp>
 #include <boost/thread/thread.hpp>
 #include <boost/thread/tss.hpp>
@@ -11,54 +13,53 @@ boost::thread_specific_ptr<int> ptr;
 
 // https://www.boost.org/doc/libs/1_60_0/libs/context/doc/html/context/context.html
 #include <boost/context/all.hpp>
-boost::context::fcontext_t fcm,fc1,fc2;
 
 void test_thread()
 {
-    if (ptr.get() == 0) {
-        ptr.reset(new int(0));
-    }
-    std::cout << "Hello, World! from thread" << std::endl;
+	if (ptr.get() == 0) {
+		ptr.reset(new int(0));
+	}
+	std::cout << "Hello, World! from thread" << std::endl;
 }
 
-void f1(intptr_t)
+int main(int, char * [])
 {
-    std::cout<<"f1: entered"<<std::endl;
-    std::cout<<"f1: call jump_fcontext( & fc1, fc2, 0)"<< std::endl;
-    boost::context::jump_fcontext(&fc1,fc2,0);
-    std::cout<<"f1: return"<<std::endl;
-    boost::context::jump_fcontext(&fc1,fcm,0);
-}
+	{
+		boost::archive::xml_oarchive oa(std::cout);
+		std::string s = "\n\n    Hello, World!\n\n";
+		oa << BOOST_SERIALIZATION_NVP(s);
+	}
 
-void f2(intptr_t)
-{
-    std::cout<<"f2: entered"<<std::endl;
-    std::cout<<"f2: call jump_fcontext( & fc2, fc1, 0)"<<std::endl;
-    boost::context::jump_fcontext(&fc2,fc1,0);
-    BOOST_ASSERT(false&&!"f2: never returns");
-}
+	{
+		boost::thread thrd(test_thread);
+		thrd.join();
+	}
 
-int main(int argc, char *argv[])
-{
-    (void)argc;
-    (void)argv;
+	{
+		namespace ctx = boost::context;
+		int data = 0;
+		ctx::continuation c = ctx::callcc([&data](ctx::continuation && c) {
+			std::cout << "f1: entered first time: " << data << std::endl;
+			data += 1;
+			c = c.resume();
+			std::cout << "f1: entered second time: " << data << std::endl;
+			data += 1;
+			c = c.resume();
+			std::cout << "f1: entered third time: " << data << std::endl;
+			return std::move(c);
+		});
+		std::cout << "f1: returned first time: " << data << std::endl;
+		data += 1;
+		c = c.resume();
+		std::cout << "f1: returned second time: " << data << std::endl;
+		data += 1;
+		c = c.resume_with([&data](ctx::continuation && c) {
+			std::cout << "f2: entered: " << data << std::endl;
+			data = -1;
+			return std::move(c);
+		});
+		std::cout << "f1: returned third time" << std::endl;
+	}
 
-    boost::archive::xml_oarchive oa(std::cout);
-    std::string s = "\n\n    Hello, World!\n\n";
-    oa << BOOST_SERIALIZATION_NVP(s);
-
-    boost::thread thrd(test_thread);
-    thrd.join();
-
-    std::size_t size(8192);
-    void* sp1(std::malloc(size));
-    void* sp2(std::malloc(size));
-
-    fc1=boost::context::make_fcontext(sp1,size,f1);
-    fc2=boost::context::make_fcontext(sp2,size,f2);
-
-    std::cout<<"main: call jump_fcontext( & fcm, fc1, 0)"<<std::endl;
-    boost::context::jump_fcontext(&fcm,fc1,0);
-
-    return 0;
+	return 0;
 }
