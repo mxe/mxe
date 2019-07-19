f7e6ee86ea3b66fad84e77a3281b9cc8e6efc472
Jonas Kvinge <jonas@jkvinge.net>
2019-07-19 22:51:51 +0200

    Update libcdio

--- src/libcdio.mk
+++ src/libcdio.mk
@@ -4,16 +4,16 @@ PKG             := libcdio
 $(PKG)_WEBSITE  := https://www.gnu.org/software/libcdio/
 $(PKG)_DESCR    := Libcdio
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.94
-$(PKG)_CHECKSUM := 96e2c903f866ae96f9f5b9048fa32db0921464a2286f5b586c0f02699710025a
+$(PKG)_VERSION  := 2.0.0
+$(PKG)_CHECKSUM := 1b481b5da009bea31db875805665974e2fc568e2b2afa516f4036733657cf958
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/libcdio/$(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := cc
 
 define $(PKG)_UPDATE
-    echo 'TODO: Updates for package libcdio need to be written.' >&2;
-    echo $(libcdio_VERSION)
+    echo 'TODO: Updates for package $(PKG) need to be written.' >&2;
+    echo $($(PKG)_VERSION)
 endef
 
 define $(PKG)_BUILD
