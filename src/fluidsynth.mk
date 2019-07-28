edb91b9a910131ed912412a3271bc6641708907b
Lothar Serra Mari <serra@scummvm.org>
2019-07-28 14:38:34 +0200

    New package: fluidsynth

---
+++ src/fluidsynth.mk
@@ -0,0 +1,18 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := fluidsynth
+$(PKG)_WEBSITE  := http://fluidsynth.org/
+$(PKG)_DESCR    := FluidSynth - a free software synthesizer based on the SoundFont 2 specifications
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.0.5
+$(PKG)_CHECKSUM := 69b244512883491e7e66b4d0151c61a0d6d867d4d2828c732563be0f78abcc51
+$(PKG)_GH_CONF  := FluidSynth/fluidsynth/tags,v
+$(PKG)_DEPS     := cc glib
+
+define $(PKG)_BUILD
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' '$(SOURCE_DIR)' \
+        -DBUILD_SHARED_LIBS=$(CMAKE_SHARED_BOOL) \
+	-DCMAKE_BUILD_TYPE=Release
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1
+endef
