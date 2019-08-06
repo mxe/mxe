bdbbd6a6c15afd7a9a22acf40f1df784bd6de2dd
Martin Müllenhaupt <mm+git@netlair.de>
2019-08-06 15:16:05 +0200

    package qtifw: update to 3.1.1, build test

--- src/qtifw.mk
+++ src/qtifw.mk
@@ -4,11 +4,11 @@ PKG             := qtifw
 $(PKG)_WEBSITE  := https://doc.qt.io/qtinstallerframework/index.html
 $(PKG)_DESCR    := Qt Installer Framework
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.0.4
-$(PKG)_CHECKSUM := a4ecafc37086f96a833463214f873caac977199e64f0b1453aa49bdd6f24f32e
-$(PKG)_SUBDIR    = qt-installer-framework-opensource-src-$($(PKG)_VERSION)
-$(PKG)_FILE     := $($(PKG)_SUBDIR).zip
-$(PKG)_URL      := https://download.qt.io/official_releases/qt-installer-framework/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_VERSION  := 3.1.1
+$(PKG)_CHECKSUM := 59b5370aaf521bb1a34a025ac451bb3bbbfa519ee271156aba9d42ee1132d1b1
+# the archive is in fact only a tar file, not a tar.gz
+$(PKG)_FILE     := qtifw-$($(PKG)_VERSION).tar
+$(PKG)_URL      := https://download.qt.io/official_releases/qt-installer-framework/$($(PKG)_VERSION)/qt-installer-framework-opensource-src-$($(PKG)_VERSION).tar.gz
 $(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
 
 $(PKG)_DEPS_$(BUILD) := cc qtbase qttools
@@ -28,4 +28,7 @@ define $(PKG)_BUILD_STATIC
     cd '$(1)' && '$(PREFIX)/$(TARGET)/qt5/bin/qmake'
     $(MAKE) -C '$(1)' -j '$(JOBS)' || $(MAKE) -C '$(1)' -j  1
     $(MAKE) -C '$(1)' -j 1 install
+
+    # build the tutorial installer in /tmp, because the binarycreator internal rename will fail if /tmp is not in the same filesystem as mxe
+    cd '$(1)examples/tutorial' && '$(PREFIX)/bin/$(BUILD)-binarycreator' -c config/config.xml -p packages -t $(PREFIX)/$(TARGET)/qt5/bin/installerbase.exe /tmp/test-$(PKG)-tutorialinstaller.exe && mv /tmp/test-$(PKG)-tutorialinstaller.exe $(PREFIX)/$(TARGET)/bin/test-$(PKG)-tutorialinstaller.exe
 endef
