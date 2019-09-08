e9026faca5d36d92fb6f82012e7bbab62c7f4e31
Tony Theodore <tonyt@logyst.com>
2019-09-08 18:31:04 +1000

    fluidsynth: update, patch cmake to remove manual sed/instal, add deps
    closes #2378

--- src/fluidsynth.mk
+++ src/fluidsynth.mk
@@ -4,25 +4,23 @@ PKG             := fluidsynth
 $(PKG)_WEBSITE  := http://fluidsynth.org/
 $(PKG)_DESCR    := FluidSynth - a free software synthesizer based on the SoundFont 2 specifications
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.0.5
-$(PKG)_CHECKSUM := 69b244512883491e7e66b4d0151c61a0d6d867d4d2828c732563be0f78abcc51
+$(PKG)_VERSION  := 2.0.6
+$(PKG)_CHECKSUM := e97e63c1045e102465f1aa848f9d712c5528c58685b8d40062e4aaf6af7edb75
 $(PKG)_GH_CONF  := FluidSynth/fluidsynth/tags,v
-$(PKG)_DEPS     := cc glib
+$(PKG)_DEPS     := cc dbus glib jack libsndfile portaudio
 
 define $(PKG)_BUILD
-    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' '$(SOURCE_DIR)'
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' '$(SOURCE_DIR)' \
+        -Dbuild-bins=OFF \
+        -Dbuild-docs=OFF \
+        -Dbuild-tests=OFF \
+        -Denable-dbus=ON \
+        -Denable-jack=$(CMAKE SHARED_BOOL) \
+        -Denable-libsndfile=ON \
+        -Denable-portaudio=ON \
+        $($(PKG)_CONFIGURE_OPTS)
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
-
-    echo 'Requires: glib-2.0' >> '$(BUILD_DIR)'/fluidsynth.pc
-    $(SED) -i -e 's/Libs: -L$${libdir} -lfluidsynth/Libs: -L$${libdir} -lfluidsynth -ldsound/g' '$(BUILD_DIR)/fluidsynth.pc'
-    $(INSTALL) -d '$(PREFIX)/$(TARGET)/include/fluidsynth'
-    $(INSTALL) -v '$(BUILD_DIR)/include/fluidsynth.h'    '$(PREFIX)/$(TARGET)/include/'
-    $(INSTALL) -v '$(BUILD_DIR)/include/fluidsynth/'*.h  '$(PREFIX)/$(TARGET)/include/fluidsynth/'
-    $(INSTALL) -v '$(SOURCE_DIR)/include/fluidsynth/'*.h '$(PREFIX)/$(TARGET)/include/fluidsynth/'
-    $(INSTALL) -v '$(BUILD_DIR)/fluidsynth.pc'           '$(PREFIX)/$(TARGET)/lib/pkgconfig/' 
-    $(INSTALL) -v '$(BUILD_DIR)/src/libfluidsynth.a'     '$(PREFIX)/$(TARGET)/lib/'
-    $(if $(BUILD_SHARED),\
-    $(INSTALL) -m755 -v '$(BUILD_DIR)/src/libfluidsynth.dll.a' '$(PREFIX)/$(TARGET)/bin/')
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1
 
     # compile test
     '$(TARGET)-gcc' \
