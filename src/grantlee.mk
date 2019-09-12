1223c429fb81ede3b31be5a4f207324973a6641d
Paul Buxton <paulbuxton.mail@googlemail.com>
2019-09-12 18:20:32 +0100

    Add website info and description.

--- src/grantlee.mk
+++ src/grantlee.mk
@@ -1,8 +1,8 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := grantlee
-$(PKG)_WEBSITE  := 
-$(PKG)_DESCR    := 
+$(PKG)_WEBSITE  := https://github.com/steveire/grantlee
+$(PKG)_DESCR    := Grantlee is a set of Free Software libraries written using the Qt framework
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 5.1.0
 $(PKG)_CHECKSUM := 3836572fe5e49d28a1d99186c6d96f88ff839644b4bc77b73b6d8208f6ccc9d1
@@ -14,5 +14,4 @@ define $(PKG)_BUILD
          $($(PKG)_CONFIGURE_OPTS)
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1
-
  endef
