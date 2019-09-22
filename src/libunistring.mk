be6cca0d153e35e83d2736c3527eedb5baa9dbad
Gregorio Litenstein <g.litenstein@gmail.com>
2019-09-22 10:23:54 -0300

    libunistring: Remove hardcoded win32threads.

--- src/libunistring.mk
+++ src/libunistring.mk
@@ -20,7 +20,7 @@ endef
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
-        --enable-threads=win32
+        --enable-threads=$(MXE_GCC_THREADS)
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     $(MAKE) -C '$(1)' -j 1 install
 endef
