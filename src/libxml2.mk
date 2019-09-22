c89ab92d700135f08de3e1c0b159f3d85dbfa1a2
Gregorio Litenstein <g.litenstein@gmail.com>
2019-09-22 10:23:38 -0300

    libxml2: Enable threads.

--- src/libxml2.mk
+++ src/libxml2.mk
@@ -24,7 +24,7 @@ define $(PKG)_BUILD
         --with-zlib='$(PREFIX)/$(TARGET)/lib' \
         --without-debug \
         --without-python \
-        --without-threads
+        --with-threads=$(MXE_GCC_THREADS)
     $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
     $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
     ln -sf '$(PREFIX)/$(TARGET)/bin/xml2-config' '$(PREFIX)/bin/$(TARGET)-xml2-config'
