b674d64583e31fb3596d4c6aaa560adef691f437
Mark Brand <mabrand@mabrand.nl>
2019-10-04 14:23:44 +0200

    update harfbuzz

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -4,8 +4,8 @@ PKG             := harfbuzz
 $(PKG)_WEBSITE  := https://wiki.freedesktop.org/www/Software/HarfBuzz/
 $(PKG)_DESCR    := HarfBuzz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.6.1
-$(PKG)_CHECKSUM := 8a5eb4ab0dc1634040307eecbc8cf1f1b65924a8b679aa346b49800aac3be692
+$(PKG)_VERSION  := 2.6.2
+$(PKG)_CHECKSUM := 56a9bc6470751ae0fd8aa1912721c1596cf15900183dca9a3e10501422bab2b9
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases
 $(PKG)_DEPS     := cc cairo freetype-bootstrap glib icu4c
 
