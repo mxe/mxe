d3020a238ae5319f4c2c15f944c19fcbc1a6e38a
Gregorio Litenstein <g.litenstein@gmail.com>
2019-10-09 01:32:35 -0300

    assimp: Update to 4.1.0

--- src/assimp.mk
+++ src/assimp.mk
@@ -4,8 +4,8 @@ PKG             := assimp
 $(PKG)_WEBSITE  := https://assimp.sourceforge.io/
 $(PKG)_DESCR    := Assimp Open Asset Import Library
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.2
-$(PKG)_CHECKSUM := 187f825c563e84b1b17527a4da0351aa3d575dfd696a9d204ae4bb19ee7df94a
+$(PKG)_VERSION  := 4.1.0
+$(PKG)_CHECKSUM := 3520b1e9793b93a2ca3b797199e16f40d61762617e072f2d525fad70f9678a71
 $(PKG)_GH_CONF  := assimp/assimp/tags, v
 $(PKG)_DEPS     := cc boost minizip
 
