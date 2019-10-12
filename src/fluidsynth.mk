19eea2c659ebd21f79a17f10d2c870ff1877166d
Tony Theodore <tonyt@logyst.com>
2019-10-13 00:51:11 +1100

    fluidsynth: enable readline support

--- src/fluidsynth.mk
+++ src/fluidsynth.mk
@@ -7,7 +7,7 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.0.6
 $(PKG)_CHECKSUM := e97e63c1045e102465f1aa848f9d712c5528c58685b8d40062e4aaf6af7edb75
 $(PKG)_GH_CONF  := FluidSynth/fluidsynth/tags,v
-$(PKG)_DEPS     := cc dbus glib jack libsndfile mman-win32 portaudio
+$(PKG)_DEPS     := cc dbus glib jack libsndfile mman-win32 portaudio readline
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && '$(TARGET)-cmake' '$(SOURCE_DIR)' \
@@ -18,6 +18,7 @@ define $(PKG)_BUILD
         -Denable-jack=$(CMAKE SHARED_BOOL) \
         -Denable-libsndfile=ON \
         -Denable-portaudio=ON \
+        -Denable-readline=ON \
         $($(PKG)_CONFIGURE_OPTS)
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1
