75977c5b2c385758d70079b2c82b6f32111baec4
Gregorio Litenstein <g.litenstein@gmail.com>
2019-10-11 22:26:52 -0300

    IlmBase / OpenEXR: Update to 2.5.1, remove patches
    My fixes have been merged upstream, rendering patches innecessary and greatly simplifying builds.

--- src/ilmbase.mk
+++ src/ilmbase.mk
@@ -4,38 +4,19 @@ PKG             := ilmbase
 $(PKG)_WEBSITE  := https://www.openexr.com/
 $(PKG)_DESCR    := IlmBase
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.3.0
-$(PKG)_CHECKSUM := 456978d1a978a5f823c7c675f3f36b0ae14dba36638aeaa3c4b0e784f12a3862
-$(PKG)_SUBDIR   := ilmbase-$($(PKG)_VERSION)
-$(PKG)_FILE     := ilmbase-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://download.savannah.nongnu.org/releases/openexr/$($(PKG)_FILE)
+$(PKG)_VERSION  := 2.5.1
+$(PKG)_CHECKSUM := 11f806bf256453e39fc33bd1cf1fa576a54f144cedcdd3e6935a177e5a89d02e
+$(PKG)_GH_CONF  := AcademySoftwareFoundation/openexr/releases,v
+$(PKG)_FILE     := openexr-v$($(PKG)_VERSION).tar.gz
+$(PKG)_SUBDIR   := openexr-$($(PKG)_VERSION)
 $(PKG)_DEPS     := cc
 
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://www.openexr.com/downloads.html' | \
-    grep 'ilmbase-' | \
-    $(SED) -n 's,.*/ilmbase-\([0-9][^>]*\)\.tar.*,\1,p' | \
-    head -1
-endef
-
-GCC_VERSION_MAJOR := $(shell echo $(gcc_VERSION) | cut -f1 -d.)
-GCC_VERSION_MINOR := $(shell echo $(gcc_VERSION) | cut -f2 -d.)
-
-$(PKG)_CXXSTD_14 := $(shell [ $(GCC_VERSION_MAJOR) -gt 6 -o \( $(GCC_VERSION_MAJOR) -eq 6 -a $(GCC_VERSION_MINOR) -ge 1 \) ] && echo true)
-
 define $(PKG)_BUILD
-    cd '$(1)' && $(SHELL) ./configure \
-        $(MXE_CONFIGURE_OPTS) \
-        --enable-threading \
-        --disable-posix-sem \
-        --enable-cxxstd=$(if $($(PKG)_CXXSTD_14),14,11) \
-        CONFIG_SHELL=$(SHELL) \
-        SHELL=$(SHELL)
-    # do the first build step by hand, because programs are built that
-    # generate source files
-    cd '$(1)/Half' && $(BUILD_CXX) eLut.cpp -o eLut
-    '$(1)/Half/eLut' > '$(1)/eLut.h'
-    cd '$(1)/Half' && $(BUILD_CXX) toFloat.cpp -o toFloat
-    '$(1)/Half/toFloat' > '$(1)/toFloat.h'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake \
+        -DOPENEXR_CXX_STANDARD=11 \
+        -DBUILD_TESTING=OFF \
+        -DILMBASE_INSTALL_PKG_CONFIG=ON \
+        "$(SOURCE_DIR)"/IlmBase
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
--- src/openexr.mk
+++ src/openexr.mk
@@ -1,76 +1,28 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
-PKG             := openexr
-$(PKG)_WEBSITE  := https://www.openexr.com/
-$(PKG)_DESCR    := OpenEXR
-$(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.3.0
-$(PKG)_CHECKSUM := fd6cb3a87f8c1a233be17b94c74799e6241d50fc5efd4df75c7a4b9cf4e25ea6
-$(PKG)_SUBDIR   := openexr-$($(PKG)_VERSION)
-$(PKG)_FILE     := openexr-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://download.savannah.nongnu.org/releases/openexr/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc ilmbase pthreads zlib
-
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://www.openexr.com/downloads.html' | \
-    grep 'openexr-' | \
-    $(SED) -n 's,.*openexr-\([0-9][^>]*\)\.tar.*,\1,p' | \
-    head -1
-endef
-
-GCC_VERSION_MAJOR := $(shell echo $(gcc_VERSION) | cut -f1 -d.)
-GCC_VERSION_MINOR := $(shell echo $(gcc_VERSION) | cut -f2 -d.)
-
-$(PKG)_CXXSTD_14 := $(shell [ $(GCC_VERSION_MAJOR) -gt 6 -o \( $(GCC_VERSION_MAJOR) -eq 6 -a $(GCC_VERSION_MINOR) -ge 1 \) ] && echo true)
+PKG              := openexr
+$(PKG)_WEBSITE   := https://www.openexr.com/
+$(PKG)_DESCR     := OpenEXR
+$(PKG)_IGNORE    = $(ilmbase_IGNORE)
+$(PKG)_VERSION   = $(ilmbase_VERSION)
+$(PKG)_CHECKSUM  = $(ilmbase_CHECKSUM)
+$(PKG)_SUBDIR    = openexr-$($(PKG)_VERSION)
+$(PKG)_FILE      = $(ilmbase_FILE)
+$(PKG)_URL       = $(ilmbase_URL)
+$(PKG)_DEPS      = cc ilmbase pthreads zlib $(BUILD)~cmake
+$(PKG)_PATCHES   = $(ilmbase_PATCHES)
 
 define $(PKG)_BUILD
-    # Update auto-stuff, except autoheader, because if fails...
-    cd '$(1)' && AUTOHEADER=true autoreconf -fi
-    # unpack and build a native version of ilmbase
-    cd '$(1)' && $(call UNPACK_PKG_ARCHIVE,ilmbase)
-# Don't apply ilmbase patches.
-#    $(foreach PKG_PATCH,$(sort $(wildcard $(TOP_DIR)/src/ilmbase-*.patch)),
- #       (cd '$(1)/$(ilmbase_SUBDIR)' && $(PATCH) -p1 -u) < $(PKG_PATCH))
-    echo 'echo $1' > '$(1)/$(ilmbase_SUBDIR)/config.sub'
-    cd '$(1)/$(ilmbase_SUBDIR)' && $(SHELL) ./configure \
-        --build="`config.guess`" \
-        --disable-shared \
-        --prefix='$(1)/ilmbase' \
-        --enable-threading \
-        --disable-posix-sem \
-        --enable-cxxstd=$(if $($(PKG)_CXXSTD_14),14,11) \
-        CONFIG_SHELL=$(SHELL) \
-        SHELL=$(SHELL)
-    $(MAKE) -C '$(1)/$(ilmbase_SUBDIR)' -j '$(JOBS)' install \
-        bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
-    cd '$(1)' && ./configure \
-        $(MXE_CONFIGURE_OPTS) \
-        --enable-threading \
-        --disable-posix-sem \
-        --enable-cxxstd=$(if $($(PKG)_CXXSTD_14),14,11) \
-        --disable-ilmbasetest \
-        PKG_CONFIG='$(PREFIX)/bin/$(TARGET)-pkg-config' \
-        CXXFLAGS="-g -O2"
-    # build the code generator manually
-    cd '$(1)/IlmImf' && $(BUILD_CXX) -O2 \
-        -I'$(1)/ilmbase/include/OpenEXR' \
-        -L'$(1)/ilmbase/lib' \
-        b44ExpLogTable.cpp \
-        -lHalf \
-        -o b44ExpLogTable
-    '$(1)/IlmImf/b44ExpLogTable' > '$(1)/IlmImf/b44ExpLogTable.h'
-    cd '$(1)/IlmImf' && $(BUILD_CXX) -O2 \
-        -I'$(1)/config' -I. \
-        -I'$(1)/ilmbase/include/OpenEXR' \
-        -L'$(1)/ilmbase/lib' \
-        dwaLookups.cpp \
-        -lHalf -lIlmThread -lIex -lpthread \
-        -o dwaLookups
-    '$(1)/IlmImf/dwaLookups' > '$(1)/IlmImf/dwaLookups.h'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake \
+        -DOPENEXR_CXX_STANDARD=11 \
+        -DOPENEXR_INSTALL_PKG_CONFIG=ON \
+        -DBUILD_TESTING=OFF \
+        "$(SOURCE_DIR)/OpenEXR"
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 
     '$(TARGET)-g++' \
-        -Wall -Wextra -std=gnu++0x \
+        -Wall -Wextra -std=c++11 \
         '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-openexr.exe' \
         `'$(TARGET)-pkg-config' OpenEXR --cflags --libs`
 endef
