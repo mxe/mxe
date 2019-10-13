7be5f6bd358fd8b4edcbc58928fb99a36115545b
Tony Theodore <tonyt@logyst.com>
2019-10-13 18:37:46 +1100

    cmake: update 3.10.0 --> 3.15.4
    replaces #2293

--- src/cmake.mk
+++ src/cmake.mk
@@ -3,8 +3,8 @@
 PKG             := cmake
 $(PKG)_WEBSITE  := https://www.cmake.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.10.0
-$(PKG)_CHECKSUM := b3345c17609ea0f039960ef470aa099de9942135990930a57c14575aae884987
+$(PKG)_VERSION  := 3.15.4
+$(PKG)_CHECKSUM := 8a211589ea21374e49b25fc1fc170e2d5c7462b795f1b29c84dd0e984301ed7a
 $(PKG)_SUBDIR   := cmake-$($(PKG)_VERSION)
 $(PKG)_FILE     := cmake-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://www.cmake.org/files/v$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
