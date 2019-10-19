ce4fc315b54f4e1321ed8f3c815e6daf0abb5e9b
Alois Schlögl <alois.schloegl@gmail.com>
2019-10-19 22:42:06 +0200

    pkg edfbrowser: upgrade to v1.70

--- src/edfbrowser.mk
+++ src/edfbrowser.mk
@@ -5,16 +5,15 @@ PKG             := edfbrowser
 $(PKG)_WEBSITE  := https://www.teuniz.net/edfbrowser/
 $(PKG)_DESCR    := EDFbrowser
 $(PKG)_IGNORE   := 
-$(PKG)_VERSION  := 1.67
-$(PKG)_CHECKSUM := fd3e1fbf5926817403ac3bef41f77cddfd921bc6c2fd63de23962f00f51128ed
-$(PKG)_SUBDIR   := edfbrowser_167_source
+$(PKG)_VERSION  := 1.70
+$(PKG)_CHECKSUM := 206a19e47416c278fa161c6d9bd78a3a7dd5f2c2b88deb270fb3495ffd3f659d
+$(PKG)_SUBDIR   := edfbrowser_170_source
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := https://www.teuniz.net/edfbrowser/$($(PKG)_FILE)
                    https://gitlab.com/Teuniz/EDFbrowser/releases
 $(PKG)_GH_CONF  := Teuniz/EDFbrowser/releases v
 $(PKG)_QT_DIR   := qt5
 $(PKG)_DEPS     := cc qtbase
-
 
 define $(PKG)_BUILD
 
