2efc4d06171da361f7f1718c7f8465f46d63b110
Mark Brand <mabrand@mabrand.nl>
2019-11-24 20:54:20 +0100

    gcc7 plugin: update to 7.5.0

--- plugins/gcc7/gcc7-overlay.mk
+++ plugins/gcc7/gcc7-overlay.mk
@@ -17,8 +17,8 @@ $(PKG)_URL      := https://isl.gforge.inria.fr/$($(PKG)_FILE)
 $(PKG)_URL_2    := https://gcc.gnu.org/pub/gcc/infrastructure/$($(PKG)_FILE)
 
 PKG             := gcc
-$(PKG)_VERSION  := 7.4.0
-$(PKG)_CHECKSUM := eddde28d04f334aec1604456e536416549e9b1aa137fc69204e65eb0c009fe51
+$(PKG)_VERSION  := 7.5.0
+$(PKG)_CHECKSUM := b81946e7f01f90528a1f7352ab08cc602b9ccc05d4e44da4bd501c5a189ee661
 $(PKG)_SUBDIR   := gcc-$($(PKG)_VERSION)
 $(PKG)_FILE     := gcc-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/gcc/gcc-$($(PKG)_VERSION)/$($(PKG)_FILE)
