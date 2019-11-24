12b085f47b7b577aa96eb441ed1dd00dfbc9685a
Mark Brand <mabrand@mabrand.nl>
2019-11-24 20:38:51 +0100

    update: libtasn1, vmime

--- src/libtasn1.mk
+++ src/libtasn1.mk
@@ -3,8 +3,8 @@
 PKG             := libtasn1
 $(PKG)_WEBSITE  := https://www.gnu.org/software/libtasn1/
 $(PKG)_DESCR    := GnuTLS
-$(PKG)_VERSION  := 4.14
-$(PKG)_CHECKSUM := 9e604ba5c5c8ea403487695c2e407405820d98540d9de884d6e844f9a9c5ba08
+$(PKG)_VERSION  := 4.15.0
+$(PKG)_CHECKSUM := dd77509fe8f5304deafbca654dc7f0ea57f5841f41ba530cff9a5bf71382739e
 $(PKG)_SUBDIR   := libtasn1-$($(PKG)_VERSION)
 $(PKG)_FILE     := libtasn1-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/libtasn1/$($(PKG)_FILE)
@@ -12,7 +12,7 @@ $(PKG)_DEPS     := cc
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- https://ftp.gnu.org/gnu/libtasn1/ | \
-    $(SED) -n 's,.*libtasn1-\([1-9]\+\.[0-9]\+\)\..*,\1,p' | \
+    $(SED) -n 's,.*libtasn1-\([1-9]\+\(\.[0-9]\+\)\+\).*,\1,p' | \
     $(SORT) -V | \
     tail -1
 endef
