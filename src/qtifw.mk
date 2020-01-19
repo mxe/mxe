b97b15372f96a2b6b7a711c65e3ada3a66760502
Tony Theodore <tonyt@logyst.com>
2020-01-19 16:22:37 +1100

    qtifw: update 3.1.1 --> 3.2.0 and fix for Qt 5.14
    also use BUILD_DIR etc

--- src/qtifw.mk
+++ src/qtifw.mk
@@ -4,34 +4,42 @@ PKG             := qtifw
 $(PKG)_WEBSITE  := https://doc.qt.io/qtinstallerframework/index.html
 $(PKG)_DESCR    := Qt Installer Framework
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.1.1
-$(PKG)_CHECKSUM := 59b5370aaf521bb1a34a025ac451bb3bbbfa519ee271156aba9d42ee1132d1b1
-# the archive is in fact only a tar file, not a tar.gz
-$(PKG)_FILE     := qtifw-$($(PKG)_VERSION).tar
+$(PKG)_VERSION  := 3.2.0
+$(PKG)_CHECKSUM := 7cd0d7b86fbe63b101f263b6532485df3a2d336cc0026b47eb866dae134e319f
+$(PKG)_FILE     := qtifw-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://download.qt.io/official_releases/qt-installer-framework/$($(PKG)_VERSION)/qt-installer-framework-opensource-src-$($(PKG)_VERSION).tar.gz
 $(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
 
 $(PKG)_DEPS_$(BUILD) := cc qtbase qttools
 $(PKG)_DEPS_STATIC   := $($(PKG)_DEPS_$(BUILD)) qtwinextras $(BUILD)~$(PKG)
 
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'https://download.qt.io/official_releases/qt-installer-framework/' | \
+    $(SED) -n 's,.*<a href="*\([0-9][^"]*\)/.*,\1,p' | \
+    $(SORT) -V | \
+    tail -1
+endef
+
 define $(PKG)_BUILD_$(BUILD)
-    cd '$(1)' && $(TARGET)-qmake-qt5
-    $(MAKE) -C '$(1)' -j '$(JOBS)'
-    $(INSTALL) -m755 '$(1)/bin/binarycreator' '$(PREFIX)/bin/$(TARGET)-binarycreator'
-    $(INSTALL) -m755 '$(1)/bin/repogen' '$(PREFIX)/bin/$(TARGET)-repogen'
-    $(INSTALL) -m755 '$(1)/bin/archivegen' '$(PREFIX)/bin/$(TARGET)-archivegen'
-    $(INSTALL) -m755 '$(1)/bin/devtool' '$(PREFIX)/bin/$(TARGET)-devtool'
+    cd '$(BUILD_DIR)' && $(TARGET)-qmake-qt5 '$(SOURCE_DIR)'
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(INSTALL) -m755 '$(BUILD_DIR)/bin/binarycreator' '$(PREFIX)/bin/$(TARGET)-binarycreator'
+    $(INSTALL) -m755 '$(BUILD_DIR)/bin/repogen' '$(PREFIX)/bin/$(TARGET)-repogen'
+    $(INSTALL) -m755 '$(BUILD_DIR)/bin/archivegen' '$(PREFIX)/bin/$(TARGET)-archivegen'
+    $(INSTALL) -m755 '$(BUILD_DIR)/bin/devtool' '$(PREFIX)/bin/$(TARGET)-devtool'
 endef
 
 # only makes sense for static builds
 define $(PKG)_BUILD_STATIC
-    cd '$(1)' && '$(PREFIX)/$(TARGET)/qt5/bin/qmake'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' || $(MAKE) -C '$(1)' -j  1
-    $(MAKE) -C '$(1)' -j 1 install
+    # *.rc file has strange encoding issues with macro
+    $(SED) -i 's,IFW_VERSION_STR_WIN32,"$($(PKG)_VERSION)",g' '$(SOURCE_DIR)/src/sdk/installerbase.rc'
+    cd '$(BUILD_DIR)' && $(TARGET)-qmake-qt5 '$(SOURCE_DIR)'
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' || $(MAKE) -C '$(BUILD_DIR)' -j  1
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 
     # build the tutorial installer in /tmp, because the binarycreator internal
     # rename will fail if /tmp is not in the same filesystem as mxe
-    cd '$(1)examples/tutorial' && \
+    cd '$(SOURCE_DIR)examples/tutorial' && \
     '$(PREFIX)/bin/$(BUILD)-binarycreator' \
         -c config/config.xml \
         -p packages \
