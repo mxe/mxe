ab653ed4ebc2ba74966d647d148e67bcff9f0629
Mark Brand <mabrand@mabrand.nl>
2020-01-23 16:56:56 +0100

    update sqlite

--- src/sqlite.mk
+++ src/sqlite.mk
@@ -4,11 +4,11 @@ PKG             := sqlite
 $(PKG)_WEBSITE  := https://www.sqlite.org/
 $(PKG)_DESCR    := SQLite
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3300100
-$(PKG)_CHECKSUM := 8c5a50db089bd2a1b08dbc5b00d2027602ca7ff238ba7658fabca454d4298e60
+$(PKG)_VERSION  := 3310000
+$(PKG)_CHECKSUM := 7c9d6e6d95751b2dd5cdc8c3e40f9883a89c2f0ac1ef6c54695227324849f1f0
 $(PKG)_SUBDIR   := $(PKG)-autoconf-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-autoconf-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://www.sqlite.org/2019/$($(PKG)_FILE)
+$(PKG)_URL      := https://www.sqlite.org/2020/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc
 
 define $(PKG)_UPDATE
