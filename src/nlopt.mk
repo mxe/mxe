f124313c9f03433274e86a2497f2e3ebb515ae67
Martin Müllenhaupt <mm+git@netlair.de>
2020-03-12 13:25:30 +0100

    update nlopt

--- src/nlopt.mk
+++ src/nlopt.mk
@@ -1,31 +1,22 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := nlopt
-$(PKG)_WEBSITE  := https://ab-initio.mit.edu/wiki/index.php/NLopt
+$(PKG)_WEBSITE  := https://nlopt.readthedocs.io/en/latest/
 $(PKG)_DESCR    := NLopt
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.4.2
-$(PKG)_CHECKSUM := 8099633de9d71cbc06cd435da993eb424bbcdbded8f803cdaa9fb8c6e09c8e89
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://ab-initio.mit.edu/$(PKG)/$($(PKG)_FILE)
+$(PKG)_VERSION  := 2.6.1
+$(PKG)_CHECKSUM := 66d63a505187fb6f98642703bd0ef006fedcae2f9a6d1efa4f362ea919a02650
+$(PKG)_GH_CONF  := stevengj/nlopt/releases/tags,v
 $(PKG)_DEPS     := cc
 
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://ab-initio.mit.edu/wiki/index.php/NLopt' | \
-    $(SED) -n 's,.*<a href=".*nlopt-\([0-9.]\+\).tar.gz".*,\1,p' | \
-    $(SORT) -V | \
-    tail -1
-endef
-
 define $(PKG)_BUILD
-    cd '$(1)' && ./configure \
-        $(MXE_CONFIGURE_OPTS) \
-        --with-cxx \
-        --without-guile \
-        --without-matlab \
-        --without-octave \
-        --without-python
-    $(MAKE) -C '$(1)' -j '$(JOBS)' LDFLAGS='-no-undefined'
-    $(MAKE) -C '$(1)' -j 1 install
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' '$(SOURCE_DIR)' \
+        -DBUILD_SHARED_LIBS=$(CMAKE_SHARED_BOOL) \
+        -DNLOPT_PYTHON=OFF \
+        -DNLOPT_OCTAVE=OFF \
+        -DNLOPT_MATLAB=OFF \
+        -DNLOPT_GUILE=OFF \
+        -DNLOPT_SWIG=OFF
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1
 endef
