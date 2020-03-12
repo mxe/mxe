da2366cc899afd8d816b4391f82d79f564f90ab8
Martin Müllenhaupt <mm+git@netlair.de>
2020-03-12 15:01:52 +0100

    update libgit2

--- src/libgit2.mk
+++ src/libgit2.mk
@@ -3,8 +3,8 @@
 PKG             := libgit2
 $(PKG)_WEBSITE  := https://libgit2.github.com/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.28.2
-$(PKG)_CHECKSUM := 42b5f1e9b9159d66d86fff0394215c5733b6ef8f9b9d054cdd8c73ad47177fc3
+$(PKG)_VERSION  := 0.99.0
+$(PKG)_CHECKSUM := 174024310c1563097a6613a0d3f7539d11a9a86517cd67ce533849065de08a11
 $(PKG)_GH_CONF  := libgit2/libgit2/releases/latest,v
 $(PKG)_DEPS     := cc libssh2
 
