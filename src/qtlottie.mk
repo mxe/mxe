4ab8b10693489afea51df1b5361b276228786171
Mark Brand <mabrand@mabrand.nl>
2020-05-26 10:36:15 +0200

    qt5: update to 5.15.0

--- src/qtlottie.mk
+++ src/qtlottie.mk
@@ -1,16 +1,15 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
-PKG             := qtsvg
+PKG             := qtlottie
 $(PKG)_WEBSITE  := https://www.qt.io/
 $(PKG)_DESCR    := Qt
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION   = $(qtbase_VERSION)
-$(PKG)_CHECKSUM := ee4d287e2e205ca8c08921b9cbe0fc58bf46be080b5359ad4d7fbdee44aeee0d
-$(PKG)_SUBDIR    = $(subst qtbase,qtsvg,$(qtbase_SUBDIR))
-$(PKG)_FILE      = $(subst qtbase,qtsvg,$(qtbase_FILE))
-$(PKG)_URL       = $(subst qtbase,qtsvg,$(qtbase_URL))
-$(PKG)_DEPS     := cc qtbase
-$(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
+$(PKG)_CHECKSUM := 2053f474dcd7184fdcae2507f47af6527f6ca25b4424483f9265853c3626c833
+$(PKG)_SUBDIR    = $(subst qtbase,qtlottie,$(qtbase_SUBDIR))
+$(PKG)_FILE      = $(subst qtbase,qtlottie,$(qtbase_FILE))
+$(PKG)_URL       = $(subst qtbase,qtlottie,$(qtbase_URL))
+$(PKG)_DEPS     := cc qtbase qtdeclarative
 
 define $(PKG)_UPDATE
     echo $(qtbase_VERSION)
--- src/qtquick3d.mk
+++ src/qtquick3d.mk
@@ -1,16 +1,15 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
-PKG             := qtsvg
+PKG             := qtquick3d
 $(PKG)_WEBSITE  := https://www.qt.io/
 $(PKG)_DESCR    := Qt
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION   = $(qtbase_VERSION)
-$(PKG)_CHECKSUM := ee4d287e2e205ca8c08921b9cbe0fc58bf46be080b5359ad4d7fbdee44aeee0d
-$(PKG)_SUBDIR    = $(subst qtbase,qtsvg,$(qtbase_SUBDIR))
-$(PKG)_FILE      = $(subst qtbase,qtsvg,$(qtbase_FILE))
-$(PKG)_URL       = $(subst qtbase,qtsvg,$(qtbase_URL))
-$(PKG)_DEPS     := cc qtbase
-$(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
+$(PKG)_CHECKSUM := 6d3b91b653ba5e33fd5b37cd785ded6cf1dd83d35250c3addb77eb35f90e52cb
+$(PKG)_SUBDIR    = $(subst qtbase,qtquick3d,$(qtbase_SUBDIR))
+$(PKG)_FILE      = $(subst qtbase,qtquick3d,$(qtbase_FILE))
+$(PKG)_URL       = $(subst qtbase,qtquick3d,$(qtbase_URL))
+$(PKG)_DEPS     := cc assimp qtbase qtdeclarative
 
 define $(PKG)_UPDATE
     echo $(qtbase_VERSION)
