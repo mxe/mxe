1f0ba2d3ff8891efd0be93aac6b642ed188f0e6a
Tony Theodore <tonyt@logyst.com>
2020-06-01 20:16:35 +1000

    gettext: pin darwin at 0.20.1

--- plugins/native/darwin/gettext.mk
+++ plugins/native/darwin/gettext.mk
@@ -1,6 +1,15 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
+$(PLUGIN_HEADER)
 
 PKG := gettext
+
+$(PKG)_VERSION  := 0.20.1
+$(PKG)_CHECKSUM := 53f02fbbec9e798b0faaf7c73272f83608e835c6288dd58be6c9bb54624a3800
+$(PKG)_SUBDIR   := gettext-$($(PKG)_VERSION)
+$(PKG)_FILE     := gettext-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://ftp.gnu.org/gnu/gettext/$($(PKG)_FILE)
+$(PKG)_URL_2    := https://ftpmirror.gnu.org/gettext/$($(PKG)_FILE)
+$(PKG)_PATCHES  := $(dir $(lastword $(MAKEFILE_LIST)))/$(PKG)-1.patch
 
 define $(PKG)_BUILD_$(BUILD)
     cd '$(SOURCE_DIR)' && autoreconf -fi
