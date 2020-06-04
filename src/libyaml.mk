17ae8f8a117f22f3505faa93bb7cc94d84f9ed24
Tony Theodore <tonyt@logyst.com>
2020-06-04 20:39:47 +1000

    libyaml: update and disable programs

--- src/libyaml.mk
+++ src/libyaml.mk
@@ -4,16 +4,15 @@ PKG             := libyaml
 $(PKG)_WEBSITE  := https://github.com/yaml/libyaml
 $(PKG)_DESCR    := A C library for parsing and emitting YAML.
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.2.4
-$(PKG)_CHECKSUM := 02265e0229675aea3a413164b43004045617174bdb2c92bf6782f618f8796b55
+$(PKG)_VERSION  := 0.2.5
+$(PKG)_CHECKSUM := fa240dbf262be053f3898006d502d514936c818e422afdcf33921c63bed9bf2e
 $(PKG)_GH_CONF  := yaml/libyaml/releases/latest
 $(PKG)_DEPS     := cc
 
 define $(PKG)_BUILD
 	cd '$(SOURCE_DIR)' && ./bootstrap
-	cd '$(BUILD_DIR)' && \
-		$(SOURCE_DIR)/configure \
+	cd '$(BUILD_DIR)' && $(SOURCE_DIR)/configure \
 		$(MXE_CONFIGURE_OPTS)
-	$(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
-	$(MAKE) -C '$(BUILD_DIR)' -j 1 install
+	$(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_PROGRAMS)
+	$(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_PROGRAMS)
 endef
