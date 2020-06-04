e9ced5e0d545d6c289d8778e097a560b7219d9f4
Tony Theodore <tonyt@logyst.com>
2020-06-04 12:50:28 +1000

    darwin: warn about pinned gettext version

--- plugins/native/darwin/gettext.mk
+++ plugins/native/darwin/gettext.mk
@@ -1,9 +1,9 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
-$(PLUGIN_HEADER)
 
 PKG := gettext
 
 $(PKG)_VERSION  := 0.20.1
+$(info $(shell printf '%-$(PRINTF_COL_1_WIDTH)s %s\n' [warning] **$(PKG)-$($(PKG)_VERSION)**))
 $(PKG)_CHECKSUM := 53f02fbbec9e798b0faaf7c73272f83608e835c6288dd58be6c9bb54624a3800
 $(PKG)_SUBDIR   := gettext-$($(PKG)_VERSION)
 $(PKG)_FILE     := gettext-$($(PKG)_VERSION).tar.xz
