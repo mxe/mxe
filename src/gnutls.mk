4fde0c1eaf52a6789ea047d6fef3b31a12ec2d4a
Mark Brand <mabrand@mabrand.nl>
2020-06-04 18:28:13 +0200

    gnutls: update and remove obsolete patches

--- src/gnutls.mk
+++ src/gnutls.mk
@@ -3,8 +3,8 @@
 PKG             := gnutls
 $(PKG)_WEBSITE  := https://www.gnu.org/software/gnutls/
 $(PKG)_DESCR    := GnuTLS
-$(PKG)_VERSION  := 3.6.13
-$(PKG)_CHECKSUM := 32041df447d9f4644570cf573c9f60358e865637d69b7e59d1159b7240b52f38
+$(PKG)_VERSION  := 3.6.14
+$(PKG)_CHECKSUM := 5630751adec7025b8ef955af4d141d00d252a985769f51b4059e5affa3d39d63
 $(PKG)_SUBDIR   := gnutls-$($(PKG)_VERSION)
 $(PKG)_FILE     := gnutls-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://gnupg.org/ftp/gcrypt/gnutls/v3.6/$($(PKG)_FILE)
