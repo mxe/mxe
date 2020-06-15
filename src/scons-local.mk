7d3a2b539886cadedb7da3788733aa3154b98f75
Tony Theodore <tonyt@logyst.com>
2020-06-15 23:50:21 +1000

    scons-local: update 3.0.1 --> 3.1.2 and define SCONS_LOCAL variable

--- src/scons-local.mk
+++ src/scons-local.mk
@@ -4,8 +4,8 @@ PKG             := scons-local
 $(PKG)_WEBSITE  := https://scons.org/
 $(PKG)_DESCR    := Standalone SCons
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.0.1
-$(PKG)_CHECKSUM := 4d6ceb7b5a628c0ffd9e7b5b0d176a4949cd70dd70ddd0cc0d18e19d71258695
+$(PKG)_VERSION  := 3.1.2
+$(PKG)_CHECKSUM := 642e90860b746fa18fac08c7a22de6bfa86110ae7c56d7f136f7e5fb0d8f4f44
 $(PKG)_SUBDIR   := .
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/scons/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
@@ -17,3 +17,6 @@ define $(PKG)_UPDATE
 endef
 
 # unpack sources into build dir and execute directly with python2
+# scons does various PATH manipulations that don't play well with ccache
+SCONS_LOCAL = \
+    PATH='$(PREFIX)/bin:$(PATH)' $(PYTHON2) '$(BUILD_DIR).scons/scons.py'
