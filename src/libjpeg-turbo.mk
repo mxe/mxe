7ae744350c3aa767df59d3aa78423c737486d07a
Mark Brand <mabrand@mabrand.nl>
2020-06-25 15:38:29 +0200

    update: curl, libjpeg-turbo

--- src/libjpeg-turbo.mk
+++ src/libjpeg-turbo.mk
@@ -3,8 +3,8 @@
 PKG             := libjpeg-turbo
 $(PKG)_WEBSITE  := https://libjpeg-turbo.virtualgl.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.0.4
-$(PKG)_CHECKSUM := 33dd8547efd5543639e890efbf2ef52d5a21df81faf41bb940657af916a23406
+$(PKG)_VERSION  := 2.0.5
+$(PKG)_CHECKSUM := 16f8f6f2715b3a38ab562a84357c793dd56ae9899ce130563c72cd93d8357b5d
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
