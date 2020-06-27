4101ece03400f72ca2256d3cadfd8bfae5a3f88b
Gregorio Litenstein <g.litenstein@gmail.com>
2020-06-27 13:58:51 -0400

    assimp: Proper fix, just build irrxml ourselves
    Decided to go with this because this thing has been a bug upstream for years in several different forms. Point is, I don’t think they can be trusted with IrrXML and since it’s a small library, it’s just cleaner to add it to mxe and declare it as a dependency for assimp.

--- src/assimp.mk
+++ src/assimp.mk
@@ -7,13 +7,14 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 4.1.0
 $(PKG)_CHECKSUM := 3520b1e9793b93a2ca3b797199e16f40d61762617e072f2d525fad70f9678a71
 $(PKG)_GH_CONF  := assimp/assimp/tags, v
-$(PKG)_DEPS     := cc boost minizip
+$(PKG)_DEPS     := cc boost minizip irrxml
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake \
         -DASSIMP_BUILD_ASSIMP_TOOLS=OFF \
         -DASSIMP_BUILD_SAMPLES=OFF \
         -DASSIMP_BUILD_TESTS=OFF \
+        -DSYSTEM_IRRXML=ON \
         '$(SOURCE_DIR)'
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
@@ -21,5 +22,5 @@ define $(PKG)_BUILD
     '$(TARGET)-g++' \
         -W -Wall -Werror -ansi -pedantic -std=c++11 \
         '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-assimp.exe' \
-        `'$(TARGET)-pkg-config' assimp minizip --cflags --libs` -lirrxml
+        `'$(TARGET)-pkg-config' assimp minizip --cflags --libs`
 endef
