4078179042b33d77c8659aad2eeee6f211c52b5e
Mark Brand <mabrand@mabrand.nl>
2020-07-29 01:52:33 +0200

    update mpg123

--- src/mpg123.mk
+++ src/mpg123.mk
@@ -3,8 +3,8 @@
 PKG             := mpg123
 $(PKG)_WEBSITE  := https://www.mpg123.de/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.26.2
-$(PKG)_CHECKSUM := 00f7bf7ea64fcec2c9d07751d6ad8849343ee09c282ea3b0d5dd486e886e2ff3
+$(PKG)_VERSION  := 1.26.3
+$(PKG)_CHECKSUM := 30c998785a898f2846deefc4d17d6e4683a5a550b7eacf6ea506e30a7a736c6e
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/mpg123/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
