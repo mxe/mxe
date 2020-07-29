27d5c11ac100f5cf4d2ac3eb48dae1bacfdf6512
Mark Brand <mabrand@mabrand.nl>
2020-07-29 02:27:38 +0200

    update poppler

--- src/poppler.mk
+++ src/poppler.mk
@@ -3,8 +3,8 @@
 PKG             := poppler
 $(PKG)_WEBSITE  := https://poppler.freedesktop.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.90.0
-$(PKG)_CHECKSUM := 94edb0ad3330b74d22caac807aa501e9cc61cbf45a8010625670428b51989685
+$(PKG)_VERSION  := 0.90.1
+$(PKG)_CHECKSUM := 984d82e72e91418d280885298c8bdc855a2fd92665fd52a1345b27235e0c71c4
 $(PKG)_SUBDIR   := poppler-$($(PKG)_VERSION)
 $(PKG)_FILE     := poppler-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://poppler.freedesktop.org/$($(PKG)_FILE)
