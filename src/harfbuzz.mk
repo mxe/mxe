f1e95a03d63adb7cc56f33609ba1bf4555298887
Mark Brand <mabrand@mabrand.nl>
2020-08-17 00:05:53 +0200

    update: harfbuzz, sqlite

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -4,8 +4,8 @@ PKG             := harfbuzz
 $(PKG)_WEBSITE  := https://wiki.freedesktop.org/www/Software/HarfBuzz/
 $(PKG)_DESCR    := HarfBuzz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.7.0
-$(PKG)_CHECKSUM := 4dba05de1fd44705f54c40d801e0e3d4833555d004cb611cc18675173feae75b
+$(PKG)_VERSION  := 2.7.1
+$(PKG)_CHECKSUM := 431c856ff18eeca89b2a36b58f2c2d56273cd522c34c0ffbc9dd6f7da4b9bd08
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases
 $(PKG)_DEPS     := cc cairo freetype-bootstrap glib icu4c
 
