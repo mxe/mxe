48d25e58989f710a9eac8626db8e7f21af06f917
klapeto <yiannispana@hotmail.com>
2020-08-24 22:01:35 +0300

    Added hwloc script

---
+++ src/hwloc.mk
@@ -0,0 +1,35 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := hwloc
+$(PKG)_WEBSITE  := https://www.open-mpi.org/projects/hwloc/
+$(PKG)_DESCR    := Portable Hardware Locality (hwloc)
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.2.0
+$(PKG)_CHECKSUM := ff6219c73e25d9930722e8f99ded0c72780a334aa525a8fee1ded0e953bec0fc
+$(PKG)_GH_CONF  := open-mpi/hwloc/tags, hwloc-
+$(PKG)_DEPS     := cc
+
+define $(PKG)_BUILD
+    cd '$(SOURCE_DIR)' && ./autogen.sh
+    cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
+        $(MXE_CONFIGURE_OPTS) \
+        --disable-gl \
+        --disable-libudev \
+        --disable-cuda \
+        --disable-nvml \
+        --disable-opencl \
+        --disable-pci \
+        --disable-io \
+        --disable-libxml2 \
+        --disable-cairo \
+        --disable-opencl \
+        --disable-opencl \
+        CONFIG_SHELL=$(SHELL)
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
+
+    '$(TARGET)-gcc' \
+        -W -Wall -Werror -ansi -pedantic \
+        '$(PWD)/src/$(PKG)-test.c' -o '$(PREFIX)/$(TARGET)/bin/test-hwloc.exe' \
+        `'$(TARGET)-pkg-config' hwloc --cflags --libs`
+endef
