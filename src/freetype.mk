9558c6ebc63c57c011631d3bb7c52a98434dddac
Mark Brand <mabrand@mabrand.nl>
2020-10-14 22:10:57 +0200

    update: curl, freetype, libgit2

--- src/freetype.mk
+++ src/freetype.mk
@@ -3,8 +3,8 @@
 PKG             := freetype
 $(PKG)_WEBSITE  := https://www.freetype.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.10.2
-$(PKG)_CHECKSUM := 1543d61025d2e6312e0a1c563652555f17378a204a61e99928c9fcef030a2d8b
+$(PKG)_VERSION  := 2.10.3
+$(PKG)_CHECKSUM := c22244bc766b2d8152f22db7370965431dcb1e408260428208c24984f78e6659
 $(PKG)_SUBDIR   := freetype-$($(PKG)_VERSION)
 $(PKG)_FILE     := freetype-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/freetype/freetype2/$(shell echo '$($(PKG)_VERSION)' | cut -d . -f 1,2,3)/$($(PKG)_FILE)
--- src/libgit2.mk
+++ src/libgit2.mk
@@ -3,8 +3,8 @@
 PKG             := libgit2
 $(PKG)_WEBSITE  := https://libgit2.github.com/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.0
-$(PKG)_CHECKSUM := 6a1fa16a7f6335ce8b2630fbdbb5e57c4027929ebc56fcd1ac55edb141b409b4
+$(PKG)_VERSION  := 1.1.0
+$(PKG)_CHECKSUM := ad73f845965cfd528e70f654e428073121a3fa0dc23caac81a1b1300277d4dba
 $(PKG)_GH_CONF  := libgit2/libgit2/releases/latest,v
 $(PKG)_DEPS     := cc libssh2
 
