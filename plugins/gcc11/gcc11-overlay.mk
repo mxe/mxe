b1987092663d84c4cc5c6658619579fb7ea65223
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2020-11-04 16:23:02 +0200

    isl: update to 0.18

--- plugins/gcc11/gcc11-overlay.mk
+++ plugins/gcc11/gcc11-overlay.mk
@@ -1,6 +1,6 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
-# override relevant cloog, isl, and gcc variables changed in:
+# override relevant cloog, and gcc variables changed in:
 # https://github.com/mxe/mxe/pull/965
 #
 # simply expanded variables (*_SUBDIR, *_FILE, etc.) need to be set
@@ -16,14 +16,6 @@ $(PKG)_FILE     := binutils-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := https://ftp.gnu.org/gnu/binutils/$($(PKG)_FILE)
 $(PKG)_URL_2    := https://ftpmirror.gnu.org/binutils/$($(PKG)_FILE)
 $(PKG)_PATCHES  := $(dir $(lastword $(MAKEFILE_LIST)))/$(PKG)-1-fixes.patch
-
-PKG             := isl
-$(PKG)_VERSION  := 0.16.1
-$(PKG)_CHECKSUM := 412538bb65c799ac98e17e8cfcdacbb257a57362acfaaff254b0fcae970126d2
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_URL      := https://isl.gforge.inria.fr/$($(PKG)_FILE)
-$(PKG)_URL_2    := https://gcc.gnu.org/pub/gcc/infrastructure/$($(PKG)_FILE)
 
 PKG             := gcc
 $(PKG)_VERSION  := 11.1.0
