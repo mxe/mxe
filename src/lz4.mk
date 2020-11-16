d2147e99f047d55e9e1f0b72dd7c8eaf59461c89
Mark Brand <mabrand@mabrand.nl>
2020-11-16 11:13:08 +0100

    update lz4

--- src/lz4.mk
+++ src/lz4.mk
@@ -4,8 +4,8 @@ PKG             := lz4
 $(PKG)_WEBSITE  := https://github.com/$(PKG)/$(PKG)
 $(PKG)_DESCR    := lossless compression algorithm optimized for speed
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.9.2
-$(PKG)_CHECKSUM := 658ba6191fa44c92280d4aa2c271b0f4fbc0e34d249578dd05e50e76d0e5efcc
+$(PKG)_VERSION  := 1.9.3
+$(PKG)_CHECKSUM := 030644df4611007ff7dc962d981f390361e6c97a34e5cbc393ddfbe019ffe2c1
 $(PKG)_GH_CONF  := lz4/lz4/tags,v
 $(PKG)_DEPS     := cc
 
@@ -14,7 +14,7 @@ define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake \
         -DCMAKE_INSTALL_BINDIR=$(BUILD_DIR)/null \
         -DCMAKE_INSTALL_MANDIR=$(BUILD_DIR)/null \
-        '$(SOURCE_DIR)/contrib/cmake_unofficial'
+        '$(SOURCE_DIR)/build/cmake'
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
     $(if $(BUILD_SHARED), $(INSTALL) '$(BUILD_DIR)/liblz4.dll' '$(PREFIX)/$(TARGET)/bin/')
