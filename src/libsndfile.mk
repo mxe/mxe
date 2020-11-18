f2eb9620d87cf2ac89582bbefabc34eb8646144e
Matthias Geier <Matthias.Geier@gmail.com>
2020-11-18 11:00:36 +0100

    libsndfile: update to version 1.0.30, add opus dependency

--- src/libsndfile.mk
+++ src/libsndfile.mk
@@ -1,10 +1,10 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := libsndfile
-$(PKG)_VERSION  := 1.0.28
-$(PKG)_CHECKSUM := 1ff33929f042fa333aed1e8923aa628c3ee9e1eb85512686c55092d1e5a9dfa9
+$(PKG)_VERSION  := 1.0.30
+$(PKG)_CHECKSUM := 9df273302c4fa160567f412e10cc4f76666b66281e7ba48370fb544e87e4611a
 $(PKG)_GH_CONF  := libsndfile/libsndfile/releases,v,,,,.tar.bz2
-$(PKG)_DEPS     := cc flac ogg vorbis
+$(PKG)_DEPS     := cc flac ogg vorbis opus
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
