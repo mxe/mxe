f065fba5cf0dc3a1cfbe63b02f0b2c8837c309ca
Matthias Geier <Matthias.Geier@gmail.com>
2020-11-18 10:53:22 +0100

    libsndfile: use _GH_CONF

--- src/libsndfile.mk
+++ src/libsndfile.mk
@@ -1,21 +1,10 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := libsndfile
-$(PKG)_WEBSITE  := http://www.mega-nerd.com/libsndfile/
-$(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.0.28
 $(PKG)_CHECKSUM := 1ff33929f042fa333aed1e8923aa628c3ee9e1eb85512686c55092d1e5a9dfa9
-$(PKG)_SUBDIR   := libsndfile-$($(PKG)_VERSION)
-$(PKG)_FILE     := libsndfile-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := http://www.mega-nerd.com/libsndfile/files/$($(PKG)_FILE)
+$(PKG)_GH_CONF  := libsndfile/libsndfile/releases,v,,,,.tar.bz2
 $(PKG)_DEPS     := cc flac ogg vorbis
-
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://www.mega-nerd.com/libsndfile/' | \
-    grep '<META NAME="Version"' | \
-    $(SED) -n 's,.*CONTENT="libsndfile-\([0-9][^"]*\)">.*,\1,p' | \
-    head -1
-endef
 
 define $(PKG)_BUILD
     cd '$(1)' && autoreconf -fi -IM4
