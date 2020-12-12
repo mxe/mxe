5d408eea94e6106b45851fb2f3dfe5b41aa3a65a
Tony Theodore <tonyt@logyst.com>
2020-12-12 13:29:23 +1100

    cmake: update 3.17.3 --> 3.19.1

--- src/cmake.mk
+++ src/cmake.mk
@@ -3,8 +3,8 @@
 PKG             := cmake
 $(PKG)_WEBSITE  := https://www.cmake.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.17.3
-$(PKG)_CHECKSUM := 0bd60d512275dc9f6ef2a2865426a184642ceb3761794e6b65bff233b91d8c40
+$(PKG)_VERSION  := 3.19.1
+$(PKG)_CHECKSUM := 1d266ea3a76ef650cdcf16c782a317cb4a7aa461617ee941e389cb48738a3aba
 $(PKG)_SUBDIR   := cmake-$($(PKG)_VERSION)
 $(PKG)_FILE     := cmake-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://www.cmake.org/files/v$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
