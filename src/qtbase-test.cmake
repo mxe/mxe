772b91c6b6d2f58bab572902d0409bc48c2e6829
Tony Theodore <tonyt@logyst.com>
2020-12-12 20:33:56 +1100

    qt6 proof of concept

--- src/qtbase-test.cmake
+++ src/qtbase-test.cmake
@@ -3,6 +3,7 @@
 # partial module - included by src/cmake/CMakeLists.txt
 
 set(TGT test-${PKG}-cmake)
+set(QT_MAJOR 5 CACHE STRING "")
 
 enable_language(CXX)
 
@@ -10,15 +11,15 @@ set(CMAKE_AUTOMOC ON)
 set(CMAKE_AUTORCC ON)
 set(CMAKE_AUTOUIC ON)
 
-find_package(Qt5 ${PKG_VERSION} EXACT REQUIRED COMPONENTS Widgets)
+find_package(Qt${QT_MAJOR} ${PKG_VERSION} EXACT REQUIRED COMPONENTS Widgets)
 
 add_executable(${TGT} ${CMAKE_CURRENT_LIST_DIR}/qt-test.cpp)
 
-target_link_libraries(${TGT} Qt5::Widgets)
+target_link_libraries(${TGT} Qt::Widgets)
 
 # reduce size of static binary by excluding unnecessary plugins
 # https://doc.qt.io/qt-5/qtcore-cmake-qt5-import-plugins.html
-qt5_import_plugins(${TGT}
+qt_import_plugins(${TGT}
     INCLUDE_BY_TYPE platforms
     EXCLUDE_BY_TYPE imageformats
     EXCLUDE_BY_TYPE sqldrivers)
