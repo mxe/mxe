45387eb7e0118641ba7b43418276cd736ad9c2ed
Mark Brand <mabrand@mabrand.nl>
2020-12-23 11:23:10 +0100

    update: harfbuzz, libgpg_error

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -4,8 +4,8 @@ PKG             := harfbuzz
 $(PKG)_WEBSITE  := https://wiki.freedesktop.org/www/Software/HarfBuzz/
 $(PKG)_DESCR    := HarfBuzz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.7.2
-$(PKG)_CHECKSUM := 8ec112ee108642477478b75fc7906422abed404d7530e47ba0a4875f553f1b59
+$(PKG)_VERSION  := 2.7.3
+$(PKG)_CHECKSUM := be01bc2c3f1e8902f31b432131280cdfdf6780b72c2389dd63cfab908f6213f3
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases
 $(PKG)_DEPS     := cc cairo freetype-bootstrap glib icu4c
 
