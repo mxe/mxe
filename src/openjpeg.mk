cc7489acae3a65147aa9d332324e743d6b569820
Mark Brand <mabrand@mabrand.nl>
2020-12-31 17:31:51 +0100

    update: harfbuzz, openjpeg

--- src/openjpeg.mk
+++ src/openjpeg.mk
@@ -5,8 +5,8 @@ PKG             := openjpeg
 $(PKG)_WEBSITE  := https://www.openjpeg.org/
 $(PKG)_DESCR    := OpenJPEG
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.3.1
-$(PKG)_CHECKSUM := 63f5a4713ecafc86de51bfad89cc07bb788e9bba24ebbf0c4ca637621aadb6a9
+$(PKG)_VERSION  := 2.4.0
+$(PKG)_CHECKSUM := 8702ba68b442657f11aaeb2b338443ca8d5fb95b0d845757968a7be31ef7f16d
 $(PKG)_GH_CONF  := uclouvain/openjpeg/tags,v,,version
 $(PKG)_DEPS     := cc lcms libpng tiff zlib
 
