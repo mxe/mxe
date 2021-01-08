cbaf707763a9018ed2d935bd7693ee34be0e580e
Tony Theodore <tonyt@logyst.com>
2021-01-08 16:37:28 +1100

    wxwidgets: move 3.1.4 plugin to src
    closes #2604, closes #2452

--- src/wxwidgets.mk
+++ src/wxwidgets.mk
@@ -4,16 +4,24 @@ PKG             := wxwidgets
 $(PKG)_WEBSITE  := https://www.wxwidgets.org/
 $(PKG)_DESCR    := wxWidgets
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.0.5.1
-$(PKG)_CHECKSUM := bae4d9f289e33a05fb8553fcc580564d30efe6a882ff08e3d4e09ef01f5f6578
-$(PKG)_GH_CONF  := wxWidgets/wxWidgets/releases/downloads,v
+$(PKG)_VERSION  := 3.1.4
+$(PKG)_CHECKSUM := 3ca3a19a14b407d0cdda507a7930c2e84ae1c8e74f946e0144d2fa7d881f1a94
+$(PKG)_GH_CONF  := wxWidgets/wxWidgets/releases/latest,v,,,,.tar.bz2
 $(PKG)_DEPS     := cc expat jpeg libiconv libpng sdl tiff zlib
 
 define $(PKG)_CONFIGURE_OPTS
+endef
+
+define $(PKG)_BUILD
+    cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
         $(MXE_CONFIGURE_OPTS) \
+        --enable-option-checking \
         --enable-gui \
+        --enable-unicode \
         --disable-stl \
+        --disable-gtktest \
         --enable-threads \
+        --enable-backtrace \
         --disable-universal \
         --with-themes=all \
         --with-msw \
@@ -26,46 +34,22 @@ define $(PKG)_CONFIGURE_OPTS
         --with-expat=sys \
         --with-sdl \
         --without-gtk \
-        --without-motif \
-        --without-mac \
         --without-macosx-sdk \
-        --without-cocoa \
-        --without-wine \
-        --without-pm \
-        --without-microwin \
         --without-libxpm \
         --without-libmspack \
-        --without-gnomeprint \
         --without-gnomevfs \
-        --without-hildon \
         --without-dmalloc \
-        --without-odbc \
-        LIBS=" `'$(TARGET)-pkg-config' --libs-only-l libtiff-4`" \
-        CXXFLAGS='-std=gnu++11' \
-        CXXCPP='$(TARGET)-g++ -E -std=gnu++11'
-endef
+        $(PKG_CONFIGURE_OPTS)
 
-define $(PKG)_BUILD
-    # build the wxWidgets variant with unicode support
-    mkdir '$(1).unicode'
-    cd    '$(1).unicode' && '$(1)/configure' \
-        $($(PKG)_CONFIGURE_OPTS) \
-        --enable-unicode
-    $(MAKE) -C '$(1).unicode' -j '$(JOBS)' \
-        $(MXE_DISABLE_CRUFT)
-    -$(MAKE) -C '$(1).unicode/locale' -j '$(JOBS)' allmo \
-        $(MXE_DISABLE_CRUFT)
-    $(MAKE) -C '$(1).unicode' -j 1 install \
-        $(if $(BUILD_SHARED),DLLDEST='/../bin') \
-        $(MXE_DISABLE_CRUFT) __install_wxrc___depname=
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_CRUFT)
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_CRUFT) __install_wxrc___depname=
+
     $(INSTALL) -m755 '$(PREFIX)/$(TARGET)/bin/wx-config' \
                      '$(PREFIX)/bin/$(TARGET)-wx-config'
 
     # build test program
     '$(TARGET)-g++' \
-        -W -Wall -Werror -Wno-error=unused-local-typedefs -pedantic -std=gnu++0x \
-        -Wno-deprecated-copy -Wno-cast-function-type \
+        -W -Wall -pedantic -std=gnu++0x \
         '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-wxwidgets.exe' \
         `'$(TARGET)-wx-config' --cflags --libs`
 endef
-
