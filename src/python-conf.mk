ff135f26ec724d6db33a841343c8096b09ec12b0
Tony Theodore <tonyt@logyst.com>
2021-01-12 18:09:39 +1100

    python-conf: avoid generating *.pyc files

--- src/python-conf.mk
+++ src/python-conf.mk
@@ -14,6 +14,7 @@ define $(PKG)_BUILD_$(BUILD)
     #create python wrapper in a directory which is in PATH
     (echo '#!/bin/sh'; \
      echo 'PYTHONPATH="$(PREFIX)/$(TARGET)/lib/python$(PY_XY_VER)/site-packages" \
+           PYTHONDONTWRITEBYTECODE=True \
            exec $(PYTHON) "$$@"';) \
              > '$(PREFIX)/bin/$(TARGET)-python$(PY_XY_VER)'
     chmod 0755 '$(PREFIX)/bin/$(TARGET)-python$(PY_XY_VER)'
