db9b49d1a841a85ef96189efa6657183a40e0f99
Mark Brand <mabrand@mabrand.nl>
2021-02-16 22:27:25 +0100

    update openssl

--- src/openssl.mk
+++ src/openssl.mk
@@ -3,8 +3,8 @@
 PKG             := openssl
 $(PKG)_WEBSITE  := https://www.openssl.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.1.1i
-$(PKG)_CHECKSUM := e8be6a35fe41d10603c3cc635e93289ed00bf34b79671a3a4de64fcee00d5242
+$(PKG)_VERSION  := 1.1.1j
+$(PKG)_CHECKSUM := aaf2fcb575cdf6491b98ab4829abf78a3dec8402b8b81efc8f23c00d443981bf
 $(PKG)_SUBDIR   := openssl-$($(PKG)_VERSION)
 $(PKG)_FILE     := openssl-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://www.openssl.org/source/$($(PKG)_FILE)
