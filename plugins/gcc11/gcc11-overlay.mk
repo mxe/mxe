3b58a3e88c36e8cefb5e88441d4dae9deea3164e
Tony Theodore <tonyt@logyst.com>
2021-05-07 20:26:02 +1000

    add gcc11 plugin

---
+++ plugins/gcc11/gcc11-overlay.mk
@@ -0,0 +1,53 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+# override relevant cloog, isl, and gcc variables changed in:
+# https://github.com/mxe/mxe/pull/965
+#
+# simply expanded variables (*_SUBDIR, *_FILE, etc.) need to be set
+
+PKG             := cloog
+$(PKG)_TARGETS  := $(MXE_TARGETS)
+
+PKG             := binutils
+$(PKG)_VERSION  := 2.36.1
+$(PKG)_CHECKSUM := 5b4bd2e79e30ce8db0abd76dd2c2eae14a94ce212cfc59d3c37d23e24bc6d7a3
+$(PKG)_SUBDIR   := binutils-$($(PKG)_VERSION)
+$(PKG)_FILE     := binutils-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := https://ftp.gnu.org/gnu/binutils/$($(PKG)_FILE)
+$(PKG)_URL_2    := https://ftpmirror.gnu.org/binutils/$($(PKG)_FILE)
+$(PKG)_PATCHES  := $(dir $(lastword $(MAKEFILE_LIST)))/$(PKG)-1-fixes.patch
+
+PKG             := isl
+$(PKG)_VERSION  := 0.16.1
+$(PKG)_CHECKSUM := 412538bb65c799ac98e17e8cfcdacbb257a57362acfaaff254b0fcae970126d2
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := https://isl.gforge.inria.fr/$($(PKG)_FILE)
+$(PKG)_URL_2    := https://gcc.gnu.org/pub/gcc/infrastructure/$($(PKG)_FILE)
+
+PKG             := gcc
+$(PKG)_VERSION  := 11.1.0
+$(PKG)_CHECKSUM := 4c4a6fb8a8396059241c2e674b85b351c26a5d678274007f076957afa1cc9ddf
+$(PKG)_SUBDIR   := gcc-$($(PKG)_VERSION)
+$(PKG)_FILE     := gcc-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://ftp.gnu.org/gnu/gcc/gcc-$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_URL_2    := https://www.mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_PATCHES  := $(dir $(lastword $(MAKEFILE_LIST)))/gcc11.patch
+$(PKG)_DEPS     := binutils mingw-w64 $(addprefix $(BUILD)~,gmp isl mpc mpfr zstd)
+
+_$(PKG)_CONFIGURE_OPTS = --with-zstd='$(PREFIX)/$(BUILD)'
+# workaround from https://bugs.gentoo.org/787662#c4
+# for https://sourceforge.net/p/mingw-w64/bugs/895
+mingw-w64-pthreads_CONFIGURE_OPTS = CFLAGS=-fno-expensive-optimizations
+
+# copy db-2-install-exe.patch to gcc7 plugin when gcc11 is default
+db_PATCHES := $(TOP_DIR)/src/db-1-fix-including-winioctl-h-lowcase.patch
+
+# set these in respective makefiles when gcc11 becomes default
+# remove from here and leave them blank for gcc5 plugin
+libssh_EXTRA_WARNINGS = -Wno-error=implicit-fallthrough
+gtkimageview_EXTRA_WARNINGS = -Wno-error=misleading-indentation
+guile_EXTRA_WARNINGS = -Wno-error=misleading-indentation
+gtkmm2_EXTRA_WARNINGS = -Wno-error=cast-function-type
+gtkmm3_EXTRA_WARNINGS = -Wno-error=cast-function-type
+gtkglextmm_EXTRA_WARNINGS = -Wno-error=cast-function-type
