bf37ec624c6d74bf03ae4ee5fc6f1dde775eeb37
Colin Bourassa <colin.bourassa@gmail.com>
2021-08-02 17:07:42 -0400

    Update libcomm14cux to version with simpler CMakeLists

--- src/libcomm14cux.mk
+++ src/libcomm14cux.mk
@@ -3,9 +3,10 @@
 PKG             := libcomm14cux
 $(PKG)_WEBSITE  := https://github.com/colinbourassa/libcomm14cux/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.1.1
-$(PKG)_CHECKSUM := 4b3d0969e2226a0f3c1250c609858e487631507ed62bf6732ce82f13f0d9fcc9
+$(PKG)_VERSION  := 2.1.3
+$(PKG)_CHECKSUM := 5c7d165ea76d036631489b6ca36431d267f08ebf1749e7c959a8787f355569a0
 $(PKG)_GH_CONF  := colinbourassa/libcomm14cux/releases/latest
+$(PKG)_URL      := https://github.com/colinbourassa/libcomm14cux/archive/$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := cc
 
 define $(PKG)_BUILD
