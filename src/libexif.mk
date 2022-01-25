e1b683a1dc85e384c7e627f3fc9e7dcec538413a
Mark Brand <mabrand@mabrand.nl>
2022-01-25 14:38:35 +0100

    libexif: fix deps and static linking

--- src/libexif.mk
+++ src/libexif.mk
@@ -7,7 +7,7 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 0.6.22
 $(PKG)_CHECKSUM := 46498934b7b931526fdee8fd8eb77a1dddedd529d5a6dbce88daf4384baecc54
 $(PKG)_GH_CONF  := libexif/libexif/releases, libexif-,-release,,_
-$(PKG)_DEPS     := cc
+$(PKG)_DEPS     := cc gettext
 
 define $(PKG)_BUILD
     cd '$(SOURCE_DIR)' && autoreconf -fi
