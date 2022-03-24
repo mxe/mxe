ecf530b6d70cb3a16c7f6706f7ceb60bbd16cf83
Cameron Cawley <ccawley2011@gmail.com>
2022-03-24 01:23:08 +0000

    sdl2_image: Fetch sources from GitHub

--- src/sdl2_image.mk
+++ src/sdl2_image.mk
@@ -5,17 +5,9 @@ $(PKG)_WEBSITE  := https://www.libsdl.org/
 $(PKG)_DESCR    := SDL2_image
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.0.5
-$(PKG)_CHECKSUM := bdd5f6e026682f7d7e1be0b6051b209da2f402a2dd8bd1c4bd9c25ad263108d0
-$(PKG)_SUBDIR   := SDL2_image-$($(PKG)_VERSION)
-$(PKG)_FILE     := SDL2_image-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://www.libsdl.org/projects/SDL_image/release/$($(PKG)_FILE)
+$(PKG)_CHECKSUM := 76b7f67f4c1a5f8368658f0e1e59bdaa4555d1cc7f3a4413178cd735019983ff
+$(PKG)_GH_CONF  := libsdl-org/SDL_image/releases/tag,release-,,
 $(PKG)_DEPS     := cc jpeg libpng libwebp sdl2 tiff
-
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://hg.libsdl.org/SDL_image/tags' | \
-    $(SED) -n 's,.*release-\([0-9][^<]*\).*,\1,p' | \
-    head -1
-endef
 
 define $(PKG)_BUILD
     $(SED) -i 's,^\(Requires:.*\),\1\nRequires.private: libtiff-4 libpng libwebp,' '$(1)/SDL2_image.pc.in'
