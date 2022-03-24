77a9a54e50f8b6f147289c540f45fd2a6e329162
Cameron Cawley <ccawley2011@gmail.com>
2022-03-24 01:22:24 +0000

    sdl2_ttf: Update to version 2.0.18

--- src/sdl2_ttf.mk
+++ src/sdl2_ttf.mk
@@ -4,26 +4,20 @@ PKG             := sdl2_ttf
 $(PKG)_WEBSITE  := https://www.libsdl.org/
 $(PKG)_DESCR    := SDL2_ttf
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.0.14
-$(PKG)_CHECKSUM := 34db5e20bcf64e7071fe9ae25acaa7d72bdc4f11ab3ce59acc768ab62fe39276
-$(PKG)_SUBDIR   := SDL2_ttf-$($(PKG)_VERSION)
-$(PKG)_FILE     := SDL2_ttf-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://www.libsdl.org/projects/SDL_ttf/release/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc freetype sdl2
-
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://hg.libsdl.org/SDL_ttf/tags' | \
-    $(SED) -n 's,.*release-\([0-9][^<]*\).*,\1,p' | \
-    head -1
-endef
+$(PKG)_VERSION  := 2.0.18
+$(PKG)_CHECKSUM := 6b61544441b72bdfa1ced89034c6396fe80228eff201eb72c5f78e500bb80bd0
+$(PKG)_GH_CONF  := libsdl-org/SDL_ttf/releases/tag,release-,,
+$(PKG)_DEPS     := cc freetype harfbuzz sdl2
 
 define $(PKG)_BUILD
-    echo 'Requires.private: freetype2' >> '$(1)/SDL2_ttf.pc.in'
+    echo 'Requires.private: harfbuzz freetype2' >> '$(1)/SDL2_ttf.pc.in'
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
         --with-sdl-prefix='$(PREFIX)/$(TARGET)' \
         --disable-sdltest \
-        --with-freetype-prefix='$(PREFIX)/$(TARGET)'
+        --with-freetype-prefix='$(PREFIX)/$(TARGET)' \
+        --disable-freetype-builtin \
+        --disable-harfbuzz-builtin
     $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 endef
 
