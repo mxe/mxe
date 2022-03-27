2c07096084e766edd1bcb529d95b8f103f7e2cce
Melroy van den Berg <melroy@melroy.org>
2022-03-27 15:04:08 +0200

    Update GTK3/Gtkmm3 + deps

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -7,14 +7,14 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 4.1.0
 $(PKG)_CHECKSUM := 0dad9332aa017d216981382cc07a9cf115740990c83b81ce3ea71ad88026d7f1
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases
-$(PKG)_DEPS     := cc cairo freetype-bootstrap glib
+$(PKG)_DEPS     := cc meson-wrapper cairo freetype-bootstrap glib
 
 define $(PKG)_BUILD
-    # mman-win32 is only a partial implementation
-    cd '$(1)' && ./autogen.sh && ./configure \
-        $(MXE_CONFIGURE_OPTS) \
-        ac_cv_header_sys_mman_h=no \
-        CXXFLAGS='-std=c++11' \
-        LIBS='-lstdc++'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+    '$(MXE_MESON_WRAPPER)' $(MXE_MESON_OPTS) \
+        -Dtests=disabled \
+        -Ddocs=disabled \
+        -Dintrospection=disabled \
+        '$(BUILD_DIR)' '$(SOURCE_DIR)' && \
+    '$(MXE_NINJA)' -C '$(BUILD_DIR)' -j '$(JOBS)' && \
+    '$(MXE_NINJA)' -C '$(BUILD_DIR)' -j '$(JOBS)' install
 endef
