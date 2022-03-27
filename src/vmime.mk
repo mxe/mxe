fcdc38dff150ae404a067c9db49f03381aa6c1ca
Mark Brand <mabrand@mabrand.nl>
2022-03-27 18:57:52 +0200

    update: expat, harfbuzz, jasper, vmime

--- src/vmime.mk
+++ src/vmime.mk
@@ -4,8 +4,8 @@ PKG             := vmime
 $(PKG)_WEBSITE  := https://www.vmime.org/
 $(PKG)_DESCR    := VMime
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5617460
-$(PKG)_CHECKSUM := 1efefcbd4e07ca9ba116714f4d1c5d30d8008cdc58a144c04169f52e7effd576
+$(PKG)_VERSION  := fc69321
+$(PKG)_CHECKSUM := e862a5eb8464f7dbe4d129494e515d6bcbbb0aba55f31de74c5808f309b35a1c
 $(PKG)_GH_CONF  := kisli/vmime/branches/master
 $(PKG)_DEPS     := cc gnutls libgsasl libiconv pthreads zlib
 
