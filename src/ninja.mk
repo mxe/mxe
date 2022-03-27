c85478ef8fd8a5b11d41a17e504a438547073b2b
Melroy van den Berg <melroy@melroy.org>
2022-03-27 15:04:08 +0200

    Update GTK3/Gtkmm3 + deps

--- src/ninja.mk
+++ src/ninja.mk
@@ -7,10 +7,9 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.10.2
 $(PKG)_CHECKSUM := ce35865411f0490368a8fc383f29071de6690cbadc27704734978221f25e2bed
 $(PKG)_GH_CONF  := ninja-build/ninja/tags,v
-$(PKG)_DEPS     := cmake
 $(PKG)_TARGETS  := $(BUILD)
 
-define $(PKG)_BUILD
+define $(PKG)_BUILD_$(BUILD)
     '$(TARGET)-cmake' -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)' \
         -DCMAKE_INSTALL_PREFIX='$(PREFIX)/$(TARGET)' \
         -DBUILD_TESTING=OFF
--- src/pangomm.mk
+++ src/pangomm.mk
@@ -4,12 +4,12 @@ PKG             := pangomm
 $(PKG)_WEBSITE  := https://www.pango.org/
 $(PKG)_DESCR    := Pangomm
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.34.0
-$(PKG)_CHECKSUM := 0e82bbff62f626692a00f3772d8b17169a1842b8cc54d5f2ddb1fec2cede9e41
+$(PKG)_VERSION  := 2.46.2
+$(PKG)_CHECKSUM := 57442ab4dc043877bfe3839915731ab2d693fc6634a71614422fb530c9eaa6f4
 $(PKG)_SUBDIR   := pangomm-$($(PKG)_VERSION)
 $(PKG)_FILE     := pangomm-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.gnome.org/sources/pangomm/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc cairomm glibmm pango
+$(PKG)_DEPS     := cc meson-wrapper cairomm glibmm pango
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://gitlab.gnome.org/GNOME/pangomm/tags' | \
@@ -18,8 +18,8 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && ./configure \
-        $(MXE_CONFIGURE_OPTS) \
-        MAKE=$(MAKE)
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+    '$(MXE_MESON_WRAPPER)' $(MXE_MESON_OPTS) \
+        '$(BUILD_DIR)' '$(SOURCE_DIR)' && \
+    '$(MXE_NINJA)' -C '$(BUILD_DIR)' -j '$(JOBS)' && \
+    '$(MXE_NINJA)' -C '$(BUILD_DIR)' -j '$(JOBS)' install
 endef
