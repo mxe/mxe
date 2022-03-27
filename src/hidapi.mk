1d3456c7543e68bb753a4a3e4a80f49d2df175f3
Melroy van den Berg <melroy@melroy.org>
2022-03-27 15:04:08 +0200

    Update GTK3/Gtkmm3 + deps

--- src/hidapi.mk
+++ src/hidapi.mk
@@ -10,13 +10,7 @@ $(PKG)_GH_CONF  := libusb/hidapi/branches/master
 $(PKG)_DEPS     := cc
 
 define $(PKG)_BUILD
-    # autoreconf when fetching from source
-    cd '$(SOURCE_DIR)' && ./bootstrap
-    cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
-        $(MXE_CONFIGURE_OPTS)
-    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
-
-    # install test
-    cp -f '$(BUILD_DIR)/hidtest/hidtest.exe' '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe'
+    '$(TARGET)-cmake' -S $(SOURCE_DIR) -B $(BUILD_DIR)
+    $(MAKE) -C '$(BUILD_DIR)' -j $(JOBS)
+    $(MAKE) -C '$(BUILD_DIR)' install
 endef
