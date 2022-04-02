66f0b187b655247f5ce9f734ee5af1c594d443d2
Tony Theodore <tonyt@logyst.com>
2022-04-02 22:14:30 +1100

    add gcc12 plugin

---
+++ plugins/gcc12/gcc12-overlay.mk
@@ -0,0 +1,57 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+# override relevant cloog, isl, and gcc variables changed in:
+# https://github.com/mxe/mxe/pull/965
+#
+# simply expanded variables (*_SUBDIR, *_FILE, etc.) need to be set
+
+PKG             := cloog
+$(PKG)_TARGETS  := $(MXE_TARGETS)
+
+PKG             := binutils
+$(PKG)_VERSION  := 2.36.1
+$(PKG)_CHECKSUM := 5b4bd2e79e30ce8db0abd76dd2c2eae14a94ce212cfc59d3c37d23e24bc6d7a3
+$(PKG)_SUBDIR   := binutils-$($(PKG)_VERSION)
+$(PKG)_FILE     := binutils-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := https://ftp.gnu.org/gnu/binutils/$($(PKG)_FILE)
+$(PKG)_URL_2    := https://ftpmirror.gnu.org/binutils/$($(PKG)_FILE)
+$(PKG)_PATCHES  := $(dir $(lastword $(MAKEFILE_LIST)))/$(PKG)-1-fixes.patch
+
+PKG             := gmp
+$(PKG)_VERSION  := 6.2.1
+$(PKG)_CHECKSUM := fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://gmplib.org/download/$(PKG)/$($(PKG)_FILE)
+
+PKG             := isl
+$(PKG)_VERSION  := 0.16.1
+$(PKG)_CHECKSUM := 412538bb65c799ac98e17e8cfcdacbb257a57362acfaaff254b0fcae970126d2
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := https://libisl.sourceforge.io/$($(PKG)_FILE)
+$(PKG)_URL_2    := https://gcc.gnu.org/pub/gcc/infrastructure/$($(PKG)_FILE)
+
+PKG             := gcc
+$(PKG)_VERSION  := 12-20220320
+$(PKG)_CHECKSUM := f2831932fbabe661e2944cecc1232400afccc25f97e56a8cdd82bcdedc8753ac
+$(PKG)_SUBDIR   := gcc-$($(PKG)_VERSION)
+$(PKG)_FILE     := gcc-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://ftp.gnu.org/gnu/gcc/gcc-$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_URL_2    := https://www.mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/LATEST-12/$($(PKG)_FILE)
+$(PKG)_PATCHES  := $(dir $(lastword $(MAKEFILE_LIST)))/gcc12.patch
+$(PKG)_DEPS     := binutils mingw-w64 $(addprefix $(BUILD)~,gmp isl mpc mpfr zstd)
+
+_$(PKG)_CONFIGURE_OPTS = --with-zstd='$(PREFIX)/$(BUILD)'
+
+# copy db-2-install-exe.patch to gcc7 plugin when gcc10 is default
+db_PATCHES := $(TOP_DIR)/src/db-1-fix-including-winioctl-h-lowcase.patch
+
+# set these in respective makefiles when gcc10 becomes default
+# remove from here and leave them blank for gcc5 plugin
+libssh_EXTRA_WARNINGS = -Wno-error=implicit-fallthrough
+gtkimageview_EXTRA_WARNINGS = -Wno-error=misleading-indentation
+guile_EXTRA_WARNINGS = -Wno-error=misleading-indentation
+gtkmm2_EXTRA_WARNINGS = -Wno-error=cast-function-type
+gtkmm3_EXTRA_WARNINGS = -Wno-error=cast-function-type
+gtkglextmm_EXTRA_WARNINGS = -Wno-error=cast-function-type
