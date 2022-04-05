105bf7df0c1a1b637c0e32de7f06aed49c943c98
Tony Theodore <tonyt@logyst.com>
2022-04-05 18:35:32 +1000

    libxslt: update 1.1.34 --> 1.1.35

--- src/libxslt.mk
+++ src/libxslt.mk
@@ -1,13 +1,13 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := libxslt
-$(PKG)_WEBSITE  := http://xmlsoft.org/XSLT/
+$(PKG)_WEBSITE  := https://gitlab.gnome.org/GNOME/libxslt/-/wikis
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.1.34
-$(PKG)_CHECKSUM := 98b1bd46d6792925ad2dfe9a87452ea2adebf69dcb9919ffd55bf926a7f93f7f
+$(PKG)_VERSION  := 1.1.35
+$(PKG)_CHECKSUM := 8247f33e9a872c6ac859aa45018bc4c4d00b97e2feac9eebc10c93ce1f34dd79
 $(PKG)_SUBDIR   := libxslt-$($(PKG)_VERSION)
-$(PKG)_FILE     := libxslt-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := http://xmlsoft.org/sources/$($(PKG)_FILE)
+$(PKG)_FILE     := libxslt-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://download.gnome.org/sources/libxslt/1.1/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc libgcrypt libxml2
 
 define $(PKG)_UPDATE
@@ -17,12 +17,12 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && autoconf
-    cd '$(1)' && ./configure \
+    cd '$(BUILD_DIR)' && $(SOURCE_DIR)/configure \
         $(MXE_CONFIGURE_OPTS) \
         --without-debug \
         --with-libxml-prefix='$(PREFIX)/$(TARGET)' \
         --without-python \
         --without-plugins
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_PROGRAMS)
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_PROGRAMS)
 endef
