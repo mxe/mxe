d3e9cf60878411081da3bbc366b8f94a9a809e08
Tony Theodore <tonyt@logyst.com>
2022-04-07 19:46:37 +1000

    libpsl: fix shared build, add libidn2 dep and options

--- src/libpsl.mk
+++ src/libpsl.mk
@@ -8,10 +8,14 @@ $(PKG)_VERSION  := 0.21.1
 $(PKG)_CHECKSUM := ac6ce1e1fbd4d0254c4ddb9d37f1fa99dec83619c1253328155206b896210d4c
 # Filter out releases <= 0.21.1 which used different naming
 $(PKG)_GH_CONF  := rockdaboot/libpsl/releases,,,libpsl
-$(PKG)_DEPS     := cc meson-wrapper glib libxml2 sqlite
+$(PKG)_DEPS     := cc meson-wrapper glib libidn2 libxml2 sqlite
 
 define $(PKG)_BUILD
-    '$(MXE_MESON_WRAPPER)' $(MXE_MESON_OPTS) '$(BUILD_DIR)' '$(SOURCE_DIR)'
+    LDFLAGS=-liconv '$(MXE_MESON_WRAPPER)' $(MXE_MESON_OPTS) \
+        -Druntime=libidn2 \
+        -Dbuiltin=libidn2 \
+        $(PKG_MESON_OPTS) \
+        '$(BUILD_DIR)' '$(SOURCE_DIR)'
     '$(MXE_NINJA)' -C '$(BUILD_DIR)' -j '$(JOBS)'
     '$(MXE_NINJA)' -C '$(BUILD_DIR)' -j '$(JOBS)' install
 endef
