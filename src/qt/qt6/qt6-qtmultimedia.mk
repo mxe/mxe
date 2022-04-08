85f1b4fbca3b746904968140d8b78e11ac07c86b
Moritz Bunkus <mo@bunkus.online>
2022-04-08 20:04:16 +0200

    Qt6: add qtmultimedia

---
+++ src/qt/qt6/qt6-qtmultimedia.mk
@@ -0,0 +1,19 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+include src/qt/qt6/qt6-conf.mk
+
+PKG := qt6-qtmultimedia
+$(eval $(QT6_METADATA))
+
+$(PKG)_CHECKSUM := ea703487c21613fcc55dd0c3fb2dc8e1ae77003ae6212a60c85d5210b64832b6
+$(PKG)_DEPS     := cc qt6-conf qt6-qtbase qt6-qtshadertools
+
+QT6_PREFIX   = '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)'
+QT6_QT_CMAKE = '$(QT6_PREFIX)/bin/qt-cmake-private' \
+                   -DCMAKE_INSTALL_PREFIX='$(QT6_PREFIX)'
+
+define $(PKG)_BUILD
+    $(QT6_QT_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)'
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' --build . -j '$(JOBS)'
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' --install .
+endef
