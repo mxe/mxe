c48dd7cd4d1e4d1d3d1cb19833240bc2b82517c3
Tony Theodore <tonyt@logyst.com>
2022-04-23 23:05:10 +1000

    mesa: update and switch to meson build

--- src/mesa.mk
+++ src/mesa.mk
@@ -3,33 +3,27 @@
 PKG             := mesa
 $(PKG)_WEBSITE  := https://mesa3d.org
 $(PKG)_DESCR    := The Mesa 3D Graphics Library
-$(PKG)_VERSION  := 20.3.0
-$(PKG)_CHECKSUM := 2999738e888731531cd62b27519fa37566cc0ea2cd7d4d97f46abaa3e949c630
+$(PKG)_VERSION  := 22.0.2
+$(PKG)_CHECKSUM := df4fa560dcce6680133067cd15b0505fc424ca703244ce9ab247c74d2fab6885
 $(PKG)_SUBDIR   := mesa-$($(PKG)_VERSION)
 $(PKG)_FILE     := mesa-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://archive.mesa3d.org/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc $(BUILD)~python-mako scons-local
+$(PKG)_DEPS     := cc meson-wrapper zlib zstd
 
 define $(PKG)_UPDATE
     $(call GET_LATEST_VERSION, https://archive.mesa3d.org, mesa-)
 endef
 
 define $(PKG)_BUILD
-    $(SCONS_PREP)
-    cd '$(SOURCE_DIR)' && \
-    MINGW_PREFIX='$(TARGET)-' $(SCONS_LOCAL) \
-        -j $(JOBS) \
-        platform=windows \
-        toolchain=crossmingw \
-        machine=$(if $(findstring x86_64,$(TARGET)),x86_64,x86) \
-        verbose=1 \
-        build=release \
-        libgl-gdi \
-        $(PKG_SCONS_OPTS)
+    '$(MXE_MESON_WRAPPER)' $(MXE_MESON_OPTS) \
+        -Dbuild-tests=false \
+        $(PKG_MESON_OPTS) \
+        '$(BUILD_DIR)' '$(SOURCE_DIR)'
+    '$(MXE_NINJA)' -C '$(BUILD_DIR)' -j '$(JOBS)'
+    '$(MXE_NINJA)' -C '$(BUILD_DIR)' -j '$(JOBS)' install
 
-    for i in EGL GLES GLES2 GLES3 KHR; do \
+    for i in EGL GLES GLES2 GLES3; do \
         $(INSTALL) -d "$(PREFIX)/$(TARGET)/include/$$i"; \
         $(INSTALL) -m 644 "$(1)/include/$$i/"* "$(PREFIX)/$(TARGET)/include/$$i/"; \
     done
-    $(INSTALL) -m 755 '$(1)/build/windows-$(if $(findstring x86_64,$(TARGET)),x86_64,x86)/gallium/targets/libgl-gdi/opengl32.dll' '$(PREFIX)/$(TARGET)/bin/'
 endef
