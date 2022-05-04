9722c0494d9d8f2574a3b231ea9c883631d4a54d
Mark Brand <mabrand@mabrand.nl>
2022-05-04 14:48:21 +0200

    update openssl

--- src/openssl.mk
+++ src/openssl.mk
@@ -3,8 +3,8 @@
 PKG             := openssl
 $(PKG)_WEBSITE  := https://www.openssl.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.0.2
-$(PKG)_CHECKSUM := 98e91ccead4d4756ae3c9cde5e09191a8e586d9f4d50838e7ec09d6411dfdb63
+$(PKG)_VERSION  := 3.0.3
+$(PKG)_CHECKSUM := ee0078adcef1de5f003c62c80cc96527721609c6f3bb42b7795df31f8b558c0b
 $(PKG)_SUBDIR   := openssl-$($(PKG)_VERSION)
 $(PKG)_FILE     := openssl-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://www.openssl.org/source/$($(PKG)_FILE)
