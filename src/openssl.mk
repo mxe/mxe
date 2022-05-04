c4894352751ee6ed3b78930428a2dc876409729b
Mark Brand <mabrand@mabrand.nl>
2022-05-04 15:01:33 +0200

    openssl: respect JOBS
    Parallel building seems to work fine for build_sw and install_sw in
    separate steps.

--- src/openssl.mk
+++ src/openssl.mk
@@ -18,6 +18,14 @@ define $(PKG)_UPDATE
     tail -1
 endef
 
+$(PKG)_MAKE = $(MAKE) -C '$(1)' -j '$(JOBS)'\
+        CC='$(TARGET)-gcc' \
+        RANLIB='$(TARGET)-ranlib' \
+        AR='$(TARGET)-ar' \
+        RC='$(TARGET)-windres' \
+        CROSS_COMPILE='$(TARGET)-' \
+        $(if $(BUILD_SHARED), ENGINESDIR='$(PREFIX)/$(TARGET)/bin/engines')
+
 define $(PKG)_BUILD
     # remove previous install
     rm -rfv '$(PREFIX)/$(TARGET)/include/openssl'
@@ -32,13 +40,8 @@ define $(PKG)_BUILD
         no-capieng \
         --prefix='$(PREFIX)/$(TARGET)' \
         --libdir='$(PREFIX)/$(TARGET)/lib'
-    $(MAKE) -C '$(1)' build_sw install_sw -j 1 \
-        CC='$(TARGET)-gcc' \
-        RANLIB='$(TARGET)-ranlib' \
-        AR='$(TARGET)-ar' \
-        RC='$(TARGET)-windres' \
-        CROSS_COMPILE='$(TARGET)-' \
-        $(if $(BUILD_SHARED), ENGINESDIR='$(PREFIX)/$(TARGET)/bin/engines')
+    $($(PKG)_MAKE) build_sw
+    $($(PKG)_MAKE) install_sw
 endef
 
 $(PKG)_BUILD_i686-w64-mingw32   = $(subst @openssl-target@,mingw,$($(PKG)_BUILD))
