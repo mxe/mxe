c3c4859174828ff70d86a59841ec29567e1d7012
Tomas Kalibera <tomas.kalibera@gmail.com>
2022-05-05 14:54:51 +0200

    gtk2: fix test build (-Wno-error=deprecated-declarations)

--- src/gtk2.mk
+++ src/gtk2.mk
@@ -39,7 +39,7 @@ define $(PKG)_BUILD
     rm -f '$(PREFIX)/$(TARGET)/lib/gailutil.def'
 
     '$(TARGET)-gcc' \
-        -W -Wall -Werror -ansi \
+        -W -Wall -Werror -Wno-error=deprecated-declarations -ansi \
         '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-gtk2.exe' \
         `'$(TARGET)-pkg-config' gtk+-2.0 gmodule-2.0 --cflags --libs`
 endef
