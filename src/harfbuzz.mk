e48a0738ebdfe15e5fbb4186800ce6e4653c8ecf
Boris Pek <tehnick-8@yandex.ru>
2022-05-12 05:50:24 +0300

    harfbuzz: build libharfbuzz-icu

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -7,7 +7,7 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 4.2.1
 $(PKG)_CHECKSUM := 99fcd30e2f4c66d05af3d61ad4cdba2abc2a51ecabb7eb6dc222520a892b50b0
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases
-$(PKG)_DEPS     := cc meson-wrapper cairo freetype-bootstrap glib
+$(PKG)_DEPS     := cc meson-wrapper cairo freetype-bootstrap glib icu4c
 
 define $(PKG)_BUILD
     '$(MXE_MESON_WRAPPER)' $(MXE_MESON_OPTS) \
