fa5adc017961aa9ec6fb3587405dfb8bb167143d
Mark Brand <mabrand@mabrand.nl>
2022-05-16 11:36:04 +0200

    qt6-qtbase: disable precompiled headers for host build
    With PCH disabled, ccache dramatically reduces rebuild time. For me,
    rebuild time drops from 9m10 to 0m50.

--- src/qt/qt6/qt6-qtbase.mk
+++ src/qt/qt6/qt6-qtbase.mk
@@ -76,6 +76,7 @@ define $(PKG)_BUILD_$(BUILD)
         -G Ninja \
         -DCMAKE_INSTALL_PREFIX='$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)' \
         -DQT_BUILD_{TESTS,EXAMPLES}=OFF \
+        -DBUILD_WITH_PCH=OFF \
         -DFEATURE_{eventfd,glib,harfbuzz,icu,opengl,openssl}=OFF \
         -DFEATURE_sql_{db2,ibase,mysql,oci,odbc,psql,sqlite}=OFF
     '$(TARGET)-cmake' --build '$(BUILD_DIR)' -j '$(JOBS)'
