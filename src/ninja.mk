98beaed121dc2f8fc0ab5882ba72d1fbfdd44e7e
Mark Brand <mabrand@mabrand.nl>
2022-05-17 12:55:51 +0200

    update ninja

--- src/ninja.mk
+++ src/ninja.mk
@@ -4,8 +4,8 @@ PKG             := ninja
 $(PKG)_WEBSITE  := https://ninja-build.org
 $(PKG)_DESCR    := A small build system with a focus on speed
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.10.2
-$(PKG)_CHECKSUM := ce35865411f0490368a8fc383f29071de6690cbadc27704734978221f25e2bed
+$(PKG)_VERSION  := 1.11.0
+$(PKG)_CHECKSUM := 3c6ba2e66400fe3f1ae83deb4b235faf3137ec20bd5b08c29bfc368db143e4c6
 $(PKG)_GH_CONF  := ninja-build/ninja/tags,v
 $(PKG)_TARGETS  := $(BUILD)
 
