9ac75a4def25ab4cf9a7ecfa44d5b2bf91b70e6e
Mark Brand <mabrand@mabrand.nl>
2022-06-29 17:15:27 +0200

    harfbuzz: update 4.3.0 -> 4.4.1

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -4,8 +4,8 @@ PKG             := harfbuzz
 $(PKG)_WEBSITE  := https://wiki.freedesktop.org/www/Software/HarfBuzz/
 $(PKG)_DESCR    := HarfBuzz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.3.0
-$(PKG)_CHECKSUM := 32184860ddc0b264ff95010e1c64e596bd746fe4c2e34014a1185340cdddeba6
+$(PKG)_VERSION  := 4.4.1
+$(PKG)_CHECKSUM := 1a95b091a40546a211b6f38a65ccd0950fa5be38d95c77b5c4fa245130b418e1
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases
 $(PKG)_DEPS     := cc meson-wrapper cairo freetype-bootstrap glib icu4c
 
