1d8972aa4c62f23b229d4fda7a3d6a2fe58830b0
Jonas Kvinge <jonas@jkvinge.net>
2022-10-01 14:58:06 +0200

    Update readline

--- src/readline.mk
+++ src/readline.mk
@@ -2,10 +2,10 @@
 
 PKG             := readline
 $(PKG)_WEBSITE  := https://tiswww.case.edu/php/chet/readline/rltop.html
-$(PKG)_DESCR    := Readline
+$(PKG)_DESCR    := GNU Readline library
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 8.1
-$(PKG)_CHECKSUM := f8ceb4ee131e3232226a17f51b164afc46cd0b9e6cef344be87c65962cb82b02
+$(PKG)_VERSION  := 8.2
+$(PKG)_CHECKSUM := 3feb7171f16a84ee82ca18a36d7b9be109a52c04f492a053331d7d1095007c35
 $(PKG)_SUBDIR   := readline-$($(PKG)_VERSION)
 $(PKG)_FILE     := readline-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/readline/$($(PKG)_FILE)
@@ -22,8 +22,9 @@ define $(PKG)_BUILD
     cd '$(1)' && bash_cv_wcwidth_broken=no \
         ./configure \
         $(MXE_CONFIGURE_OPTS) \
+        --disable-install-examples \
         --enable-multibyte \
-        --without-purify \
         --without-curses
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
 endef
