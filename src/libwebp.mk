e05dd21c717b09fb245f443ec1630bdb8090804d
Mark Brand <mabrand@mabrand.nl>
2022-10-05 12:28:10 +0200

    libwebp: update 1.2.2 -> 1.2.4

--- src/libwebp.mk
+++ src/libwebp.mk
@@ -3,17 +3,16 @@
 PKG             := libwebp
 $(PKG)_WEBSITE  := https://developers.google.com/speed/webp/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.2.2
-$(PKG)_CHECKSUM := 7656532f837af5f4cec3ff6bafe552c044dc39bf453587bd5b77450802f4aee6
+$(PKG)_VERSION  := 1.2.4
+$(PKG)_CHECKSUM := 7bf5a8a28cc69bcfa8cb214f2c3095703c6b73ac5fba4d5480c205331d9494df
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://storage.googleapis.com/downloads.webmproject.org/releases/webp/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://developers.google.com/speed/webp/download' | \
-    $(SED) -n 's,.*<a href="//storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-\([0-9][^"]*\)\.tar.gz">Download</a> |,\1,p' | \
-    $(SORT) -V | \
+    $(WGET) -q -O- 'https://storage.googleapis.com/downloads.webmproject.org/releases/webp/index.html' | \
+    $(SED) -n 's,<a href="[^"]*libwebp-\([0-9.]\+\)\.tar.gz".*,\1,p' | \
     tail -1
 endef
 
