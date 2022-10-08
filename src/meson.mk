af6c38a127b3b0ac7011f2bae56a81c279e7cd48
Mark Brand <mabrand@mabrand.nl>
2022-10-08 11:41:13 +0200

    meson: update 0.63.2 -> 0.63.3

--- src/meson.mk
+++ src/meson.mk
@@ -3,8 +3,8 @@
 PKG             := meson
 $(PKG)_WEBSITE  := https://mesonbuild.com/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.63.2
-$(PKG)_CHECKSUM := 16222f17ef76be0542c91c07994f9676ae879f46fc21c0c786a21ef2cb518bbf
+$(PKG)_VERSION  := 0.63.3
+$(PKG)_CHECKSUM := 519c0932e1a8b208741f0fdce90aa5c0b528dd297cf337009bf63539846ac056
 $(PKG)_GH_CONF  := mesonbuild/meson/releases
 $(PKG)_TARGETS  := $(BUILD)
 $(PKG)_DEPS_$(BUILD) := ninja
