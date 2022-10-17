b84f8564395e91a7425bc634f3a7439ec7e6613e
Mark Brand <mabrand@mabrand.nl>
2022-10-17 08:31:31 +0200

    gettext: update 0.21 -> 0.21.1

--- src/gettext.mk
+++ src/gettext.mk
@@ -3,8 +3,8 @@
 PKG             := gettext
 $(PKG)_WEBSITE  := https://www.gnu.org/software/gettext/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.21
-$(PKG)_CHECKSUM := 435b546e3880ab767c967c0731b20629a0cb0ba620e6bac2f590f401c10ebb45
+$(PKG)_VERSION  := 0.21.1
+$(PKG)_CHECKSUM := 53ae95b7ac8b6f6996a85076cf4f7c8b32c8c4027387b7b989dd0796333c8c8f
 $(PKG)_SUBDIR   := gettext-$($(PKG)_VERSION)
 $(PKG)_FILE     := gettext-$($(PKG)_VERSION).tar.lz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/gettext/$($(PKG)_FILE)
