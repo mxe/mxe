9e7885eb9612c680dba8c6a80ba16a25b264fa2e
Mark Brand <mabrand@mabrand.nl>
2022-10-21 13:54:09 +0200

    fontconfig: update 2.14.0 -> 2.14.1

--- src/fontconfig.mk
+++ src/fontconfig.mk
@@ -3,8 +3,8 @@
 PKG             := fontconfig
 $(PKG)_WEBSITE  := https://fontconfig.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.14.0
-$(PKG)_CHECKSUM := dcbeb84c9c74bbfdb133d535fe1c7bedc9f2221a8daf3914b984c44c520e9bac
+$(PKG)_VERSION  := 2.14.1
+$(PKG)_CHECKSUM := 298e883f6e11d2c5e6d53c8a8394de58d563902cfab934e6be12fb5a5f361ef0
 $(PKG)_SUBDIR   := fontconfig-$($(PKG)_VERSION)
 $(PKG)_FILE     := fontconfig-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://fontconfig.org/release/$($(PKG)_FILE)
