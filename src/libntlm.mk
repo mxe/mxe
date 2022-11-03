8be93c2b92aee570344515d7620dd6cb29c2ffa8
Mark Brand <mabrand@mabrand.nl>
2022-11-03 17:40:43 +0100

    libntlm: update upstream URLs and fix update macro

--- src/libntlm.mk
+++ src/libntlm.mk
@@ -1,18 +1,18 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := libntlm
-$(PKG)_WEBSITE  := https://www.nongnu.org/libntlm/
+$(PKG)_WEBSITE  := https://gitlab.com/gsasl/libntlm/
 $(PKG)_DESCR    := Libntlm
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.6
 $(PKG)_CHECKSUM := f2376b87b06d8755aa3498bb1226083fdb1d2cf4460c3982b05a9aa0b51d6821
 $(PKG)_SUBDIR   := libntlm-$($(PKG)_VERSION)
 $(PKG)_FILE     := libntlm-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://www.nongnu.org/libntlm/releases/$($(PKG)_FILE)
+$(PKG)_URL      := https://download.savannah.nongnu.org/releases/libntlm/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://www.nongnu.org/libntlm/releases/'  | \
+    $(WGET) -q -O- 'https://download.savannah.nongnu.org/releases/libntlm/'  | \
     $(SED) -n 's,.*libntlm-\([0-9]\+\)\(\.[0-9]\+\)*\..*,\1\2,p' |\
     tail -1
 endef
