ffe3bf45aced343bf54866b7ffc9e24c9b2a5fed
Mark Brand <mabrand@mabrand.nl>
2022-11-10 01:16:31 +0100

    mesa: update 22.2.2 -> 22.2.3

--- src/mesa.mk
+++ src/mesa.mk
@@ -3,8 +3,8 @@
 PKG             := mesa
 $(PKG)_WEBSITE  := https://mesa3d.org
 $(PKG)_DESCR    := The Mesa 3D Graphics Library
-$(PKG)_VERSION  := 22.2.2
-$(PKG)_CHECKSUM := 2de11fb74fc5cc671b818e49fe203cea0cd1d8b69756e97cdb06a2f4e78948f9
+$(PKG)_VERSION  := 22.2.3
+$(PKG)_CHECKSUM := ee7d026f7b1991dbae0861d359b671145c3a86f2a731353b885d2ea2d5c098d6
 $(PKG)_SUBDIR   := mesa-$($(PKG)_VERSION)
 $(PKG)_FILE     := mesa-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://archive.mesa3d.org/$($(PKG)_FILE)
