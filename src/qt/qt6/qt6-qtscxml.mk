80cc826cd89596983044588862c4cb853635ea28
Petzep <Petzep@users.noreply.github.com>
2023-03-23 12:56:11 +0100

    Added QtSCXML
    The Qt SCXML module provides functionality to create state machines from SCXML files. This includes both dynamically creating state machines (loading the SCXML file and instantiating states and transitions) and generating a C++ file that has a class implementing the state machine. It also contains functionality to support data models and executable content.

---
+++ src/qt/qt6/qt6-qtscxml.mk
@@ -0,0 +1,19 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+include src/qt/qt6/qt6-conf.mk
+
+PKG := qt6-qtscxml
+$(eval $(QT6_METADATA))
+
+$(PKG)_CHECKSUM := fe722c016d434c02e713e3c7d5d4a845998efe69d8b645923e83f1f4f7b72f3a
+$(PKG)_DEPS     := cc qt6-conf qt6-qtbase
+
+QT6_PREFIX   = '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)'
+QT6_QT_CMAKE = '$(QT6_PREFIX)/bin/qt-cmake-private' \
+                   -DCMAKE_INSTALL_PREFIX='$(QT6_PREFIX)'
+
+define $(PKG)_BUILD
+    $(QT6_QT_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)'
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' --build . -j '$(JOBS)'
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' --install .
+endef
