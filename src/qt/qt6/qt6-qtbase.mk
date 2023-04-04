mxe/mxe

commit 2e0f8150fa69e91666c4ab4a60d0406d944a7628

qt6-qtbase: work around QTBUG-103019 pkgconfig breakage
The Qt6Platform.pc file that is generated during the build contains
-D_UNICODE> due to a bug in Qt's pkgconfig generation code. See MinGW
Qt6Platform.pc has an extra '>' after '-D_UNICODE'
(https://bugreports.qt.io/browse/QTBUG-103019) for details.

Work around the issue by fixing up the Qt6Platform.pc file.

Signed-off-by: Stefan Hajnoczi <stefanha@jammr.net>

src/qt/qt6/qt6-qtbase.mk
@@ -65,6 +65,11 @@ define $(PKG)_BUILD
 	      -e 's/^QMAKE_PRL_LIBS_FOR_CMAKE .*/&;-lodbc32/;' \
               '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)/plugins/sqldrivers/qsqlodbc.prl',)
 
+    # QTBUG-103019 MinGW Qt6Platform.pc has an extra '>' after '-D_UNICODE'
+    # https://bugreports.qt.io/browse/QTBUG-103019
+    $(SED) -i 's/-D_UNICODE>/-D_UNICODE/' \
+              '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)/lib/pkgconfig/Qt6Platform.pc'
+
     mkdir -p '$(CMAKE_TOOLCHAIN_DIR)'
     echo 'set(QT_HOST_PATH "$(PREFIX)/$(BUILD)/$(MXE_QT6_ID)")' \
         > '$(CMAKE_TOOLCHAIN_DIR)/$(PKG).cmake'

commit aafbbc4ee0999c44588ad466e35eafa94cac9f3d

qt6-qtbase: don't try to fix non-existant .pc file in static builds
2e0f8150fa69 introduced a sed hack to fix the defect in Qt6Platform.pc,
but there don't seem to be any .pc files installed for static builds.

src/qt/qt6/qt6-qtbase.mk
@@ -67,8 +67,9 @@ define $(PKG)_BUILD
 
     # QTBUG-103019 MinGW Qt6Platform.pc has an extra '>' after '-D_UNICODE'
     # https://bugreports.qt.io/browse/QTBUG-103019
-    $(SED) -i 's/-D_UNICODE>/-D_UNICODE/' \
-              '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)/lib/pkgconfig/Qt6Platform.pc'
+    # However, qt6 seems to install .pc files only for shared builds.
+    $(if $(BUILD_SHARED),$(SED) -i 's/-D_UNICODE>/-D_UNICODE/' \
+              '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)/lib/pkgconfig/Qt6Platform.pc',)
 
     mkdir -p '$(CMAKE_TOOLCHAIN_DIR)'
     echo 'set(QT_HOST_PATH "$(PREFIX)/$(BUILD)/$(MXE_QT6_ID)")' \