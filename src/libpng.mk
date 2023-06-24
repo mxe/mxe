24e8ff82991cdfdbced890f7d96f4108d4bd2678
Mark Brand <mabrand@mabrand.nl>
2023-06-25 01:32:25 +0200

    libpng: update 1.6.39 -> 1.6.40

--- src/libpng.mk
+++ src/libpng.mk
@@ -3,8 +3,8 @@
 PKG             := libpng
 $(PKG)_WEBSITE  := http://www.libpng.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.6.39
-$(PKG)_CHECKSUM := 1f4696ce70b4ee5f85f1e1623dc1229b210029fa4b7aee573df3e2ba7b036937
+$(PKG)_VERSION  := 1.6.40
+$(PKG)_CHECKSUM := 535b479b2467ff231a3ec6d92a525906fb8ef27978be4f66dbe05d3f3a01b3a1
 $(PKG)_SUBDIR   := libpng-$($(PKG)_VERSION)
 $(PKG)_FILE     := libpng-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/libpng/libpng16/$($(PKG)_VERSION)/$($(PKG)_FILE)
