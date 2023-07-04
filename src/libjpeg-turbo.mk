be2e83b741257ea2cb081ecb1f5a679501884dde
Mark Brand <mabrand@mabrand.nl>
2023-07-04 11:47:56 +0200

    libjpeg-turbo: update 2.1.5.1 -> 3.0.0

--- src/libjpeg-turbo.mk
+++ src/libjpeg-turbo.mk
@@ -3,8 +3,8 @@
 PKG             := libjpeg-turbo
 $(PKG)_WEBSITE  := https://libjpeg-turbo.virtualgl.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.1.5.1
-$(PKG)_CHECKSUM := 2fdc3feb6e9deb17adec9bafa3321419aa19f8f4e5dea7bf8486844ca22207bf
+$(PKG)_VERSION  := 3.0.0
+$(PKG)_CHECKSUM := c77c65fcce3d33417b2e90432e7a0eb05f59a7fff884022a9d931775d583bfaa
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
