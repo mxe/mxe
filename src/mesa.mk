b401c2fded22f39771ca66840a8caab24b295496
Mark Brand <mabrand@mabrand.nl>
2023-07-26 23:55:23 +0200

    mesa: update 22.3.3 -> 23.1.4

--- src/mesa.mk
+++ src/mesa.mk
@@ -3,8 +3,8 @@
 PKG             := mesa
 $(PKG)_WEBSITE  := https://mesa3d.org
 $(PKG)_DESCR    := The Mesa 3D Graphics Library
-$(PKG)_VERSION  := 22.3.3
-$(PKG)_CHECKSUM := bed799788bf2bd9ef079d97cd8e09348bf53cb086818578e40773b2b17812922
+$(PKG)_VERSION  := 23.1.4
+$(PKG)_CHECKSUM := 7261a17fb94867e3dc5a90d8a1f100fa04b0cbbde51d25302c0872b5e9a10959
 $(PKG)_SUBDIR   := mesa-$($(PKG)_VERSION)
 $(PKG)_FILE     := mesa-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://archive.mesa3d.org/$($(PKG)_FILE)
