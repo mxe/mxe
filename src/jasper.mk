02dd13809df5172927937274b3159d7a6ec5b471
Mark Brand <mabrand@mabrand.nl>
2023-11-07 12:58:38 +0100

    jasper: update 4.0.0 -> 4.1.0

--- src/jasper.mk
+++ src/jasper.mk
@@ -4,8 +4,8 @@ PKG             := jasper
 $(PKG)_WEBSITE  := https://www.ece.uvic.ca/~mdadams/jasper/
 $(PKG)_DESCR    := JasPer
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.0.0
-$(PKG)_CHECKSUM := 977c4c2e4210f4e37313cd2232d99e73d57ab561917b3c060bcdd5e83a0a13f1
+$(PKG)_VERSION  := 4.1.0
+$(PKG)_CHECKSUM := 32879db502f59c1fde58e65b80d9b7db759f0173b7933c003517caf8f3892230
 $(PKG)_GH_CONF  := mdadams/jasper/tags, version-
 $(PKG)_DEPS     := cc jpeg
 
