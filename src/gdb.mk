e5da70cea454e029d16dce0f69c014c35a8b29c9
Mark Brand <mabrand@mabrand.nl>
2023-12-05 20:57:05 +0100

    gdb: update 13.2 -> 14.1

--- src/gdb.mk
+++ src/gdb.mk
@@ -2,13 +2,13 @@
 
 PKG             := gdb
 $(PKG)_WEBSITE  := https://www.gnu.org/software/gdb/
-$(PKG)_VERSION  := 13.2
-$(PKG)_CHECKSUM := fd5bebb7be1833abdb6e023c2f498a354498281df9d05523d8915babeb893f0a
+$(PKG)_VERSION  := 14.1
+$(PKG)_CHECKSUM := d66df51276143451fcbff464cc8723d68f1e9df45a6a2d5635a54e71643edb80
 $(PKG)_SUBDIR   := gdb-$($(PKG)_VERSION)
 $(PKG)_FILE     := gdb-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/$(PKG)/$($(PKG)_FILE)
 $(PKG)_URL_2    := https://ftpmirror.gnu.org/$(PKG)/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc dlfcn-win32 expat gmp libiconv mman-win32 readline zlib
+$(PKG)_DEPS     := cc dlfcn-win32 expat gmp libiconv mman-win32 mpfr readline zlib
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://ftp.gnu.org/gnu/gdb/?C=M;O=D' | \
