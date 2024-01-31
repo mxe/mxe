0e43be5dfaae1109b3ba16d0fa48658f4b970298
Mark Brand <mabrand@mabrand.nl>
2024-02-01 00:34:29 +0100

    curl: update 8.5.0 -> 8.6.0

--- src/curl.mk
+++ src/curl.mk
@@ -4,12 +4,12 @@ PKG             := curl
 $(PKG)_WEBSITE  := https://curl.haxx.se/libcurl/
 $(PKG)_DESCR    := cURL
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 8.5.0
-$(PKG)_CHECKSUM := 42ab8db9e20d8290a3b633e7fbb3cec15db34df65fd1015ef8ac1e4723750eeb
+$(PKG)_VERSION  := 8.6.0
+$(PKG)_CHECKSUM := 3ccd55d91af9516539df80625f818c734dc6f2ecf9bada33c76765e99121db15
 $(PKG)_SUBDIR   := curl-$($(PKG)_VERSION)
 $(PKG)_FILE     := curl-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://curl.haxx.se/download/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc brotli libidn2 libssh2 pthreads
+$(PKG)_DEPS     := cc brotli libidn2 libpsl libssh2 pthreads
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://curl.haxx.se/download/?C=M;O=D' | \
@@ -25,7 +25,7 @@ define $(PKG)_BUILD
         --enable-sspi \
         --enable-ipv6 \
         --with-libssh2 \
-        LIBS="`'$(TARGET)-pkg-config' libbrotlidec pthreads --libs`"
+        LIBS="`'$(TARGET)-pkg-config' libpsl libbrotlidec pthreads --libs`"
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_DOCS)
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_DOCS)
     ln -sf '$(PREFIX)/$(TARGET)/bin/curl-config' '$(PREFIX)/bin/$(TARGET)-curl-config'
