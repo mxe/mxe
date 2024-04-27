3e3e023f985cb5b8756b563eb79fe880043b409e
jakint0sh <96398097+jakint0sh@users.noreply.github.com>
2024-04-27 08:34:42 -0700

    Fix build failure for pakage `gtk2` (#3063)
    Switch to g++ in test command for GTK2 to fix build failure.

--- src/gtk2.mk
+++ src/gtk2.mk
@@ -36,7 +36,7 @@ define $(PKG)_BUILD
     # and *.def files aren't really relevant for MXE
     rm -f '$(PREFIX)/$(TARGET)/lib/gailutil.def'
 
-    '$(TARGET)-gcc' \
+    '$(TARGET)-g++' \
         -W -Wall -Werror -Wno-error=deprecated-declarations -ansi \
         '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-gtk2.exe' \
         `'$(TARGET)-pkg-config' gtk+-2.0 gmodule-2.0 --cflags --libs`
