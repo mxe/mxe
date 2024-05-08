b449d21aafe0bed27a8e84caaf93fd9b407cce5d
Mark Brand <mabrand@mabrand.nl>
2024-05-08 16:31:40 +0200

    unrtf: update 0.21.9 -> 0.21.10

--- src/unrtf.mk
+++ src/unrtf.mk
@@ -3,8 +3,8 @@
 PKG             := unrtf
 $(PKG)_WEBSITE  := https://www.gnu.org/software/unrtf/
 $(PKG)_DESCR    := unRTF
-$(PKG)_VERSION  := 0.21.9
-$(PKG)_CHECKSUM := 22a37826f96d754e335fb69f8036c068c00dd01ee9edd9461a36df0085fb8ddd
+$(PKG)_VERSION  := 0.21.10
+$(PKG)_CHECKSUM := b49f20211fa69fff97d42d6e782a62d7e2da670b064951f14bbff968c93734ae
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/$(PKG)/$($(PKG)_FILE)
@@ -20,6 +20,7 @@ define $(PKG)_BUILD
     cd '$(1)' && ./bootstrap
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
-        LIBS='-liconv -lws2_32'
+        CFLAGS='-Wno-discarded-qualifiers -Wno-implicit-function-declaration' \
+        LIBS='-liconv -lshlwapi -lws2_32'
     $(MAKE) -C '$(1)' -j '$(JOBS)' install
 endef
