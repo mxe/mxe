674c2d2e5c4d145d5c35de28379284abece3d2fc
Mark Brand <mabrand@mabrand.nl>
2024-05-14 17:40:20 +0200

    harfbuzz: update 8.4.0 -> 8.5.0

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -4,8 +4,8 @@ PKG             := harfbuzz
 $(PKG)_WEBSITE  := https://wiki.freedesktop.org/www/Software/HarfBuzz/
 $(PKG)_DESCR    := HarfBuzz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 8.4.0
-$(PKG)_CHECKSUM := af4ea73e25ab748c8c063b78c2f88e48833db9b2ac369e29bd115702e789755e
+$(PKG)_VERSION  := 8.5.0
+$(PKG)_CHECKSUM := 77e4f7f98f3d86bf8788b53e6832fb96279956e1c3961988ea3d4b7ca41ddc27
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases,,,,,.tar.xz
 $(PKG)_DEPS     := cc meson-wrapper brotli cairo freetype-bootstrap glib icu4c
 
