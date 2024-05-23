bb30fb7b84924bf5a5dd4ecc17825726c0952187
Mark Brand <mabrand@mabrand.nl>
2024-05-23 11:46:37 +0200

    update: cmake, meson, ninja

--- src/meson.mk
+++ src/meson.mk
@@ -3,8 +3,8 @@
 PKG             := meson
 $(PKG)_WEBSITE  := https://mesonbuild.com/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.2.1
-$(PKG)_CHECKSUM := b1db3a153087549497ee52b1c938d2134e0338214fe14f7efd16fecd57b639f5
+$(PKG)_VERSION  := 1.4.0
+$(PKG)_CHECKSUM := 8fd6630c25c27f1489a8a0392b311a60481a3c161aa699b330e25935b750138d
 $(PKG)_GH_CONF  := mesonbuild/meson/releases
 $(PKG)_TARGETS  := $(BUILD)
 $(PKG)_DEPS_$(BUILD) := ninja
