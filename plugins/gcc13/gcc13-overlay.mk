ea6b1d1feaaaecaf23bf39142b241d152d8f0f52
Mark Brand <mabrand@mabrand.nl>
2024-05-24 17:12:41 +0200

    gcc13: update release 13.3.0

--- plugins/gcc13/gcc13-overlay.mk
+++ plugins/gcc13/gcc13-overlay.mk
@@ -19,11 +19,11 @@
 
 PKG             := gcc
 # version used for tarball, will be X-YYYYMMDD for snapshots
-$(PKG)_VERSION  := 13.2.0
+$(PKG)_VERSION  := 13.3.0
 # release used for install dirs, will be X.0.1 for snapshots
 # change to $($(PKG)_VERSION) variable on release X.Y[>0].Z
 $(PKG)_RELEASE  := $($(PKG)_VERSION)
-$(PKG)_CHECKSUM := e275e76442a6067341a27f04c5c6b83d8613144004c0413528863dc6b5c743da
+$(PKG)_CHECKSUM := 0845e9621c9543a13f484e94584a49ffc0129970e9914624235fc1d061a0c083
 $(PKG)_SUBDIR   := gcc-$($(PKG)_VERSION)
 $(PKG)_FILE     := gcc-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/gcc/gcc-$($(PKG)_VERSION)/$($(PKG)_FILE)
