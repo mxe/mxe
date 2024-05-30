387e7145ec05c3dab6c2b54f2447c8e43d5d8241
Mark Brand <mabrand@mabrand.nl>
2024-05-30 11:47:12 +0200

    nghttp2: fix GH_CONF path

--- src/nghttp2.mk
+++ src/nghttp2.mk
@@ -6,7 +6,7 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.56.0
 $(PKG)_CHECKSUM := eb00ded354db1159dcccabc11b0aaeac893b7c9b154f8187e4598c4b8f3446b5
 $(PKG)_FILE     := nghttp2-$($(PKG)_VERSION).tar.gz
-$(PKG)_GH_CONF  := nghttp2/nghttp2/tags, v
+$(PKG)_GH_CONF  := nghttp2/nghttp2/releases/tags,v
 $(PKG)_DEPS     := cc
 
 define $(PKG)_BUILD
