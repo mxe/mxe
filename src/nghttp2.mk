5eea5817cab484fc1a1e5fab185194a589151b78
Mark Brand <mabrand@mabrand.nl>
2024-05-30 11:55:35 +0200

    rename libnghttp2 to nghttp2 to match project name

--- src/nghttp2.mk
+++ src/nghttp2.mk
@@ -1,6 +1,6 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
-PKG             := libnghttp2
+PKG             := nghttp2
 $(PKG)_WEBSITE  := https://nghttp2.org/
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.56.0
