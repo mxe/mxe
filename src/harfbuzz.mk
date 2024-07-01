56ae612c29d616d518baf8e42aa738464d2b453f
Mark Brand <mabrand@mabrand.nl>
2024-07-01 12:30:50 +0200

    harfbuzz: update 8.5.0 -> 9.0.0

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -4,14 +4,15 @@ PKG             := harfbuzz
 $(PKG)_WEBSITE  := https://wiki.freedesktop.org/www/Software/HarfBuzz/
 $(PKG)_DESCR    := HarfBuzz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 8.5.0
-$(PKG)_CHECKSUM := 77e4f7f98f3d86bf8788b53e6832fb96279956e1c3961988ea3d4b7ca41ddc27
+$(PKG)_VERSION  := 9.0.0
+$(PKG)_CHECKSUM := a41b272ceeb920c57263ec851604542d9ec85ee3030506d94662067c7b6ab89e
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases,,,,,.tar.xz
 $(PKG)_DEPS     := cc meson-wrapper brotli cairo freetype-bootstrap glib icu4c
 
 define $(PKG)_BUILD
     '$(MXE_MESON_WRAPPER)' $(MXE_MESON_OPTS) \
         -Dchafa=disabled \
+        -Dbenchmark=disabled \
         -Dtests=disabled \
         -Ddocs=disabled \
         -Dintrospection=disabled \
