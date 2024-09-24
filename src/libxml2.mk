bad1e2dd4546b17b04f21f703614b001157a1c8f
Mark Brand <mabrand@mabrand.nl>
2024-09-24 10:49:00 +0200

    libxml2: update 2.11.1 -> 2.13.4

--- src/libxml2.mk
+++ src/libxml2.mk
@@ -3,11 +3,11 @@
 PKG             := libxml2
 $(PKG)_WEBSITE  := http://xmlsoft.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.11.1
-$(PKG)_CHECKSUM := 3d39b294b856bfe3bafd5fb126e1f8487004261e78eabb8df9513e927915a995
+$(PKG)_VERSION  := 2.13.4
+$(PKG)_CHECKSUM := 65d042e1c8010243e617efb02afda20b85c2160acdbfbcb5b26b80cec6515650
 $(PKG)_SUBDIR   := libxml2-$($(PKG)_VERSION)
 $(PKG)_FILE     := libxml2-$($(PKG)_VERSION).tar.xz
-$(PKG)_URL      := https://download.gnome.org/sources/libxml2/2.11/$($(PKG)_FILE)
+$(PKG)_URL      := https://download.gnome.org/sources/libxml2/2.13/$($(PKG)_FILE)
 $(PKG)_URL_2    := https://ftp.osuosl.org/pub/blfs/conglomeration/libxml2/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc xz zlib
 
