e418de8b41d6fca54d17188a855609b36a86f196
Mark Brand <mabrand@mabrand.nl>
2024-11-02 10:46:28 +0100

    qt6-qtwebview: declare qt6-qtdeclarative dependency
    Fixes build failure:
        CMake Error at src/quick/CMakeLists.txt:9 (qt_internal_add_qml_module):
            Unknown CMake command "qt_internal_add_qml_module".

--- src/qt/qt6/qt6-qtwebview.mk
+++ src/qt/qt6/qt6-qtwebview.mk
@@ -6,7 +6,7 @@ PKG := qt6-qtwebview
 $(eval $(QT6_METADATA))
 
 $(PKG)_CHECKSUM := 7cb89d41593876b176368b15c1cded3d5a3c4fdf7e0a10f0c61021e3e8c179f3
-$(PKG)_DEPS     := cc qt6-conf qt6-qtbase
+$(PKG)_DEPS     := cc qt6-conf qt6-qtbase qt6-qtdeclarative
 
 QT6_PREFIX   = '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)'
 QT6_QT_CMAKE = '$(QT6_PREFIX)/$(if $(findstring mingw,$(TARGET)),bin,libexec)/qt-cmake-private' \
