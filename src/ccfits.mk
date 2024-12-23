0509c8ba74c0fa944e942158c93222dfb06a4cd3
Tony Theodore <tonyt@logyst.com>
2024-12-24 01:36:30 +1100

    ccfits: update 2.5 --> 2.6 and enable shared

--- src/ccfits.mk
+++ src/ccfits.mk
@@ -4,24 +4,24 @@ PKG             := ccfits
 $(PKG)_WEBSITE  := https://heasarc.gsfc.nasa.gov/fitsio/ccfits
 $(PKG)_DESCR    := CCfits
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.5
-$(PKG)_CHECKSUM := 938ecd25239e65f519b8d2b50702416edc723de5f0a5387cceea8c4004a44740
-$(PKG)_SUBDIR   := CCfits
+$(PKG)_VERSION  := 2.6
+$(PKG)_CHECKSUM := 2bb439db67e537d0671166ad4d522290859e8e56c2f495c76faa97bc91b28612
+$(PKG)_SUBDIR   := CCfits-$($(PKG)_VERSION)
 $(PKG)_FILE     := CCfits-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://heasarc.gsfc.nasa.gov/fitsio/CCfits/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc cfitsio
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- "https://heasarc.gsfc.nasa.gov/docs/software/fitsio/ccfits/" | \
-    grep -i '<a href="CCfits.*tar' | \
+    $(WGET) -q -O- "https://heasarc.gsfc.nasa.gov/fitsio/ccfits/" | \
+    grep -i '<a href=".*CCfits.*tar' | \
     $(SED) -n 's,.*CCfits-\([0-9][^>]*\)\.tar.*,\1,p' | \
     head -1
 endef
 
 define $(PKG)_BUILD
-    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)'
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
+        -DBUILD_TESTING=OFF \
+        -DBUILD_PROGRAMS=OFF
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
-
-$(PKG)_BUILD_SHARED =
