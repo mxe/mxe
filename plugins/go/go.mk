28ed9e8961244e59f4c1dc4b5469981bab14e4f4
Boris Nagaev <bnagaev@gmail.com>
2024-12-31 16:46:17 +0000

    go plugin: utilize system Go installation
    This update allows any Go installation to be used for cross-compilation,
    provided the appropriate environment variables are configured. Building an
    additional Go installation within MXE is unnecessary and inefficient.
    Additionally, modern versions of Go are implemented in Go itself, requiring a
    contemporary Go version for bootstrapping. This process would otherwise involve
    building Go multiple times (at least 4+ iterations) to transition from C.
    
    Go <= 1.4: a C toolchain.
    1.5 <= Go <= 1.19: a Go 1.4 compiler.
    1.20 <= Go <= 1.21: a Go 1.17 compiler.
    1.22 <= Go <= 1.23: a Go 1.20 compiler.
    Going forward, Go version 1.N will require a Go 1.M compiler, where M is N-2
    rounded down to an even number. Example: Go 1.24 and 1.25 require Go 1.22.
    https://go.dev/doc/install/source#bootstrapFromCrosscompiledSource

--- plugins/go/go.mk
+++ plugins/go/go.mk
@@ -3,36 +3,16 @@
 PKG             := go
 $(PKG)_WEBSITE  := https://golang.org/
 $(PKG)_OWNER    := https://github.com/starius
-$(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.8
-$(PKG)_CHECKSUM := 406865f587b44be7092f206d73fc1de252600b79b3cacc587b74b5ef5c623596
 $(PKG)_SUBDIR   := go
-$(PKG)_FILE     := go$($(PKG)_VERSION).src.tar.gz
-$(PKG)_URL      := https://storage.googleapis.com/golang/$($(PKG)_FILE)
-$(PKG)_DEPS     := $(BUILD)~go-native
+$(PKG)_TYPE     := meta
 
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://golang.org/dl/' | \
-    $(SED) -n 's,.*go\(1.[0-9][^>]*\)\.src\.tar.*,\1,p' | \
-    $(SORT) -h | tail -1
-endef
+GO := $(shell which go)
 
 define $(PKG)_BUILD
-    cd '$(1)/src' && \
-        GOROOT_BOOTSTRAP='$(PREFIX)/$(BUILD)/go' \
-        GOROOT_FINAL='$(PREFIX)/$(TARGET)/go' \
-        GOOS=windows \
-        GOARCH='$(if $(findstring x86_64,$(TARGET)),amd64,386)' \
-        DYLD_INSERT_LIBRARIES= \
-        ./make.bash
+    # Make sure Go binary was found.
+    $(GO) version
 
-    rm -rf '$(PREFIX)/$(TARGET)/go'
-    mkdir -p '$(PREFIX)/$(TARGET)/go'
-    for d in include src bin pkg; do \
-        cp -a '$(1)'/$$d '$(PREFIX)/$(TARGET)/go/' ; \
-    done
-
-    #create prefixed go wrapper script
+    # Create prefixed go wrapper script.
     mkdir -p '$(PREFIX)/bin/$(TARGET)'
     (echo '#!/usr/bin/env bash'; \
      echo 'echo "== Using MXE Go wrapper: $(PREFIX)/bin/$(TARGET)-go"'; \
@@ -44,16 +24,14 @@ define $(PKG)_BUILD
      echo 'CC=$(PREFIX)/bin/$(TARGET)-gcc \'; \
      echo 'CXX=$(PREFIX)/bin/$(TARGET)-g++ \'; \
      echo 'PKG_CONFIG=$(PREFIX)/bin/$(TARGET)-pkg-config \'; \
-     echo 'exec $(PREFIX)/$(TARGET)/go/bin/go \'; \
+     echo 'exec $(GO) \'; \
      echo '"$$@"'; \
     ) \
              > '$(PREFIX)/bin/$(TARGET)-go'
     chmod 0755 '$(PREFIX)/bin/$(TARGET)-go'
 
-    GOPATH=$(PWD)/plugins/go \
-    '$(TARGET)-go' build \
-        -o '$(PREFIX)/$(TARGET)/go/bin/test-go.exe' \
-        mxe-test
+    cd $(PWD)/plugins/go/mxe-test && '$(TARGET)-go' build \
+        -o '$(PREFIX)/$(TARGET)/bin/test-go.exe' .
 endef
 
 # -buildmode=shared not supported on windows
