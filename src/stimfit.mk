55d14c127fb844bc0f083bfd3b279fb1fc6407dd
Alois Schlögl <alois.schloegl@gmail.com>
2025-02-08 02:17:59 +0100

    stimfit v0.16.5

--- src/stimfit.mk
+++ src/stimfit.mk
@@ -3,18 +3,19 @@
 
 PKG             := stimfit
 $(PKG)_IGNORE   := 
-$(PKG)_VERSION  := 0.16.4
-$(PKG)_CHECKSUM := 9d7e8b9ca3ab10990230b17d8a47ac2bd25d32c7d501fac1e1768980c548195e
-$(PKG)_SUBDIR   := stimfit-$($(PKG)_VERSION)
-$(PKG)_FILE     := stimfit-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/neurodroid/stimfit/archive/refs/tags/v$($(PKG)_VERSION).tar.gz
-$(PKG)_DEPS     := cc biosig wxwidgets hdf5 fftw levmar lapack
+$(PKG)_VERSION  := 0.16.5
+# $(PKG)_CHECKSUM := 9d7e8b9ca3ab10990230b17d8a47ac2bd25d32c7d501fac1e1768980c548195e
+$(PKG)_SUBDIR   := stimfit-$($(PKG)_VERSION)debian
+$(PKG)_FILE     := stimfit-$($(PKG)_VERSION)debian.tar.gz
+# $(PKG)_URL      := https://github.com/neurodroid/stimfit/archive/refs/tags/v0.16.5debian.tar.gz
+# https://github.com/neurodroid/stimfit/archive/refs/tags/v$($(PKG)_VERSION).tar.gz
+$(PKG)_DEPS     := cc biosig wxwidgets hdf5 fftw levmar openblas
 
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://github.com/neurodroid/stimfit/tags' | \
-    $(SED) -n 's_.*<a href="/neurodroid/stimfit/tags/\(0.[0-9\.]*\.*).tar.gz" rel="nofollow">.*_\1_ip' \
-    head -1
-endef
+#define $(PKG)_UPDATE
+#    $(WGET) -q -O- 'https://github.com/neurodroid/stimfit/tags' | \
+#    $(SED) -n 's_.*<a href="/neurodroid/stimfit/tags/\(0.[0-9\.]*\.*).tar.gz" rel="nofollow">.*_\1_ip' \
+#    head -1
+#endef
 
 define $(PKG)_BUILD
 
