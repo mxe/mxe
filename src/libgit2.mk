917d2607dece33d58d48a41bcb40e17f9274739c
Tomas Kalibera <kalibera@users.noreply.github.com>
2025-02-22 01:12:07 +0100

    libgit2: enable SSH support. Always use system PCRE. Declare dependency on zlib. (#3192)

--- src/libgit2.mk
+++ src/libgit2.mk
@@ -6,11 +6,13 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.7.1
 $(PKG)_CHECKSUM := 17d2b292f21be3892b704dddff29327b3564f96099a1c53b00edc23160c71327
 $(PKG)_GH_CONF  := libgit2/libgit2/releases/latest,v
-$(PKG)_DEPS     := cc libssh2
+$(PKG)_DEPS     := cc libssh2 pcre2 zlib
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
-        -DDLLTOOL='$(PREFIX)/bin/$(TARGET)-dlltool'
+        -DDLLTOOL='$(PREFIX)/bin/$(TARGET)-dlltool' \
+        -DUSE_SSH=ON \
+        -DREGEX_BACKEND=pcre2
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1 || $(MAKE) -C '$(BUILD_DIR)' -j 1 VERBOSE=1
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1
 endef
