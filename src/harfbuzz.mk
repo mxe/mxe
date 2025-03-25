30facf1a99f3236aee6adcad05278f02dc55bda1
Mark Brand <mabrand@mabrand.nl>
2025-03-25 09:43:39 +0100

    harfbuzz: update 10.4.0 -> 11.0.0

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -4,8 +4,8 @@ PKG             := harfbuzz
 $(PKG)_WEBSITE  := https://wiki.freedesktop.org/www/Software/HarfBuzz/
 $(PKG)_DESCR    := HarfBuzz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 10.4.0
-$(PKG)_CHECKSUM := 480b6d25014169300669aa1fc39fb356c142d5028324ea52b3a27648b9beaad8
+$(PKG)_VERSION  := 11.0.0
+$(PKG)_CHECKSUM := f16351bafe214725fe2c1d5b59f0d93e49905a4b247899fb90d70cff953a2b9b
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases,,,,,.tar.xz
 $(PKG)_DEPS     := cc meson-wrapper brotli cairo freetype-bootstrap glib icu4c
 
