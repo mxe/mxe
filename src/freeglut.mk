7824729426c8aba808a68a52fcb9ad14296c4d33
Mark Brand <mabrand@mabrand.nl>
2025-04-04 18:46:53 +0200

    freeglut: update 3.2.1 -> 3.6.0

--- src/freeglut.mk
+++ src/freeglut.mk
@@ -3,8 +3,8 @@
 PKG             := freeglut
 $(PKG)_WEBSITE  := https://freeglut.sourceforge.io/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.2.1
-$(PKG)_CHECKSUM := d4000e02102acaf259998c870e25214739d1f16f67f99cb35e4f46841399da68
+$(PKG)_VERSION  := 3.6.0
+$(PKG)_CHECKSUM := 9c3d4d6516fbfa0280edc93c77698fb7303e443c1aaaf37d269e3288a6c3ea52
 $(PKG)_SUBDIR   := freeglut-$($(PKG)_VERSION)
 $(PKG)_FILE     := freeglut-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/freeglut/freeglut/$($(PKG)_VERSION)/$($(PKG)_FILE)
