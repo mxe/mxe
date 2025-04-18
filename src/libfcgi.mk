6337f7130c79dbcad0eb6b3b6b2e7609d2886e42
Mark Brand <mabrand@mabrand.nl>
2025-04-18 11:54:42 +0200

    libfcgi: update 2.4.2 -> 2.4.5

--- src/libfcgi.mk
+++ src/libfcgi.mk
@@ -5,15 +5,16 @@ PKG             := libfcgi
 $(PKG)_WEBSITE  := https://github.com/FastCGI-Archives
 $(PKG)_DESCR    := FastCGI
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.4.2
-$(PKG)_CHECKSUM := 1fe83501edfc3a7ec96bb1e69db3fd5ea1730135bd73ab152186fd0b437013bc
+$(PKG)_VERSION  := 2.4.5
+$(PKG)_CHECKSUM := 92b0111a98d8636e06c128444a3d4d7a720bdd54e6ee4dd0c7b67775b1b0abff
 $(PKG)_GH_CONF  := FastCGI-Archives/fcgi2/tags
 $(PKG)_DEPS     := cc
 
 define $(PKG)_BUILD
     cd '$(SOURCE_DIR)' && autoreconf -fi
     cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
-        $(MXE_CONFIGURE_OPTS)
+        $(MXE_CONFIGURE_OPTS) \
+        CFLAGS='-Wno-incompatible-pointer-types'
 
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_CRUFT)
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_CRUFT)
