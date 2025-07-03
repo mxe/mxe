e334ca44c7098459cef7b14b083d923e8b0a14be
Mark Brand <mabrand@mabrand.nl>
2025-07-03 21:17:50 +0200

    openssl: update 3.5.0 -> 3.5.1

--- src/openssl.mk
+++ src/openssl.mk
@@ -3,8 +3,8 @@
 PKG             := openssl
 $(PKG)_WEBSITE  := https://www.openssl.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.5.0
-$(PKG)_CHECKSUM := 344d0a79f1a9b08029b0744e2cc401a43f9c90acd1044d09a530b4885a8e9fc0
+$(PKG)_VERSION  := 3.5.1
+$(PKG)_CHECKSUM := 529043b15cffa5f36077a4d0af83f3de399807181d607441d734196d889b641f
 $(PKG)_GH_CONF  := openssl/openssl/releases,openssl-
 $(PKG)_SUBDIR   := openssl-$($(PKG)_VERSION)
 $(PKG)_FILE     := openssl-$($(PKG)_VERSION).tar.gz
