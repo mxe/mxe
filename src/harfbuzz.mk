dfcc5008a7c7b4f4521e5eb63ae6822bd9759e7b
Mark Brand <mabrand@mabrand.nl>
2025-09-29 14:27:19 +0200

    harfbuzz: update 11.5.1 -> 12.0.0

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -4,9 +4,9 @@ PKG             := harfbuzz
 $(PKG)_WEBSITE  := https://wiki.freedesktop.org/www/Software/HarfBuzz/
 $(PKG)_DESCR    := HarfBuzz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 11.5.1
-$(PKG)_CHECKSUM := 972a60a8d274d49e70361da6920c3a73dfb0fb4387f6c6811906a47ba634d8a1
-$(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases,,,,,.tar.xz
+$(PKG)_VERSION  := 12.0.0
+$(PKG)_CHECKSUM := 9f63c4a0fcdcb90b5a2c54dd17ad23429dfb281de6c0986863b8a89a05e5bc27
+$(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases,,,v0,,.tar.xz
 $(PKG)_DEPS     := cc meson-wrapper brotli cairo freetype-bootstrap glib icu4c
 
 define $(PKG)_BUILD
