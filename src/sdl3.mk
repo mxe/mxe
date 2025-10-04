ff038dd8ef5a6b697bdaf17e03be48ce391334d7
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-10-04 15:46:31 +0200

    sdl3: update to version 3.2.24

--- src/sdl3.mk
+++ src/sdl3.mk
@@ -4,10 +4,10 @@ PKG             := sdl3
 $(PKG)_WEBSITE  := https://www.libsdl.org/
 $(PKG)_DESCR    := SDL3
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.2.20
+$(PKG)_VERSION  := 3.2.24
 $(PKG)_SUBDIR   := SDL3-$($(PKG)_VERSION)
 $(PKG)_FILE     := SDL3-$($(PKG)_VERSION).tar.gz
-$(PKG)_CHECKSUM := 467600ae090dd28616fa37369faf4e3143198ff1da37729b552137e47f751a67
+$(PKG)_CHECKSUM := 81cc0fc17e5bf2c1754eeca9af9c47a76789ac5efdd165b3b91cbbe4b90bfb76
 $(PKG)_GH_CONF  := libsdl-org/SDL/releases/tag,release-,,
 $(PKG)_DEPS     := cc libiconv libsamplerate
 
