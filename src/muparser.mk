6d5b0951ce98f5598aaee43bda6900dafcb31cdc
Chris Toney <jctoney@gmail.com>
2026-01-11 16:12:47 -0700

    muparser: update to 2.3.5

--- src/muparser.mk
+++ src/muparser.mk
@@ -4,16 +4,16 @@ PKG             := muparser
 $(PKG)_WEBSITE  := https://beltoforion.de/article.php?a=muparser
 $(PKG)_DESCR    := muParser
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.2.5
-$(PKG)_CHECKSUM := 0666ef55da72c3e356ca85b6a0084d56b05dd740c3c21d26d372085aa2c6e708
+$(PKG)_VERSION  := 2.3.5
+$(PKG)_CHECKSUM := 20b43cc68c655665db83711906f01b20c51909368973116dfc8d7b3c4ddb5dd4
 $(PKG)_GH_CONF  := beltoforion/muparser/tags,v
 $(PKG)_DEPS     := cc
 
 define $(PKG)_BUILD
-    cd '$(BUILD_DIR)' && $(SOURCE_DIR)/configure \
-        $(MXE_CONFIGURE_OPTS) \
-        --disable-samples \
-        --disable-debug
-    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' '$(SOURCE_DIR)' \
+        -DENABLE_SAMPLES=OFF \
+        -DBUILD_SHARED_LIBS:BOOL=$(if $(BUILD_SHARED),ON,OFF)
+
+    $(MAKE) -C '$(BUILD_DIR)' -j $(JOBS)
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
