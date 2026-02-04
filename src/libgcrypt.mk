a77031fc360a363dce1229b40834bb026c94d843
Mark Brand <mabrand@mabrand.nl>
2026-02-04 21:06:40 +0100

    libgcrypt: update 1.11.2 -> 1.12.0

--- src/libgcrypt.mk
+++ src/libgcrypt.mk
@@ -3,8 +3,8 @@
 PKG             := libgcrypt
 $(PKG)_WEBSITE  := https://directory.fsf.org/wiki/Libgcrypt
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.11.2
-$(PKG)_CHECKSUM := 6ba59dd192270e8c1d22ddb41a07d95dcdbc1f0fb02d03c4b54b235814330aac
+$(PKG)_VERSION  := 1.12.0
+$(PKG)_CHECKSUM := 0311454e678189bad62a7e9402a9dd793025efff6e7449898616e2fc75e0f4f5
 $(PKG)_SUBDIR   := libgcrypt-$($(PKG)_VERSION)
 $(PKG)_FILE     := libgcrypt-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := https://gnupg.org/ftp/gcrypt/libgcrypt/$($(PKG)_FILE)
