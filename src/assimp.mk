1ac4171e4b32c786d45a85af0616cd741510f4ed
Mark Brand <mabrand@mabrand.nl>
2026-05-19 22:44:55 +0200

    assimp: update 6.0.2 -> 6.0.5
    and gcc16 compatibility options

--- src/assimp.mk
+++ src/assimp.mk
@@ -4,16 +4,16 @@ PKG             := assimp
 $(PKG)_WEBSITE  := https://www.assimp.org/
 $(PKG)_DESCR    := Assimp Open Asset Import Library
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 6.0.2
-$(PKG)_CHECKSUM := d1822d9a19c9205d6e8bc533bf897174ddb360ce504680f294170cc1d6319751
+$(PKG)_VERSION  := 6.0.5
+$(PKG)_CHECKSUM := edf3749559c2b7d1f758ffb66fc5bec62186221e623b7f2e8969f17ee46ecb6f
 $(PKG)_GH_CONF  := assimp/assimp/tags, v
 $(PKG)_DEPS     := cc minizip
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake \
         -DASSIMP_BUILD_TESTS=OFF \
-        -DCMAKE_C_FLAGS='-Wno-error=array-bounds -Wno-error=maybe-uninitialized' \
-        -DCMAKE_CXX_FLAGS='-Wno-error=array-bounds -Wno-error=maybe-uninitialized' \
+        -DCMAKE_C_FLAGS='-Wno-error=array-bounds -Wno-error=maybe-uninitialized -Wno-error=unused-but-set-variable= -Wno-error=uninitialized' \
+        -DCMAKE_CXX_FLAGS='-Wno-error=array-bounds -Wno-error=maybe-uninitialized -Wno-error=unused-but-set-variable= -Wno-error=uninitialized' \
         '$(SOURCE_DIR)'
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
