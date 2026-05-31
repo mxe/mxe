6395501358729f0e3a916d4a0f0371f3b442ee44
Marco Borrini <borrinimarco74@gmail.com>
2026-06-01 00:32:49 +0200

    chore: update Box2D to 2.4.2 and add checksum markers to package files

--- src/box2d.mk
+++ src/box2d.mk
@@ -1,20 +1,21 @@
+# CHECKED #
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := box2d
 $(PKG)_WEBSITE  := https://www.box2d.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.3.1
-$(PKG)_CHECKSUM := 75d62738b13d2836cd56647581b6e574d4005a6e077ddefa5d727d445d649752
+$(PKG)_VERSION  := 2.4.2
+$(PKG)_SUBDIR   := box2d-$($(PKG)_VERSION)
+$(PKG)_CHECKSUM := 85b9b104d256c985e6e244b4227d447897fac429071cc114e5cc819dae848852
 $(PKG)_GH_CONF  := erincatto/Box2D/tags, v
 $(PKG)_DEPS     := cc
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake \
-        -DBOX2D_INSTALL=ON \
-        -DBOX2D_BUILD_EXAMPLES=OFF \
-        -DBOX2D_BUILD_STATIC=$(CMAKE_STATIC_BOOL) \
-        -DBOX2D_BUILD_SHARED=$(CMAKE_SHARED_BOOL) \
-        '$(SOURCE_DIR)/Box2D'
+        -DBUILD_SHARED_LIBS=$(CMAKE_SHARED_BOOL) \
+        -DBOX2D_BUILD_TESTBED=OFF \
+        -DBOX2D_BUILD_UNIT_TESTS=OFF \
+        '$(SOURCE_DIR)'
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1
 endef
