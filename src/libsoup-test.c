55ccd2f2e3caa87efba972ce4945a2d59d5ce159
Mark Brand <mabrand@mabrand.nl>
2026-06-27 01:08:51 +0200

    libsoup: update 2.74.2 -> 3.7.1

--- src/libsoup-test.c
+++ src/libsoup-test.c
@@ -9,7 +9,7 @@ int main()
     SoupServer *server;
     GError *error;
 
-    server = soup_server_new (SOUP_SERVER_SERVER_HEADER, "simple-httpd ", SOUP_SERVER_TLS_CERTIFICATE, NULL, NULL);
+    server = soup_server_new ("server-header", "simple-httpd ", "tls_certificate", NULL, NULL);
     soup_server_listen_all (server, 1234, SOUP_SERVER_LISTEN_HTTPS, &error);
 
     return 0;
