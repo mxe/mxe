0fd5d208c7fb5e6e9267e4ae3aebc2ac816a5647
Marco Borrini <borrinimarco74@gmail.com>
2026-08-04 23:59:04 +0200

    Add new package kf6-kdnssd

---
+++ src/kf/kf6/kf6-kdnssd.mk
@@ -0,0 +1,18 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+include src/kf/kf6/kf6-conf.mk
+
+PKG              := kf6-kdnssd
+$(eval $(call KF6_METADATA))
+
+$(PKG)_CHECKSUM  := f4fe731aad56ae010c2b42f2bd56d4499339bb0839ae2251035132f1a3708df2
+$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase qt6-qttools
+$(PKG)_TARGETS   := $(MXE_TARGETS)
+$(PKG)_IGNORE    := 
+
+define $(PKG)_BUILD
+    $(KF6_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)'
+    
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' --build . -j '$(JOBS)'
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' --install .
+endef
