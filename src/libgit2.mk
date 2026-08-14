58daa3d6936a918346b74f62b81234cceb193006
Mark Brand <mabrand@mabrand.nl>
2026-08-14 15:11:43 +0200

    libgit2: update 1.7.1 -> 1.9.7

--- src/libgit2.mk
+++ src/libgit2.mk
@@ -3,8 +3,8 @@
 PKG             := libgit2
 $(PKG)_WEBSITE  := https://libgit2.github.com/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.7.1
-$(PKG)_CHECKSUM := 17d2b292f21be3892b704dddff29327b3564f96099a1c53b00edc23160c71327
+$(PKG)_VERSION  := 1.9.7
+$(PKG)_CHECKSUM := 1a4fbe7589e814777ae76b64734ad80f4ecad22cd33a22682a2aaea4ae5375e7
 $(PKG)_GH_CONF  := libgit2/libgit2/releases/latest,v
 $(PKG)_DEPS     := cc libssh2 pcre2 zlib
 
