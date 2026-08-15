17941011ba639a9d6272feef9a4974ff7d8774d2
Mark Brand <mabrand@mabrand.nl>
2026-08-15 12:07:01 +0200

    librtmp: update f1b83c1 -> 6f6bb13

--- src/librtmp.mk
+++ src/librtmp.mk
@@ -3,8 +3,8 @@
 PKG             := librtmp
 $(PKG)_WEBSITE  := https://rtmpdump.mplayerhq.hu/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := f1b83c1
-$(PKG)_CHECKSUM := f616525e1540b666c79038a9b7aa80ca73f6535dda6fd91ee1cb9d9bd2e219ba
+$(PKG)_VERSION  := 6f6bb13
+$(PKG)_CHECKSUM := 9ea43963306a6d1d0769dea9180345f6ce4438b82b34a56a7086a803d2412721
 $(PKG)_GH_CONF  := mirror/rtmpdump/branches/master
 $(PKG)_DEPS     := cc gnutls zlib
 
