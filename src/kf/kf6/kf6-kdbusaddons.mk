6973a14b6789e06cb0b56d960659561875538a14
marco.borrini <marco.borrini@lepida.it>
2026-08-20 02:42:19 +0200

    Update kf6-kdbusaddons: apply implementation plan fixes

--- src/kf/kf6/kf6-kdbusaddons.mk
+++ src/kf/kf6/kf6-kdbusaddons.mk
@@ -6,7 +6,7 @@ PKG              := kf6-kdbusaddons
 $(eval $(call KF6_METADATA))
 
 $(PKG)_CHECKSUM  := 94ff8745ce65507986a05bffbab905bfd894936e8f53b4b6e2d9b3a96cb2d6f4
-$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase
+$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase qt6-qttools
 $(PKG)_TARGETS   := $(MXE_TARGETS)
 $(PKG)_IGNORE    := 
 
