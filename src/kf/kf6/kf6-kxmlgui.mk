98ed9d7e4565e7611f3a58a721686a7c82a46481
marco.borrini <marco.borrini@lepida.it>
2026-08-20 02:42:20 +0200

    Update kf6-kxmlgui: apply implementation plan fixes

--- src/kf/kf6/kf6-kxmlgui.mk
+++ src/kf/kf6/kf6-kxmlgui.mk
@@ -6,7 +6,7 @@ PKG              := kf6-kxmlgui
 $(eval $(call KF6_METADATA))
 
 $(PKG)_CHECKSUM  := e40b86ebb9f1be00255cd4835ab0b0ac8650c47d0eb17a47d9df7d4b5658df58
-$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase kf6-kcoreaddons kf6-kitemviews kf6-kconfig kf6-kconfigwidgets kf6-kguiaddons kf6-ki18n kf6-kiconthemes
+$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase kf6-kcoreaddons kf6-kitemviews kf6-kconfig kf6-kconfigwidgets kf6-kguiaddons kf6-ki18n kf6-kiconthemes kf6-kwidgetsaddons
 $(PKG)_TARGETS   := $(MXE_TARGETS)
 $(PKG)_IGNORE    := 
 
