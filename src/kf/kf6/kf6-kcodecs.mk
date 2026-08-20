63247d069fd315fb59997c26bb6d348827302352
marco.borrini <marco.borrini@lepida.it>
2026-08-20 02:42:19 +0200

    Update kf6-kcodecs: apply implementation plan fixes

--- src/kf/kf6/kf6-kcodecs.mk
+++ src/kf/kf6/kf6-kcodecs.mk
@@ -6,7 +6,7 @@ PKG              := kf6-kcodecs
 $(eval $(call KF6_METADATA))
 
 $(PKG)_CHECKSUM  := b0a9ae34b389c6460adad5323d21bb9e43638acb58da9685f8dc8d821c38a4d8
-$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase
+$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase qt6-qttools
 $(PKG)_TARGETS   := $(MXE_TARGETS)
 $(PKG)_IGNORE    := 
 
