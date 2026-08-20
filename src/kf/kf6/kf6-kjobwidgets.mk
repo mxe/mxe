7bdcb93de51844d2207e982700d9f0efecc0d42e
marco.borrini <marco.borrini@lepida.it>
2026-08-20 02:42:20 +0200

    Update kf6-kjobwidgets: apply implementation plan fixes

--- src/kf/kf6/kf6-kjobwidgets.mk
+++ src/kf/kf6/kf6-kjobwidgets.mk
@@ -6,7 +6,7 @@ PKG              := kf6-kjobwidgets
 $(eval $(call KF6_METADATA))
 
 $(PKG)_CHECKSUM  := a13fcc9c861a90a540e5f93028fa7fae48f4c8643dadd287f3d54f5f24fab3da
-$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase kf6-kcoreaddons kf6-knotifications kf6-kwidgetsaddons
+$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase qt6-qttools kf6-kcoreaddons kf6-knotifications kf6-kwidgetsaddons
 $(PKG)_TARGETS   := $(MXE_TARGETS)
 $(PKG)_IGNORE    := 
 
