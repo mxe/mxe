70340778dc456091352ec754f24b2d2d97c8a78c
marco.borrini <marco.borrini@lepida.it>
2026-08-20 02:42:19 +0200

    Update kf6-kcrash: apply implementation plan fixes

--- src/kf/kf6/kf6-kcrash.mk
+++ src/kf/kf6/kf6-kcrash.mk
@@ -6,7 +6,7 @@ PKG              := kf6-kcrash
 $(eval $(call KF6_METADATA))
 
 $(PKG)_CHECKSUM  := ddcc14c0c40e24f4c0dc04246b87d11b650e6fd8be2cb00e4f2cc2ee9e605702
-$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase kf6-kcoreaddons kf6-kwindowsystem
+$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase kf6-kcoreaddons
 $(PKG)_TARGETS   := $(MXE_TARGETS)
 $(PKG)_IGNORE    := 
 
