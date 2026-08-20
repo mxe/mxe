5a7854fea4de90fc31181b096dbac5d4845bed78
marco.borrini <marco.borrini@lepida.it>
2026-08-20 02:42:20 +0200

    Update kf6-kwindowsystem: apply implementation plan fixes

--- src/kf/kf6/kf6-kwindowsystem.mk
+++ src/kf/kf6/kf6-kwindowsystem.mk
@@ -6,7 +6,7 @@ PKG              := kf6-kwindowsystem
 $(eval $(call KF6_METADATA))
 
 $(PKG)_CHECKSUM  := 5adbdf9c82b1ecbb92bda6498ea1b8f88c08f9ec57dbff70d582e2453bf16b12
-$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase qt6-qtdeclarative
+$(PKG)_DEPS      := kf6-conf kf6-extra-cmake-modules qt6-qtbase qt6-qttools qt6-qtdeclarative
 $(PKG)_TARGETS   := $(MXE_TARGETS)
 $(PKG)_IGNORE    := 
 
